1088d68fd826030afc368a12e0e702e62f7b1db7
Krzysztof Cwalina <kcwalina@microsoft.com>
2018-05-07 15:42:41 -0700

    Benchmark

---
+++ test/Microsoft.ML.Benchmarks/Program.cs
@@ -0,0 +1,46 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using System.IO;
+
+namespace Microsoft.ML.Benchmarks
+{
+    public class Program
+    {
+        /// <summary>
+        /// execute dotnet run -c Release and choose the benchmarks you want to run
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            BenchmarkSwitcher
+                .FromAssembly(typeof(Program).Assembly)
+                .Run(null, CreateClrVsCoreConfig());
+        }
+
+        private static IConfig CreateClrVsCoreConfig()
+        {
+            var config = DefaultConfig.Instance.With(
+                Job.ShortRun.
+                With(BenchmarkDotNet.Environments.Platform.X64)).
+                With(MemoryDiagnoser.Default);
+            return config;
+        }
+
+        internal static string GetDataPath(string name)
+            => Path.GetFullPath(Path.Combine(_dataRoot, name));
+
+        static readonly string _dataRoot;
+        static Program()
+        {
+            var currentAssemblyLocation = new FileInfo(typeof(Program).Assembly.Location);
+            var rootDir = currentAssemblyLocation.Directory.Parent.Parent.Parent.Parent.FullName;
+            _dataRoot = Path.Combine(rootDir, "test", "data");
+        }
+    }
+}
---
+++ test/Microsoft.ML.Benchmarks/TrainPredictionBench.cs
@@ -0,0 +1,79 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using BenchmarkDotNet.Attributes;
+using Microsoft.ML.Runtime.Api;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms;
+
+
+namespace Microsoft.ML.Benchmarks
+{
+    public class TrainPredictionBench
+    {
+        [Benchmark]
+        [MemoryDiagnoser]
+        public void Iris()
+        {
+            var dataPath = Program.GetDataPath("iris.txt");
+            var pipeline = new LearningPipeline();
+
+            pipeline.Add(new TextLoader<IrisData>(dataPath, useHeader: true, separator: "tab"));
+            pipeline.Add(new ColumnConcatenator(outputColumn: "Features",
+                "SepalLength", "SepalWidth", "PetalLength", "PetalWidth"));
+
+            pipeline.Add(new StochasticDualCoordinateAscentClassifier());
+
+            PredictionModel<IrisData, IrisPrediction> model = pipeline.Train<IrisData, IrisPrediction>();
+
+            IrisPrediction prediction = model.Predict(new IrisData()
+            {
+                SepalLength = 3.3f,
+                SepalWidth = 1.6f,
+                PetalLength = 0.2f,
+                PetalWidth = 5.1f,
+            });
+
+            prediction = model.Predict(new IrisData()
+            {
+                SepalLength = 3.1f,
+                SepalWidth = 5.5f,
+                PetalLength = 2.2f,
+                PetalWidth = 6.4f,
+            });
+
+            prediction = model.Predict(new IrisData()
+            {
+                SepalLength = 3.1f,
+                SepalWidth = 2.5f,
+                PetalLength = 1.2f,
+                PetalWidth = 4.4f,
+            });
+        }
+
+        public class IrisData
+        {
+            [Column("0")]
+            public float Label;
+
+            [Column("1")]
+            public float SepalLength;
+
+            [Column("2")]
+            public float SepalWidth;
+
+            [Column("3")]
+            public float PetalLength;
+
+            [Column("4")]
+            public float PetalWidth;
+        }
+
+        public class IrisPrediction
+        {
+            [ColumnName("Score")]
+            public float[] PredictedLabels;
+        }
+    }
+}
