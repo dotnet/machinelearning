e7c9b2911f97e0d138f1038bea9c617899f53054
Anipik <anirudhagnihotry098@gmail.com>
2018-05-10 08:51:35 -0700

    fixing tests for non-english windwos

--- test/Microsoft.ML.TestFramework/BaseTestBaseline.cs
+++ test/Microsoft.ML.TestFramework/BaseTestBaseline.cs
@@ -8,6 +8,7 @@ using Microsoft.ML.Runtime.Tools;
 using Microsoft.ML.TestFramework;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Reflection;
 using System.Runtime.InteropServices;
@@ -615,7 +616,8 @@ namespace Microsoft.ML.Runtime.RunTests
 
         private static void GetNumbersFromFile(ref string firstString, ref string secondString, decimal precision)
         {
-            Regex _matchNumer = new Regex(@"\b[0-9]+\.?[0-9]*\b", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+            string decimalSeparator = CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+            Regex _matchNumer = new Regex(@"\b[0-9]+\" + decimalSeparator + "?[0-9]*\b", RegexOptions.IgnoreCase | RegexOptions.Compiled);
             MatchCollection firstCollection = _matchNumer.Matches(firstString);
             MatchCollection secondCollection = _matchNumer.Matches(secondString);
 
