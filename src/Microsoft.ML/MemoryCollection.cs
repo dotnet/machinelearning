62ab5755ee31b9e21f75bb36328e2eec1eb7ef09
Ivan Matantsev <ivmatan@microsoft.com>
2018-05-10 09:51:04 -0700

    hide collectionloader

--- src/Microsoft.ML/MemoryCollection.cs
+++ src/Microsoft.ML/MemoryCollection.cs
@@ -16,7 +16,7 @@ namespace Microsoft.ML
         /// <summary>
         /// Creates memory collection loader.
         /// </summary>
-        public static MemoryCollectionLoader<T> Create<T>(IList<T> data) where T:class
+        public static ILearningPipelineLoader Create<T>(IList<T> data) where T : class
         {
             return new MemoryCollectionLoader<T>(data);
         }
@@ -24,71 +24,72 @@ namespace Microsoft.ML
         /// <summary>
         /// Creates memory collection loader.
         /// </summary>
-        public static MemoryCollectionLoader<T> Create<T>(IEnumerable<T> data) where T : class
+        public static ILearningPipelineLoader Create<T>(IEnumerable<T> data) where T : class
         {
             return new MemoryCollectionLoader<T>(data);
         }
-    }
 
-    /// <summary>
-    /// Allows you to convert your memory collection into IDataview.
-    /// </summary>
-    /// <typeparam name="TInput"></typeparam>
-    public class MemoryCollectionLoader<TInput> : ILearningPipelineLoader
-        where TInput : class
-    {
-        public ILearningPipelineStep ApplyStep(ILearningPipelineStep previousStep, Experiment experiment)
-        {
-            Contracts.Assert(previousStep == null);
-            _dataViewEntryPoint = new Data.DataViewReference();
-            var importOutput = experiment.Add(_dataViewEntryPoint);
-            return new MemoryCollectionPipelineStep(importOutput.Data);
-        }
-
-        private readonly IList<TInput> _listCollection;
-        private readonly IEnumerable<TInput> _enumerableCollection;
-
-        private Data.DataViewReference _dataViewEntryPoint;
-        private IDataView _dataView;
 
         /// <summary>
-        /// Creates IDataview on top of collection
+        /// Allows you to convert your memory collection into IDataview.
         /// </summary>
-        public MemoryCollectionLoader(IList<TInput> collection)
+        /// <typeparam name="TInput"></typeparam>
+        private class MemoryCollectionLoader<TInput> : ILearningPipelineLoader
+           where TInput : class
         {
-            Contracts.CheckParamValue(Utils.Size(collection) > 0, collection, nameof(collection), "Must be non-empty");
-            _listCollection = collection;
-        }
-
-        /// <summary>
-        /// Creates IDataview on top of collection
-        /// </summary>
-        public MemoryCollectionLoader(IEnumerable<TInput> collection)
-        {
-            Contracts.CheckValue(collection,nameof(collection), "Must be non-null");
-            _enumerableCollection = collection;
-            
-        }
-
-        public void SetInput(IHostEnvironment env, Experiment experiment)
-        {
-            if (_listCollection != null)
-                _dataView = ComponentCreation.CreateDataView(env, _listCollection);
-            if (_enumerableCollection != null)
-                _dataView = ComponentCreation.CreateStreamingDataView(env, _listCollection);
-            env.CheckValue(_dataView, nameof(_dataView));
-            experiment.SetInput(_dataViewEntryPoint.Data, _dataView);
-        }
-
-        private class MemoryCollectionPipelineStep : ILearningPipelineDataStep
-        {
-            public MemoryCollectionPipelineStep(Var<IDataView> data)
+            public ILearningPipelineStep ApplyStep(ILearningPipelineStep previousStep, Experiment experiment)
             {
-                Data = data;
+                Contracts.Assert(previousStep == null);
+                _dataViewEntryPoint = new Data.DataViewReference();
+                var importOutput = experiment.Add(_dataViewEntryPoint);
+                return new MemoryCollectionPipelineStep(importOutput.Data);
             }
 
-            public Var<IDataView> Data { get; }
-            public Var<ITransformModel> Model  => null;
+            private readonly IList<TInput> _listCollection;
+            private readonly IEnumerable<TInput> _enumerableCollection;
+
+            private Data.DataViewReference _dataViewEntryPoint;
+            private IDataView _dataView;
+
+            /// <summary>
+            /// Creates IDataview on top of collection
+            /// </summary>
+            public MemoryCollectionLoader(IList<TInput> collection)
+            {
+                Contracts.CheckParamValue(Utils.Size(collection) > 0, collection, nameof(collection), "Must be non-empty");
+                _listCollection = collection;
+            }
+
+            /// <summary>
+            /// Creates IDataview on top of collection
+            /// </summary>
+            public MemoryCollectionLoader(IEnumerable<TInput> collection)
+            {
+                Contracts.CheckValue(collection, nameof(collection));
+                _enumerableCollection = collection;
+
+            }
+
+            public void SetInput(IHostEnvironment env, Experiment experiment)
+            {
+                if (_listCollection != null)
+                    _dataView = ComponentCreation.CreateDataView(env, _listCollection);
+                if (_enumerableCollection != null)
+                    _dataView = ComponentCreation.CreateStreamingDataView(env, _enumerableCollection);
+                env.CheckValue(_dataView, nameof(_dataView));
+                experiment.SetInput(_dataViewEntryPoint.Data, _dataView);
+            }
+
+            private class MemoryCollectionPipelineStep : ILearningPipelineDataStep
+            {
+                public MemoryCollectionPipelineStep(Var<IDataView> data)
+                {
+                    Data = data;
+                }
+
+                public Var<IDataView> Data { get; }
+                public Var<ITransformModel> Model => null;
+            }
         }
     }
 }
--- test/Microsoft.ML.Tests/MemoryCollectionTests.cs
+++ test/Microsoft.ML.Tests/MemoryCollectionTests.cs
@@ -133,21 +133,36 @@ namespace Microsoft.ML.EntryPoints.Tests
         public void CanTrain()
         {
             var pipeline = new LearningPipeline();
-            var collection = MemoryCollection.Create(new List<IrisData>() {
+            var data = new List<IrisData>() {
                 new IrisData { SepalLength = 1f, SepalWidth = 1f ,PetalLength=0.3f, PetalWidth=5.1f, Label=1},
                 new IrisData { SepalLength = 1f, SepalWidth = 1f ,PetalLength=0.3f, PetalWidth=5.1f, Label=1},
                 new IrisData { SepalLength = 1.2f, SepalWidth = 0.5f ,PetalLength=0.3f, PetalWidth=5.1f, Label=0}
-            });
+            };
+            var collection = MemoryCollection.Create(data);
 
             pipeline.Add(collection);
-
             pipeline.Add(new ColumnConcatenator(outputColumn: "Features",
                 "SepalLength", "SepalWidth", "PetalLength", "PetalWidth"));
-
             pipeline.Add(new StochasticDualCoordinateAscentClassifier());
             PredictionModel<IrisData, IrisPrediction> model = pipeline.Train<IrisData, IrisPrediction>();
 
             IrisPrediction prediction = model.Predict(new IrisData()
+            {
+                SepalLength = 3.3f,
+                SepalWidth = 1.6f,
+                PetalLength = 0.2f,
+                PetalWidth = 5.1f,
+            });
+
+            pipeline = new LearningPipeline();
+            collection = MemoryCollection.Create(data.AsEnumerable());
+            pipeline.Add(collection);
+            pipeline.Add(new ColumnConcatenator(outputColumn: "Features",
+                "SepalLength", "SepalWidth", "PetalLength", "PetalWidth"));
+            pipeline.Add(new StochasticDualCoordinateAscentClassifier());
+            model = pipeline.Train<IrisData, IrisPrediction>();
+
+            prediction = model.Predict(new IrisData()
             {
                 SepalLength = 3.3f,
                 SepalWidth = 1.6f,
