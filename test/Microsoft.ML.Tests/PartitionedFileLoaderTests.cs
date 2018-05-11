4549388a41d09f74484fadd21e9c6676044fae9c
tyclintw <tyclintw@microsoft.com>
2018-05-11 12:50:32 -0700

    Remove unused namespaces.

--- test/Microsoft.ML.Tests/PartitionedFileLoaderTests.cs
+++ test/Microsoft.ML.Tests/PartitionedFileLoaderTests.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.ML.Runtime.RunTests;
-using Microsoft.ML.TestFramework;
-using System;
-using System.Collections.Generic;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.ML.Runtime.RunTests;
 using System.IO;
-using System.Text;
 using Xunit;
 using Xunit.Abstractions;
 
