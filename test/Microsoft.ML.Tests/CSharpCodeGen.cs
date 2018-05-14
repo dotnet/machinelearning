3f33bc512c3e9b0cf2357653903d0427c48a5eab
George Montanez <38017604+george-microsoft@users.noreply.github.com>
2018-05-14 11:27:38 -0700

    Reverted change made to test, so that it again skips.

--- test/Microsoft.ML.Tests/CSharpCodeGen.cs
+++ test/Microsoft.ML.Tests/CSharpCodeGen.cs
@@ -15,7 +15,7 @@ namespace Microsoft.ML.Tests
         {
         }
 
-        [Fact]//(Skip = "Temporary solution(Windows ONLY) to regenerate codegenerated CSharpAPI.cs")]
+        [Fact(Skip = "Temporary solution(Windows ONLY) to regenerate codegenerated CSharpAPI.cs")]
         public void GenerateCSharpAPI()
         {
             var cSharpAPIPath = Path.Combine(RootDir, @"src\\Microsoft.ML\\CSharpApi.cs");
