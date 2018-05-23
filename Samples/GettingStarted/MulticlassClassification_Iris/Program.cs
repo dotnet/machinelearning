c90ad8a97b941fe3bf2e43877db4711423baa997
oliag <oliag@microsoft.com>
2018-05-23 11:36:12 -0700

    Address review comments

--- Samples/GettingStarted/MulticlassClassification_Iris/Program.cs
+++ Samples/GettingStarted/MulticlassClassification_Iris/Program.cs
@@ -1,64 +1,81 @@
 ﻿using System;
 using System.IO;
+using System.Threading.Tasks;
+using Microsoft.ML;
 using Microsoft.ML.Models;
 using Microsoft.ML.Trainers;
 using Microsoft.ML.Transforms;
-using Microsoft.ML;
-using System.Threading.Tasks;
 
 namespace MulticlassClassification_Iris
 {
     public static partial class Program
     {
         private static string AppPath => Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
-        private static string TrainDataPath => Path.Combine(AppPath, @"..\..\..\..\datasets\", "iris_train.txt");
-        private static string TestDataPath => Path.Combine(AppPath, @"..\..\..\..\datasets\", "iris_test.txt");
+        private static string TrainDataPath => Path.Combine(AppPath, "..", "..", "..", "..",  "datasets", "iris_train.txt");
+        private static string TestDataPath => Path.Combine(AppPath,  "..", "..", "..", "..", "datasets", "iris_test.txt");
         private static string ModelPath => Path.Combine(AppPath,  "Models", "IrisModel.zip");
 
         private static async Task Main(string[] args)
         {
+            // ML task includes 3 steps: training a ML model, evaluating how good it is,
+            // and if the quality is acceptable, using this model for predictions.
             var model = await TrainAsync();
 
             Evaluate(model);
 
             Console.WriteLine();
             var prediction = model.Predict(TestIrisData.Iris1);
-            Console.WriteLine($"Actual: type 1.     Predicted probability: type 1: {prediction.Score[0]:0.####}");
-            Console.WriteLine($"                                           type 2: {prediction.Score[1]:0.####}");
-            Console.WriteLine($"                                           type 3: {prediction.Score[2]:0.####}");
+            Console.WriteLine($"Actual: setosa.     Predicted probability: setosa:      {prediction.Score[0]:0.####}");
+            Console.WriteLine($"                                           versicolor:  {prediction.Score[1]:0.####}");
+            Console.WriteLine($"                                           virginica:   {prediction.Score[2]:0.####}");
             Console.WriteLine();
             
             prediction = model.Predict(TestIrisData.Iris2);
-            Console.WriteLine($"Actual: type 3.     Predicted probability: type 2: {prediction.Score[0]:0.####}");
-            Console.WriteLine($"                                           type 2: {prediction.Score[1]:0.####}");
-            Console.WriteLine($"                                           type 3: {prediction.Score[2]:0.####}");
+            Console.WriteLine($"Actual: virginica.  Predicted probability: setosa:      {prediction.Score[0]:0.####}");
+            Console.WriteLine($"                                           versicolor:  {prediction.Score[1]:0.####}");
+            Console.WriteLine($"                                           virginica:   {prediction.Score[2]:0.####}");
             Console.WriteLine();
 
             prediction = model.Predict(TestIrisData.Iris3);
-            Console.WriteLine($"Actual: type 2.     Predicted probability: type 1: {prediction.Score[0]:0.####}");
-            Console.WriteLine($"                                           type 2: {prediction.Score[1]:0.####}");
-            Console.WriteLine($"                                           type 3: {prediction.Score[2]:0.####}");
+            Console.WriteLine($"Actual: versicolor. Predicted probability: setosa:      {prediction.Score[0]:0.####}");
+            Console.WriteLine($"                                           versicolor:  {prediction.Score[1]:0.####}");
+            Console.WriteLine($"                                           virginica:   {prediction.Score[2]:0.####}");
             
             Console.ReadLine();
         }
 
         internal static async Task<PredictionModel<IrisData, IrisPrediction>> TrainAsync()
         {
+            // LearningPipeline holds all steps of the learning process: data, transforms, learners.
             var pipeline = new LearningPipeline
             {
+                // The TextLoader loads a dataset. The schema of the dataset is specified by passing a class containing
+                // all the column names and their types. This will be used to create the model, and train it. 
                 new TextLoader<IrisData>(TrainDataPath, useHeader: false),
+                
+                // Transforms
+                // When ML model starts training, it looks for two columns: Label and Features.
+                // Label:   values that should be predicted. If you have a field named Label in your data type,
+                //              like in this example, no extra actions required.
+                //          If you don’t have it, copy the column you want to predict with ColumnCopier transform:
+                //              new ColumnCopier(("FareAmount", "Label"))
+                // Features: all data used for prediction. At the end of all transforms you need to concatenate
+                //              all columns except the one you want to predict into Features column with
+                //              ColumnConcatenator transform:
                 new ColumnConcatenator("Features",
                     "SepalLength",
                     "SepalWidth",
                     "PetalLength",
                     "PetalWidth"),
+                // StochasticDualCoordinateAscentClassifier is an algorithm that will be used to train the model.
                 new StochasticDualCoordinateAscentClassifier()
             };
 
             Console.WriteLine("=============== Training model ===============");
-
+            // The pipeline is trained on the dataset that has been loaded and transformed.
             var model = pipeline.Train<IrisData, IrisPrediction>();
 
+            // Saving the model as a .zip file.
             await model.WriteAsync(ModelPath);
 
             Console.WriteLine("=============== End training ===============");
@@ -69,8 +86,11 @@ namespace MulticlassClassification_Iris
 
         private static void Evaluate(PredictionModel<IrisData, IrisPrediction> model)
         {
+            // To evaluate how good the model predicts values, the model is ran against new set
+            // of data (test data) that was not involved in training.
             var testData = new TextLoader<IrisData>(TestDataPath, useHeader: false);
 
+            // ClassificationEvaluator performs evaluation for Multiclass Classification type of ML problems.
             var evaluator = new ClassificationEvaluator {OutputTopKAcc = 3};
             
             Console.WriteLine("=============== Evaluating model ===============");
--- Samples/GitHubLabeler/README.md
+++ Samples/GitHubLabeler/README.md
@@ -2,7 +2,7 @@
 This is a simple prototype application to demonstrate how to use [ML.NET](https://www.nuget.org/packages/Microsoft.ML/) APIs. The main focus is on creating, training, and using ML (Machine Learning) model that is implemented in Predictor.cs class.
 
 ## Overview
-GitHubLabeler is a .NET Core console application that runs from command-line interface (CLI) and allows to:
+GitHubLabeler is a .NET Core console application that runs from command-line interface (CLI) and allows you to:
 * train ML model on your labeled GitHub issues to teach the model what label should be assigned for a new issue. (As an example, you can use `corefx_issues.tsv` file that contains issues from public [corefx](https://github.com/dotnet/corefx) repository)
 * label a new issue. The application will get all unlabeled open issues from specified in `App.config` GitHub repository and label them using the trained ML model created on the step above.  
 
@@ -21,10 +21,10 @@ This ML model is using multi-class classification algorithm and text capabilitie
     a.  You can use existing `corefx_issues.tsv` data file for experimenting  with the program. In this case the predicted labels will be chosen among labels from [corefx](https://github.com/dotnet/corefx) repository. No changes required.
     
     b. To work with labels from your GitHub repository, you will need to train the model on your data. To do so, export GitHub issues from your repository into `.tsv` file with the following columns:
-    * ID - issue’s ID
-    * Area - issue’s label (named this way to avoid confusion with the Label concept in ML.NET)
-    * Title - issue’s title
-    * Description - issue’s description
+    * ID - issue's ID
+    * Area - issue's label (named this way to avoid confusion with the Label concept in ML.NET)
+    * Title - issue's title
+    * Description - issue's description
     
     and add the file in `Data` folder. Update `Predictor.DataPath` field to match your file's name:
 ```csharp
@@ -36,14 +36,14 @@ Training is a process of running an ML model through known examples (in our case
 
 To perform the training run the application with `"train"` key:
 ```
-C:\GitHubLabeler\GitHubLabeler\bin\Debug\netcoreapp2.0>dotnet GitHubLabeler.dll train
+GitHubLabeler\GitHubLabeler\bin\Debug\netcoreapp2.0>dotnet GitHubLabeler.dll train
 ```
 After the training is completed, the model is saved as a .zip file in `Models\Model.zip`.
 
 ## Labeling
 When the model is trained, it can be used for predicting new issue's label. To do so, run the application with `"label"` key:
 ```
-C:\GitHubLabeler\GitHubLabeler\bin\Debug\netcoreapp2.0>dotnet GitHubLabeler.dll label
+GitHubLabeler\GitHubLabeler\bin\Debug\netcoreapp2.0>dotnet GitHubLabeler.dll label
 ```
 For testing convenience only open and not labeled issues that were created in the past 10 minutes are subject to labeling:
 ```csharp
