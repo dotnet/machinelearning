d0a4af56304277aa62583b4c3b1d87fa826d753f
Zeeshan Ahmed <zeahmed@microsoft.com>
2018-06-07 14:49:17 -0700

    Using CSharpCodeProvider.IsValidIdentifier for validating column name.

--- src/Microsoft.ML/Data/TextLoader.cs
+++ src/Microsoft.ML/Data/TextLoader.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Microsoft.CSharp;
 using Microsoft.ML.Runtime;
 using Microsoft.ML.Runtime.Api;
 using Microsoft.ML.Runtime.Data;
@@ -84,7 +85,7 @@ namespace Microsoft.ML.Data
                         $"Valid characters are 0-9, *, - and ~");
 
                 var name = mappingAttr.Name ?? field.Name;
-                if (name.Any(c => !Char.IsLetterOrDigit(c) && c != '_'))
+                if (!new CSharpCodeProvider().IsValidIdentifier(name))
                     throw Contracts.Except($"{name} can only have alphanumeric or '_'.");
 
                 Runtime.Data.TextLoader.Range[] sources;
