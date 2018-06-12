d0c57c69f4d1c184d798e1e9cd04a4a8c013e0e0
Zeeshan Ahmed <zeahmed@microsoft.com>
2018-06-12 12:33:03 -0700

    Resolved merge conflicts.

--- src/Microsoft.ML/Runtime/Internal/Tools/CSharpApiGenerator.cs
+++ src/Microsoft.ML/Runtime/Internal/Tools/CSharpApiGenerator.cs
@@ -313,16 +313,16 @@ namespace Microsoft.ML.Runtime.Internal.Tools
             writer.Outdent();
             writer.WriteLine("}");
             writer.WriteLine("");
-            writer.WriteLine($"public {className}(params ValueTuple<string, string>[] inputOutput{fieldName}s)");
+            writer.WriteLine($"public {className}(params (string inputColumn, string outputColumn)[] inputOutput{fieldName}s)");
             writer.WriteLine("{");
             writer.Indent();
             writer.WriteLine($"if (inputOutput{fieldName}s != null)");
             writer.WriteLine("{");
             writer.Indent();
-            writer.WriteLine($"foreach (ValueTuple<string, string> inputOutput in inputOutput{fieldName}s)");
+            writer.WriteLine($"foreach (var inputOutput in inputOutput{fieldName}s)");
             writer.WriteLine("{");
             writer.Indent();
-            writer.WriteLine($"Add{fieldName}(inputOutput.Item2, inputOutput.Item1);");
+            writer.WriteLine($"Add{fieldName}(inputOutput.outputColumn, inputOutput.inputColumn);");
             writer.Outdent();
             writer.WriteLine("}");
             writer.Outdent();
@@ -330,31 +330,31 @@ namespace Microsoft.ML.Runtime.Internal.Tools
             writer.Outdent();
             writer.WriteLine("}");
             writer.WriteLine("");
-            writer.WriteLine($"public void Add{fieldName}(string source)");
+            writer.WriteLine($"public void Add{fieldName}(string inputColumn)");
             writer.WriteLine("{");
             writer.Indent();
             if (isArray)
             {
                 writer.WriteLine($"var list = {fieldName} == null ? new List<{generatedType}>() : new List<{generatedType}>({fieldName});");
-                writer.WriteLine($"list.Add(OneToOneColumn<{generatedType}>.Create(source));");
+                writer.WriteLine($"list.Add(OneToOneColumn<{generatedType}>.Create(inputColumn));");
                 writer.WriteLine($"{fieldName} = list.ToArray();");
             }
             else
-                writer.WriteLine($"{fieldName} = OneToOneColumn<{generatedType}>.Create(source);");
+                writer.WriteLine($"{fieldName} = OneToOneColumn<{generatedType}>.Create(inputColumn);");
             writer.Outdent();
             writer.WriteLine("}");
             writer.WriteLine();
-            writer.WriteLine($"public void Add{fieldName}(string name, string source)");
+            writer.WriteLine($"public void Add{fieldName}(string outputColumn, string inputColumn)");
             writer.WriteLine("{");
             writer.Indent();
             if (isArray)
             {
                 writer.WriteLine($"var list = {fieldName} == null ? new List<{generatedType}>() : new List<{generatedType}>({fieldName});");
-                writer.WriteLine($"list.Add(OneToOneColumn<{generatedType}>.Create(name, source));");
+                writer.WriteLine($"list.Add(OneToOneColumn<{generatedType}>.Create(outputColumn, inputColumn));");
                 writer.WriteLine($"{fieldName} = list.ToArray();");
             }
             else
-                writer.WriteLine($"{fieldName} = OneToOneColumn<{generatedType}>.Create(name, source);");
+                writer.WriteLine($"{fieldName} = OneToOneColumn<{generatedType}>.Create(outputColumn, inputColumn);");
             writer.Outdent();
             writer.WriteLine("}");
             writer.WriteLine();
