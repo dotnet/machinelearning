c512a164025d0d64fe2f48cd47b44897b1b53e75
Ross Smith <rosmith@microsoft.com>
2018-06-15 17:32:55 +0100

    Changed List to HashSet to ensure that there are no duplicates

---
+++ test/Microsoft.ML.Sweeper.Tests/SweeperTest.cs
@@ -0,0 +1,65 @@
+using Microsoft.ML.Runtime;
+using Microsoft.ML.Runtime.CommandLine;
+using Microsoft.ML.Runtime.Data;
+using Microsoft.ML.Runtime.RunTests;
+using Microsoft.ML.Runtime.Sweeper;
+using System;
+using System.IO;
+using Xunit;
+
+namespace Microsoft.ML.Sweeper.Tests
+{
+    public class SweeperTest
+    {
+        [Fact]
+        public void UniformRandomSweeperReturnsDistinctValuesWhenProposeSweep()
+        {
+            DiscreteValueGenerator valueGenerator = CreateDiscreteValueGenerator();
+
+            using (var writer = new StreamWriter(new MemoryStream()))
+            using (var env = new TlcEnvironment(42, outWriter: writer, errWriter: writer))
+            {
+                var sweeper = new UniformRandomSweeper(env,
+                    new SweeperBase.ArgumentsBase(),
+                    new[] { valueGenerator });
+
+                var results = sweeper.ProposeSweeps(3);
+                Assert.NotNull(results);
+
+                int length = results.Length;
+                Assert.Equal(2, length);
+            }
+        }
+
+        [Fact]
+        public void RandomGridSweeperReturnsDistinctValuesWhenProposeSweep()
+        {
+            DiscreteValueGenerator valueGenerator = CreateDiscreteValueGenerator();
+
+            using (var writer = new StreamWriter(new MemoryStream()))
+            using (var env = new TlcEnvironment(42, outWriter: writer, errWriter: writer))
+            {
+                var sweeper = new RandomGridSweeper(env,
+                    new RandomGridSweeper.Arguments(),
+                    new[] { valueGenerator });
+
+                var results = sweeper.ProposeSweeps(3);
+                Assert.NotNull(results);
+
+                int length = results.Length;
+                Assert.Equal(2, length);
+            }
+        }
+
+        private static DiscreteValueGenerator CreateDiscreteValueGenerator()
+        {
+            var args = new DiscreteParamArguments()
+            {
+                Name = "TestParam",
+                Values = new string[] { "one", "two" }
+            };
+
+            return new DiscreteValueGenerator(args);
+        }
+    }
+}
