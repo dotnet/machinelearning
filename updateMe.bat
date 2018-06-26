2b27ffecc79653acb8b9208e14e719675921d5e5
Senja Filipi <sefilipi@microsoft.com>
2018-06-26 12:50:25 -0700

    removing utility script

--- updateMe.bat
+++
@@ -1,5 +0,0 @@
-::Update the https://github.com/sfilipi/machinelearning-1 from https://github.com/dotnet/machinelearning
-git fetch upstream
-git checkout master
-git merge upstream/master
-git push -u origin master
