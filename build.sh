fd003b89ac3cc99438a9440876843ae869624ff8
Zeeshan Siddiqui <mzs@microsoft.com>
2018-06-25 17:26:30 -0700

    remove brew and gcc from build script.

--- build.sh
+++ build.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-if [ "$(uname)" == "Darwin" ]; then
-    #Mac OS X platform
-	yes | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-	brew install gcc
-fi
-
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
   DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
