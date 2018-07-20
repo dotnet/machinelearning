90b5129f30580d45663bd07ec33bb8f38fb4d235
Tom Finley <tfinley@gmail.com>
2018-07-20 07:42:10 -0700

    Shift analyzer to .NET Standard 1.3 from 2.0 to avoid problems with IDE1003.
    While builds work fine both inside and outside of Visual Studio, and
    VS does not complain on my work computer, VS on my home computer
    complains with IDE1003. Following the advice here, the analyzer will
    use .NET Standard 1.3, which requires only minor adjustments.
    
    https://github.com/dotnet/roslyn/issues/22368
    
    My understanding was that VS should support 2.0 referencing analyzers,
    but seemingly that is incorrect.

--- tools-local/Microsoft.ML.CodeAnalyzer/Descriptions.Designer.cs
+++ tools-local/Microsoft.ML.CodeAnalyzer/Descriptions.Designer.cs
@@ -10,6 +10,7 @@
 
 namespace Microsoft.ML.CodeAnalyzer {
     using System;
+    using System.Reflection;
     
     
     /// <summary>
@@ -39,7 +40,7 @@ namespace Microsoft.ML.CodeAnalyzer {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.ML.CodeAnalyzer.Descriptions", typeof(Descriptions).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.ML.CodeAnalyzer.Descriptions", typeof(Descriptions).GetTypeInfo().Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
