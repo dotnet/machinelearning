3409b9f0fb5949f14a20b155c148212be9636dcd
xavier dupré <xavier.dupre@gmail.com>
2018-07-27 10:28:56 +0200

    fix #586 and cursor creation on dataview with sparse vectors

--- test/Microsoft.ML.TestFramework/TestSparseDataView.cs
+++ test/Microsoft.ML.TestFramework/TestSparseDataView.cs
@@ -23,37 +23,48 @@ namespace Microsoft.ML.Runtime.RunTests
     {
         const string Cat = "DataView";
 
-        public TestSparseDataView(ITestOutputHelper obj): base(obj)
+        public TestSparseDataView(ITestOutputHelper obj) : base(obj)
         {
         }
 
-        class ExampleA
+        class DenseExample<T>
         {
             [VectorType(2)]
-            public float[] X;
+            public T[] X;
         }
 
-        class ExampleASparse
+        class SparseExample<T>
         {
             [VectorType(5)]
-            public VBuffer<float> X;
+            public VBuffer<T> X;
         }
 
         [Fact]
         [TestCategory(Cat)]
         public void SparseDataView()
         {
+            GenericSparseDataView(new[] { 1f, 2f, 3f }, new[] { 1f, 10f, 100f });
+            GenericSparseDataView(new DvInt4[] { 1, 2, 3 }, new DvInt4[] { 1, 10, 100 });
+            GenericSparseDataView(new DvBool[] { true, true, true }, new DvBool[] { false, false, false });
+            GenericSparseDataView(new double[] { 1, 2, 3 }, new double[] { 1, 10, 100 });
+            GenericSparseDataView(new DvText[] { new DvText("a"), new DvText("b"), new DvText("c") },
+                                  new DvText[] { new DvText("aa"), new DvText("bb"), new DvText("cc") });
+            Done();
+        }
+
+        void GenericSparseDataView<T>(T[] v1, T[] v2)
+        {
             var inputs = new[] {
-                new ExampleASparse() { X = new VBuffer<float> (5, 3, new float[] { 1, 10, 100 }, new int[] { 0, 2, 4 }) },
-                new ExampleASparse() { X = new VBuffer<float> (5, 3, new float[] { 2, 3, 5 }, new int[] { 0, 1, 3 }) }
+                new SparseExample<T>() { X = new VBuffer<T> (5, 3, v1, new int[] { 0, 2, 4 }) },
+                new SparseExample<T>() { X = new VBuffer<T> (5, 3, v2, new int[] { 0, 1, 3 }) }
             };
             var host = new TlcEnvironment();
             var data = host.CreateStreamingDataView(inputs);
-            VBuffer<float> value = new VBuffer<float>();
+            var value = new VBuffer<T>();
             int n = 0;
             using (var cur = data.GetRowCursor(i => true))
             {
-                var getter = cur.GetGetter<VBuffer<float>>(0);
+                var getter = cur.GetGetter<VBuffer<T>>(0);
                 while (cur.MoveNext())
                 {
                     getter(ref value);
@@ -62,24 +73,34 @@ namespace Microsoft.ML.Runtime.RunTests
                 }
             }
             Assert.True(n == 2);
-            Done();
         }
 
         [Fact]
         [TestCategory(Cat)]
         public void DenseDataView()
         {
+            GenericDenseDataView(new[] { 1f, 2f, 3f }, new[] { 1f, 10f, 100f });
+            GenericDenseDataView(new DvInt4[] { 1, 2, 3 }, new DvInt4[] { 1, 10, 100 });
+            GenericDenseDataView(new DvBool[] { true, true, true }, new DvBool[] { false, false, false });
+            GenericDenseDataView(new double[] { 1, 2, 3 }, new double[] { 1, 10, 100 });
+            GenericDenseDataView(new DvText[] { new DvText("a"), new DvText("b"), new DvText("c") },
+                                 new DvText[] { new DvText("aa"), new DvText("bb"), new DvText("cc") });
+            Done();
+        }
+
+        void GenericDenseDataView<T>(T[] v1, T[] v2)
+        {
             var inputs = new[] {
-                new ExampleA() { X = new float[] { 1, 10, 100 } },
-                new ExampleA() { X = new float[] { 2, 3, 5 } }
+                new DenseExample<T>() { X = v1 },
+                new DenseExample<T>() { X = v2 }
             };
             var host = new TlcEnvironment();
             var data = host.CreateStreamingDataView(inputs);
-            VBuffer<float> value = new VBuffer<float>();
+            var value = new VBuffer<T>();
             int n = 0;
             using (var cur = data.GetRowCursor(i => true))
             {
-                var getter = cur.GetGetter<VBuffer<float>>(0);
+                var getter = cur.GetGetter<VBuffer<T>>(0);
                 while (cur.MoveNext())
                 {
                     getter(ref value);
@@ -88,7 +109,6 @@ namespace Microsoft.ML.Runtime.RunTests
                 }
             }
             Assert.True(n == 2);
-            Done();
         }
     }
 }
