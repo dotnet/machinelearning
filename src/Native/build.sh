573781dacb45f28759b702e53e5cfde4b4991079
Zeeshan Siddiqui <mzs@microsoft.com>
2018-08-01 02:53:05 -0700

    Rpath for linux and os x.

--- src/Native/build.sh
+++ src/Native/build.sh
@@ -99,6 +99,6 @@ cmake "$DIR" -G "Unix Makefiles" $__cmake_defines
 set +x # turn off trace
 make install
 echo "Changing libMklImports.dylib's executable path within libSymSgdNative.dylib so that loader can find it."
-if [[ "$OSTYPE" == "linux"* ]]
+if [[ "$OSTYPE" == "linux"* ]]; then
     install_name_tool "-change" @loader_path/libMklImports.dylib @rpath/libMklImports.dylib "$RootRepo"/bin/x64.Debug/Native/libSymSgdNative.dylib
 fi
