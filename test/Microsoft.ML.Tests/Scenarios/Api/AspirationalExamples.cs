13bbc43cdf65ee9e6b7fbda5c5890c68ec1d7228
Pete Luferenko <pelufere@microsoft.com>
2018-08-01 14:25:06 -0700

    Adding 'aspirational examples' and some more baseline scenarios

---
+++ test/Microsoft.ML.Tests/Scenarios/Api/AspirationalExamples.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.ML.Tests.Scenarios.Api
+{
+    public class AspirationalExamples
+    {
+        public class IrisPrediction
+        {
+            public string PredictedLabel;
+        }
+
+        public class IrisData
+        {
+            public float[] Features;
+        }
+
+        public void FirstExperienceWithML()
+        {
+            // This is the 'getting started with ML' example, how we see it in our new API.
+            // It currently doesn't compile, let alone work, but we still can discuss and improve the syntax.
+
+            // Load the data into the system.
+            string dataPath = "iris-data.txt";
+            var data = TextReader.ReadFile(dataPath, c => (Label: c.LoadString(0), Features: c.LoadFloat(1, 4)));
+
+            // Assign numeric values to text in the "Label" column, because only
+            // numbers can be processed during model training.
+            var transformer = data.MakeTransformer(row => (Label: row.Label.Dictionarize(), row.Features));
+            var trainingData = transformer.Transform(data);
+
+            // Train a multiclass linear classifier.
+            var learner = new StochasticDualCoordinateAscentClassifier();
+            var classifier = learner.Train(trainingData);
+
+            // Obtain some predictions.
+            var predictionEngine = new PredictionEngine<float[], string>(classifier, inputColumn: "Features", outputColumn: "PredictedLabel");
+            string prediction = predictionEngine.Predict(new[] { 3.3f, 1.6f, 0.2f, 5.1f });
+        }
+    }
+}
