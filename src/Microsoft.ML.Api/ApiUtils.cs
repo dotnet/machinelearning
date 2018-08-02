3d5355904491d6d86bb94c7b97a568ff7ae2e8f1
Don Syme <dsyme@users.noreply.github.com>
2018-08-02 11:30:24 +0100

    Update ApiUtils.cs

--- src/Microsoft.ML.Api/ApiUtils.cs
+++ src/Microsoft.ML.Api/ApiUtils.cs
@@ -109,7 +109,7 @@ namespace Microsoft.ML.Runtime.Api
 
             il.Emit(OpCodes.Ldarg_3);               // push arg3
             il.Emit(OpCodes.Ldarg_1);               // push arg1
-            il.Emit(opcode, minfo);                 // push [stack top].[propertyInfo]
+            il.Emit(opcode, minfo);                 // call [stack top].get_[propertyInfo]()
             // Stobj needs to coupled with a type.
             if (assignmentOpCode == OpCodes.Stobj)  // [stack top-1] = [stack top]
                 il.Emit(assignmentOpCode, propertyInfo.PropertyType);
@@ -181,7 +181,7 @@ namespace Microsoft.ML.Runtime.Api
 
             il.Emit(OpCodes.Ldarg_1);               // push arg1
             il.Emit(OpCodes.Ldarg_2);               // push arg2
-            il.Emit(opcode, minfo);                 // [stack top-1].[propertyInfo] <- [stack top]
+            il.Emit(opcode, minfo);                 // call [stack top-1].set_[propertyInfo]([stack top])
             il.Emit(OpCodes.Ret);                   // ret
             return mb.CreateDelegate(typeof(Poke<TRow, TValue>), null);
         }
