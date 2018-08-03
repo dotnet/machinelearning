96661bba7f22eb620380bf54f2a1f1a43c7e0458
Pete Luferenko <pelufere@microsoft.com>
2018-08-02 17:47:43 -0700

    Rework the example with Tom

--- test/Microsoft.ML.Tests/Scenarios/Api/AspirationalExamples.cs
+++ test/Microsoft.ML.Tests/Scenarios/Api/AspirationalExamples.cs
@@ -11,7 +11,7 @@ namespace Microsoft.ML.Tests.Scenarios.Api
             public string PredictedLabel;
         }
 
-        public class IrisData
+        public class IrisExample
         {
             public float[] Features;
         }
@@ -21,22 +21,27 @@ namespace Microsoft.ML.Tests.Scenarios.Api
             // This is the 'getting started with ML' example, how we see it in our new API.
             // It currently doesn't compile, let alone work, but we still can discuss and improve the syntax.
 
-            // Load the data into the system.
-            string dataPath = "iris-data.txt";
-            var data = TextReader.ReadFile(dataPath, c => (Label: c.LoadString(0), Features: c.LoadFloat(1, 4)));
+            // Initialize the environment.
+            using (var env = new TlcEnvironment())
+            {
+                // Load the data into the system.
+                string dataPath = "iris-data.txt";
+                var data = TextReader.FitAndRead(env, dataPath, c => (Label: c.LoadString(0), Features: c.LoadFloat(1, 4)));
 
-            // Assign numeric values to text in the "Label" column, because only
-            // numbers can be processed during model training.
-            var transformer = data.MakeTransformer(row => (Label: row.Label.Dictionarize(), row.Features));
-            var trainingData = transformer.Transform(data);
+                // Convert string label to integer for training.
+                var preprocess = data.MakeEstimator(row => (Label: row.Label.Dictionarize(), row.Features));
 
-            // Train a multiclass linear classifier.
-            var learner = new StochasticDualCoordinateAscentClassifier();
-            var classifier = learner.Train(trainingData);
+                // Create a learner and train it.
+                var learner = preprocess.MakeEstimator(row => row.Label.SdcaPredict(row.Features, l1Coefficient: 0.1));
+                var classifier = learner.Fit(preprocess.FitAndTransform(data));
 
-            // Obtain some predictions.
-            var predictionEngine = new PredictionEngine<float[], string>(classifier, inputColumn: "Features", outputColumn: "PredictedLabel");
-            string prediction = predictionEngine.Predict(new[] { 3.3f, 1.6f, 0.2f, 5.1f });
+                // Add another transformer that converts the integer prediction into string.
+                var finalTransformer = classifier.AppendTransformer(row => row.PredictedLabel.KeyToValue());
+
+                // Make a prediction engine and predict.
+                engine = bundle.MakePredictionEngine<IrisExample, IrisPrediction>();
+                IrisPrediction prediction = engine.Predict(new IrisExample { Features = new[] { 3.3f, 1.6f, 0.2f, 5.1f } });
+            }
         }
     }
 }
