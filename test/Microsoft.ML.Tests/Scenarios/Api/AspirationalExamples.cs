1e90463cb45227d6c65b0965e02e0c1455479594
Pete Luferenko <pelufere@microsoft.com>
2018-08-06 08:29:10 -0700

    Updated for PR comments

--- test/Microsoft.ML.Tests/Scenarios/Api/AspirationalExamples.cs
+++ test/Microsoft.ML.Tests/Scenarios/Api/AspirationalExamples.cs
@@ -13,7 +13,10 @@ namespace Microsoft.ML.Tests.Scenarios.Api
 
         public class IrisExample
         {
-            public float[] Features;
+            public float SepalWidth { get; set; }
+            public float SepalLength { get; set; }
+            public float PetalWidth { get; set; }
+            public float PetalLength { get; set; }
         }
 
         public void FirstExperienceWithML()
@@ -21,27 +24,38 @@ namespace Microsoft.ML.Tests.Scenarios.Api
             // This is the 'getting started with ML' example, how we see it in our new API.
             // It currently doesn't compile, let alone work, but we still can discuss and improve the syntax.
 
-            // Initialize the environment.
-            using (var env = new TlcEnvironment())
-            {
-                // Load the data into the system.
-                string dataPath = "iris-data.txt";
-                var data = TextReader.FitAndRead(env, dataPath, c => (Label: c.LoadString(0), Features: c.LoadFloat(1, 4)));
+            // Load the data into the system.
+            string dataPath = "iris-data.txt";
+            var data = TextReader.FitAndRead(env, dataPath, row => (
+                Label: row.ReadString(0),
+                SepalWidth: row.ReadFloat(1),
+                SepalLength: row.ReadFloat(2),
+                PetalWidth: row.ReadFloat(3),
+                PetalLength: row.ReadFloat(4)));
 
-                // Convert string label to integer for training.
-                var preprocess = data.MakeEstimator(row => (Label: row.Label.Dictionarize(), row.Features));
 
-                // Create a learner and train it.
-                var learner = preprocess.MakeEstimator(row => row.Label.SdcaPredict(row.Features, l1Coefficient: 0.1));
-                var classifier = learner.Fit(preprocess.FitAndTransform(data));
+            var preprocess = data.Schema.MakeEstimator(row => (
+                // Convert string label to key.
+                Label: row.Label.DictionarizeLabel(),
+                // Concatenate all features into a vector.
+                Features: row.SepalWidth.ConcatWith(row.SepalLength, row.PetalWidth, row.PetalLength)));
 
-                // Add another transformer that converts the integer prediction into string.
-                var finalTransformer = classifier.AppendTransformer(row => row.PredictedLabel.KeyToValue());
+            // Create a learner and train it.
+            var learner = preprocess.AppendEstimator(row => row.Label.SdcaPredict(row.Features, l1Coefficient: 0.1));
+            var classifier = learner.Fit(data);
 
-                // Make a prediction engine and predict.
-                engine = bundle.MakePredictionEngine<IrisExample, IrisPrediction>();
-                IrisPrediction prediction = engine.Predict(new IrisExample { Features = new[] { 3.3f, 1.6f, 0.2f, 5.1f } });
-            }
+            // Add another transformer that converts the integer prediction into string.
+            var finalTransformer = classifier.AppendTransformer(row => row.PredictedLabel.KeyToValue());
+
+            // Make a prediction engine and predict.
+            engine = finalTransformer.MakePredictionEngine<IrisExample, IrisPrediction>();
+            IrisPrediction prediction = engine.Predict(new IrisExample
+                {
+                    SepalWidth = 3.3f,
+                    SepalLength = 1.6f,
+                    PetalWidth = 0.2f,
+                    PetalLength = 5.1f
+                });
         }
     }
 }
