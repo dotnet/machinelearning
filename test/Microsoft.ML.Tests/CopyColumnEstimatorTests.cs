fbe349b61697f95b74d590b8ac0f5fed34e09365
Ivan Matantsev <ivmatan@microsoft.com>
2018-08-23 17:23:02 -0700

    header!

--- test/Microsoft.ML.Tests/CopyColumnEstimatorTests.cs
+++ test/Microsoft.ML.Tests/CopyColumnEstimatorTests.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.ML.Runtime;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.ML.Runtime;
 using Microsoft.ML.Runtime.Api;
 using Microsoft.ML.Runtime.Data;
 using Microsoft.ML.Runtime.Model;
 using Microsoft.ML.Runtime.Tools;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using Xunit;
 
