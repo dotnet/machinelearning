60ae981223e83b174ecaaf528bd51814a6b0835c
Pete Luferenko <41337831+Zruty0@users.noreply.github.com>
2018-08-24 14:08:48 -0700

    SDCA trainers become Estimators (#716)
    Converted three SDCA trainers to estimators

--- test/Microsoft.ML.Tests/Scenarios/Api/Estimators/SimpleTrainAndPredict.cs
+++ test/Microsoft.ML.Tests/Scenarios/Api/Estimators/SimpleTrainAndPredict.cs
@@ -29,7 +29,7 @@ namespace Microsoft.ML.Tests.Scenarios.Api
                 // Pipeline.
                 var pipeline = new MyTextLoader(env, MakeSentimentTextLoaderArgs())
                     .Append(new MyTextTransform(env, MakeSentimentTextTransformArgs()))
-                    .Append(new MySdca(env, new LinearClassificationTrainer.Arguments { NumThreads = 1 }, "Features", "Label"));
+                    .Append(new LinearClassificationTrainer(env, new LinearClassificationTrainer.Arguments { NumThreads = 1 }, "Features", "Label"));
 
                 // Train.
                 var model = pipeline.Fit(new MultiFileSource(dataPath));
