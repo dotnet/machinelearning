8a8bd5db73b3142bfef6cf2bb75a7cb02dd15350
Zeeshan Siddiqui <mzs@microsoft.com>
2018-08-27 18:17:45 -0700

    trigger build.

--- test/Microsoft.ML.TestFramework/TestCommandBase.cs
+++ test/Microsoft.ML.TestFramework/TestCommandBase.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.ML.Runtime.Command;
@@ -2032,6 +2033,10 @@ namespace Microsoft.ML.Runtime.RunTests
         [Fact]
         public void DataTypes()
         {
+            //Skip for linux because DATE/TIME format is different.
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                return;
+
             string idvPath = GetDataPath("datatypes.idv");
             OutputPath textOutputPath = CreateOutputPath("datatypes.txt");
             TestCore("savedata", idvPath, "loader=binary", "saver=text", textOutputPath.Arg("dout"));
