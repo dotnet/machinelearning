469e2a1530c5f03ac4da1a17d78b460bed844634
Brian Lui <t-brlui@microsoft.com>
2018-08-30 17:00:34 -0700

    Added back necessary usings

--- test/Microsoft.ML.CpuMath.PerformanceTests/Program.cs
+++ test/Microsoft.ML.CpuMath.PerformanceTests/Program.cs
@@ -2,6 +2,11 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Toolchains.InProcess;
+
 namespace Microsoft.ML.CpuMath.PerformanceTests
 {
     class Program
