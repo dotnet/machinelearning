7dcf5f63e4beb0bd28adb10585beaaaf661853a2
Senja Filipi <sefilipi@microsoft.com>
2018-09-04 16:45:13 -0700

    adding example for the simple transformation, and applying the model to another data file. Addressing most of code comments.

--- test/Microsoft.ML.Tests/Scenarios/Api/AspirationalExamples.cs
+++ test/Microsoft.ML.Tests/Scenarios/Api/AspirationalExamples.cs
@@ -29,25 +29,23 @@ namespace Microsoft.ML.Tests.Scenarios.Api
         public void FirstExperienceWithML()
         {
             // Load the data into the system.
-            string dataPath = "iris-data.txt";
-            var data = TextReader.FitAndRead(env, dataPath, row => (
-                Label: row.ReadString(0),
-                SepalWidth: row.ReadFloat(1),
-                SepalLength: row.ReadFloat(2),
-                PetalWidth: row.ReadFloat(3),
-                PetalLength: row.ReadFloat(4)));
+            string dataPath = "iris.txt";
+            var data = TextReader.CreateReader(env, dataPath, row => (
+                label: row.LoadText(0),
+                sepalWidth: row.LoadFloat(1),
+                sepalLength: row.LoadFloat(2),
+                petalWidth: row.LoadFloat(3),
+                petalLength: row.LoadFloat(4)));
 
 
-            var preprocess = data.Schema.MakeEstimator(row => (
+            var preprocess = data.MakeEstimator()
+                .Append(row => (
                 // Convert string label to key.
-                Label: row.Label.DictionarizeLabel(),
+                label: row.Label.DictionarizeLabel(),
                 // Concatenate all features into a vector.
-                Features: row.SepalWidth.ConcatWith(row.SepalLength, row.PetalWidth, row.PetalLength)));
-
-            var pipeline = preprocess
-                // Append the trainer to the training pipeline.
-                .AppendEstimator(row => row.Label.PredictWithSdca(row.Features))
-                .AppendEstimator(row => row.PredictedLabel.KeyToValue());
+                features: row.SepalWidth.ConcatWith(row.sepalLength, row.petalWidth, row.petalLength)))
+                .Append(row => row.label.PredictWithSdca(row.features))
+                .Append(row => row.predictedLabel.KeyToValue());
 
             // Train the model and make some predictions.
             var model = pipeline.Fit<IrisExample, IrisPrediction>(data);
@@ -79,35 +77,19 @@ namespace Microsoft.ML.Tests.Scenarios.Api
                                             dataSource,
                                             useHeader: true));
 
-            var data = reader.Read(dataPath).AsDynamic;
+            var data = reader.Read(dataPath);
 
-            var schema = textData.Schema;
-
-            // Verify that the columns are there. 
-            // There ought to be at least one column corresponding to the identifiers in the tuple.
-            CheckSchemaHasColumn(schema, "area", out int areaIdx);
-            CheckSchemaHasColumn(schema, "title", out int titleIdx);
-            CheckSchemaHasColumn(schema, "description", out int descriptionIdx);
-
-            // Next verify they have the expected types.
-            Assert.Equal(TextType.Instance, schema.GetColumnType(areaIdx));
-            Assert.Equal(TextType.Instance, schema.GetColumnType(titleIdx));
-            Assert.Equal(TextType.Instance, schema.GetColumnType(descriptionIdx));
-
-            // sefilipi: Do we need to inspect the data, as in Tom's example?	
-
-            var estimator = Estimator.MakeNew(data)
+            var estimator = data.MakeEstimator()
                 .Append(row => (
                     // Convert string label to key. 
                     label: row.area.Dictionarize(),
                     // featurizes 'description'
-                    description1: row.description.FeaturizeText(),
+                    description: row.description.FeaturizeText(),
                     // featurizes 'title'
-                    title1: row.title.FeaturizeText()))
+                    title: row.title.FeaturizeText()))
                 .Append(row => (
                     // Concatenate the two features into a vector.
-                    // Do we need to make other use of the TextTransform output columns?
-                    features: row.description1.ConcatWith(r.title1),
+                    features: row.description.ConcatWith(r.title),
                     // preserve the label
                     label: row.label))
                 // how do we specify arguments for the trainer?
@@ -127,23 +109,63 @@ namespace Microsoft.ML.Tests.Scenarios.Api
 
             // we don't currently have the ReadAsync
             var model = await PredictionModel.ReadAsync(ModelPath);
-
-            //create an example issue:
-            const string data = "29338	" +
-                                "area-System.Net 	" +
-                                "Include fragment and query in Uri.LocalPath on Unix	" +
-                                "While testing XmlUriResolver, @pjanotti discovered that any segments of a file path following a '#' symbol will be cut out of Uri.LocalPath on Unix. Based on additional tests, this also occurs for the '?' symbol. This is happening because the Unix specific case for local path only uses the path component of the URI:  https://github.com/dotnet/corefx/blob/9e8d443ff78c4f0a9a6bedf7f95961cf96ceff0a/src/System.Private.Uri/src/System/Uri.cs#L1032-L1037    The fix here is to include the fragment and query in LocalPath in the Unix path specific case. This PR enables the test case in XmlUriResolver that uncovered this issues, and adds some additional cases to our URI tests.    Fixes: #28486 ";
-            var dataSource = new BytesStreamSource(data);
-
-            var text = TextLoader.CreateReader(env, ctx =>
-                                          (area: ctx.LoadText(1),
-                                            title: ctx.LoadText(2),
-                                            description: ctx.LoadText(3),
-                                            dataSource,
-                                            useHeader: true));
+            var predictor = model.MakePredictionFunction<IssueInput, IssuePrediction>();
 
             // (Scalar<float> score, Scalar<float> probability, Scalar<bool> predictedLabel) 
-            var prediction = model.Predict();
+            var prediction = predictor.Predict(new IssueInput
+                {
+                    ID = "29338\t",
+                    Area = "area-System.Net\t", 
+                    Title = "Include fragment and query in Uri.LocalPath on Unix\t", 
+                    Description = "While testing XmlUriResolver, @pjanotti discovered that any segments of a file path following a '#' symbol will be cut out of Uri.LocalPath on Unix. Based on additional tests, this also occurs for the '?' symbol. This is happening because the Unix specific case for local path only uses the path component of the URI:  https://github.com/dotnet/corefx/blob/9e8d443ff78c4f0a9a6bedf7f95961cf96ceff0a/src/System.Private.Uri/src/System/Uri.cs#L1032-L1037    The fix here is to include the fragment and query in LocalPath in the Unix path specific case. This PR enables the test case in XmlUriResolver that uncovered this issues, and adds some additional cases to our URI tests.    Fixes: #28486 "
+                });
+        }
+    }
+
+    public class SimpleTransform
+    {
+        public void ScaleData()
+        {
+            var env = new TlcEnvironment(new SysRandom(0), verbose: true);
+
+            string dataPath = "iris.txt";
+
+            // Create reader with specific schema.
+            var dataReader = TextLoader.CreateReader(env, ctx => (
+               label: ctx.LoadText(0),
+               sepalWidth: ctx.LoadFloat(1),
+               sepalLength: ctx.LoadFloat(2),
+               petalWidth: ctx.LoadFloat(3),
+               petalLength: ctx.LoadFloat(4)),
+               dataPath);
+
+            // Define estimator
+            var estimator = data.MakeEstimator()
+                 .Append(row => (
+                    // Convert string label to key. 
+                    sepalWidthNorm: row.SepalWidth.Normalize(Normalizer.NormalizerMode.MinMax),
+                    // featurizes 'description'
+                    sepalLengthNorm: row.sepalLength.Normalize(Normalizer.NormalizerMode.MeanVariance),
+                    //preserve the columns
+                    petalWidth: row.petalWidth,
+                    petalLength: row.petalLength
+                    ));
+
+            // read the data
+            var data = dataReader.Read(dataPath);
+
+            // fit/train the model
+            //var is ITransformer id data is IDataView
+            var model = estimator.Fit(data);
+
+            // let's assume this iris-data.txt is a file with the same schema as the one previously used
+            // but with different values.
+            // should use the filters to split the file in two parts, to make it less confusing. 
+            string anotherDataFile = "iris.txt";
+
+            // apply the model/transformation to the new data
+            var transformedData = model.Transform(dataReader.Read(anotherDataFile));
+
         }
     }
 }
