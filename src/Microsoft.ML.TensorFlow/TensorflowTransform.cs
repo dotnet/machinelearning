1c0202f62cf3af07b4d6aa3751531f110c623052
Senja Filipi <sefilipi@microsoft.com>
2018-09-08 08:21:24 -0700

    Restructuring the doc so it includes the examples. Removing the reference from the CsharpApi, since the transform is already created in the right namespace: Microsoft.ML.Transforms.
    Changing the visibility of the Summary, UserName and ShortName to internal.

--- src/Microsoft.ML.TensorFlow/TensorflowTransform.cs
+++ src/Microsoft.ML.TensorFlow/TensorflowTransform.cs
@@ -296,9 +296,9 @@ namespace Microsoft.ML.Transforms
             public string OutputColumn;
         }
 
-        public const string Summary = "Transforms the data using the TensorFlow model.";
-        public const string UserName = "TensorFlowTransform";
-        public const string ShortName = "TFTransform";
+        internal const string Summary = "Transforms the data using the TensorFlow model.";
+        internal const string UserName = "TensorFlowTransform";
+        internal const string ShortName = "TFTransform";
         private const string RegistrationName = "TensorFlowTransform";
 
         /// <summary>
@@ -373,12 +373,7 @@ namespace Microsoft.ML.Transforms
             }
         }
 
-        [TlcModule.EntryPoint(Name = "Transforms.TensorFlowScorer",
-            Desc = Summary,
-            UserName = UserName,
-            ShortName = ShortName,
-            XmlInclude = new[] { @"<include file='../Microsoft.ML.TensorFlow/doc.xml' path='doc/members/member[@name=""TensorflowTransform""]/*' />",
-                                 @"<include file='../Microsoft.ML.TensorFlow/doc.xml' path='doc/members/example[@name=""TensorflowTransform""]/*' />" })]
+        [TlcModule.EntryPoint(Name = "Transforms.TensorFlowScorer", Desc = Summary, UserName = UserName, ShortName = ShortName)]
         public static CommonOutputs.TransformOutput TensorFlowScorer(IHostEnvironment env, Arguments input)
         {
             Contracts.CheckValue(env, nameof(env));
