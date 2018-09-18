13505875c4380e29a63d17e0cdfebad5f27a59a8
Pete Luferenko <pelufere@microsoft.com>
2018-09-17 20:24:45 -0700

    Fixed most important PR comments

--- test/Microsoft.ML.StaticPipelineTesting/StaticPipeTests.cs
+++ test/Microsoft.ML.StaticPipelineTesting/StaticPipeTests.cs
@@ -2,13 +2,12 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using Microsoft.ML.Transforms.Text;
 using Microsoft.ML.Data.StaticPipe;
-using Microsoft.ML.Runtime;
 using Microsoft.ML.Runtime.Data;
 using Microsoft.ML.Runtime.Data.IO;
 using Microsoft.ML.Runtime.Internal.Utilities;
 using Microsoft.ML.TestFramework;
+using Microsoft.ML.Transforms.Text;
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
