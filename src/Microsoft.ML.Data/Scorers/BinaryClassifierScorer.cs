46ba4306cec5ccbbcad91d407efa052a8a6dbac8
Tom Finley <tfinley@gmail.com>
2018-09-21 14:43:10 -0700

    PredictionEngine uses IRowToRowMapper, ITransformer can create IRowToRowMapper (#973)
    * * IRowToRowMapper enhancements, unification of some interfaces.
    * ITransformer has IRowToRowMapper accessor.
    * Introduce InputSchema to IRowToRowMapper.
    * Prediction engine now uses IRowToRowMapper

--- src/Microsoft.ML.Data/Scorers/BinaryClassifierScorer.cs
+++ src/Microsoft.ML.Data/Scorers/BinaryClassifierScorer.cs
@@ -93,7 +93,7 @@ namespace Microsoft.ML.Runtime.Data
             if (rowMapper == null)
                 return false; // We could cover this case, but it is of no practical worth as far as I see, so I decline to do so.
 
-            ISchema outSchema = mapper.OutputSchema;
+            ISchema outSchema = mapper.Schema;
             int scoreIdx;
             if (!outSchema.TryGetColumnIndex(MetadataUtils.Const.ScoreValueKind.Score, out scoreIdx))
                 return false; // The mapper doesn't even publish a score column to attach the metadata to.
@@ -223,7 +223,7 @@ namespace Microsoft.ML.Runtime.Data
         protected override Delegate GetPredictedLabelGetter(IRow output, out Delegate scoreGetter)
         {
             Host.AssertValue(output);
-            Host.Assert(output.Schema == Bindings.RowMapper.OutputSchema);
+            Host.Assert(output.Schema == Bindings.RowMapper.Schema);
             Host.Assert(output.IsColumnActive(Bindings.ScoreColumnIndex));
 
             ValueGetter<Float> mapperScoreGetter = output.GetGetter<Float>(Bindings.ScoreColumnIndex);
