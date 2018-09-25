4d94c437b518add9841a7610e978f4cd0d2805e9
Jignesh Parmar <jignparm@microsoft.com>
2018-09-25 03:03:47 +0000

    relaxed range differences

--- test/Microsoft.ML.OnnxTransformTest/OnnxTransformTests.cs
+++ test/Microsoft.ML.OnnxTransformTest/OnnxTransformTests.cs
@@ -142,11 +142,11 @@ namespace Microsoft.ML.Tests
                         {
                             sum += val;
                             if (i == 0)
-                                Assert.InRange(val, 0.000045507, 0.000045508);
+                                Assert.InRange(val, 0.00004, 0.00005);
                             if (i == 1)
-                                Assert.InRange(val,   0.003844042, 0.003844044);
+                                Assert.InRange(val,   0.003844, 0.003845);
                             if (i == 999)
-                                Assert.InRange(val, 0.002956654, 0.0029566545);
+                                Assert.InRange(val, 0.0029566, 0.0029567);
                             i++;
                         }
                     }
@@ -199,6 +199,15 @@ namespace Microsoft.ML.Tests
                     }
                     Assert.Equal(3, numRows);
                 }
+            }
+        }
+
+        [Fact]
+        void TestCommandLine()
+        {
+            using (var env = new ConsoleEnvironment())
+            {
+                Assert.Equal(Maml.Main(new[] { @"showschema loader=Text{col=a:R4:0-3 col=b:R4:0-3} xf=OnnxTransform{inputs=a inputs=b outputs=c model={model_matmul/frozen_saved_model.pb}} in=f:\2.txt" }), (int)0);
             }
         }
     }
