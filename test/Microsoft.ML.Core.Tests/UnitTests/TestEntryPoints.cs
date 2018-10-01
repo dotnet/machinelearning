aa93772a31e4c7bd940827496123611e6340d8aa
J W <woodjon@gmail.com>
2018-10-01 14:29:07 -0400

    Regen entry points after merging

--- test/Microsoft.ML.Core.Tests/UnitTests/TestEntryPoints.cs
+++ test/Microsoft.ML.Core.Tests/UnitTests/TestEntryPoints.cs
@@ -240,7 +240,7 @@ namespace Microsoft.ML.Runtime.RunTests
 #endif
         }
 
-        [Fact(Skip = "Execute this test if you want to regenerate ep-list and _manifest.json")]
+        [Fact]
         public void RegenerateEntryPointCatalog()
         {
             var (epListContents, jObj) = BuildManifests();
