e0c85cde4f52df86a45fb9395a4d97648ee65a74
Artidoro Pagnoni <arpagnon@microsoft.com>
2018-10-05 10:32:34 -0700

    started conversion work

--- src/Microsoft.ML.Transforms/NAIndicatorTransform.cs
+++ src/Microsoft.ML.Transforms/NAIndicatorTransform.cs
@@ -4,25 +4,34 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 using Microsoft.ML.Runtime;
 using Microsoft.ML.Runtime.CommandLine;
 using Microsoft.ML.Runtime.Data;
 using Microsoft.ML.Runtime.Data.Conversion;
+using Microsoft.ML.Runtime.Data.IO;
 using Microsoft.ML.Runtime.EntryPoints;
 using Microsoft.ML.Runtime.Internal.Utilities;
 using Microsoft.ML.Runtime.Model;
 
-[assembly: LoadableClass(typeof(NAIndicatorTransform), typeof(NAIndicatorTransform.Arguments), typeof(SignatureDataTransform),
-   NAIndicatorTransform.FriendlyName, "NAIndicatorTransform", "NAIndicator", NAIndicatorTransform.ShortName, DocName = "transform/NAHandle.md")]
+[assembly: LoadableClass(NAIndicatorTransform.Summary, typeof(IDataTransform), typeof(NAIndicatorTransform), typeof(NAIndicatorTransform.Arguments), typeof(SignatureDataTransform),
+    NAIndicatorTransform.FriendlyName, NAIndicatorTransform.LoadName, "NAIndicator", NAIndicatorTransform.ShortName, DocName = "transform/NAHandle.md")]
 
-[assembly: LoadableClass(typeof(NAIndicatorTransform), null, typeof(SignatureLoadDataTransform),
-    NAIndicatorTransform.FriendlyName, NAIndicatorTransform.LoaderSignature)]
+[assembly: LoadableClass(NAIndicatorTransform.Summary, typeof(IDataTransform), typeof(NAIndicatorTransform), null, typeof(SignatureLoadDataTransform),
+    NAIndicatorTransform.FriendlyName, NAIndicatorTransform.LoadName)]
+
+[assembly: LoadableClass(NAIndicatorTransform.Summary, typeof(NAIndicatorTransform), null, typeof(SignatureLoadModel),
+    NAIndicatorTransform.FriendlyName, NAIndicatorTransform.LoadName)]
+
+[assembly: LoadableClass(typeof(IRowMapper), typeof(NAIndicatorTransform), null, typeof(SignatureLoadRowMapper),
+   NAIndicatorTransform.FriendlyName, NAIndicatorTransform.LoadName)]
 
 namespace Microsoft.ML.Runtime.Data
 {
     /// <include file='doc.xml' path='doc/members/member[@name="NAIndicator"]'/>
-    public sealed class NAIndicatorTransform : OneToOneTransformBase
+    public sealed class NAIndicatorTransform : OneToOneTransformerBase
     {
         public sealed class Column : OneToOneColumn
         {
@@ -49,7 +58,7 @@ namespace Microsoft.ML.Runtime.Data
             public Column[] Column;
         }
 
-        public const string LoaderSignature = "NaIndicatorTransform";
+        public const string LoadName = "NaIndicatorTransform";
 
         private static VersionInfo GetVersionInfo()
         {
@@ -59,7 +68,7 @@ namespace Microsoft.ML.Runtime.Data
                 verWrittenCur: 0x00010001, // Initial
                 verReadableCur: 0x00010001,
                 verWeCanReadBack: 0x00010001,
-                loaderSignature: LoaderSignature,
+                loaderSignature: LoadName,
                 loaderAssemblyName: typeof(NAIndicatorTransform).Assembly.FullName);
         }
 
@@ -68,77 +77,145 @@ namespace Microsoft.ML.Runtime.Data
         internal const string FriendlyName = "NA Indicator Transform";
         internal const string ShortName = "NAInd";
 
-        private static string TestType(ColumnType type)
+        internal static string TestType(ColumnType type)
         {
             // Item type must have an NA value. We'll get the predicate again later when we're ready to use it.
             Delegate del;
             if (Conversions.Instance.TryGetIsNAPredicate(type.ItemType, out del))
                 return null;
             return string.Format("Type '{0}' is not supported by {1} since it doesn't have an NA value",
-                type, LoaderSignature);
+                type, LoadName);
+        }
+
+        // TODO: Why do we even need an object for this? maybe because we want to deal with array of these things
+        public class ColumnInfo
+        {
+            public readonly string Input;
+            public readonly string Output;
+
+            /// <summary>
+            /// Describes how the transformer handles one column pair.
+            /// </summary>
+            /// <param name="input">Name of input column.</param>
+            /// <param name="output">Name of output column.</param>
+            public ColumnInfo(string input, string output)
+            {
+                Input = input;
+                Output = output;
+            }
+        }
+
+        private static (string input, string output)[] GetColumnPairs(ColumnInfo[] columns)
+        {
+            Contracts.CheckValue(columns, nameof(columns));
+            return columns.Select(x => (x.Input, x.Output)).ToArray();
         }
 
         private const string RegistrationName = "NaIndicator";
 
         // The output column types, parallel to Infos.
-        private readonly ColumnType[] _types;
+        private readonly ColumnType[] _replaceTypes;
+
+        protected override void CheckInputColumn(ISchema inputSchema, int col, int srcCol)
+        {
+            var type = inputSchema.GetColumnType(srcCol);
+            string reason = TestType(type);
+            if (reason != null)
+                throw Host.ExceptParam(nameof(inputSchema), reason);
+        }
 
         /// <summary>
         /// Convenience constructor for public facing API.
         /// </summary>
         /// <param name="env">Host Environment.</param>
         /// <param name="input">Input <see cref="IDataView"/>. This is the output from previous transform or loader.</param>
-        /// <param name="name">Name of the output column.</param>
-        /// <param name="source">Name of the column to be transformed. If this is null '<paramref name="name"/>' will be used.</param>
-        public NAIndicatorTransform(IHostEnvironment env, IDataView input, string name, string source = null)
-            : this(env, new Arguments() { Column = new[] { new Column() { Source = source ?? name, Name = name } } }, input)
+        /// <param name="columns"></param>
+        public NAIndicatorTransform(IHostEnvironment env, IDataView input, params ColumnInfo[] columns)
+            : base(Contracts.CheckRef(env, nameof(env)).Register(nameof(NAReplaceTransform)), GetColumnPairs(columns))
         {
+            // Check that all the input columns are present and correct.
+            for (int i = 0; i < ColumnPairs.Length; i++)
+            {
+                if (!input.Schema.TryGetColumnIndex(ColumnPairs[i].input, out int srcCol))
+                    throw Host.ExceptSchemaMismatch(nameof(input), "input", ColumnPairs[i].input);
+                CheckInputColumn(input.Schema, i, srcCol);
+            }
         }
 
-        /// <summary>
-        /// Public constructor corresponding to SignatureDataTransform.
-        /// </summary>
-        public NAIndicatorTransform(IHostEnvironment env, Arguments args, IDataView input)
-            : base(env, RegistrationName, Contracts.CheckRef(args, nameof(args)).Column,
-                input, TestType)
-        {
-            Host.AssertNonEmpty(Infos);
-            Host.Assert(Infos.Length == Utils.Size(args.Column));
-
-            _types = GetTypesAndMetadata();
-        }
-
-        private NAIndicatorTransform(IHost host, ModelLoadContext ctx, IDataView input)
-            : base(host, ctx, input, TestType)
+        private NAIndicatorTransform(IHost host, ModelLoadContext ctx)
+            : base(host, ctx)
         {
             Host.AssertValue(ctx);
-            Host.AssertNonEmpty(Infos);
-
-            // *** Binary format ***
-            // <base>
-
-            _types = GetTypesAndMetadata();
         }
 
-        public static NAIndicatorTransform Create(IHostEnvironment env, ModelLoadContext ctx, IDataView input)
+        // Factory method for SignatureDataTransform.
+        public static IDataTransform Create(IHostEnvironment env, Arguments args, IDataView input)
         {
             Contracts.CheckValue(env, nameof(env));
-            var h = env.Register(RegistrationName);
-            h.CheckValue(ctx, nameof(ctx));
-            h.CheckValue(input, nameof(input));
+            env.CheckValue(args, nameof(args));
+            env.CheckValue(input, nameof(input));
+
+            env.CheckValue(args.Column, nameof(args.Column));
+            var cols = new ColumnInfo[args.Column.Length];
+            for (int i = 0; i < cols.Length; i++)
+            {
+                var item = args.Column[i];
+
+                cols[i] = new ColumnInfo(item.Source, item.Name);
+            };
+            return new NAIndicatorTransform(env, input, cols).MakeDataTransform(input);
+        }
+
+        public static NAIndicatorTransform Create(IHostEnvironment env, ModelLoadContext ctx)
+        {
+            Contracts.CheckValue(env, nameof(env));
+            var host = env.Register(RegistrationName);
+
+            host.CheckValue(ctx, nameof(ctx));
             ctx.CheckAtModel(GetVersionInfo());
-            return h.Apply("Loading Model", ch => new NAIndicatorTransform(h, ctx, input));
+
+            return new NAIndicatorTransform(host, ctx);
+        }
+
+        public static IDataTransform Create(IHostEnvironment env, IDataView input, params ColumnInfo[] columns)
+            => new NAIndicatorTransform(env, input, columns).MakeDataTransform(input);
+
+        // Factory method for SignatureLoadDataTransform.
+        public static IDataTransform Create(IHostEnvironment env, ModelLoadContext ctx, IDataView input)
+            => Create(env, ctx).MakeDataTransform(input);
+
+        // Factory method for SignatureLoadRowMapper.
+        public static IRowMapper Create(IHostEnvironment env, ModelLoadContext ctx, ISchema inputSchema)
+            => Create(env, ctx).MakeRowMapper(inputSchema);
+
+        private void WriteTypeAndValue<T>(Stream stream, BinarySaver saver, ColumnType type, T rep)
+        {
+            Host.AssertValue(stream);
+            Host.AssertValue(saver);
+            Host.Assert(type.RawType == typeof(T) || type.ItemType.RawType == typeof(T));
+
+            if (!saver.TryWriteTypeAndValue<T>(stream, type, ref rep, out int bytesWritten))
+                throw Host.Except("We do not know how to serialize terms of type '{0}'", type);
         }
 
         public override void Save(ModelSaveContext ctx)
         {
             Host.CheckValue(ctx, nameof(ctx));
+
             ctx.CheckAtModel();
             ctx.SetVersionInfo(GetVersionInfo());
 
-            // *** Binary format ***
-            // <base>
-            SaveBase(ctx);
+            SaveColumns(ctx);
+            var saver = new BinarySaver(Host, new BinarySaver.Arguments());
+            for (int iinfo = 0; iinfo < _replaceTypes.Length; iinfo++)
+            {
+                var repType = _replaceTypes[iinfo].ItemType;
+                object[] args = new object[] { ctx.Writer.BaseStream, saver, repType, ???? };
+                Action<Stream, BinarySaver, ColumnType, int> func = WriteTypeAndValue<int>;
+                Host.Assert(repValue.GetType() == _replaceTypes[iinfo].RawType || repValue.GetType() == _replaceTypes[iinfo].ItemType.RawType);
+                var meth = func.GetMethodInfo().GetGenericMethodDefinition().MakeGenericMethod(repValue.GetType());
+                meth.Invoke(this, args);
+            }
         }
 
         private ColumnType[] GetTypesAndMetadata()
@@ -384,6 +461,10 @@ namespace Microsoft.ML.Runtime.Data
 
                 dst = new VBuffer<bool>(srcLength, dstValues, dstIndices);
             }
+        }
+
+        protected override IRowMapper MakeRowMapper(ISchema schema)
+        {
         }
     }
 }
