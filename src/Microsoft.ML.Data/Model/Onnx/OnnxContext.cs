41982ff32e53abcbc418cc7a20a3ab8971063bea
Wei-Sheng Chin <wschin@outlook.com>
2018-10-09 12:34:39 -0700

    Fix a conversion bug in KeyToVectorTransform. (#992)
    * Fix a conversion bug in KeyToVectorTransform.
    
    (1) A ReduceSum is missing when Bag+ for merging
        all features' one-hot vectors
    (2) Since this conversion depends on the input shape,
        an API is added to retrieve shape of existing
        variables.
    
    * Fix KeyToVector's key range so that it can consume output produced by TermTransform
    
    * Make AddVariable private again
    
    * Address comments
    
    * Fix baseline file
    
    * Fix index bound
    
    * Add a baseline comparison to make sure ReduceSum is added if Bag+

--- src/Microsoft.ML.Data/Model/Onnx/OnnxContext.cs
+++ src/Microsoft.ML.Data/Model/Onnx/OnnxContext.cs
@@ -100,6 +100,13 @@ namespace Microsoft.ML.Runtime.Model.Onnx
             => CreateNode(opType, new[] { input }, new[] { output }, name, domain);
 
         /// <summary>
+        /// Retrieve the shape of an ONNX variable. Returns null if no shape for the specified variable can be found.
+        /// </summary>
+        /// <param name="variableName">The ONNX name of the returned shape</param>
+        /// <returns>The shape of the retrieved variable</returns>
+        public abstract List<long> RetrieveShapeOrNull(string variableName);
+
+        /// <summary>
         /// Call this function can declare a global float
         /// </summary>
         /// <param name="value">The float number which is going to be added</param>
