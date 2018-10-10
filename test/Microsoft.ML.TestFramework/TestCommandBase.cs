024bd4452e1d3660214c757237a19d6123f951ca
Eric Erhardt <eric.erhardt@microsoft.com>
2018-10-09 22:19:20 -0500

    Enable a QuantileRegression Test & Fix Duplicated Baseline Files (#1193)
    * Enable a QuantileRegression test
    
    Enable CommandTrainScoreEvaluateQuantileRegression, add the dataset and the necessary baselines.
    
    The only baseline changes were of the form that were caused by https://github.com/dotnet/corefx/issues/31847.
    
    * Allow BaseTestBaseline to check Common first.
    
    A lot of baseline tests have duplicated baselines between debug and release. Allow BaseTestBaseline to check the Common baseline directory for a baseline file first.
    
    Fix #410
    
    * PR feedback
    
    Add more info to the README.
    Add column headers to the housing.txt data file.

--- test/Microsoft.ML.TestFramework/TestCommandBase.cs
+++ test/Microsoft.ML.TestFramework/TestCommandBase.cs
@@ -1662,15 +1662,15 @@ namespace Microsoft.ML.Runtime.RunTests
         }
 
         [TestCategory(Cat), TestCategory("FastForest")]
-        [Fact(Skip = "Need CoreTLC specific baseline update")]
+        [Fact]
         public void CommandTrainScoreEvaluateQuantileRegression()
         {
             RunMTAThread(() =>
             {
                 // First run a training.
-                string pathData = GetDataPath(@"..\Housing (regression)", "housing.txt");
+                string pathData = GetDataPath("housing.txt");
                 OutputPath trainModel = ModelPath();
-                TestCore("train", pathData, "loader=text", "lab=Label feat=Features tr=FastForestRegression{dt+}");
+                TestCore("train", pathData, "loader=Text{header+}", "lab=Label feat=Features tr=FastForestRegression{dt+}");
 
                 // Then, run the score.
                 _step++;
