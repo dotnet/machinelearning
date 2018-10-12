6e32d8b9f24bdec16de90928d08bcf3bb68ffead
Senja Filipi <sfilipi@users.noreply.github.com>
2018-10-11 21:23:02 -0700

    Adding a sample for Vector<T> ConcatWith<T>(this Scalar<T> me, params ScalarOrVector<T>[] others) (#1189)
    * Adding a sample for
     Vector<T> ConcatWith<T>(this Scalar<T> me, params ScalarOrVector<T>[] others)
    * separating utilities related to downloading datasets, and data snippets in their own project
    fixign the links per recommendations from the docs team

--- docs/samples/Microsoft.ML.Samples/Trainers.cs
+++ docs/samples/Microsoft.ML.Samples/Trainers.cs
@@ -2,34 +2,43 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using Microsoft.ML.Runtime.Data;
-using Microsoft.ML.Runtime.Learners;
-using Microsoft.ML.Trainers;
-using System;
+// the alignment of the usings with the methods is intentional so they can display on the same level in the docs site. 
+        using Microsoft.ML.Runtime.Data;
+        using Microsoft.ML.Runtime.Learners;
+        using Microsoft.ML.Trainers;
+        using System;
 
+// NOTE: WHEN ADDING TO THE FILE, ALWAYS APPEND TO THE END OF IT. 
+// If you change the existinc content, check that the files referencing it in the XML documentation are still correct, as they reference
+// line by line. 
 namespace Microsoft.ML.Samples
 {
     public static class Trainers
     { 
+    
         public static void SdcaRegression()
         {
-            var (trainDataPath, testDataPath) = DatasetCreator.CreateRegressionDataset();
+            // Downloading a regression dataset from github.com/dotnet/machinelearning
+            // this will create a housing.txt file in the filsystem this code will run
+            // you can open the file to see the data. 
+            string dataFile = SamplesUtils.DatasetUtils.DownloadHousingRegressionDataset();
 
-            //creating the ML.Net IHostEnvironment object, needed for the pipeline
+            // Creating the ML.Net IHostEnvironment object, needed for the pipeline
             var env = new LocalEnvironment(seed: 0);
 
-            // creating the ML context, based on the task performed.
+            // Creating the ML context, based on the task performed.
             var regressionContext = new RegressionContext(env);
 
             // Creating a data reader, based on the format of the data
             var reader = TextLoader.CreateReader(env, c => (
-                     label: c.LoadFloat(2),
-                     features: c.LoadFloat(0, 1)
-                 ),
-                separator: ',', hasHeader: true);
+                        label: c.LoadFloat(0),
+                        features: c.LoadFloat(1, 6)
+                    ),
+                separator: '\t', hasHeader: true);
 
-            // Read the data
-            var trainData = reader.Read(new MultiFileSource(trainDataPath));
+            // Read the data, and leave 10% out, so we can use them for testing
+            var data = reader.Read(new MultiFileSource(dataFile));
+            var (trainData, testData) = regressionContext.TrainTestSplit(data, testFraction: 0.1);
 
             // The predictor that gets produced out of training
             LinearRegressionPredictor pred = null;
@@ -45,28 +54,25 @@ namespace Microsoft.ML.Samples
                                 )
                         );
 
-            // fit this pipeline to the training data
+            // Fit this pipeline to the training data
             var model = learningPipeline.Fit(trainData);
 
-            // check the weights that the model learned
+            // Check the weights that the model learned
             VBuffer<float> weights = default;
             pred.GetFeatureWeights(ref weights);
 
             Console.WriteLine($"weight 0 - {weights.Values[0]}");
             Console.WriteLine($"weight 1 - {weights.Values[1]}");
 
-            // test the model we just trained, using the test file. 
-            var testData = reader.Read(new MultiFileSource(testDataPath));
-            var data = model.Transform(testData);
+            // Evaluate how the model is doing on the test data
+            var dataWithPredictions = model.Transform(testData);
+            var metrics = regressionContext.Evaluate(dataWithPredictions, r => r.label, r => r.score);
 
-            //Evaluate how the model is doing on the test data
-            var metrics = regressionContext.Evaluate(data, r => r.label, r => r.score);
-
-            Console.WriteLine($"L1 - {metrics.L1}");
-            Console.WriteLine($"L2 - {metrics.L2}");
-            Console.WriteLine($"LossFunction - {metrics.LossFn}");
-            Console.WriteLine($"RMS - {metrics.Rms}");
-            Console.WriteLine($"RSquared - {metrics.RSquared}");
+            Console.WriteLine($"L1 - {metrics.L1}");  // 3.7226085
+            Console.WriteLine($"L2 - {metrics.L2}");  // 24.250636
+            Console.WriteLine($"LossFunction - {metrics.LossFn}");  // 24.25063
+            Console.WriteLine($"RMS - {metrics.Rms}");  // 4.924493
+            Console.WriteLine($"RSquared - {metrics.RSquared}");  // 0.565467
         }
     }
 }
