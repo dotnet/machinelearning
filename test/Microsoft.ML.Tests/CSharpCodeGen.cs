4e4082aae64c76ea00f40e13a5d43510034e2267
Scott Inglis <singlis@microsoft.com>
2018-10-16 14:47:59 -0700

    -Reverting change for GenerateCSharpAPI test.

--- test/Microsoft.ML.Tests/CSharpCodeGen.cs
+++ test/Microsoft.ML.Tests/CSharpCodeGen.cs
@@ -15,8 +15,7 @@ namespace Microsoft.ML.Tests
         {
         }
 
-        //[Fact(Skip = "Execute this test if you want to regenerate CSharpApi file")]
-        [Fact]
+        [Fact(Skip = "Execute this test if you want to regenerate CSharpApi file")]
         public void RegenerateCSharpApi()
         {
             var basePath = GetDataPath("../../src/Microsoft.ML.Legacy/CSharpApi.cs");
