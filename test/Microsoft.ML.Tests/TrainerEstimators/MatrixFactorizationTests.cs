a2d37ae4616d3c1d5aada2e69d21f9fbec0f95da
Wei-Sheng Chin <wechi@microsoft.com>
2018-10-17 14:54:12 -0700

    Add two tests and enable SSE

--- test/Microsoft.ML.Tests/TrainerEstimators/MatrixFactorizationTests.cs
+++ test/Microsoft.ML.Tests/TrainerEstimators/MatrixFactorizationTests.cs
@@ -1,8 +1,8 @@
-﻿using Microsoft.ML.Runtime.Data;
+﻿using Microsoft.ML.Runtime.Api;
+using Microsoft.ML.Runtime.Data;
 using Microsoft.ML.Runtime.Recommend;
 using Microsoft.ML.Runtime.RunTests;
 using Xunit;
-using Xunit.Abstractions;
 
 namespace Microsoft.ML.Tests.TrainerEstimators
 {
@@ -28,6 +28,72 @@ namespace Microsoft.ML.Tests.TrainerEstimators
             TestEstimatorCore(est, data);
 
             Done();
+        }
+
+        [Fact]
+        public void MatrixFactorizationSimpleTrainAndPredict()
+        {
+            using (var env = new LocalEnvironment(seed: 1, conc: 1))
+            {
+                // Specific column names of the considered data set
+                string labelColumnName = "Label";
+                string userColumnName = "User";
+                string itemColumnName = "Item";
+                string scoreColumnName = "Score";
+
+                // Create reader for both of training and test data sets
+                var reader = new TextLoader(env, GetMfTrivialLoaderArgs(labelColumnName, userColumnName, itemColumnName));
+
+                // Read training data as an IDataView object
+                var data = reader.Read(new MultiFileSource(GetDataPath(TestDatasets.trivialMatrixFactorization.trainFilename)));
+
+                // Create a pipeline with a single operator.
+                var pipeline = new MatrixFactorizationTrainer(env, labelColumnName, userColumnName, itemColumnName, 
+                    advancedSettings:s=>
+                    {
+                        s.NumIterations = 3;
+                        s.NumThreads = 1; // To eliminate randomness, # of threads must be 1.
+                        s.K = 7;
+                    });
+
+                // Train a matrix factorization model.
+                var model = pipeline.Fit(data);
+
+                // Read the test data set as an IDataView
+                var testData = reader.Read(new MultiFileSource(GetDataPath(TestDatasets.trivialMatrixFactorization.testFilename)));
+
+                // Apply the trained model to the test set
+                var prediction = model.Transform(testData);
+
+                // Retrieve label column's index from the test IDataView
+                testData.Schema.TryGetColumnIndex(labelColumnName, out int labelColumnId);
+
+                // Retrieve score column's index from the IDataView produced by the trained model
+                prediction.Schema.TryGetColumnIndex(scoreColumnName, out int scoreColumnId);
+
+                // Go through the whole test set and calculate the expected statistics.
+                double error = 0;
+                long count = 0;
+                using (var cursor = testData.GetRowCursor(c => c == labelColumnId))
+                using (var scoreCursor = prediction.GetRowCursor(c => c == scoreColumnId))
+                {
+                    float label = 0;
+                    float score = 0;
+                    ValueGetter<float> labelGetter = RowCursorUtils.GetGetterAs<float>(NumberType.R4, cursor, labelColumnId);
+                    ValueGetter<float> scoreGetter = RowCursorUtils.GetGetterAs<float>(NumberType.R4, scoreCursor, scoreColumnId);
+                    while (cursor.MoveNext() && scoreCursor.MoveNext())
+                    {
+                        labelGetter(ref label);
+                        scoreGetter(ref score);
+                        error += (score - label) * (score - label);
+                        ++count;
+                    }
+                }
+                error /= count;
+                double tolerance = System.Math.Pow(10, -DigitsOfPrecision);
+                double expectedError = 0.6151565;
+                Assert.True(System.Math.Abs(error - expectedError) < tolerance);
+            }
         }
 
         private TextLoader.Arguments GetMfTrivialLoaderArgs(string labelColumnName, string xColumnName, string yColumnName)
