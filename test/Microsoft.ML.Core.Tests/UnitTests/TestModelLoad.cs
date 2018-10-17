d5407f7f7f0cb7f8a1b2be5acccd82e59266105d
Eric Erhardt <eric.erhardt@microsoft.com>
2018-10-17 17:39:19 -0500

    Loading old model files is broken.
    Older model files are failing to load due to FpTail checks in the model validation code. These checks weren't happening in the old model loading code, and they now fail on some older models.
    
    Fix this by returning early when it is an older model, and there are no strings. This preserves the old behavior.
    
    Fix #1289

---
+++ test/Microsoft.ML.Core.Tests/UnitTests/TestModelLoad.cs
@@ -0,0 +1,34 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.ML.Runtime.Data;
+using Microsoft.ML.Runtime.Model;
+using Microsoft.ML.TestFramework;
+using System.IO;
+using Xunit;
+
+namespace Microsoft.ML.Runtime.RunTests
+{
+    public class TestModelLoad
+    {
+        /// <summary>
+        /// Tests loading a model file that was saved using an older version still loads correctly.
+        /// </summary>
+        [Fact]
+        public void LoadOriginalBinaryLoaderModel()
+        {
+            using (var env = new LocalEnvironment()
+                .AddStandardComponents())
+            using (var modelStream = File.OpenRead("OriginalVersionBinaryLoader.zip"))
+            using (var rep = RepositoryReader.Open(modelStream, env))
+            {
+                IDataLoader result = ModelFileUtils.LoadLoader(env, rep, new MultiFileSource(null), true);
+
+                Assert.Equal(2, result.Schema.ColumnCount);
+                Assert.Equal("Image", result.Schema[0].Name);
+                Assert.Equal("Class", result.Schema[1].Name);
+            }
+        }
+    }
+}
