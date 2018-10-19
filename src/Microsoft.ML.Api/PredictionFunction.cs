b9cb0c3e1000ad509815a05a9c7dca26f910d77a
Pete Luferenko <41337831+Zruty0@users.noreply.github.com>
2018-10-19 14:04:27 -0700

    PredictionFunction doesn't reuse output (#1293)

--- src/Microsoft.ML.Api/PredictionFunction.cs
+++ src/Microsoft.ML.Api/PredictionFunction.cs
@@ -17,6 +17,11 @@ namespace Microsoft.ML.Runtime.Data
     {
         private readonly PredictionEngine<TSrc, TDst> _engine;
 
+        /// <summary>
+        /// Create an instance of <see cref="PredictionFunction{TSrc, TDst}"/>.
+        /// </summary>
+        /// <param name="env">The host environment.</param>
+        /// <param name="transformer">The model (transformer) to use for prediction.</param>
         public PredictionFunction(IHostEnvironment env, ITransformer transformer)
         {
             Contracts.CheckValue(env, nameof(env));
@@ -26,7 +31,21 @@ namespace Microsoft.ML.Runtime.Data
             _engine = env.CreatePredictionEngine<TSrc, TDst>(transformer);
         }
 
+        /// <summary>
+        /// Perform one prediction using the model.
+        /// </summary>
+        /// <param name="example">The object that holds values to predict from.</param>
+        /// <returns>The object populated with prediction results.</returns>
         public TDst Predict(TSrc example) => _engine.Predict(example);
+
+        /// <summary>
+        /// Perform one prediction using the model.
+        /// Reuses the provided prediction object, which is more efficient in high-load scenarios.
+        /// </summary>
+        /// <param name="example">The object that holds values to predict from.</param>
+        /// <param name="prediction">The object to store the predictions in. If it's <c>null</c>, a new object is created,
+        /// otherwise the provided object is used.</param>
+        public void Predict(TSrc example, ref TDst prediction) => _engine.Predict(example, ref prediction);
     }
 
     public static class PredictionFunctionExtensions
