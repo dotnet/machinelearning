b7aed91ee9be32912e4533438afc63cfb4df31cb
Fernanda Leite <contato@nandaleite.com>
2018-10-19 13:48:53 -0300

    Remove <c> tag from bullet list

--- src/Microsoft.ML.Legacy/Models/OnnxConverter.cs
+++ src/Microsoft.ML.Legacy/Models/OnnxConverter.cs
@@ -29,17 +29,17 @@ namespace Microsoft.ML.Legacy.Models
         ///
         /// <para>This API supports the following arguments:</para>
         /// <list type="bullet">
-        /// <item><description><c><see cref="Onnx"/></c> indicates the file to write the ONNX protocol buffer file to. This is required.</description></item>
-        /// <item><description><c><see cref="Json"/></c> indicates the file to write the JSON representation of the ONNX model. This is optional.</description></item>
-        /// <item><description><c><see cref="Name"/></c> indicates the name property in the ONNX model. If left unspecified, it will
+        /// <item><description><see cref="Onnx"/> indicates the file to write the ONNX protocol buffer file to. This is required.</description></item>
+        /// <item><description><see cref="Json"/> indicates the file to write the JSON representation of the ONNX model. This is optional.</description></item>
+        /// <item><description><see cref="Name"/> indicates the name property in the ONNX model. If left unspecified, it will
         /// be the extension-less name of the file specified in the onnx indicates the protocol buffer file
         /// to write the ONNX representation to.</description></item>
-        /// <item><description><c><see cref="Domain"/></c> indicates the domain name of the model. ONNX uses reverse domain name space indicators.
+        /// <item><description><see cref="Domain"/> indicates the domain name of the model. ONNX uses reverse domain name space indicators.
         /// For example com.microsoft.cognitiveservices. This is a required field.</description></item>
-        /// <item><description><c><see cref="InputsToDrop"/></c> is a string array of input column names to omit from the input mapping.
+        /// <item><description><see cref="InputsToDrop"/> is a string array of input column names to omit from the input mapping.
         /// A common scenario might be to drop the label column, for instance, since it may not be practically
         /// useful for the pipeline. Note that any columns depending on these naturally cannot be saved.</description></item>
-        /// <item><description><c><see cref="OutputsToDrop"/></c> is similar, except for the output schema. Note that the pipeline handler
+        /// <item><description><see cref="OutputsToDrop"/> is similar, except for the output schema. Note that the pipeline handler
         /// is currently not intelligent enough to drop intermediate calculations that produce this value: this will
         /// merely omit that value from the actual output.</description></item>
         /// </list>
