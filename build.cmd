b0f541e099598343c15ff340cac22716e00cd0e3
Wei-Sheng Chin <wechi@microsoft.com>
2018-10-23 11:14:03 -0700

    Move submodule update to yml files

--- build.cmd
+++ build.cmd
@@ -1,3 +1,2 @@
-git submodule update --init
 @call "%~dp0run.cmd" build %*
 @exit /b %ERRORLEVEL%
--- build.sh
+++ build.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-git submodule update --init
-
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
   DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
