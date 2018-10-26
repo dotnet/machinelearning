38fecff3c23371df1277eb197cde2f9be2f333c0
vaeksare <42353187+vaeksare@users.noreply.github.com>
2018-10-26 16:48:31 -0700

    Reworked to use estimator chain

--- src/Microsoft.ML.OnnxTransform/DnnImageFeaturizerTransform.cs
+++ src/Microsoft.ML.OnnxTransform/DnnImageFeaturizerTransform.cs
@@ -9,57 +9,20 @@ using System.Text;
 
 namespace Microsoft.ML.Transforms
 {
-    public sealed class DnnImageFeaturizerEstimator : TrivialEstimator<TransformerChain<OnnxTransform>>
+    public sealed class DnnImageFeaturizerEstimator : IEstimator<TransformerChain<OnnxTransform>>
     {
-        private OnnxTransform _prepTransform;
-        private OnnxTransform _mainTransform;
+        private readonly EstimatorChain<OnnxTransform> _modelChain;
 
-        public DnnImageFeaturizerEstimator(IHostEnvironment env, DnnModelType model, string input, string output)
-            : this(env, CreateChain(env, model, input, output))
+        public DnnImageFeaturizerEstimator(IHostEnvironment env, string input, string output, DnnModelType model)
         {
-        }
-
-        public DnnImageFeaturizerEstimator(IHostEnvironment env, TransformerChain<OnnxTransform> transformer)
-            : base(Contracts.CheckRef(env, nameof(env)).Register(nameof(TransformerChain<OnnxTransform>)), transformer)
-        {
-        }
-
-        private TransformerChain<OnnxTransform> CreateChain(IHostEnvironment env, DnnModelType model, string input, string output)
-        {
+            _modelChain = new EstimatorChain<OnnxTransform>();
             _modelsPreprocess.TryGetValue(model, out string prepModel);
             _modelsMain.TryGetValue(model, out string mainModel);
-            string tempCol = "onnxDnnPrep";
-            _prepTransform = new OnnxTransform(env, prepModel, input, tempCol);
-            _mainTransform = new OnnxTransform(env, mainModel, tempCol, output);
-            return new TransformerChain<OnnxTransform>(_prepTransform, _mainTransform);
-        }
-
-        public override SchemaShape GetOutputSchema(SchemaShape inputSchema)
-        {
-            throw new NotImplementedException();
-        }
-
-        private SchemaShape GetIntermediateSchema(SchemaShape inputSchema, OnnxTransform transformer)
-        {
-            Host.CheckValue(inputSchema, nameof(inputSchema));
-            var result = inputSchema.Columns.ToDictionary(x => x.Name);
-            var resultDic = inputSchema.Columns.ToDictionary(x => x.Name);
-
-            var input = transformer.Input;
-            if (!inputSchema.TryFindColumn(input, out var col))
-                throw Host.ExceptSchemaMismatch(nameof(inputSchema), "input", input);
-            if (!(col.Kind == SchemaShape.Column.VectorKind.VariableVector || col.Kind == SchemaShape.Column.VectorKind.Vector))
-                throw Host.ExceptSchemaMismatch(nameof(inputSchema), "input", input, nameof(VectorType), col.GetTypeString());
-            var inputNodeInfo = transformer.Model.ModelInfo.InputsInfo[0];
-            var expectedType = OnnxUtils.OnnxToMlNetType(inputNodeInfo.Type);
-            if (col.ItemType != expectedType)
-                throw Host.ExceptSchemaMismatch(nameof(inputSchema), "input", input, expectedType.ToString(), col.ItemType.ToString());
-
-            resultDic[transformer.Output] = new SchemaShape.Column(transformer.Output,
-                transformer.OutputType.IsKnownSizeVector ? SchemaShape.Column.VectorKind.Vector
-                : SchemaShape.Column.VectorKind.VariableVector, NumberType.R4, false);
-
-            return new SchemaShape(resultDic.Values);
+            var tempCol = "onnxDnnPrep";
+            var prepEstimator = new OnnxEstimator(env, prepModel, input, tempCol);
+            var mainEstimator = new OnnxEstimator(env, mainModel, tempCol, output);
+            _modelChain.Append(prepEstimator);
+            _modelChain.Append(mainEstimator);
         }
 
         public enum DnnModelType : byte
@@ -72,7 +35,7 @@ namespace Microsoft.ML.Transforms
 
         private static Dictionary<DnnModelType, string> _modelsPreprocess = new Dictionary<DnnModelType, string>()
         {
-             { DnnModelType.Resnet18, "glove.6B.50d.txt" },
+             { DnnModelType.Resnet18, "C:\\Models\\DnnImageFeat\\Results\\FinalOnnx\\Prep\\resnetPreprocess.onnx" },
              { DnnModelType.Resnet50, "glove.6B.100d.txt" },
              { DnnModelType.Resnet101, "glove.6B.200d.txt" },
              { DnnModelType.Alexnet, "glove.6B.300d.txt" },
@@ -80,10 +43,20 @@ namespace Microsoft.ML.Transforms
 
         private static Dictionary<DnnModelType, string> _modelsMain = new Dictionary<DnnModelType, string>()
         {
-             { DnnModelType.Resnet18, "glove.6B.50d.txt" },
+             { DnnModelType.Resnet18, "C:\\Models\\DnnImageFeat\\Results\\FinalOnnx\\ResNet18\\resnet18.onnx" },
              { DnnModelType.Resnet50, "glove.6B.100d.txt" },
              { DnnModelType.Resnet101, "glove.6B.200d.txt" },
              { DnnModelType.Alexnet, "glove.6B.300d.txt" },
         };
+
+        public TransformerChain<OnnxTransform> Fit(IDataView input)
+        {
+            return _modelChain.Fit(input);
+        }
+
+        public SchemaShape GetOutputSchema(SchemaShape inputSchema)
+        {
+            return _modelChain.GetOutputSchema(inputSchema);
+        }
     }
 }
