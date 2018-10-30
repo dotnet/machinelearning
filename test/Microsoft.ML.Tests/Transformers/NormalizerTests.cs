201225c834fc0cd66290a87ecca662e33964101b
Artidoro Pagnoni <arpagnon@microsoft.com>
2018-10-30 14:51:31 -0700

    unused usings

--- test/Microsoft.ML.Tests/Transformers/NormalizerTests.cs
+++ test/Microsoft.ML.Tests/Transformers/NormalizerTests.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using Microsoft.ML;
 using Microsoft.ML.Runtime.Data;
 using Microsoft.ML.Runtime.Data.IO;
 using Microsoft.ML.Runtime.Model;
@@ -10,7 +9,6 @@ using Microsoft.ML.Runtime.RunTests;
 using Microsoft.ML.Runtime.Tools;
 using Microsoft.ML.Transforms;
 using Microsoft.ML.Transforms.Normalizers;
-using System;
 using System.IO;
 using Xunit;
 using Xunit.Abstractions;
