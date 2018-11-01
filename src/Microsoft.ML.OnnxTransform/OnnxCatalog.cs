d2a0db33cbcda5db4f62aefe7ccee0b8ee6acbe5
Senja Filipi <sefilipi@microsoft.com>
2018-10-31 23:38:28 -0700

    Adding more transform extensions

---
+++ src/Microsoft.ML.OnnxTransform/OnnxCatalog.cs
@@ -0,0 +1,34 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.ML.Runtime;
+using Microsoft.ML.Runtime.Data;
+using Microsoft.ML.Transforms;
+
+namespace Microsoft.ML
+{
+    public static class OnnxCatalog
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="OnnxScoringEstimator"/>.
+        /// </summary>
+        /// <param name="catalog">The transform's catalog.</param>
+        /// <param name="modelFile">The path of the file containing the ONNX model.</param>
+        /// <param name="inputColumn">The input column.</param>
+        /// <param name="outputColumn">The output column resulting from the transformation.</param>
+        public static OnnxScoringEstimator ApplyOnnxModel(this TransformsCatalog.CategoricalTransforms catalog,
+            string modelFile,
+            string inputColumn,
+            string outputColumn)
+        => new OnnxScoringEstimator(CatalogUtils.GetEnvironment(catalog), modelFile, inputColumn, outputColumn);
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="OnnxScoringEstimator"/>.
+        /// </summary>
+        /// <param name="catalog">The transform's catalog.</param>
+        /// <param name="transformer">The ONNX transformer.</param>
+        public static OnnxScoringEstimator ApplyOnnxModel(this TransformsCatalog.CategoricalTransforms catalog, OnnxTransform transformer)
+            => new OnnxScoringEstimator(CatalogUtils.GetEnvironment(catalog), transformer);
+    }
+}
---
+++ src/Microsoft.ML.PCA/PCACatalog.cs
@@ -0,0 +1,40 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.ML.Runtime;
+using Microsoft.ML.Runtime.Data;
+using Microsoft.ML.Transforms.PCA;
+
+namespace Microsoft.ML
+{
+    public static class PcaCatalog
+    {
+
+        /// <summary>Initializes a new instance of <see cref="PrincipalComponentAnalysisEstimator"/>.</summary>
+        /// <param name="catalog">The transform's catalog.</param>
+        /// <param name="inputColumn">Input column to apply PrincipalComponentAnalysis on.</param>
+        /// <param name="outputColumn">Optional output column. Null means <paramref name="inputColumn"/> is replaced.</param>
+        /// <param name="weightColumn">The name of the weight column.</param>
+        /// <param name="rank">The number of principal components.</param>
+        /// <param name="overSampling">Oversampling parameter for randomized PrincipalComponentAnalysis training.</param>
+        /// <param name="center">If enabled, data is centered to be zero mean.</param>
+        /// <param name="seed">The seed for random number generation.</param>
+        public static PrincipalComponentAnalysisEstimator ProjectToPrincipalComponents(this TransformsCatalog.ProjectionTransforms catalog,
+            string inputColumn,
+            string outputColumn = null,
+            string weightColumn = PrincipalComponentAnalysisEstimator.Defaults.WeightColumn,
+            int rank = PrincipalComponentAnalysisEstimator.Defaults.Rank,
+            int overSampling = PrincipalComponentAnalysisEstimator.Defaults.Oversampling,
+            bool center = PrincipalComponentAnalysisEstimator.Defaults.Center,
+            int? seed = null)
+            => new PrincipalComponentAnalysisEstimator(CatalogUtils.GetEnvironment(catalog),
+                inputColumn, outputColumn, weightColumn, rank, overSampling, center, seed);
+
+        /// <summary>Initializes a new instance of <see cref="PrincipalComponentAnalysisEstimator"/>.</summary>
+        /// <param name="catalog">The transform's catalog.</param>
+        /// <param name="columns">Input columns to apply PrincipalComponentAnalysis on.</param>
+        public static PrincipalComponentAnalysisEstimator ProjectToPrincipalComponents(this TransformsCatalog.ProjectionTransforms catalog, params PcaTransform.ColumnInfo[] columns)
+            => new PrincipalComponentAnalysisEstimator(CatalogUtils.GetEnvironment(catalog), columns);
+    }
+}
