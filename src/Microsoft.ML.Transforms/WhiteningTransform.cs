6a278c5b2c41d26486a81952f2b1e5fda411d0df
Artidoro Pagnoni <arpagnon@microsoft.com>
2018-11-05 13:45:59 -0800

    adding debugging statements

--- src/Microsoft.ML.Transforms/WhiteningTransform.cs
+++ src/Microsoft.ML.Transforms/WhiteningTransform.cs
@@ -343,7 +343,11 @@ namespace Microsoft.ML.Transforms.Projections
 
                 // Compute covariance matrix (sigma).
                 var u = new Float[ccol * ccol];
+
+                // REMOVE AFTER DEBUGGING
                 ch.Info("Computing covariance matrix...");
+                ch.Info("data = " + ArrayPrint(data));
+
                 Mkl.Gemm(Layout, Mkl.Transpose.Trans, Mkl.Transpose.NoTrans,
                     ccol, ccol, crow, 1 / (Float)crow, data, ccol, data, ccol, 0, u, ccol);
 
@@ -358,8 +362,9 @@ namespace Microsoft.ML.Transforms.Projections
                 int r = Mkl.Svd(Layout, Mkl.SvdJob.MinOvr, Mkl.SvdJob.None,
                     ccol, ccol, u, ccol, eigValues, null, ccol, null, ccol, unconv);
                 // REMOVE AFTER DEBUGGING:
-                ch.Info("cov mat = " + ArrayPrint(eigValues));
+                ch.Info("eig mat = " + ArrayPrint(eigValues));
                 ch.Info("r = " + r.ToString());
+
                 ch.Assert(r == 0);
                 if (r > 0)
                     throw ch.Except("SVD did not converge.");
