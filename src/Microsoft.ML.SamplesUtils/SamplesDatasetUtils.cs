888108e685d9cddb607a124693194a34776639c3
Bojan Misic <misic.bojan@gmail.com>
2018-11-06 22:27:39 +0100

    Documentation samples for binary classifiers (Static API) (#1456)
    * Static SDCA Trainer Example rename
    
    * Add SDCA Binary Classification example to Microsoft.ML.Samples
    
    * Add Fast Tree Binary Classification example to Microsoft.ML.Samples
    
    * Add Averaged Perceptron binary classification example to Microsoft.ML.Samples
    
    * Add LightGBM Binary Classification example to Microsoft.ML.Samples
    
    * Renaming in Microsoft.ML.Samples Static examples

--- src/Microsoft.ML.SamplesUtils/SamplesDatasetUtils.cs
+++ src/Microsoft.ML.SamplesUtils/SamplesDatasetUtils.cs
@@ -17,10 +17,16 @@ namespace Microsoft.ML.SamplesUtils
         => Download("https://raw.githubusercontent.com/dotnet/machinelearning/024bd4452e1d3660214c757237a19d6123f951ca/test/data/housing.txt", "housing.txt");
 
         /// <summary>
-        /// Downloads the adult dataset from the ML.NET repo
+        /// Downloads the wikipedia detox dataset from the ML.NET repo
         /// </summary>
         public static string DownloadSentimentDataset()
         => Download("https://github.com/dotnet/machinelearning/blob/76cb2cdf5cc8b6c88ca44b8969153836e589df04/test/data/wikipedia-detox-250-line-data.tsv", "sentiment.tsv");
+
+        /// <summary>
+        /// Downloads the adult dataset from the ML.NET repo
+        /// </summary>
+        public static string DownloadAdultDataset()
+            => Download("https://raw.githubusercontent.com/dotnet/machinelearning/244a8c2ac832657af282aa312d568211698790aa/test/data/adult.train", "adult.txt");
 
         private static string Download(string baseGitPath, string dataFile)
         {
