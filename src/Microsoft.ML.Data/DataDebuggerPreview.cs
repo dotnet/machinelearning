529194634399d9e8f771eb210b4bec13250401b3
Pete Luferenko <41337831+Zruty0@users.noreply.github.com>
2018-11-06 09:26:05 -0800

    Debugger preview for data, schema, metadata, VBuffer (#1518)
    Fixes #1242 . Adds debugger previews for data, schema, metadata, VBuffer

---
+++ src/Microsoft.ML.Data/DataDebuggerPreview.cs
@@ -0,0 +1,112 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.ML.Runtime;
+using Microsoft.ML.Runtime.Data;
+using Microsoft.ML.Runtime.Internal.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace Microsoft.ML.Data
+{
+    /// <summary>
+    /// This class represents an eager 'preview' of a <see cref="IDataView"/>.
+    /// </summary>
+    public sealed class DataDebuggerPreview
+    {
+        internal static class Defaults
+        {
+            public const int MaxRows = 100;
+        }
+
+        public Schema Schema { get; }
+        public ImmutableArray<ColumnInfo> ColumnView { get; }
+        public ImmutableArray<RowInfo> RowView { get; }
+
+        internal DataDebuggerPreview(IDataView data, int maxRows = Defaults.MaxRows)
+        {
+            Contracts.CheckValue(data, nameof(data));
+            Contracts.CheckParam(maxRows >= 0, nameof(maxRows));
+            Schema = data.Schema;
+
+            int n = data.Schema.ColumnCount;
+
+            var rows = new List<RowInfo>();
+            var columns = new List<object>[n];
+            for (int i = 0; i < columns.Length; i++)
+                columns[i] = new List<object>();
+
+            using (var cursor = data.GetRowCursor(c => true))
+            {
+                var setters = new Action<RowInfo, List<object>>[n];
+                for (int i = 0; i < n; i++)
+                    setters[i] = Utils.MarshalInvoke(MakeSetter<int>, data.Schema[i].Type.RawType, cursor, i);
+
+                int count = 0;
+                while (count < maxRows && cursor.MoveNext())
+                {
+                    var curRow = new RowInfo(n);
+                    for (int i = 0; i < setters.Length; i++)
+                        setters[i](curRow, columns[i]);
+
+                    rows.Add(curRow);
+                    count++;
+                }
+            }
+            RowView = rows.ToImmutableArray();
+            ColumnView = Enumerable.Range(0, n).Select(c => new ColumnInfo(data.Schema[c], columns[c].ToArray())).ToImmutableArray();
+        }
+
+        public override string ToString()
+            => $"{Schema.ColumnCount} columns, {RowView.Length} rows";
+
+        private Action<RowInfo, List<object>> MakeSetter<T>(IRow row, int col)
+        {
+            var getter = row.GetGetter<T>(col);
+            string name = row.Schema[col].Name;
+            Action<RowInfo, List<object>> result = (rowInfo, list) =>
+            {
+                T value = default;
+                getter(ref value);
+                rowInfo.Values[col] = new KeyValuePair<string, object>(name, value);
+
+                // Call getter again on another buffer, since we store it in two places.
+                value = default;
+                getter(ref value);
+                list.Add(value);
+            };
+            return result;
+        }
+
+        public sealed class RowInfo
+        {
+            [System.Diagnostics.DebuggerBrowsable(System.Diagnostics.DebuggerBrowsableState.RootHidden)]
+            public KeyValuePair<string, object>[] Values { get; }
+
+            public override string ToString()
+                => $"{Values.Length} columns";
+
+            internal RowInfo(int n)
+            {
+                Values = new KeyValuePair<string, object>[n];
+            }
+        }
+
+        public sealed class ColumnInfo
+        {
+            public Schema.Column Column { get; }
+            public object[] Values { get; }
+
+            public override string ToString() => $"{Column.Name}: {Column.Type}";
+
+            internal ColumnInfo(Schema.Column column, object[] values)
+            {
+                Column = column;
+                Values = values;
+            }
+        }
+    }
+}
---
+++ src/Microsoft.ML.Data/DebuggerExtensions.cs
@@ -0,0 +1,66 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.ML.Core.Data;
+using Microsoft.ML.Data;
+using Microsoft.ML.Runtime;
+using Microsoft.ML.Runtime.Data;
+using Microsoft.ML.Transforms;
+
+namespace Microsoft.ML
+{
+    /// <summary>
+    /// Static extensions for data preview.
+    /// </summary>
+    public static class DebuggerExtensions
+    {
+        /// <summary>
+        /// Extract a 'head' of the data view in a view that is convenient to debug.
+        /// </summary>
+        /// <param name="data">The data view to preview</param>
+        /// <param name="maxRows">Maximum number of rows to pull</param>
+        public static DataDebuggerPreview Preview(this IDataView data, int maxRows = DataDebuggerPreview.Defaults.MaxRows)
+            => new DataDebuggerPreview(data, maxRows);
+
+        /// <summary>
+        /// Preview an effect of the <paramref name="transformer"/> on a given <paramref name="data"/>.
+        /// </summary>
+        /// <param name="transformer">The transformer which effect we are previewing</param>
+        /// <param name="data">The data view to use for preview</param>
+        /// <param name="maxRows">Maximum number of rows to pull</param>
+        public static DataDebuggerPreview Preview(this ITransformer transformer, IDataView data, int maxRows = DataDebuggerPreview.Defaults.MaxRows)
+            => new DataDebuggerPreview(transformer.Transform(data), maxRows);
+
+        /// <summary>
+        /// Preview an effect of the <paramref name="estimator"/> on a given <paramref name="data"/>.
+        /// </summary>
+        /// <param name="estimator">The estimnator which effect we are previewing</param>
+        /// <param name="data">The data view to use for preview</param>
+        /// <param name="maxRows">Maximum number of rows to show in preview</param>
+        /// <param name="maxTrainingRows">Maximum number of rows to fit the estimator</param>
+        public static DataDebuggerPreview Preview(this IEstimator<ITransformer> estimator, IDataView data, int maxRows = DataDebuggerPreview.Defaults.MaxRows,
+            int maxTrainingRows = DataDebuggerPreview.Defaults.MaxRows)
+        {
+            Contracts.CheckValue(estimator, nameof(estimator));
+            Contracts.CheckValue(data, nameof(data));
+            Contracts.CheckParam(maxRows >= 0, nameof(maxRows));
+            Contracts.CheckParam(maxTrainingRows >= 0, nameof(maxTrainingRows));
+
+            using (var env = new LocalEnvironment(conc: 1))
+            {
+                var trainData = SkipTakeFilter.Create(env, new SkipTakeFilter.TakeArguments { Count = maxTrainingRows }, data);
+                return new DataDebuggerPreview(estimator.Fit(trainData).Transform(data), maxRows);
+            }
+        }
+
+        /// <summary>
+        /// Preview an effect of the <paramref name="reader"/> on a given <paramref name="source"/>.
+        /// </summary>
+        /// <param name="reader">The data reader to preview</param>
+        /// <param name="source">The source to pull the data from</param>
+        /// <param name="maxRows">Maximum number of rows to pull</param>
+        public static DataDebuggerPreview Preview<TSource>(this IDataReader<TSource> reader, TSource source, int maxRows = DataDebuggerPreview.Defaults.MaxRows)
+            => new DataDebuggerPreview(reader.Read(source), maxRows);
+    }
+}
