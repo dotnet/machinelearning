565ff9e814b0e43ea55d2db9dce24b6613b812c5
Adam Sitnik <adam.sitnik@gmail.com>
2018-11-09 21:01:38 +0100

    write unit tests!

---
+++ src/Microsoft.ML.Transforms/Text/LineParser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.ML.Transforms.Text
+{
+    public static class LineParser
+    {
+        public static (bool isSuccess, string key, float[] values) ParseKeyThenNumbers(string line)
+        {
+            string key = null;
+            float[] values = null;
+
+            return (true, key, values);
+        }
+    }
+}
---
+++ test/Microsoft.ML.Tests/Transformers/LineParserTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Microsoft.ML.Tests.Transformers
+{
+    public class LineParserTests
+    {
+        public static IEnumerable<object[]> ValidInputs()
+        {
+            foreach (var line in new string[]
+            {
+                "key 0.1 0.2 0.3", "key 0.1 0.2 0.3 ",
+                "key\t0.1\t0.2\t0.3", "key\t0.1\t0.2\t0.3\t" // tab can also be a separator
+            })
+            {
+                yield return new object[] { line, "key", new float[] { 0.1f, 0.2f, 0.3f } };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(ValidInputs))]
+        public void WhenProvidedAValidInputParserParsesKeyAndValues(string input, string expectedKey, float[] expectedValues)
+        {
+            var result = Transforms.Text.LineParser.ParseKeyThenNumbers(input);
+
+            Assert.True(result.isSuccess);
+            Assert.Equal(expectedKey, result.key);
+            Assert.True(expectedValues.SequenceEqual(result.values));
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("key 0.1 NOT_A_NUMBER")] // invalid number
+        public void WhenProvidedAnInvalidInputParserReturnsFailure(string input)
+        {
+            Assert.False(Transforms.Text.LineParser.ParseKeyThenNumbers(input).isSuccess);
+        }
+    }
+}
