4cdd1eba826cbade2abc82dce241e0b2b5fb9c49
vaeksare <42353187+vaeksare@users.noreply.github.com>
2018-11-09 18:02:34 -0800

    Address comments

--- src/Microsoft.ML.OnnxTransform/DnnImageFeaturizerTransform.cs
+++ src/Microsoft.ML.OnnxTransform/DnnImageFeaturizerTransform.cs
@@ -67,8 +67,8 @@ namespace Microsoft.ML.Transforms
         {
             public PipelineColumn Input { get; }
 
-            public OutColumn(Vector<float> input, Func<DnnImageModelSelector, IHostEnvironment, string, string, EstimatorChain<OnnxTransform>> model)
-                : base(new Reconciler(model), input)
+            public OutColumn(Vector<float> input, Func<DnnImageModelSelector, IHostEnvironment, string, string, EstimatorChain<OnnxTransform>> modelFactory)
+                : base(new Reconciler(modelFactory), input)
             {
                 Input = input;
             }
@@ -76,11 +76,11 @@ namespace Microsoft.ML.Transforms
 
         private sealed class Reconciler : EstimatorReconciler
         {
-            private readonly Func<DnnImageModelSelector, IHostEnvironment, string, string, EstimatorChain<OnnxTransform>> _model;
+            private readonly Func<DnnImageModelSelector, IHostEnvironment, string, string, EstimatorChain<OnnxTransform>> _modelFactory;
 
-            public Reconciler(Func<DnnImageModelSelector, IHostEnvironment, string, string, EstimatorChain<OnnxTransform>> model)
+            public Reconciler(Func<DnnImageModelSelector, IHostEnvironment, string, string, EstimatorChain<OnnxTransform>> modelFactory)
             {
-                _model = model;
+                _modelFactory = modelFactory;
             }
 
             public override IEstimator<ITransformer> Reconcile(IHostEnvironment env,
@@ -92,14 +92,23 @@ namespace Microsoft.ML.Transforms
                 Contracts.Assert(toOutput.Length == 1);
 
                 var outCol = (OutColumn)toOutput[0];
-                return new DnnImageFeaturizerEstimator(env, _model, inputNames[outCol.Input], outputNames[outCol]);
+                return new DnnImageFeaturizerEstimator(env, _modelFactory, inputNames[outCol.Input], outputNames[outCol]);
             }
         }
 
-        public static Vector<float> DnnImageFeaturizer(this Vector<float> input, Func<DnnImageModelSelector, IHostEnvironment, string, string, EstimatorChain<OnnxTransform>> model)
+        /// <summary>
+        /// Creates a DnnImageFeaturizer transform to be used by the static API.
+        /// <see cref="DnnImageFeaturizerEstimator"/> for more information about how the transformation works.
+        /// </summary>
+        /// <param name="input">Vector of image pixel weights.</param>
+        /// <param name="modelFactory">An extension method on the <see cref="DnnImageModelSelector"/> that creates a chain of two
+        /// <see cref="OnnxScoringEstimator"/>s with specific models included in a package together with that extension method.
+        /// For an example, see Microsoft.ML.DnnImageFeaturizer.ResNet18 </param>
+        /// <returns>A vector of float feature weights based on the input image.</returns>
+        public static Vector<float> DnnImageFeaturizer(this Vector<float> input, Func<DnnImageModelSelector, IHostEnvironment, string, string, EstimatorChain<OnnxTransform>> modelFactory)
         {
             Contracts.CheckValue(input, nameof(input));
-            return new OutColumn(input, model);
+            return new OutColumn(input, modelFactory);
         }
     }
 }
