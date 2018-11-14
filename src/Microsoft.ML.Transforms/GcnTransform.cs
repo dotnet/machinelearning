82c4f2d8f44a016fcf461efebdf92dbf74d36306
Ivan Matantsev <ivmatan@microsoft.com>
2018-11-14 10:27:29 -0800

    some more changes

--- src/Microsoft.ML.Transforms/GcnTransform.cs
+++ src/Microsoft.ML.Transforms/GcnTransform.cs
@@ -317,7 +317,7 @@ namespace Microsoft.ML.Transforms.Projections
                 return "Expected float or float vector of known size";
 
             if ((long)type.ValueCount * type.ValueCount > Utils.ArrayMaxSize)
-                return "Vector size exceeds limit";
+                return "Vector size exceeds maximum size for one dimensional array (2 146 435 071 elements)";
 
             return null;
         }
@@ -885,113 +885,5 @@ namespace Microsoft.ML.Transforms.Projections
         {
 
         }
-    }
-
-    /// <summary>
-    /// Extensions for statically typed <see cref="LpNormalizingEstimator"/>.
-    /// </summary>
-    public static class LpNormNormalizerExtensions
-    {
-        private sealed class OutPipelineColumn : Vector<float>
-        {
-            public readonly Vector<float> Input;
-
-            public OutPipelineColumn(Vector<float> input, LpNormalizingEstimatorBase.NormalizerKind normKind, bool subMean)
-                : base(new Reconciler(normKind, subMean), input)
-            {
-                Input = input;
-            }
-        }
-
-        private sealed class Reconciler : EstimatorReconciler
-        {
-            private readonly LpNormalizingEstimatorBase.NormalizerKind _normKind;
-            private readonly bool _subMean;
-
-            public Reconciler(LpNormalizingEstimatorBase.NormalizerKind normKind, bool subMean)
-            {
-                _normKind = normKind;
-                _subMean = subMean;
-            }
-
-            public override IEstimator<ITransformer> Reconcile(IHostEnvironment env,
-                PipelineColumn[] toOutput,
-                IReadOnlyDictionary<PipelineColumn, string> inputNames,
-                IReadOnlyDictionary<PipelineColumn, string> outputNames,
-                IReadOnlyCollection<string> usedNames)
-            {
-                Contracts.Assert(toOutput.Length == 1);
-
-                var pairs = new List<(string input, string output)>();
-                foreach (var outCol in toOutput)
-                    pairs.Add((inputNames[((OutPipelineColumn)outCol).Input], outputNames[outCol]));
-
-                return new LpNormalizingEstimator(env, pairs.ToArray(), _normKind, _subMean);
-            }
-        }
-
-        /// <include file='doc.xml' path='doc/members/member[@name="LpNormalize"]/*'/>
-        /// <param name="input">The column to apply to.</param>
-        /// <param name="normKind">Type of norm to use to normalize each sample.</param>
-        /// <param name="subMean">Subtract mean from each value before normalizing.</param>
-        public static Vector<float> LpNormalize(this Vector<float> input,
-            LpNormalizingEstimatorBase.NormalizerKind normKind = LpNormalizingEstimatorBase.Defaults.NormKind,
-            bool subMean = LpNormalizingEstimatorBase.Defaults.LpSubMean) => new OutPipelineColumn(input, normKind, subMean);
-    }
-
-    /// <summary>
-    /// Extensions for statically typed <see cref="GcnNormalizingEstimator"/>.
-    /// </summary>
-    public static class GcNormalizerExtensions
-    {
-        private sealed class OutPipelineColumn : Vector<float>
-        {
-            public readonly Vector<float> Input;
-
-            public OutPipelineColumn(Vector<float> input, bool subMean, bool useStdDev, float scale)
-                : base(new Reconciler(subMean, useStdDev, scale), input)
-            {
-                Input = input;
-            }
-        }
-
-        private sealed class Reconciler : EstimatorReconciler
-        {
-            private readonly bool _subMean;
-            private readonly bool _useStdDev;
-            private readonly float _scale;
-
-            public Reconciler(bool subMean, bool useStdDev, float scale)
-            {
-                _subMean = subMean;
-                _useStdDev = useStdDev;
-                _scale = scale;
-            }
-
-            public override IEstimator<ITransformer> Reconcile(IHostEnvironment env,
-                PipelineColumn[] toOutput,
-                IReadOnlyDictionary<PipelineColumn, string> inputNames,
-                IReadOnlyDictionary<PipelineColumn, string> outputNames,
-                IReadOnlyCollection<string> usedNames)
-            {
-                Contracts.Assert(toOutput.Length == 1);
-
-                var pairs = new List<(string input, string output)>();
-                foreach (var outCol in toOutput)
-                    pairs.Add((inputNames[((OutPipelineColumn)outCol).Input], outputNames[outCol]));
-
-                return new GcnNormalizingEstimator(env, pairs.ToArray(), _subMean, _useStdDev, _scale);
-            }
-        }
-
-        /// <include file='doc.xml' path='doc/members/member[@name="GcNormalize"]/*'/>
-        /// <param name="input">The column to apply to.</param>
-        /// <param name="subMean">Subtract mean from each value before normalizing.</param>
-        /// <param name="useStdDev">Normalize by standard deviation rather than L2 norm.</param>
-        /// <param name="scale">Scale features by this value.</param>
-        public static Vector<float> GlobalContrastNormalize(this Vector<float> input,
-            bool subMean = LpNormalizingEstimatorBase.Defaults.GcnSubMean,
-            bool useStdDev = LpNormalizingEstimatorBase.Defaults.UseStdDev,
-            float scale = LpNormalizingEstimatorBase.Defaults.Scale) => new OutPipelineColumn(input, subMean, useStdDev, scale);
     }
 }
--- src/Microsoft.ML.Transforms/TransformsStatic.cs
+++ src/Microsoft.ML.Transforms/TransformsStatic.cs
@@ -11,6 +11,74 @@ using System.Collections.Generic;
 namespace Microsoft.ML.StaticPipe
 {
     /// <summary>
+    /// Extensions for statically typed Whitening estimator.
+    /// </summary>
+    public static class VectorWhiteningExtensions
+    {
+        private sealed class OutPipelineColumn : Vector<float>
+        {
+            public readonly Vector<float> Input;
+
+            public OutPipelineColumn(Vector<float> input, WhiteningKind kind, float eps, int maxRows, int pcaNum)
+                : base(new Reconciler(kind, eps, maxRows, pcaNum), input)
+            {
+                Input = input;
+            }
+        }
+
+        private sealed class Reconciler : EstimatorReconciler
+        {
+            private readonly WhiteningKind _kind;
+            private readonly float _eps;
+            private readonly int _maxRows;
+            private readonly int _pcaNum;
+
+            public Reconciler(WhiteningKind kind, float eps, int maxRows, int pcaNum)
+            {
+                _kind = kind;
+                _eps = eps;
+                _maxRows = maxRows;
+                _pcaNum = pcaNum;
+            }
+
+            public override IEstimator<ITransformer> Reconcile(IHostEnvironment env,
+                PipelineColumn[] toOutput,
+                IReadOnlyDictionary<PipelineColumn, string> inputNames,
+                IReadOnlyDictionary<PipelineColumn, string> outputNames,
+                IReadOnlyCollection<string> usedNames)
+            {
+                Contracts.Assert(toOutput.Length == 1);
+
+                var infos = new VectorWhiteningTransformer.ColumnInfo[toOutput.Length];
+                for (int i = 0; i < toOutput.Length; i++)
+                    infos[i] = new VectorWhiteningTransformer.ColumnInfo(inputNames[((OutPipelineColumn)toOutput[i]).Input], outputNames[toOutput[i]], _kind, _eps, _maxRows, _pcaNum);
+
+                return new VectorWhiteningEstimator(env, infos);
+            }
+        }
+
+        /// <include file='doc.xml' path='doc/members/member[@name="Whitening"]/*'/>
+        /// <param name="input">The column to which the transform will be applied.</param>
+        /// <param name="eps">Whitening constant, prevents division by zero when scaling the data by inverse of eigenvalues.</param>
+        /// <param name="maxRows">Maximum number of rows used to train the transform.</param>
+        /// <param name="pcaNum">In case of PCA whitening, indicates the number of components to retain.</param>
+        public static Vector<float> PcaWhitening(this Vector<float> input,
+            float eps = VectorWhiteningTransformer.Defaults.Eps,
+            int maxRows = VectorWhiteningTransformer.Defaults.MaxRows,
+            int pcaNum = VectorWhiteningTransformer.Defaults.PcaNum)
+            => new OutPipelineColumn(input, WhiteningKind.Pca, eps, maxRows, pcaNum);
+
+        /// <include file='doc.xml' path='doc/members/member[@name="Whitening"]/*'/>
+        /// <param name="input">The column to which the transform will be applied.</param>
+        /// <param name="eps">Whitening constant, prevents division by zero.</param>
+        /// <param name="maxRows">Maximum number of rows used to train the transform.</param>
+        public static Vector<float> ZcaWhitening(this Vector<float> input,
+            float eps = VectorWhiteningTransformer.Defaults.Eps,
+            int maxRows = VectorWhiteningTransformer.Defaults.MaxRows)
+            => new OutPipelineColumn(input, WhiteningKind.Zca, eps, maxRows, VectorWhiteningTransformer.Defaults.PcaNum);
+    }
+
+    /// <summary>
     /// Extensions for statically typed <see cref="LpNormalizingEstimator"/>.
     /// </summary>
     public static class LpNormNormalizerExtensions
