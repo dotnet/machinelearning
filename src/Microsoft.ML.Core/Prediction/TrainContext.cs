a6c9e40f84fcdd5af19b768f88a95cef663c34d2
Wei-Sheng Chin <wschin@outlook.com>
2018-11-14 10:53:14 -0800

    Printing out test scores in training phase for FastTree (#1571)
    * Draft of printing out test scores in training phase for FastTree
    
    * Make Train command working with test set if specified
    
    * Fix some binary classification tests.
    1. Update baseline files
    2. Truncate column activation indicators if needed.
       This is reasonable because, for example, if a data
       set contains 2 features while a dev wants to active
       3 features by using [1, 1, 1] as the indicator vector,
       we can truncate [1, 1, 1] to [1, 1].
    
    * Fix all FastTree binary classification tests
    
    * Fix other formulations
    
    * Update release mode beseline files

--- src/Microsoft.ML.Core/Prediction/TrainContext.cs
+++ src/Microsoft.ML.Core/Prediction/TrainContext.cs
@@ -26,6 +26,14 @@ namespace Microsoft.ML.Runtime
         public RoleMappedData ValidationSet { get; }
 
         /// <summary>
+        /// The test set, whose uses are very similar to validation set but it should not directly and indirectly
+        /// affect the training process. One major difference between validation set and test test is that validation
+        /// can affect the training process by, for example, early stopping. Note that early stopping is a technique
+        /// which terminates the training process once the scores computed on validation set starts getting worse.
+        /// </summary>
+        public RoleMappedData TestSet { get; }
+
+        /// <summary>
         /// The initial predictor, for incremental training. Note that if a <see cref="ITrainer"/> implementor
         /// does not support incremental training, then it can ignore it similarly to how one would ignore
         /// <see cref="ValidationSet"/>. However, if the trainer does support incremental training and there
@@ -38,8 +46,9 @@ namespace Microsoft.ML.Runtime
         /// </summary>
         /// <param name="trainingSet">Will set <see cref="TrainingSet"/> to this value. This must be specified</param>
         /// <param name="validationSet">Will set <see cref="ValidationSet"/> to this value if specified</param>
+        /// <param name="testSet">Will set <see cref="TestSet"/> to this value if specified</param>
         /// <param name="initialPredictor">Will set <see cref="InitialPredictor"/> to this value if specified</param>
-        public TrainContext(RoleMappedData trainingSet, RoleMappedData validationSet = null, IPredictor initialPredictor = null)
+        public TrainContext(RoleMappedData trainingSet, RoleMappedData validationSet = null, RoleMappedData testSet = null, IPredictor initialPredictor = null)
         {
             Contracts.CheckValue(trainingSet, nameof(trainingSet));
             Contracts.CheckValueOrNull(validationSet);
@@ -50,6 +59,7 @@ namespace Microsoft.ML.Runtime
 
             TrainingSet = trainingSet;
             ValidationSet = validationSet;
+            TestSet = testSet;
             InitialPredictor = initialPredictor;
         }
     }
