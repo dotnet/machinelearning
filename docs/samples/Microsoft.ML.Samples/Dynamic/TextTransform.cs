61f4f3b9154ed0bb692855efc6034ad43bcfdd6f
Alexander Alexeev <alenalexa@gmail.com>
2018-11-16 19:52:26 +0300

    Remove the copyright from the samples files (#1619)
    * Remove the copyright from the samples files
    
    * Remove using' tabs and tab comment, rename partial classes according to functions contained.
    
    * Remove last forgotten TrainerSample

--- docs/samples/Microsoft.ML.Samples/Dynamic/TextTransform.cs
+++ docs/samples/Microsoft.ML.Samples/Dynamic/TextTransform.cs
@@ -1,18 +1,13 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
-
-        // the alignment of the usings with the methods is intentional so they can display on the same level in the docs site.
-        using Microsoft.ML.Runtime.Data;
-        using Microsoft.ML.Runtime.Api;
-        using Microsoft.ML.Data;
-        using Microsoft.ML.Transforms.Text;
-        using System;
-        using System.Collections.Generic;
+﻿using Microsoft.ML.Runtime.Data;
+using Microsoft.ML.Runtime.Api;
+using Microsoft.ML.Data;
+using Microsoft.ML.Transforms.Text;
+using System;
+using System.Collections.Generic;
 
 namespace Microsoft.ML.Samples.Dynamic
 {
-    public partial class TransformSamples
+    public class TextTransformExample
     {
         public static void TextTransform()
         {
