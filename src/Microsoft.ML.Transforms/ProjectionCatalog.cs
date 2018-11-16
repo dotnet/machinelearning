80b60e23c36c401cbe04ba31d8b3a2d6a08ae3a7
Artidoro Pagnoni <arpagnon@microsoft.com>
2018-11-16 14:14:13 -0800

    Moved WhiteningTransform to HalLearners (#1583)
    * moved whiteningtransform and projection catalog for the transform
    
    * finished changes
    
    * added line end of file

--- src/Microsoft.ML.Transforms/ProjectionCatalog.cs
+++ src/Microsoft.ML.Transforms/ProjectionCatalog.cs
@@ -41,40 +41,6 @@ namespace Microsoft.ML
             => new RandomFourierFeaturizingEstimator(CatalogUtils.GetEnvironment(catalog), columns);
 
         /// <summary>
-        /// Takes column filled with a vector of random variables with a known covariance matrix into a set of new variables whose covariance is the identity matrix,
-        /// meaning that they are uncorrelated and each have variance 1.
-        /// </summary>
-        /// <param name="catalog">The transform's catalog.</param>
-        /// <param name="inputColumn">Name of the input column.</param>
-        /// <param name="outputColumn">Name of the column resulting from the transformation of <paramref name="inputColumn"/>. Null means <paramref name="inputColumn"/> is replaced. </param>
-        /// <param name="kind">Whitening kind (PCA/ZCA).</param>
-        /// <param name="eps">Whitening constant, prevents division by zero.</param>
-        /// <param name="maxRows">Maximum number of rows used to train the transform.</param>
-        /// <param name="pcaNum">In case of PCA whitening, indicates the number of components to retain.</param>
-        /// <example>
-        /// <format type="text/markdown">
-        /// <![CDATA[
-        /// [!code-csharp[VectorWhiten](~/../docs/samples/docs/samples/Microsoft.ML.Samples/Dynamic/ProjectionTransforms.cs?range=5-11,16-115)]
-        /// ]]>
-        /// </format>
-        /// </example>
-        public static VectorWhiteningEstimator VectorWhiten(this TransformsCatalog.ProjectionTransforms catalog, string inputColumn, string outputColumn = null,
-            WhiteningKind kind = VectorWhiteningTransformer.Defaults.Kind,
-            float eps = VectorWhiteningTransformer.Defaults.Eps,
-            int maxRows = VectorWhiteningTransformer.Defaults.MaxRows,
-            int pcaNum = VectorWhiteningTransformer.Defaults.PcaNum)
-            => new VectorWhiteningEstimator(CatalogUtils.GetEnvironment(catalog), inputColumn, outputColumn, kind, eps, maxRows, pcaNum);
-
-        /// <summary>
-        /// Takes columns filled with a vector of random variables with a known covariance matrix into a set of new variables whose covariance is the identity matrix,
-        /// meaning that they are uncorrelated and each have variance 1.
-        /// </summary>
-        /// <param name="catalog">The transform's catalog.</param>
-        /// <param name="columns"> Describes the parameters of the whitening process for each column pair.</param>
-        public static VectorWhiteningEstimator VectorWhiten(this TransformsCatalog.ProjectionTransforms catalog, params VectorWhiteningTransformer.ColumnInfo[] columns)
-            => new VectorWhiteningEstimator(CatalogUtils.GetEnvironment(catalog), columns);
-
-        /// <summary>
         /// Takes column filled with a vector of floats and computes L-p norm of it.
         /// </summary>
         /// <param name="catalog">The transform's catalog.</param>
