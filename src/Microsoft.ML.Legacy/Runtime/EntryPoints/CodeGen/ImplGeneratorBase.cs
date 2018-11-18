e6e07ed50f7b631f6cc1801411778083ffa6ea7d
feiyun0112 <feiyun0112@gmail.com>
2018-11-19 05:11:45 +0800

    Remove IndentingTextWriter in favor of the existing IndentedTextWriter(#1241) (#1416)

--- src/Microsoft.ML.Legacy/Runtime/EntryPoints/CodeGen/ImplGeneratorBase.cs
+++ src/Microsoft.ML.Legacy/Runtime/EntryPoints/CodeGen/ImplGeneratorBase.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.CodeDom.Compiler;
 using System.Linq;
 using Microsoft.ML.Runtime.CommandLine;
 using Microsoft.ML.Runtime.Data;
@@ -12,7 +13,7 @@ namespace Microsoft.ML.Runtime.EntryPoints.CodeGen
 {
     internal abstract class ImplGeneratorBase : GeneratorBase
     {
-        protected override void GenerateContent(IndentingTextWriter writer, string prefix, ComponentCatalog.LoadableClassInfo component, string moduleId)
+        protected override void GenerateContent(IndentedTextWriter writer, string prefix, ComponentCatalog.LoadableClassInfo component, string moduleId)
         {
             GenerateImplFields(writer, component, (w, a) => GenerateFieldsOrProperties(w, a, "", GenerateField));
             GenerateImplFields(writer, component, (w, a) => GenerateFieldsOrProperties(w, a, "", GenerateProperty));
@@ -20,8 +21,8 @@ namespace Microsoft.ML.Runtime.EntryPoints.CodeGen
             GenerateImplBody(writer, component);
         }
 
-        protected void GenerateImplFields(IndentingTextWriter w, ComponentCatalog.LoadableClassInfo component,
-            Action<IndentingTextWriter, CmdParser.ArgInfo.Arg> fieldGenerator)
+        protected void GenerateImplFields(IndentedTextWriter w, ComponentCatalog.LoadableClassInfo component,
+            Action<IndentedTextWriter, CmdParser.ArgInfo.Arg> fieldGenerator)
         {
             var argumentInfo = CmdParser.GetArgInfo(component.ArgType, component.CreateArguments());
             var arguments = argumentInfo.Args.Where(a => !a.IsHidden).ToArray();
@@ -33,8 +34,8 @@ namespace Microsoft.ML.Runtime.EntryPoints.CodeGen
         /// Generates private fields and public properties for all the fields in the arguments.
         /// Recursively generate fields and properties for subcomponents.
         /// </summary>
-        protected void GenerateFieldsOrProperties(IndentingTextWriter w, CmdParser.ArgInfo.Arg arg, string argSuffix,
-            Action<IndentingTextWriter, string, string, string, bool, string> oneFieldGenerator)
+        protected void GenerateFieldsOrProperties(IndentedTextWriter w, CmdParser.ArgInfo.Arg arg, string argSuffix,
+            Action<IndentedTextWriter, string, string, string, bool, string> oneFieldGenerator)
         {
             if (Exclude.Contains(arg.LongName))
                 return;
@@ -45,14 +46,14 @@ namespace Microsoft.ML.Runtime.EntryPoints.CodeGen
             oneFieldGenerator(w, typeName, arg.LongName + argSuffix, defVal, arg.ItemType == typeof(bool), arg.HelpText);
         }
 
-        protected static void GenerateField(IndentingTextWriter w, string typeName, string argName, string defVal,
+        protected static void GenerateField(IndentedTextWriter w, string typeName, string argName, string defVal,
             bool isBool, string helpText)
         {
             w.WriteLine("private {0} {1}{2};", typeName, argName, defVal);
             w.WriteLine();
         }
 
-        protected static void GenerateProperty(IndentingTextWriter w, string typeName, string argName, string defVal,
+        protected static void GenerateProperty(IndentedTextWriter w, string typeName, string argName, string defVal,
             bool isBool, string helpText)
         {
             var help = helpText ?? argName;
@@ -69,12 +70,12 @@ namespace Microsoft.ML.Runtime.EntryPoints.CodeGen
             w.WriteLine();
         }
 
-        protected abstract void GenerateMethodSignature(IndentingTextWriter w, string prefix,
+        protected abstract void GenerateMethodSignature(IndentedTextWriter w, string prefix,
             ComponentCatalog.LoadableClassInfo component);
 
-        protected abstract void GenerateImplBody(IndentingTextWriter w, ComponentCatalog.LoadableClassInfo component);
+        protected abstract void GenerateImplBody(IndentedTextWriter w, ComponentCatalog.LoadableClassInfo component);
 
-        protected void GenerateImplBody(IndentingTextWriter w, CmdParser.ArgInfo.Arg arg, string argSuffix)
+        protected void GenerateImplBody(IndentedTextWriter w, CmdParser.ArgInfo.Arg arg, string argSuffix)
         {
             if (Exclude.Contains(arg.LongName))
                 return;
@@ -92,7 +93,7 @@ namespace Microsoft.ML.Runtime.EntryPoints.CodeGen
                 w.WriteLine("args{0}.{1} = {2};", argSuffix, arg.LongName, arg.LongName + argSuffix);
         }
 
-        protected override void GenerateUsings(IndentingTextWriter w)
+        protected override void GenerateUsings(IndentedTextWriter w)
         {
             w.WriteLine("using System;");
             w.WriteLine("using System.Linq;");
