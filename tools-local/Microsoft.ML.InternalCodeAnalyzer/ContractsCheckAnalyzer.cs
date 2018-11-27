9bd1d0d90e074c53c891f5c7658a437252af01a1
Wei-Sheng Chin <wechi@microsoft.com>
2018-11-27 11:21:07 -0800

    Fix comment

--- tools-local/Microsoft.ML.InternalCodeAnalyzer/ContractsCheckAnalyzer.cs
+++ tools-local/Microsoft.ML.InternalCodeAnalyzer/ContractsCheckAnalyzer.cs
@@ -196,7 +196,7 @@ namespace Microsoft.ML.InternalCodeAnalyzer
             var containingSymbolName = methodSymbol.ContainingSymbol.ToString();
             // The "internal" version is one used by some projects that want to benefit from Contracts,
             // but for some reason cannot reference MLCore.
-            // Contract functions defined Microsoft.ML.Hidden are introduced for breaking the dependencies
+            // Contract functions defined Microsoft.ML.Runtime.Internal.CpuMath.Core are introduced for breaking the dependencies
             // from CpuMath project to Microsoft.ML.Core.
             if (containingSymbolName != "Microsoft.ML.Runtime.Contracts" &&
                 containingSymbolName != "Microsoft.ML.Runtime.Internal.CpuMath.Core.Contracts")
