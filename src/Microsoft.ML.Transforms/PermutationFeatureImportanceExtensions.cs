213ef9ed99d810f09f6b99704ca95641e19ecbfb
Rogan Carr <rogan.carr@hotmail.com>
2018-11-27 15:03:43 -0800

    Add Permutation Feature Importance for Binary Classification (#1735)
    Adding support for binary classification in Permutation Feature Importance

--- src/Microsoft.ML.Transforms/PermutationFeatureImportanceExtensions.cs
+++ src/Microsoft.ML.Transforms/PermutationFeatureImportanceExtensions.cs
@@ -55,5 +55,53 @@ namespace Microsoft.ML
                 lossFunction: a.LossFn - b.LossFn,
                 rSquared: a.RSquared - b.RSquared);
         }
+
+        /// <summary>
+        /// Permutation Feature Importance is a technique that calculates how much each feature 'matters' to the predictions.
+        /// Namely, how much the model's predictions will change if we randomly permute the values of one feature across the evaluation set.
+        /// If the quality doesn't change much, this feature is not very important. If the quality drops drastically, this was a really important feature.
+        /// </summary>
+        /// <param name="ctx">The binary classification context.</param>
+        /// <param name="model">The model to evaluate.</param>
+        /// <param name="data">The evaluation data set.</param>
+        /// <param name="label">Label column name.</param>
+        /// <param name="features">Feature column names.</param>
+        /// <param name="useFeatureWeightFilter">Use features weight to pre-filter features.</param>
+        /// <param name="topExamples">Limit the number of examples to evaluate on. null means examples (up to ~ 2 bln) from input will be used.</param>
+        /// <returns>Array of per-feature 'contributions' to the score.</returns>
+        public static ImmutableArray<BinaryClassifierEvaluator.Result>
+            PermutationFeatureImportance(
+                this BinaryClassificationContext ctx,
+                IPredictionTransformer<IPredictor> model,
+                IDataView data,
+                string label = DefaultColumnNames.Label,
+                string features = DefaultColumnNames.Features,
+                bool useFeatureWeightFilter = false,
+                int? topExamples = null)
+        {
+            return PermutationFeatureImportance<BinaryClassifierEvaluator.Result>.GetImportanceMetricsMatrix(
+                            CatalogUtils.GetEnvironment(ctx),
+                            model,
+                            data,
+                            idv => ctx.Evaluate(idv, label),
+                            BinaryClassifierDelta,
+                            features,
+                            useFeatureWeightFilter,
+                            topExamples);
+        }
+
+        private static BinaryClassifierEvaluator.Result BinaryClassifierDelta(
+            BinaryClassifierEvaluator.Result a, BinaryClassifierEvaluator.Result b)
+        {
+            return new BinaryClassifierEvaluator.Result(
+                auc: a.Auc - b.Auc,
+                accuracy: a.Accuracy - b.Accuracy,
+                positivePrecision: a.PositivePrecision - b.PositivePrecision,
+                positiveRecall: a.PositiveRecall - b.PositiveRecall,
+                negativePrecision: a.NegativePrecision - b.NegativePrecision,
+                negativeRecall: a.NegativeRecall - b.NegativeRecall,
+                f1Score: a.F1Score - b.F1Score,
+                auprc: a.Auprc - b.Auprc);
+        }
     }
 }
