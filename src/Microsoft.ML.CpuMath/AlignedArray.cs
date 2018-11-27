9d0b89268b9a12a262f01f7e3880ab5aa3c737f9
Wei-Sheng Chin <wechi@microsoft.com>
2018-11-27 11:07:17 -0800

    Swap usings

--- src/Microsoft.ML.CpuMath/AlignedArray.cs
+++ src/Microsoft.ML.CpuMath/AlignedArray.cs
@@ -2,8 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using Microsoft.ML.Runtime.Internal.CpuMath.Core;
+using System;
 
 namespace Microsoft.ML.Runtime.Internal.CpuMath
 {
--- src/Microsoft.ML.CpuMath/CpuAligenedMathUtils.cs
+++ src/Microsoft.ML.CpuMath/CpuAligenedMathUtils.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using Microsoft.ML.Runtime.Internal.CpuMath.Core;
+
 namespace Microsoft.ML.Runtime.Internal.CpuMath
 {
     [BestFriend]
