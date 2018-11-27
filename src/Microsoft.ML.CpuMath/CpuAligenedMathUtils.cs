870cf7ede474beeaadc6893351ecf060623d41ba
Wei-Sheng Chin <wschin@outlook.com>
2018-11-27 12:25:17 -0800

    Make CpuMath not depending on ML.Core again (#1724)
    * Make CpuMath not depending on ML.Core again
    
    * Modify BestFriend attributes and their analyzer
    
    * Update src/Microsoft.ML.Core/Properties/PublicKey.cs
    
    * Rename PRIVATE_CONTRACTS compiler flag to CPUMATH_INFRASTRUCTURE
    
    * Fix comments

--- src/Microsoft.ML.CpuMath/CpuAligenedMathUtils.cs
+++ src/Microsoft.ML.CpuMath/CpuAligenedMathUtils.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Microsoft.ML.Runtime.Internal.CpuMath.Core;
+
 namespace Microsoft.ML.Runtime.Internal.CpuMath
 {
     [BestFriend]
--- tools-local/Microsoft.ML.InternalCodeAnalyzer/ContractsCheckAnalyzer.cs
+++ tools-local/Microsoft.ML.InternalCodeAnalyzer/ContractsCheckAnalyzer.cs
@@ -196,8 +196,10 @@ namespace Microsoft.ML.InternalCodeAnalyzer
             var containingSymbolName = methodSymbol.ContainingSymbol.ToString();
             // The "internal" version is one used by some projects that want to benefit from Contracts,
             // but for some reason cannot reference MLCore.
+            // Contract functions defined Microsoft.ML.Runtime.Internal.CpuMath.Core are introduced for breaking the dependencies
+            // from CpuMath project to Microsoft.ML.Core.
             if (containingSymbolName != "Microsoft.ML.Runtime.Contracts" &&
-                containingSymbolName != "Microsoft.ML.Runtime.Internal.Contracts")
+                containingSymbolName != "Microsoft.ML.Runtime.Internal.CpuMath.Core.Contracts")
             {
                 return;
             }
