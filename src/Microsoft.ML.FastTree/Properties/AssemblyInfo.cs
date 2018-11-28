58aa330f9469c8cb2d2ce5f9480e9b9a22f100d4
REDMOND\nakazmi <nakazmi@microsoft.com>
2018-11-27 16:05:55 -0800

    Making FastTree best friends with LightGBM

--- src/Microsoft.ML.FastTree/Properties/AssemblyInfo.cs
+++ src/Microsoft.ML.FastTree/Properties/AssemblyInfo.cs
@@ -6,5 +6,6 @@ using System.Runtime.CompilerServices;
 using Microsoft.ML;
 
 [assembly: InternalsVisibleTo(assemblyName: "Microsoft.ML.Core.Tests" + PublicKey.TestValue)]
+[assembly: InternalsVisibleTo(assemblyName: "Microsoft.ML.LightGBM" + PublicKey.TestValue)]
 
 [assembly: WantsToBeBestFriends]
