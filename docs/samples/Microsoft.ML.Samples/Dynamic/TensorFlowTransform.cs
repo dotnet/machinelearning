89311334f55807e26e46b1d77c4ffd95d9858676
Abhishek Goswami <abgoswam@gmail.com>
2018-12-04 00:33:52 +0000

    example cleanup; link from catalog

--- docs/samples/Microsoft.ML.Samples/Dynamic/TensorFlowTransform.cs
+++ docs/samples/Microsoft.ML.Samples/Dynamic/TensorFlowTransform.cs
@@ -4,7 +4,6 @@
 
 using Microsoft.ML.Runtime.Api;
 using Microsoft.ML.Runtime.Data;
-using Microsoft.ML.Transforms;
 using System;
 using System.Linq;
 
@@ -13,21 +12,20 @@ namespace Microsoft.ML.Samples.Dynamic
     class TensorFlowTransformExample
     {
         /// <summary>
-        /// Example use of TensorFlowEstimator in an ML.NET pipeline
+        /// Example use of TensorFlowEstimator in a ML.NET pipeline
         /// </summary>
-        public static void TFTransformSample()
+        public static void TensorFlowScoringSample()
         {
-            Console.WriteLine("Hello World!");
-            // var model_location = @"D:\machinelearning\bin\AnyCPU.Debug\Microsoft.ML.Tests\netcoreapp2.1\cifar_model\frozen_model.pb";
-            var model_location = @"C:\Users\agoswami\Downloads\resnet_v2_101_299_frozen.pb";
+            // Download the ResNet 101 model from the location below
+            // https://storage.googleapis.com/download.tensorflow.org/models/tflite_11_05_08/resnet_v2_101.tgz
+            var modelLocation = @"resnet_v2_101/resnet_v2_101_299_frozen.pb";
 
-            // Create ML pipeline to score the data using OnnxScoringEstimator
             var mlContext = new MLContext();
             var data = GetTensorData();
-
             var idv = mlContext.CreateStreamingDataView(data);
-            // var pipeline = new TensorFlowEstimator(mlContext, model_location, new[] { "Input" }, new[] { "Output" });
-            var pipeline = new TensorFlowEstimator(mlContext, model_location, new[] { "input" }, new[] { "output" });
+
+            // Create a ML pipeline to score the data using TensorFlowEstimator
+            var pipeline = mlContext.Transforms.ScoreTensorFlowModel(modelLocation, new[] { "input" }, new[] { "output" });
 
             // Run the pipeline and get the transformed values
             var estimator = pipeline.Fit(idv);
@@ -40,44 +38,47 @@ namespace Microsoft.ML.Samples.Dynamic
             foreach (var prediction in outScores)
             {
                 int numClasses = 0;
-                //foreach (var classScore in prediction.Output.Take(3))
                 foreach (var classScore in prediction.output.Take(3))
                 {
                     Console.WriteLine($"Class #{numClasses++} score = {classScore}");
                 }
                 Console.WriteLine(new string('-', 10));
             }
+
+            // Results look like below...
+            //Class #0 score = -0.8092947
+            //Class #1 score = -0.3310375
+            //Class #2 score = 0.1119193
+            //----------
+            //Class #0 score = -0.7807726
+            //Class #1 score = -0.2158062
+            //Class #2 score = 0.1153686
+            //----------
         }
 
-        /// <summary>
-        /// inputSize is the overall dimensions of the model input tensor.
-        /// </summary>
-        private const int imageHeight = 324;
-        private const int imageWidth = 324;
+        private const int imageHeight = 224;
+        private const int imageWidth = 224;
         private const int numChannels = 3;
         private const int inputSize = imageHeight * imageWidth * numChannels;
 
         /// <summary>
         /// A class to hold sample tensor data. Member name should match  
-        /// the inputs that the model expects (in this case, Input)
+        /// the inputs that the model expects (in this case, input)
         /// </summary>
         public class TensorData
         {
             [VectorType(imageHeight, imageWidth, numChannels)]
-            // public float[] Input { get; set; }
             public float[] input { get; set; }
         }
 
         /// <summary>
         /// Method to generate sample test data. Returns 2 sample rows.
         /// </summary>
-        /// <returns></returns>
         public static TensorData[] GetTensorData()
         {
             // This can be any numerical data. Assume image pixel values.
             var image1 = Enumerable.Range(0, inputSize).Select(x => (float)x / inputSize).ToArray();
             var image2 = Enumerable.Range(0, inputSize).Select(x => (float)(x + 10000) / inputSize).ToArray();
-            //return new TensorData[] { new TensorData() { Input = image1 }, new TensorData() { Input = image2 } };
             return new TensorData[] { new TensorData() { input = image1 }, new TensorData() { input = image2 } };
         }
 
@@ -87,8 +88,6 @@ namespace Microsoft.ML.Samples.Dynamic
         /// </summary>
         class OutputScores
         {
-            [VectorType(1000)]
-            //public float[] Output { get; set; }
             public float[] output { get; set; }
         }
     }
