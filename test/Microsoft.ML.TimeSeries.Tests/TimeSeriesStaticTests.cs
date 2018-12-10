3b09025f08abfb8c2f5f21ac918af4fa8117c6d3
Monte Hoover <mohoov@microsoft.com>
2018-12-10 11:16:44 -0800

    Revert "Change precision from 6 to 5 sig digits because of varying values with intrinsics."
    This reverts commit 1551a8de5ada3ac336f87fcffcc95a2abb40d672.

--- test/Microsoft.ML.TimeSeries.Tests/TimeSeriesStaticTests.cs
+++ test/Microsoft.ML.TimeSeries.Tests/TimeSeriesStaticTests.cs
@@ -117,10 +117,10 @@ namespace Microsoft.ML.Tests
             {
                 row = enumerator.Current;
                 
-                Assert.Equal(expectedValues[index++], row.Data[0], precision: 5);  // Alert
-                Assert.Equal(expectedValues[index++], row.Data[1], precision: 5);  // Raw score
-                Assert.Equal(expectedValues[index++], row.Data[2], precision: 5);  // P-Value score
-                Assert.Equal(expectedValues[index++], row.Data[3], precision: 5);  // Martingale score
+                Assert.Equal(expectedValues[index++], row.Data[0], precision: 6);  // Alert
+                Assert.Equal(expectedValues[index++], row.Data[1], precision: 6);  // Raw score
+                Assert.Equal(expectedValues[index++], row.Data[2], precision: 6);  // P-Value score
+                Assert.Equal(expectedValues[index++], row.Data[3], precision: 6);  // Martingale score
             }
         }
 
