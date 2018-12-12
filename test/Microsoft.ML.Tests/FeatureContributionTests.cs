10816f5e3add1fbed6de1a5626b8681f581dd4db
Artidoro Pagnoni <arpagnon@microsoft.com>
2018-12-11 17:22:18 -0800

    added tests for most predictors

--- test/Microsoft.ML.Tests/FeatureContributionTests.cs
+++ test/Microsoft.ML.Tests/FeatureContributionTests.cs
@@ -6,13 +6,15 @@ using Microsoft.ML.Data;
 using Microsoft.ML.Runtime.Data;
 using Microsoft.ML.Runtime.RunTests;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using Xunit;
 using Xunit.Abstractions;
-using Microsoft.ML.Runtime.Training;
 using Microsoft.ML.Runtime;
+using Microsoft.ML.Runtime.Data.IO;
 using Microsoft.ML.Runtime.Internal.Internallearn;
+using Microsoft.ML.Runtime.Internal.Utilities;
+using Microsoft.ML.Runtime.Training;
+using Microsoft.ML.Transforms;
+using System.IO;
 
 namespace Microsoft.ML.Tests
 {
@@ -33,150 +35,13 @@ namespace Microsoft.ML.Tests
         {
         }
 
-        public IDataView GenerateRegressionData()
-        {
-            // Setup synthetic dataset.
-            const int numberOfInstances = 1000;
-            var rand = new Random(10);
-            float[] yArray = new float[numberOfInstances],
-                x1Array = new float[numberOfInstances],
-                x2Array = new float[numberOfInstances],
-                x3Array = new float[numberOfInstances],
-                x4RandArray = new float[numberOfInstances];
-
-            for (var i = 0; i < numberOfInstances; i++)
-            {
-                var x1 = rand.Next(1000);
-                x1Array[i] = x1;
-                var x2Important = rand.Next(10000);
-                x2Array[i] = x2Important;
-                var x3 = rand.Next(5000);
-                x3Array[i] = x3;
-                var x4Rand = rand.Next(1000);
-                x4RandArray[i] = x4Rand;
-
-                var noise = rand.Next(50);
-                yArray[i] = (float)(10 * x1 + 20 * x2Important + 5.5 * x3 + noise);
-            }
-
-            // Create data view.
-            var bldr = new ArrayDataViewBuilder(ML);
-            bldr.AddColumn("X1", NumberType.Float, x1Array);
-            bldr.AddColumn("X2Important", NumberType.Float, x2Array);
-            bldr.AddColumn("X3", NumberType.Float, x3Array);
-            bldr.AddColumn("X4Rand", NumberType.Float, x4RandArray);
-            bldr.AddColumn("Label", NumberType.Float, yArray);
-            return bldr.GetDataView();
-        }
-
-        /// <summary>
-        /// Features: x1, x2, x3, xRand; y = 10*x1 + 20x2 + 5.5x3 + e, xRand- random, Label y is dependant on xRand.
-        /// Test verifies that feature contribution scores are outputted along with a score for predicted data. 
-        /// </summary>
-        private void TestFeatureImportance(
-            ITrainerEstimator<ISingleFeaturePredictionTransformer<IFeatureContributionMapper>, IFeatureContributionMapper> trainer,
-            List<float[]> expectedValues,
-            int precision = 6)
-        {
-            // Setup synthetic dataset.
-            const int numFeatures = 4;
-            var srcDV = GenerateRegressionData();
-
-            var pipeline = ML.Transforms.Concatenate("Features", "X1", "X2Important", "X3", "X4Rand")
-                .AppendCacheCheckpoint(ML)
-                .Append(ML.Transforms.Normalize("Features"));
-            var data = pipeline.Fit(srcDV).Transform(srcDV);
-            var model = trainer.Fit(data);
-
-            var output = new FeatureContributionCalculatingTransformer(ML, model.Model, model.FeatureColumn).Transform(data);
-
-            var transformedOutput = output.AsEnumerable<ScoreAndContribution>(Env, true);
-            int rowIndex = 0;
-            foreach (var row in transformedOutput.Take(expectedValues.Count))
-            {
-                var expectedValue = expectedValues[rowIndex++];
-                for (int i = 0; i < numFeatures; i++)
-                    Assert.Equal(expectedValue[i], row.FeatureContributions[i], precision);
-            }
-
-            Done();
-        }
-
-        [Fact]
-        public void TestOrdinaryLeastSquares()
-        {
-            var expectedValues = new List<float[]> {
-                new float[4] { 0.06319684F, 1, 0.1386623F, 4.46209469E-06F },
-                new float[4] { 0.03841561F, 1, 0.1633037F, 2.68303256E-06F },
-                new float[4] { 0.12006103F, 1, 0.254072F, 1.18671605E-05F },
-                new float[4] { 0.20861618F, 0.99999994F, 0.407312155F, 6.963478E-05F },
-                new float[4] { 0.024050576F, 0.99999994F, 0.31106182F, 8.456762E-06F }, };
-
-            TestFeatureImportance(ML.Regression.Trainers.OrdinaryLeastSquares(), expectedValues);
-        }
-
-        /// <summary>
-        /// Features: x1, x2, x3, xRand; y = 10*x1 + 20x2 + 5.5x3 + e, xRand- random, Label y is dependant on xRand.
-        /// Test verifies that feature contribution scores are outputted along with a score for predicted data. 
-        /// </summary>
-        [Fact]
-        public void TestFeatureContribution()
-        {
-            var srcDV = GenerateRegressionData();
-            var pipeline = ML.Transforms.Concatenate("Features", "X1", "X2Important", "X3", "X4Rand")
-                .AppendCacheCheckpoint(ML)
-                .Append(ML.Transforms.Normalize("Features"));
-            var data = pipeline.Fit(srcDV).Transform(srcDV);
-            var model = ML.Regression.Trainers.OrdinaryLeastSquares().Fit(data);
-
-            var outputTran = new FeatureContributionCalculatingTransformer(ML, model.Model, model.FeatureColumn).Transform(data);
-            var outputEst = new FeatureContributionCalculatingEstimator(ML, model.Model, model.FeatureColumn).Fit(data).Transform(data);
-
-            // Get prediction scores and contributions
-            var enumeratorTran = outputTran.AsEnumerable<ScoreAndContribution>(ML, true).GetEnumerator();
-            ScoreAndContribution row = null;
-            var expectedValues = new List<float[]>();
-            expectedValues.Add(new float[4] { 0.06319684F, 1, 0.1386623F, 4.46209469E-06F });
-            expectedValues.Add(new float[4] { 0.03841561F, 1, 0.1633037F, 2.68303256E-06F });
-            expectedValues.Add(new float[4] { 0.12006103F, 1, 0.254072F, 1.18671605E-05F });
-            expectedValues.Add(new float[4] { 0.20861618F, 0.99999994F, 0.407312155F, 6.963478E-05F });
-            expectedValues.Add(new float[4] { 0.024050576F, 0.99999994F, 0.31106182F, 8.456762E-06F });
-            int index = 0;
-            while (enumeratorTran.MoveNext() && index < expectedValues.Count)
-            {
-                row = enumeratorTran.Current;
-                // We set predicion to 6 because the limit of floating-point numbers is 7.
-                Assert.Equal(expectedValues[index][0], row.FeatureContributions[0], 6);
-                Assert.Equal(expectedValues[index][1], row.FeatureContributions[1], 6);
-                Assert.Equal(expectedValues[index][2], row.FeatureContributions[2], 6);
-                Assert.Equal(expectedValues[index++][3], row.FeatureContributions[3], 6);
-            }
-
-            var enumeratorEst = outputEst.AsEnumerable<ScoreAndContribution>(ML, true).GetEnumerator();
-            while (enumeratorEst.MoveNext() && index < expectedValues.Count)
-            {
-                row = enumeratorEst.Current;
-                // We set predicion to 6 because the limit of floating-point numbers is 7.
-                Assert.Equal(expectedValues[index][0], row.FeatureContributions[0], 6);
-                Assert.Equal(expectedValues[index][1], row.FeatureContributions[1], 6);
-                Assert.Equal(expectedValues[index][2], row.FeatureContributions[2], 6);
-                Assert.Equal(expectedValues[index++][3], row.FeatureContributions[3], 6);
-            }
-
-            Done();
-        }
-
         [Fact]
         public void FeatureContributionEstimatorWorkout()
         {
-            var srcDV = GenerateRegressionData();
-            var pipeline = ML.Transforms.Concatenate("Features", "X1", "X2Important", "X3", "X4Rand")
-                .AppendCacheCheckpoint(ML)
-                .Append(ML.Transforms.Normalize("Features"));
-            var data = pipeline.Fit(srcDV).Transform(srcDV);
+            var data = GetSparseDataset();
             var model = ML.Regression.Trainers.OrdinaryLeastSquares().Fit(data);
 
-            var estPipe = new FeatureContributionCalculatingEstimator(ML, model.Model, model.FeatureColumn, stringify: true)
+            var estPipe = new FeatureContributionCalculatingEstimator(ML, model.Model, model.FeatureColumn, stringify: true);
                 .Append(new FeatureContributionCalculatingEstimator(ML, model.Model, model.FeatureColumn))
                 .Append(new FeatureContributionCalculatingEstimator(ML, model.Model, model.FeatureColumn, top: 0))
                 .Append(new FeatureContributionCalculatingEstimator(ML, model.Model, model.FeatureColumn, bottom: 0))
@@ -184,6 +49,253 @@ namespace Microsoft.ML.Tests
             TestEstimatorCore(estPipe, data);
 
             Done();
+        }
+
+        // Tests for regression trainers that implement IFeatureContributionMapper interface.
+        [Fact]
+        public void TestOrdinaryLeastSquares()
+        {
+            TestFeatureContribution(ML.Regression.Trainers.OrdinaryLeastSquares(), GetSparseDataset(), "LeastSquaresRegression");
+        }
+
+        [Fact]
+        public void TestLightGbmRegression()
+        {
+            TestFeatureContribution(ML.Regression.Trainers.LightGbm(), GetSparseDataset(), "LightGbmRegression");
+        }
+
+        [Fact]
+        public void TestFastTreeRegression()
+        {
+            TestFeatureContribution(ML.Regression.Trainers.FastTree(), GetSparseDataset(), "FastTreeRegression");
+        }
+
+        [Fact]
+        public void TestFastForestRegression()
+        {
+            TestFeatureContribution(ML.Regression.Trainers.FastForest(), GetSparseDataset(), "FastForestRegression");
+        }
+
+        [Fact]
+        public void TestFastTreeTweedieRegression()
+        {
+            TestFeatureContribution(ML.Regression.Trainers.FastTreeTweedie(), GetSparseDataset(), "FastTreeTweedieRegression");
+        }
+
+        [Fact(Skip = "Results vary a lot from run to run, need to investigate error.")]
+        public void TestSDCARegression()
+        {
+            TestFeatureContribution(ML.Regression.Trainers.StochasticDualCoordinateAscent(), GetSparseDataset(), "SDCARegression");
+        }
+
+        [Fact]
+        public void TestOnlineGradientDescentRegression()
+        {
+            TestFeatureContribution(ML.Regression.Trainers.OnlineGradientDescent(), GetSparseDataset(), "OnlineGradientDescentRegression");
+        }
+
+        [Fact]
+        public void TestPoissonRegression()
+        {
+            TestFeatureContribution(ML.Regression.Trainers.PoissonRegression(), GetSparseDataset(), "PoissonRegression");
+        }
+
+        // Tests for ranking trainers that implement IFeatureContributionMapper interface.
+        [Fact]
+        public void TestFastTreeRanking()
+        {
+            TestFeatureContribution(ML.Ranking.Trainers.FastTree(), GetSparseDataset(TaskType.Ranking), "FastTreeRanking");
+        }
+
+        [Fact]
+        public void TestLightGbmRanking()
+        {
+            TestFeatureContribution(ML.Ranking.Trainers.LightGbm(), GetSparseDataset(TaskType.Ranking), "LightGbmRanking");
+        }
+
+        // Tests for binary classification trainers that implement IFeatureContributionMapper interface.
+        [Fact]
+        public void TestAveragePerceptronBinary()
+        {
+            TestFeatureContribution(ML.BinaryClassification.Trainers.AveragedPerceptron(), GetSparseDataset(TaskType.BinaryClassification), "AveragePerceptronBinary");
+        }
+
+        [Fact]
+        public void TestSVMBinary()
+        {
+            TestFeatureContribution(ML.BinaryClassification.Trainers.LinearSupportVectorMachines(), GetSparseDataset(TaskType.BinaryClassification), "SVMBinary");
+        }
+
+        [Fact]
+        public void TestLogisticRegressionBinary()
+        {
+            TestFeatureContribution(ML.BinaryClassification.Trainers.LogisticRegression(), GetSparseDataset(TaskType.BinaryClassification), "LogisticRegressionBinary");
+        }
+
+        // Tests for multiclass classification trainers that implement IFeatureContributionMapper interface.
+        // None right now.
+        
+        // Predictors that do not implement IFeatureContributionMapper: 
+        // MulticlassClassification: StochasticDualCoordinateAscent, LogistiRegression, LightGbm, NaiveBayes, OneVersusAll
+        // BinaryClassification: FastForest, LightGbm, FastTree, StochasticDualCoordinateAscent, StochasticGradientDescent, SymbolicStochasticGradientDescent
+
+        private void TestFeatureContribution(
+            ITrainerEstimator<ISingleFeaturePredictionTransformer<IFeatureContributionMapper>, IFeatureContributionMapper> trainer,
+            IDataView data,
+            string testFile,
+            int precision = 6)
+        {
+            // Train the model.
+            var model = trainer.Fit(data);
+
+            // Calculate feature contributions.
+            var est = new FeatureContributionCalculatingEstimator(ML, model.Model, model.FeatureColumn);
+
+            // Verify output.
+            var outputPath = GetOutputPath("FeatureContribution", testFile + ".tsv");
+            using (var ch = Env.Start("save"))
+            {
+                var saver = new TextSaver(ML, new TextSaver.Arguments { Silent = true, OutputHeader = false });
+                IDataView savedData = TakeFilter.Create(ML, est.Fit(data).Transform(data), 4);
+                using (var fs = File.Create(outputPath))
+                    DataSaverUtils.SaveDataView(ch, saver, savedData, fs, keepHidden: true);
+            }
+            CheckEquality("FeatureContribution", testFile + ".tsv");
+            Done();
+        }
+
+        /// <summary>
+        /// Features: x1, x2vBuff(sparce vector), x3. 
+        /// y = 10x1 + 10x2vBuff + 30x3 + e.
+        /// Within xBuff feature  2nd slot will be sparse most of the time.
+        /// 2nd slot of xBuff has the least importance: Evaluation metrics do not change a lot when this slot is permuted.
+        /// x3 has the biggest importance.
+        /// </summary>
+        private IDataView GetSparseDataset(TaskType task = TaskType.Regression)
+        {
+            // Setup synthetic dataset.
+            const int numberOfInstances = 10000;
+            var rand = new Random(10);
+            float[] yArray = new float[numberOfInstances],
+                x1Array = new float[numberOfInstances],
+                x3Array = new float[numberOfInstances];
+
+            VBuffer<float>[] vbArray = new VBuffer<float>[numberOfInstances];
+
+            for (var i = 0; i < numberOfInstances; i++)
+            {
+                var x1 = rand.Next(1000);
+                x1Array[i] = x1;
+                var x3Important = rand.Next(10000);
+                x3Array[i] = x3Important;
+
+                VBuffer<float> vb;
+
+                if (i % 10 != 0)
+                {
+                    vb = new VBuffer<float>(4, 3, new float[] { rand.Next(1000), rand.Next(1000), rand.Next(1000) }, new int[] { 0, 2, 3 });
+                }
+                else
+                {
+                    vb = new VBuffer<float>(4, 4, new float[] { rand.Next(1000), rand.Next(1000), rand.Next(1000), rand.Next(1000) }, new int[] { 0, 1, 2, 3 });
+                }
+
+                vbArray[i] = vb;
+
+                float vbSum = 0;
+                foreach (var vbValue in vb.DenseValues())
+                {
+                    vbSum += vbValue * 10;
+                }
+
+                var noise = rand.Next(50);
+                yArray[i] = 10 * x1 + vbSum + 20 * x3Important + noise;
+            }
+
+            // If binary classification, modify the labels
+            if (task == TaskType.BinaryClassification ||
+                task == TaskType.MulticlassClassification)
+                GetBinaryClassificationLabels(yArray);
+            else if (task == TaskType.Ranking)
+                GetRankingLabels(yArray);
+
+            // Create data view.
+            var bldr = new ArrayDataViewBuilder(Env);
+            bldr.AddColumn("X1", NumberType.Float, x1Array);
+            bldr.AddColumn("X2VBuffer", NumberType.Float, vbArray);
+            bldr.AddColumn("X3Important", NumberType.Float, x3Array);
+            bldr.AddColumn("Label", NumberType.Float, yArray);
+            if (task == TaskType.Ranking)
+                bldr.AddColumn("GroupId", NumberType.U4, CreateGroupIds(yArray.Length));
+            var srcDV = bldr.GetDataView();
+
+            var pipeline = ML.Transforms.Concatenate("Features", "X1", "X2VBuffer", "X3Important")
+                .Append(ML.Transforms.Normalize("Features"));
+
+            // Create a keytype for Ranking
+            if (task == TaskType.Ranking)
+                return pipeline.Append(ML.Transforms.Conversion.MapValueToKey("GroupId"))
+                    .Fit(srcDV).Transform(srcDV);
+
+            return pipeline.Fit(srcDV).Transform(srcDV);
+        }
+
+        private void GetBinaryClassificationLabels(float[] rawScores)
+        {
+            float averageScore = GetArrayAverage(rawScores);
+
+            // Center the response and then take the sigmoid to generate the classes
+            for (int i = 0; i < rawScores.Length; i++)
+                rawScores[i] = MathUtils.Sigmoid(rawScores[i] - averageScore) > 0.5 ? 1 : 0;
+        }
+
+        private void GetRankingLabels(float[] rawScores)
+        {
+            var min = MathUtils.Min(rawScores);
+            var max = MathUtils.Max(rawScores);
+
+            for (int i = 0; i < rawScores.Length; i++)
+            {
+                // Bin from [zero,one), then expand out to [0,5) and truncate
+                rawScores[i] = (int)(5 * (rawScores[i] - min) / (max - min));
+                if (rawScores[i] == 5)
+                    rawScores[i] = 4;
+            }
+        }
+
+        private float GetArrayAverage(float[] scores)
+        {
+            // Compute the average so we can center the response
+            float averageScore = 0.0f;
+            for (int i = 0; i < scores.Length; i++)
+                averageScore += scores[i];
+            averageScore /= scores.Length;
+
+            return averageScore;
+        }
+
+        private uint[] CreateGroupIds(int numRows, int rowsPerGroup = 5)
+        {
+            var groupIds = new uint[numRows];
+            // Construct groups of rowsPerGroup using a modulo counter
+            uint group = 0;
+            for (int i = 0; i < groupIds.Length; i++)
+            {
+                if (i % rowsPerGroup == 0)
+                    group++;
+                groupIds[i] = group;
+            }
+
+            return groupIds;
+        }
+
+        private enum TaskType
+        {
+            Regression,
+            BinaryClassification,
+            MulticlassClassification,
+            Ranking,
+            Clustering
         }
     }
 }
