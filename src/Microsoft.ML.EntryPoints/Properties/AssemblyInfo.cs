104d4b224a3955013d0da334f2efc87f5b8581ff
Monte Hoover <37886197+montebhoover@users.noreply.github.com>
2018-12-14 14:14:39 -0800

    Move entrypoints from Microsoft.ML.Legacy to Microsoft.ML.Entrypoints. (#1859)

---
+++ src/Microsoft.ML.EntryPoints/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.ML;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Microsoft.ML.Tests" + PublicKey.TestValue)]
+[assembly: InternalsVisibleTo("Microsoft.ML.Core.Tests" + PublicKey.TestValue)]
