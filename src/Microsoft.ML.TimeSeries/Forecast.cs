04e7193a3b83228e0d3ae07d7c7ec70381badf41
Zeeshan Siddiqui <mzs@microsoft.com>
2018-12-17 17:38:25 -0800

    cleanup.

--- src/Microsoft.ML.TimeSeries/Forecast.cs
+++ src/Microsoft.ML.TimeSeries/Forecast.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.ML.Runtime;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.ML.Runtime;
 using Microsoft.ML.Runtime.Data;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Microsoft.ML.TimeSeries
 {
