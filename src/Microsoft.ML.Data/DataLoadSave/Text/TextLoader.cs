8836dbab9a2e78bd0853719051698fb627538772
Abhishek Goswami <abgoswam@gmail.com>
2018-12-21 23:21:18 +0000

    fix build break (caused by bad merge with master in previous commit)

--- src/Microsoft.ML.Data/DataLoadSave/Text/TextLoader.cs
+++ src/Microsoft.ML.Data/DataLoadSave/Text/TextLoader.cs
@@ -406,7 +406,7 @@ namespace Microsoft.ML.Runtime.Data
         /// Used as an input column range.
         /// A variable length segment (extending to the end of the input line) is represented by Lim == SrcLim.
         /// </summary>
-        private struct Segment
+        internal struct Segment
         {
             public int Min;
             public int Lim;
@@ -441,7 +441,7 @@ namespace Microsoft.ML.Runtime.Data
         /// <summary>
         /// Information for an output column.
         /// </summary>
-        private sealed class ColInfo
+        internal sealed class ColInfo
         {
             public readonly string Name;
             // REVIEW: Fix this for keys.
