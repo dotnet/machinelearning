39bcd5bdbd07edbe2dc015492a9bc4c9ea50ce2c
Ivan Matantsev <ivmatan@microsoft.com>
2019-01-02 14:27:56 -0800

    remove test

--- test/Microsoft.ML.Tests/TrainerEstimators/OnlineLinearTests.cs
+++ test/Microsoft.ML.Tests/TrainerEstimators/OnlineLinearTests.cs
@@ -55,57 +55,5 @@ namespace Microsoft.ML.Tests.TrainerEstimators
             Done();
 
         }
-
-
-        [Fact]
-        public void OnlineLinearWorkout1()
-        {
-            // load data
-            var textLoader = new TextLoader(ML,
-                new TextLoader.Arguments()
-                {
-                    Separator = ",",
-                    HasHeader = true,
-                    Column = new[]
-                    {
-            new TextLoader.Column("Age", DataKind.R4, 0),
-            new TextLoader.Column("Workclass", DataKind.TX, 1),
-            new TextLoader.Column("Fnlwgt", DataKind.R4, 2),
-            new TextLoader.Column("Education", DataKind.TX, 3),
-            new TextLoader.Column("EducationNum", DataKind.R4, 4),
-            new TextLoader.Column("MaritalStatus", DataKind.TX, 5),
-            new TextLoader.Column("Occupation", DataKind.TX, 6),
-            new TextLoader.Column("Relationship", DataKind.TX, 7),
-            new TextLoader.Column("Race", DataKind.TX, 8),
-            new TextLoader.Column("Sex", DataKind.TX, 9),
-            new TextLoader.Column("CapitalGain", DataKind.R4, 10),
-            new TextLoader.Column("CapitalLoss", DataKind.R4, 11),
-            new TextLoader.Column("HoursPerWeek", DataKind.R4, 12),
-            new TextLoader.Column("NativeCountry", DataKind.TX, 13),
-            new TextLoader.Column("Label", DataKind.Bool, 14),
-                    }
-                });
-            var trainDataPath = "F:/tlc/test/adult.train";
-            var trainData = textLoader.Read(trainDataPath);
-            var validationData = textLoader.Read(trainDataPath);
-            var testData = textLoader.Read(trainDataPath);
-
-            // preprocess
-            var preprocessorEstimator = ML.Transforms.Categorical.OneHotEncoding("Workclass", "Workclass")
-                .Append(ML.Transforms.Categorical.OneHotEncoding("Education", "Education"))
-                .Append(ML.Transforms.Categorical.OneHotEncoding("MaritalStatus", "MaritalStatus"))
-                .Append(ML.Transforms.Categorical.OneHotEncoding("Occupation", "Occupation"))
-                .Append(ML.Transforms.Categorical.OneHotEncoding("Relationship", "Relationship"))
-                .Append(ML.Transforms.Categorical.OneHotEncoding("Race", "Race"))
-                .Append(ML.Transforms.Categorical.OneHotEncoding("Sex", "Sex"))
-                .Append(ML.Transforms.Categorical.OneHotEncoding("NativeCountry", "NativeCountry"))
-                .Append(ML.Transforms.Concatenate(DefaultColumnNames.Features,
-                    "Age", "Workclass", "Fnlwgt", "Education", "EducationNum", "MaritalStatus", "Occupation", "Relationship",
-                    "Race", "Sex", "CapitalGain", "CapitalLoss", "HoursPerWeek", "NativeCountry"));
-            // train model
-            var trainer = ML.BinaryClassification.Trainers.LinearSupportVectorMachines();
-            var estimatorChain = preprocessorEstimator.Append(trainer);
-            var model = estimatorChain.Fit(trainData);
-        }
     }
 }
