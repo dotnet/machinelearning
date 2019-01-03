f595681ed769d672effb6a673e10904bec397521
Wei-Sheng Chin <wechi@microsoft.com>
2019-01-03 10:13:31 -0800

    Discard unexpected changed file

--- src/Microsoft.ML.Data/DataView/ZipBinding.cs
+++ src/Microsoft.ML.Data/DataView/ZipBinding.cs
@@ -3,8 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using Microsoft.ML.Internal.Utilities;
 
 namespace Microsoft.ML.Data
