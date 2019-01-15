ef638f480a2be117b181ea52a1786e391244e5d2
Scott Inglis <singlis@microsoft.com>
2019-01-15 10:08:55 -0800

    Metadata fixes for the ValueMappingEstimator (#2098)
    * The ValueMappingEstimator had a couple of issues when using setting the
    Values as KeyTypes:
    1) The output schema for the Estimator did not contain the KeyType
    information in the metadata.
    2) The reverse lookup of the metadata had the incorrect value.
    
    This now sets the correct metadata on the output schema and uses the
    value data for the reverse lookup. A test was added to confirm the
    changes using the KeyToValueMapping appended to a ValueMappingEstimator
    for the reverse lookup.
    
    Fixes #2086
    Fixes #2083

--- src/Microsoft.ML.Data/Transforms/ValueMappingTransformer.cs
+++ src/Microsoft.ML.Data/Transforms/ValueMappingTransformer.cs
@@ -100,13 +100,14 @@ namespace Microsoft.ML.Transforms.Conversions
             var isKey = Transformer.ValueColumnType is KeyType;
             var columnType = (isKey) ? PrimitiveType.FromKind(DataKind.U4) :
                                     Transformer.ValueColumnType;
+            var metadataShape = SchemaShape.Create(Transformer.ValueColumnMetadata.Schema);
             foreach (var (Input, Output) in _columns)
             {
                 if (!inputSchema.TryFindColumn(Input, out var originalColumn))
                     throw Host.ExceptSchemaMismatch(nameof(inputSchema), "input", Input);
 
-                // Get the type from TOutputType
-                var col = new SchemaShape.Column(Output, vectorKind, columnType, isKey, originalColumn.Metadata);
+                // Create the Value column
+                var col = new SchemaShape.Column(Output, vectorKind, columnType, isKey, metadataShape);
                 resultDic[Output] = col;
             }
             return new SchemaShape(resultDic.Values);
@@ -191,18 +192,14 @@ namespace Microsoft.ML.Transforms.Conversions
                 // set of values. This is used for generating the metadata of
                 // the column.
                 HashSet<TValue> valueSet = new HashSet<TValue>();
-                HashSet<TKey> keySet = new HashSet<TKey>();
-                for (int i = 0; i < values.Count(); ++i)
+                foreach (var v in values)
                 {
-                    var v = values.ElementAt(i);
                     if (valueSet.Contains(v))
                         continue;
                     valueSet.Add(v);
-
-                    var k = keys.ElementAt(i);
-                    keySet.Add(k);
                 }
-                var metaKeys = keySet.ToArray();
+
+                var metaKeys = valueSet.ToArray();
 
                 // Key Values are treated in one of two ways:
                 // If the values are of type uint or ulong, these values are used directly as the keys types and no new keys are created.
@@ -387,7 +384,7 @@ namespace Microsoft.ML.Transforms.Conversions
             Host.CheckNonEmpty(valueColumn, nameof(valueColumn), "A value column must be specified when passing in an IDataView for the value mapping");
             _valueMap = CreateValueMapFromDataView(lookupMap, keyColumn, valueColumn);
             int valueColumnIdx = 0;
-            Host.Assert(lookupMap.Schema.TryGetColumnIndex(valueColumn, out valueColumnIdx));
+            Host.Check(lookupMap.Schema.TryGetColumnIndex(valueColumn, out valueColumnIdx));
             _valueMetadata = lookupMap.Schema[valueColumnIdx].Metadata;
 
             // Create the byte array of the original IDataView, this is used for saving out the data.
