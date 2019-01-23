69d33d84d222109ed1f401f2c738d1dd27e9cb0e
Rogan Carr <rocarr@microsoft.com>
2019-01-23 13:59:19 -0800

    Cleaning up usings.

--- test/Microsoft.ML.Core.Tests/UnitTests/TestUtilities.cs
+++ test/Microsoft.ML.Core.Tests/UnitTests/TestUtilities.cs
@@ -2,10 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.ML;
 using Microsoft.ML.Internal.Utilities;
 using Microsoft.ML.RunTests;
 using Microsoft.ML.Trainers.FastTree.Internal;
