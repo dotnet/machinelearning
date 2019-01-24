5caf142e0428ff5a405cefc9aa92017cd308b312
daholste <43974253+daholste@users.noreply.github.com>
2019-01-23 19:01:32 -0800

    add conversions tests (#25)

---
+++ src/Test/ConversionTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.ML.Auto.Test
+{
+    [TestClass]
+    public class ConversionTests
+    {
+        [TestMethod]
+        public void ConvertFloatMissingValues()
+        {
+            var missingValues = new string[]
+            {
+                "?",
+                "na", "n/a", "nan",
+                "NA", "N/A", "NaN", "NAN"
+            };
+
+            foreach(var missingValue in missingValues)
+            {
+                float value;
+                var success = Conversions.TryParse(missingValue.AsMemory(), out value);
+                Assert.IsTrue(success);
+                Assert.AreEqual(value, float.NaN);
+            }
+        }
+
+        [TestMethod]
+        public void ConvertFloatParseFailure()
+        {
+            var values = new string[]
+            {
+                "a", "aa", "nb", "aaa", "naa", "nba", "n/b" 
+            };
+
+            foreach (var value in values)
+            {
+                var success = Conversions.TryParse(value.AsMemory(), out float _);
+                Assert.IsFalse(success);
+            }
+        }
+
+        [TestMethod]
+        public void ConvertBoolMissingValues()
+        {
+            var missingValues = new string[]
+            {
+                "",
+                "no", "NO", "+1", "-1",
+                "yes", "YES",
+                "true", "TRUE",
+                "false", "FALSE"
+            };
+
+            foreach (var missingValue in missingValues)
+            {
+                var success = Conversions.TryParse(missingValue.AsMemory(), out bool _);
+                Assert.IsTrue(success);
+            }
+        }
+
+        [TestMethod]
+        public void ConvertBoolParseFailure()
+        {
+            var values = new string[]
+            {
+                "aa", "na", "+a", "-a",
+                "aaa", "yaa", "yea",
+                "aaaa", "taaa", "traa", "trua",
+                "aaaaa", "fbbbb", "faaaa", "falaa", "falsa"
+            };
+
+            foreach (var value in values)
+            {
+                var success = Conversions.TryParse(value.AsMemory(), out bool _);
+                Assert.IsFalse(success);
+            }
+        }
+    }
+}
