0a4cb1f09b326b10f785d4ba4dbc645534fa6810
Paul Freedman <paul.freedman19@googlemail.com>
2019-02-02 15:11:11 +0000

    Split DataReader extensions between Microsoft.ML.Data and Microsoft.ML.StaticPipe

---
+++ src/Microsoft.ML.Data/DataLoadSave/DataReaderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.DataView;
+using Microsoft.ML.Core.Data;
+using Microsoft.ML.Data;
+
+namespace Microsoft.ML
+{
+    public static class DataReaderExtensions
+    {
+        public static IDataView Read(this IDataReader<IMultiStreamSource> reader, string path)
+        {
+            return reader.Read(new MultiFileSource(path));
+        }
+
+        public static IDataView Read(this IDataReader<IMultiStreamSource> reader, params string[] path)
+        {
+            return reader.Read(new MultiFileSource(path));
+        }
+    }
+}
--- src/Microsoft.ML.StaticPipe/LocalPathReader.cs
+++ src/Microsoft.ML.StaticPipe/LocalPathReader.cs
@@ -1,13 +1,11 @@
-﻿using Microsoft.Data.DataView;
-using Microsoft.ML.Core.Data;
-using Microsoft.ML.Data;
+﻿using Microsoft.ML.Data;
 using Microsoft.ML.StaticPipe;
 
 namespace Microsoft.ML
 {
     public static class LocalPathReader
     {
-        public static IDataView Read(this IDataReader<IMultiStreamSource> reader, params string[] path)
+        public static DataView<TShape> Read<TShape>(this DataReader<IMultiStreamSource, TShape> reader, string path)
         {
             return reader.Read(new MultiFileSource(path));
         }
