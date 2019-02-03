54c5200b706a12eab41a5b9e5dccaeb76fa30f6a
Paul Freedman <paul.freedman19@googlemail.com>
2019-02-03 10:37:30 +0000

    Added standard header to new files and changed namespace for static DataReader extensions (#1797)

--- src/Microsoft.ML.StaticPipe/LocalPathReader.cs
+++ src/Microsoft.ML.StaticPipe/LocalPathReader.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.ML.Data;
-using Microsoft.ML.StaticPipe;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 
-namespace Microsoft.ML
+using Microsoft.ML.Data;
+
+namespace Microsoft.ML.StaticPipe
 {
     public static class LocalPathReader
     {
