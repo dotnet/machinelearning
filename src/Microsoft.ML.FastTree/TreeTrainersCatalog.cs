6e1b08eabe8ab53cb7a05a9a749626ef5e971704
Hakim BEN AMAR <Hakim@hf-online.be>
2019-02-06 11:24:03 +0100

    Update the document as described in the issue 2177

--- src/Microsoft.ML.FastTree/TreeTrainersCatalog.cs
+++ src/Microsoft.ML.FastTree/TreeTrainersCatalog.cs
@@ -17,9 +17,9 @@ namespace Microsoft.ML
         /// Predict a target using a decision tree regression model trained with the <see cref="FastTreeRegressionTrainer"/>.
         /// </summary>
         /// <param name="catalog">The <see cref="RegressionCatalog"/>.</param>
-        /// <param name="labelColumn">The label column.</param>
-        /// <param name="featureColumn">The feature column.</param>
-        /// <param name="weights">The optional weights column.</param>
+        /// <param name="labelColumn">The name of the label column.</param>
+        /// <param name="featureColumn">The name of the feature column.</param>
+        /// <param name="weights">The name of the optional weights column.</param>
         /// <param name="numTrees">Total number of decision trees to create in the ensemble.</param>
         /// <param name="numLeaves">The maximum number of leaves per decision tree.</param>
         /// <param name="minDatapointsInLeaves">The minimal number of datapoints allowed in a leaf of a regression tree, out of the subsampled data.</param>
