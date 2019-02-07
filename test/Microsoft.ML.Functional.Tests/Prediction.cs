363b083e28b325bd7d8f0666f182783c6ad26ed9
Rogan Carr <rocarr@microsoft.com>
2019-02-07 15:00:00 -0800

    Adding a project, functional tests, without internal access to the ML.NET Library.

---
+++ test/Microsoft.ML.Functional.Tests/Prediction.cs
@@ -0,0 +1,49 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.ML.SamplesUtils;
+using Xunit;
+
+namespace Microsoft.ML.Functional.Tests
+{
+    public partial class PredictionScenarios
+    {
+        /// <summary>
+        /// Reconfigurable predictions: The following should be possible: A user trains a binary classifier,
+        /// and through the test evaluator gets a PR curve, the based on the PR curve picks a new threshold
+        /// and configures the scorer (or more precisely instantiates a new scorer over the same predictor)
+        /// with some threshold derived from that.
+        /// </summary>
+        [Fact(Skip = "Blocked by issue #2465")]
+        public void ReconfigurablePrediction()
+        {
+            var mlContext = new MLContext(seed: 789);
+
+            // Get the dataset, create a train and test
+            var dataset = DatasetUtils.LoadHousingRegressionDataset(mlContext);
+            (var train, var test) = mlContext.BinaryClassification.TrainTestSplit(dataset, testFraction: 0.2);
+
+            // Create a pipeline to train on the housing data
+            var pipeline = mlContext.Transforms.Concatenate("Features", new string[] {
+                    "CrimesPerCapita", "PercentResidental", "PercentNonRetail", "CharlesRiver", "NitricOxides", "RoomsPerDwelling",
+                    "PercentPre40s", "EmploymentDistance", "HighwayDistance", "TaxRate", "TeacherRatio"})
+                .Append(mlContext.Transforms.CopyColumns("Label", "MedianHomeValue"))
+                .Append(mlContext.Regression.Trainers.OrdinaryLeastSquares());
+
+            var model = pipeline.Fit(train);
+
+            var scoredTest = model.Transform(test);
+            var metrics = mlContext.Regression.Evaluate(scoredTest);
+
+            // This is no longer possible in the API
+            //var newModel = new BinaryPredictionTransformer<IPredictorProducing<float>>(ml, model.Model, trainData.Schema, model.FeatureColumn, threshold: 0.01f, thresholdColumn: DefaultColumnNames.Probability);
+            //var newScoredTest = newModel.Transform(pipeline.Transform(testData));
+            //var newMetrics = mlContext.BinaryClassification.Evaluate(scoredTest);
+
+            // And the Threshold and ThresholdColumn properties are not settable.
+            // var predictor = model.LastTransformer;
+            // predictor.Threshold = 0.01; // Not possible
+        }
+    }
+}
---
+++ test/Microsoft.ML.Functional.Tests/Validation.cs
@@ -0,0 +1,48 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Data.DataView;
+using Microsoft.ML.Data;
+using Microsoft.ML.SamplesUtils;
+using Microsoft.ML.Trainers.HalLearners;
+using Xunit;
+
+namespace Microsoft.ML.Functional.Tests
+{
+    public partial class ValidationScenarios
+    {
+        /// <summary>
+        /// Cross-validation: Have a mechanism to do cross validation, that is, you come up with
+        /// a data source (optionally with stratification column), come up with an instantiable transform
+        /// and trainer pipeline, and it will handle (1) splitting up the data, (2) training the separate
+        /// pipelines on in-fold data, (3) scoring on the out-fold data, (4) returning the set of
+        /// evaluations and optionally trained pipes. (People always want metrics out of xfold,
+        /// they sometimes want the actual models too.)
+        /// </summary>
+        [Fact]
+        void CrossValidation()
+        {
+            var mlContext = new MLContext(seed: 789);
+
+            // Get the dataset, create a train and test
+            var data = DatasetUtils.LoadHousingRegressionDataset(mlContext);
+
+            // Create a pipeline to train on the sentiment data
+            var pipeline = mlContext.Transforms.Concatenate("Features", new string[] {
+                    "CrimesPerCapita", "PercentResidental", "PercentNonRetail", "CharlesRiver", "NitricOxides", "RoomsPerDwelling",
+                    "PercentPre40s", "EmploymentDistance", "HighwayDistance", "TaxRate", "TeacherRatio"})
+                .Append(mlContext.Transforms.CopyColumns("Label", "MedianHomeValue"))
+                .Append(mlContext.Regression.Trainers.OrdinaryLeastSquares());
+
+            // Compute the CV result
+            var cvResult = mlContext.Regression.CrossValidate(data, pipeline, numFolds: 5);
+
+            // Check that the results are valid
+            Assert.IsType<RegressionMetrics>(cvResult[0].metrics);
+            Assert.IsType<TransformerChain<RegressionPredictionTransformer<OlsLinearRegressionModelParameters>>>(cvResult[0].model);
+            Assert.True(cvResult[0].scoredTestData is IDataView);
+            Assert.Equal(5, cvResult.Length);
+        }
+    }
+}
