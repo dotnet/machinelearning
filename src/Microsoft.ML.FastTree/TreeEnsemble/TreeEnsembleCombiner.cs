0755ea82f4f74d58530c6ebe19dc9478c7217563
Yael Dekel <yaeld@microsoft.com>
2019-02-12 16:27:47 -0800

    Fix merge errors

--- src/Microsoft.ML.FastTree/TreeEnsemble/TreeEnsembleCombiner.cs
+++ src/Microsoft.ML.FastTree/TreeEnsemble/TreeEnsembleCombiner.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using Microsoft.ML;
 using Microsoft.ML.Data;
 using Microsoft.ML.Internal.Calibration;
-using Microsoft.ML.Trainers.Ensemble;
 using Microsoft.ML.Trainers.FastTree;
 
 [assembly: LoadableClass(typeof(TreeEnsembleCombiner), null, typeof(SignatureModelCombiner), "Fast Tree Model Combiner", "FastTreeCombiner")]
