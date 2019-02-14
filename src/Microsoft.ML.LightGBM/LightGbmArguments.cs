fc60edd665b9c60dd937977eb2c8ed9a7c21d2f3
Zeeshan Ahmed <zeahmed@microsoft.com>
2019-02-14 14:18:07 -0800

    Created two parameters for Booster. One for EntryPoint and one for cmdline.

--- src/Microsoft.ML.LightGBM/LightGbmArguments.cs
+++ src/Microsoft.ML.LightGBM/LightGbmArguments.cs
@@ -310,7 +310,10 @@ namespace Microsoft.ML.LightGBM
         [Argument(ArgumentType.AtMostOnce, HelpText = "Max number of bucket bin for features.", ShortName = "mb")]
         public int MaxBin = 255;
 
-        [Argument(ArgumentType.Multiple, HelpText = "Which booster to use, can be gbtree, gblinear or dart. gbtree and dart use tree based model while gblinear uses linear function.", SortOrder = 3)]
+        [Argument(ArgumentType.Multiple, HelpText = "Which booster to use, can be gbtree, gblinear or dart. gbtree and dart use tree based model while gblinear uses linear function.", SortOrder = 3, Visibility = ArgumentAttribute.VisibilityType.EntryPointsOnly)]
+        internal ISupportBoosterParameterFactory BoosterFactory = new TreeBooster.Arguments();
+
+        [Argument(ArgumentType.Multiple, HelpText = "Which booster to use, can be gbtree, gblinear or dart. gbtree and dart use tree based model while gblinear uses linear function.", SortOrder = 3, Visibility = ArgumentAttribute.VisibilityType.CmdLineOnly)]
         public TreeBooster.Arguments Booster = new TreeBooster.Arguments();
 
         [Argument(ArgumentType.AtMostOnce, HelpText = "Verbose", ShortName = "v")]
