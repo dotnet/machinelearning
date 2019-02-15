14c7c17bdeaff81aa12b155dbe0191d2e972d564
Rogan Carr <rocarr@microsoft.com>
2019-02-15 15:17:00 -0800

    Adding functional tests for explainability

---
+++ test/Microsoft.ML.Functional.Tests/Datasets/FeatureContributionOutput.cs
@@ -0,0 +1,16 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.ML.Data;
+
+namespace Microsoft.ML.Functional.Tests.Datasets
+{
+    /// <summary>
+    /// A schematized class for loading the HousingRegression dataset.
+    /// </summary>
+    internal sealed class FeatureContributionOutput
+    {
+        public float[] FeatureContributions { get; set; }
+    }
+}
