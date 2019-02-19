13f6ff9b7874567754e1a70ad075b95bd0e2fb5f
Anirudh Agnihotry <anirudhagnihotry098@gmail.com>
2019-02-19 14:18:06 -0800

    Adding a new ubuntu CI leg (#2516)
    * addding a new ci lef for ubuntu
    
    * Adding netcore3.0 windows ci, removing extra compile flags, using proper docker image tag
    
    * correcting the argument number
    
    * using netcoreapp2.1 for ubuntuCI, using getlibcversion and runtime infotmation
    
    * using version to compare version numbers

--- test/Microsoft.ML.TestFramework/Attributes/OnnxTheoryAttribute.cs
+++ test/Microsoft.ML.TestFramework/Attributes/OnnxTheoryAttribute.cs
@@ -19,7 +19,7 @@ namespace Microsoft.ML.TestFramework.Attributes
         /// <inheritdoc />
         protected override bool IsEnvironmentSupported()
         {
-            return Environment.Is64BitProcess && RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+            return Environment.Is64BitProcess && (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) || AttributeHelpers.CheckLibcVersionGreaterThanMinimum(new Version(2, 23)));
         }
     }
 }
