db837caf30f490cfd301c58825481020b91c005d
J W <woodjon@gmail.com>
2019-02-28 21:57:56 -0500

    Add sample file to read and write binary files

---
+++ docs/samples/Microsoft.ML.Samples/Dynamic/DataOperations/SaveAndReadFromBinary.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Microsoft.Data.DataView;
+using Microsoft.ML.SamplesUtils;
+
+namespace Microsoft.ML.Samples.Dynamic.DataOperations
+{
+    public class SaveAndReadFromBinary
+    {
+        public static void Example()
+        {
+            var mlContext = new MLContext();
+
+            // Get a small dataset as an IEnumerable.
+            IEnumerable<DatasetUtils.SampleTemperatureData> enumerableOfData = DatasetUtils.GetSampleTemperatureData(5);
+
+            // Load dataset into an IDataView. 
+            IDataView data = mlContext.Data.LoadFromEnumerable(enumerableOfData);
+
+            // Creating a FileStream object to create a file and use             
+            // the stream to create a binary file.
+            using (var stream = new FileStream("./sample-temp-data.idv", FileMode.Create))
+            {
+                mlContext.Data.SaveAsBinary(data, stream);
+            }
+
+            // Load a binary file by file path.
+            var binaryData = mlContext.Data.LoadFromBinary("./sample-temp-data.idv");
+        }
+    }
+}
