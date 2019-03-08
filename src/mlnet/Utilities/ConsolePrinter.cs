001b8df522c15b93526038ece19f56a51386ec83
Justin Ormont <justinormont@users.noreply.github.com>
2019-03-08 10:50:20 -0800

    Printing additional metrics in CLI for binary classification (#274)
    * Printing additional metrics in CLI for binary classification
    
    * Update src/mlnet/Utilities/ConsolePrinter.cs

--- src/mlnet/Utilities/ConsolePrinter.cs
+++ src/mlnet/Utilities/ConsolePrinter.cs
@@ -14,7 +14,7 @@ namespace Microsoft.ML.CLI.Utilities
 
         internal static void PrintBinaryClassificationMetrics(int iteration, string trainerName, BinaryClassificationMetrics metrics)
         {
-            logger.Log(LogLevel.Info, $"{iteration,-4} {trainerName,-35} {metrics.Accuracy,9:F4} {metrics.Auc,8:F4}");
+            logger.Log(LogLevel.Info, $"{iteration,-4} {trainerName,-35} {metrics.Accuracy,9:F4} {metrics.Auc,8:F4} {metrics.Auprc,8:F4} {metrics.F1Score,9:F4}");
         }
 
         internal static void PrintMulticlassClassificationMetrics(int iteration, string trainerName, MultiClassClassifierMetrics metrics)
@@ -32,7 +32,7 @@ namespace Microsoft.ML.CLI.Utilities
             logger.Log(LogLevel.Info, $"*************************************************");
             logger.Log(LogLevel.Info, $"*       {Strings.MetricsForBinaryClassModels}     ");
             logger.Log(LogLevel.Info, $"*------------------------------------------------");
-            logger.Log(LogLevel.Info, $"{" ",-4} {"Trainer",-35} {"Accuracy",9} {"AUC",8}");
+            logger.Log(LogLevel.Info, $"{" ",-4} {"Trainer",-35} {"Accuracy",9} {"AUC",8} {"AUPRC",8} {"F1-score",9}");
         }
 
         internal static void PrintMulticlassClassificationMetricsHeader()
