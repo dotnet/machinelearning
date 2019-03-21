a5b7a4ea467a7dd9a800d8d78b7d404c08994139
Senja Filipi <sefilipi@microsoft.com>
2019-03-20 23:08:57 -0700

    comments

--- docs/samples/Microsoft.ML.Samples/Dynamic/Transforms/Conversion/Hash.cs
+++ docs/samples/Microsoft.ML.Samples/Dynamic/Transforms/Conversion/Hash.cs
@@ -5,21 +5,13 @@ namespace Microsoft.ML.Samples.Dynamic
 {
     public static class Hash
     {
-        private class InputData
-        {
-            public string Category;
-            public uint Age;
-        }
-
-        private class TransformedData : InputData
-        {
-            public uint CategoryHashed;
-            public uint AgeHashed;
-        }
-
         public static void Example()
         {
+            // Create a new ML context, for ML.NET operations. It can be used for exception tracking and logging, 
+            // as well as the source of randomness.
             var mlContext = new MLContext(seed: 1);
+
+            // Get a small dataset as an IEnumerable.
             var rawData = new[] {
                 new InputData() { Category = "MLB" , Age = 18 },
                 new InputData() { Category = "NFL" , Age = 14 },
@@ -30,15 +22,15 @@ namespace Microsoft.ML.Samples.Dynamic
 
             var data = mlContext.Data.LoadFromEnumerable(rawData);
 
-            // Construct the pipeline.
+            // Construct the pipeline that would hash the two columns and store the results in new columns.
             var pipeline = mlContext.Transforms.Conversion.Hash("CategoryHashed", "Category", numberOfBits: 16, maximumNumberOfInverts: 2)
                           .Append(mlContext.Transforms.Conversion.Hash("AgeHashed", "Age", numberOfBits: 8));
 
-            // Let's train our pipeline, and then apply it to the same data.
+            // Let's fit our pipeline, and then apply it to the same data.
             var transformer = pipeline.Fit(data);
             var transformedData = transformer.Transform(data);
 
-            // Display original column 'Survived' (boolean) and converted column 'SurvivedInt32' (Int32)
+            // Store the post transformation from the IDataView format, to an IEnumerable<TransformedData> for easy consumption
             var convertedData = mlContext.Data.CreateEnumerable<TransformedData>(transformedData, true);
             foreach (var item in convertedData)
             {
@@ -64,10 +56,25 @@ namespace Microsoft.ML.Samples.Dynamic
             for (int i = 0; i < indices.Length; i++)
                 Console.WriteLine($"The original value of the {indices[i]} category is {categoryNames[i]}");
 
+            // Output Data
+            // 
             // The original value of the 6012 category is MLS
             // The original value of the 19014 category is NFL
             // The original value of the 36205 category is MLB
 
         }
+
+        private class InputData
+        {
+            public string Category;
+            public uint Age;
+        }
+
+        private class TransformedData : InputData
+        {
+            public uint CategoryHashed;
+            public uint AgeHashed;
+        }
+
     }
 }
