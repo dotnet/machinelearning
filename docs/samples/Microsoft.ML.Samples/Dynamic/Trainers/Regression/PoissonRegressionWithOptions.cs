b8cffee2b0ce026defd28c186ef2a630a2dabf34
Shahab Moradi <shmoradi@microsoft.com>
2019-03-22 19:48:32 -0400

    Added samples for Poisson and OGD regression (#3067)
    * Added samples for Poisson and OGD regression
    
    * Cleaned up csproj file.
    
    * Addressed PR comments

---
+++ docs/samples/Microsoft.ML.Samples/Dynamic/Trainers/Regression/PoissonRegressionWithOptions.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.ML.Data;
+using Microsoft.ML.Trainers;
+
+namespace Microsoft.ML.Samples.Dynamic.Trainers.Regression
+{
+    public static class PoissonRegressionWithOptions
+    {
+        public static void Example()
+        {
+            // Create a new context for ML.NET operations. It can be used for exception tracking and logging, 
+            // as a catalog of available operations and as the source of randomness.
+            // Setting the seed to a fixed number in this example to make outputs deterministic.
+            var mlContext = new MLContext(seed: 0);
+
+            // Create a list of training data points.
+            var dataPoints = GenerateRandomDataPoints(1000);
+
+            // Convert the list of data points to an IDataView object, which is consumable by ML.NET API.
+            var trainingData = mlContext.Data.LoadFromEnumerable(dataPoints);
+
+            // Define trainer options.
+            var options = new PoissonRegressionTrainer.Options
+            {
+                // Reduce optimization tolerance to speed up training at the cost of accuracy.
+                OptmizationTolerance = 1e-4f,
+                // Decrease history size to speed up training at the cost of accuracy.
+                HistorySize = 30,
+                // Specify scale for initial weights.
+                InitialWeightsDiameter = 0.2f
+            };
+
+            // Define the trainer.
+            var pipeline = mlContext.Regression.Trainers.PoissonRegression(options);
+
+            // Train the model.
+            var model = pipeline.Fit(trainingData);
+
+            // Create testing data. Use different random seed to make it different from training data.
+            var testData = mlContext.Data.LoadFromEnumerable(GenerateRandomDataPoints(500, seed:123));
+
+            // Run the model on test data set.
+            var transformedTestData = model.Transform(testData);
+
+            // Convert IDataView object to a list.
+            var predictions = mlContext.Data.CreateEnumerable<Prediction>(transformedTestData, reuseRowObject: false).ToList();
+
+            // Look at 5 predictions
+            foreach (var p in predictions.Take(5))
+                Console.WriteLine($"Label: {p.Label:F3}, Prediction: {p.Score:F3}");
+
+            // Expected output:
+            //   Label: 0.985, Prediction: 1.110
+            //   Label: 0.155, Prediction: 0.169
+            //   Label: 0.515, Prediction: 0.400
+            //   Label: 0.566, Prediction: 0.415
+            //   Label: 0.096, Prediction: 0.169
+
+            // Evaluate the overall metrics
+            var metrics = mlContext.Regression.Evaluate(transformedTestData);
+            SamplesUtils.ConsoleUtils.PrintMetrics(metrics);
+            
+            // Expected output:
+            //   Mean Absolute Error: 0.07
+            //   Mean Squared Error: 0.01
+            //   Root Mean Squared Error: 0.09
+            //   RSquared: 0.90
+        }
+
+        private static IEnumerable<DataPoint> GenerateRandomDataPoints(int count, int seed=0)
+        {
+            var random = new Random(seed);
+            float randomFloat() => (float)random.NextDouble();
+            for (int i = 0; i < count; i++)
+            {
+                float label = randomFloat();
+                yield return new DataPoint
+                {
+                    Label = label,
+                    // Create random features that are correlated with the label.
+                    Features = Enumerable.Repeat(label, 50).Select(x => x + randomFloat()).ToArray()
+                };
+            }
+        }
+
+        // Example with label and 50 feature values. A data set is a collection of such examples.
+        private class DataPoint
+        {
+            public float Label { get; set; }
+            [VectorType(50)]
+            public float[] Features { get; set; }
+        }
+
+        // Class used to capture predictions.
+        private class Prediction
+        {
+            // Original label.
+            public float Label { get; set; }
+            // Predicted score from the trainer.
+            public float Score { get; set; }
+        }
+    }
+}
+
