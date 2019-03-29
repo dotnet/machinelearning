d6b84e0957d63026c4016b977891b0a1016add3b
Abhishek Goswami <abgoswam@gmail.com>
2019-03-29 22:28:21 +0000

    updating equality checks for floats

--- test/Microsoft.ML.Tests/TrainerEstimators/SdcaTests.cs
+++ test/Microsoft.ML.Tests/TrainerEstimators/SdcaTests.cs
@@ -114,9 +114,10 @@ namespace Microsoft.ML.Tests.TrainerEstimators
             var modelWithoutWeights = sdcaWithoutWeightBinary.Fit(data);
             var modelWithWeights = sdcaWithWeightBinary.Fit(data);
 
-            // Verify the metrics produced are different.
             var prediction1 = modelWithoutWeights.Transform(data);
             var prediction2 = modelWithWeights.Transform(data);
+
+            // Verify the metrics produced are different.
             var metrics1 = mlContext.BinaryClassification.Evaluate(prediction1);
             var metrics2 = mlContext.BinaryClassification.Evaluate(prediction2);
             Assert.Equal(0.9658, metrics1.AreaUnderRocCurve, 4);
@@ -124,15 +125,21 @@ namespace Microsoft.ML.Tests.TrainerEstimators
             Assert.Equal(0.9596, metrics2.AreaUnderRocCurve, 4);
             Assert.Equal(0.3591, metrics2.LogLoss, 4);
 
-            // Verify the SDCA model parameters are different. 
-            // (bias).
-            var bias1 = modelWithoutWeights.Model.SubModel.Bias;
-            var bias2 = modelWithWeights.Model.SubModel.Bias;
-            Assert.NotEqual(bias1, bias2);
-            // (model weights).
-            var weights1 = modelWithoutWeights.Model.SubModel.Weights;
-            var weights2 = modelWithWeights.Model.SubModel.Weights;
-            Assert.False(weights1.SequenceEqual(weights2));
+            // Verify the raw scores are different.
+            var scores1 = prediction1.GetColumn<float>(prediction1.Schema["Score"]).ToArray();
+            var scores2 = prediction2.GetColumn<float>(prediction2.Schema["Score"]).ToArray();
+            Assert.True(scores1.Length == scores2.Length);
+
+            bool sameScores = true;
+            for (int i = 0; i < scores1.Length; i++)
+            {
+                if(!CompareNumbersWithTolerance(scores1[i], scores2[i]))
+                {
+                    sameScores = false;
+                    break;
+                }
+            }
+            Assert.False(sameScores);
         }
 
         [Fact]
@@ -164,9 +171,10 @@ namespace Microsoft.ML.Tests.TrainerEstimators
             var modelWithoutWeights = sdcaWithoutWeightMulticlass.Fit(data);
             var modelWithWeights = sdcaWithWeightMulticlass.Fit(data);
 
-            // Verify the metrics produced are different.
             var prediction1 = modelWithoutWeights.Transform(data);
             var prediction2 = modelWithWeights.Transform(data);
+
+            // Verify the metrics produced are different.
             var metrics1 = mlContext.MulticlassClassification.Evaluate(prediction1, labelColumnName: "LabelIndex", topKPredictionCount: 1);
             var metrics2 = mlContext.MulticlassClassification.Evaluate(prediction2, labelColumnName: "LabelIndex", topKPredictionCount: 1);
             Assert.Equal(0.9100, metrics1.TopKAccuracy, 4);
@@ -174,24 +182,21 @@ namespace Microsoft.ML.Tests.TrainerEstimators
             Assert.Equal(0.8800, metrics2.TopKAccuracy, 4);
             Assert.Equal(0.2464, metrics2.LogLoss, 4);
 
-            // Extract the linear model from the pipeline.
-            var sdcaModelWithoutWeights = modelWithoutWeights.LastTransformer.Model;
-            var sdcaModelWithWeights = modelWithWeights.LastTransformer.Model;
+            // Verify the raw scores are different.
+            var scores1 = prediction1.GetColumn<float[]>(prediction1.Schema["Score"]).ToArray();
+            var scores2 = prediction2.GetColumn<float[]>(prediction2.Schema["Score"]).ToArray();
+            Assert.True(scores1.Length == scores2.Length);
 
-            // Verify the SDCA model parameters are different. 
-            // (bias).
-            var bias1 = sdcaModelWithoutWeights.GetBiases();
-            var bias2 = sdcaModelWithWeights.GetBiases();
-            Assert.False(bias1.SequenceEqual(bias2));
-
-            // (model weights).
-            VBuffer<float>[] modelWeights1 = null;
-            VBuffer<float>[] modelWeights2 = null;
-            sdcaModelWithoutWeights.GetWeights(ref modelWeights1, out int c1);
-            sdcaModelWithWeights.GetWeights(ref modelWeights2, out int c2);
-            Assert.Equal(c1, c2);
-            Assert.False(modelWeights1[0].DenseValues().SequenceEqual(modelWeights2[0].DenseValues()));
-            Assert.False(modelWeights1[1].DenseValues().SequenceEqual(modelWeights2[1].DenseValues()));
+            bool sameScores = true;
+            for (int i = 0; i < scores1.Length; i++)
+            {
+                if (!CompareNumbersWithTolerance(scores1[i][0], scores2[i][0]))
+                {
+                    sameScores = false;
+                    break;
+                }
+            }
+            Assert.False(sameScores);
         }
 
         [Fact]
