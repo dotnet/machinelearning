391969f44b8c35d79561330576e95f17728357bd
Gani Nazirov <ganaziro@microsoft.com>
2019-04-02 15:23:14 -0700

    Use tt templates

--- docs/samples/Microsoft.ML.Samples/Dynamic/Trainers/MulticlassClassification/PairwiseCoupling.cs
+++ docs/samples/Microsoft.ML.Samples/Dynamic/Trainers/MulticlassClassification/PairwiseCoupling.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.ML.Data;
-using Microsoft.ML.SamplesUtils;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.ML.Data;
 
 namespace Microsoft.ML.Samples.Dynamic.Trainers.MulticlassClassification
 {
@@ -12,45 +14,87 @@ namespace Microsoft.ML.Samples.Dynamic.Trainers.MulticlassClassification
             // Setting the seed to a fixed number in this example to make outputs deterministic.
             var mlContext = new MLContext(seed: 0);
 
-            // Create a list of data examples.
-            var examples = DatasetUtils.GenerateRandomMulticlassClassificationExamples(1000);
+            // Create a list of training data points.
+            var dataPoints = GenerateRandomDataPoints(1000);
 
-            // Convert the examples list to an IDataView object, which is consumable by ML.NET API.
-            var dataView = mlContext.Data.LoadFromEnumerable(examples);
+            // Convert the list of data points to an IDataView object, which is consumable by ML.NET API.
+            var trainingData = mlContext.Data.LoadFromEnumerable(dataPoints);
 
-            //////////////////// Data Preview ////////////////////
-            // Label    Features
-            // AA       0.7262433,0.8173254,0.7680227,0.5581612,0.2060332,0.5588848,0.9060271,0.4421779,0.9775497,0.2737045
-            // BB       0.4919063,0.6673147,0.8326591,0.6695119,1.182151,0.230367,1.06237,1.195347,0.8771811,0.5145918
-            // CC       1.216908,1.248052,1.391902,0.4326252,1.099942,0.9262842,1.334019,1.08762,0.9468155,0.4811099
-            // DD       0.7871246,1.053327,0.8971719,1.588544,1.242697,1.362964,0.6303943,0.9810045,0.9431419,1.557455
-
-            // Create a pipeline. 
+            // Define the trainer.
             var pipeline =
                     // Convert the string labels into key types.
                     mlContext.Transforms.Conversion.MapValueToKey("Label")
-                    // Apply Pairwise Coupling multiclass trainer on top of SDCA Logistic Regression binary trainer.
+                    // Apply PairwiseCoupling multiclass meta trainer on top of binary trainer.
                     .Append(mlContext.MulticlassClassification.Trainers.PairwiseCoupling(mlContext.BinaryClassification.Trainers.SdcaLogisticRegression()));
 
-            // Split the data into training and test sets. Only training set is used in fitting
-            // the created pipeline. Metrics are computed on the test.
-            var split = mlContext.Data.TrainTestSplit(dataView, testFraction: 0.1);
-
             // Train the model.
-            var model = pipeline.Fit(split.TrainSet);
+            var model = pipeline.Fit(trainingData);
 
-            // Do prediction on the test set.
-            var dataWithPredictions = model.Transform(split.TestSet);
+            // Create testing data. Use different random seed to make it different from training data.
+            var testData = mlContext.Data.LoadFromEnumerable(GenerateRandomDataPoints(500, seed:123));
 
-            // Evaluate the trained model using the test set.
-            var metrics = mlContext.MulticlassClassification.Evaluate(dataWithPredictions);
-            SamplesUtils.ConsoleUtils.PrintMetrics(metrics);
+            // Run the model on test data set.
+            var transformedTestData = model.Transform(testData);
+
+            // Convert IDataView object to a list.
+            var predictions = mlContext.Data.CreateEnumerable<Prediction>(transformedTestData, reuseRowObject: false).ToList();
+
+            // Look at 5 predictions
+            foreach (var p in predictions.Take(5))
+                Console.WriteLine($"Label: {p.Label}, Prediction: {p.PredictedLabel}");
 
             // Expected output:
-            //   Micro Accuracy: 0.75
-            //   Macro Accuracy: 0.73
-            //   Log Loss: 0.70
-            //   Log Loss Reduction: 0.49
+            //   Label: 1, Prediction: 1
+            //   Label: 2, Prediction: 2
+            //   Label: 3, Prediction: 2
+            //   Label: 2, Prediction: 2
+            //   Label: 3, Prediction: 2
+            
+            // Evaluate the overall metrics
+            var metrics = mlContext.MulticlassClassification.Evaluate(transformedTestData);
+            SamplesUtils.ConsoleUtils.PrintMetrics(metrics);
+            
+            // Expected output:
+            // Expected output:
+            // Micro Accuracy: 0.90
+            // Macro Accuracy: 0.90
+            // Log Loss: 0.37
+            // Log Loss Reduction: 0.67
+        }
+
+        private static IEnumerable<DataPoint> GenerateRandomDataPoints(int count, int seed=0)
+        {
+            var random = new Random(seed);
+            float randomFloat() => (float)random.NextDouble();
+            for (int i = 0; i < count; i++)
+            {
+                // Generate Labels that are integers 0, 1 or 2
+                var label = random.Next(3);
+                yield return new DataPoint
+                {
+                    Label = (uint)label,
+                    // Create random features that are correlated with the label.
+                    // The feature values are slightly increased by adding a constant multiple of label.
+                    Features = Enumerable.Repeat(label, 20).Select(x => randomFloat() + label * 0.2f).ToArray()
+                };
+            }
+        }
+
+        // Example with label and 50 feature values. A data set is a collection of such examples.
+        private class DataPoint
+        {
+            public uint Label { get; set; }
+            [VectorType(20)]
+            public float[] Features { get; set; }
+        }
+
+        // Class used to capture predictions.
+        private class Prediction
+        {
+            // Original label.
+            public uint Label { get; set; }
+            // Predicted label from the trainer.
+            public uint PredictedLabel { get; set; }
         }
     }
 }
