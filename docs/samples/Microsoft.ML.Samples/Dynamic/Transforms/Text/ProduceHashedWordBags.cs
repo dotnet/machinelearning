d288792d5fc74f3f6ac657846d096a591ef867b2
Zeeshan Ahmed <zeahmed@microsoft.com>
2019-04-03 16:21:54 -0700

    Addressed reviewers' comments.

--- docs/samples/Microsoft.ML.Samples/Dynamic/Transforms/Text/ProduceHashedWordBags.cs
+++ docs/samples/Microsoft.ML.Samples/Dynamic/Transforms/Text/ProduceHashedWordBags.cs
@@ -32,10 +32,14 @@ namespace Microsoft.ML.Samples.Dynamic
             // The following call to 'ProduceHashedWordBags' implicitly tokenizes the text/string into words/tokens.
             // Please note that the length of the output feature vector depends on the 'numberOfBits' settings.
             var textPipeline = mlContext.Transforms.Text.ProduceHashedWordBags("BagOfWordFeatures", "Text",
-                numberOfBits: 5, ngramLength: 3, useAllLengths: false);
+                numberOfBits: 5,
+                ngramLength: 3,
+                useAllLengths: false,
+                maximumNumberOfInverts: 1);
 
             // Fit to data.
             var textTransformer = textPipeline.Fit(dataview);
+            var transformedDataView = textTransformer.Transform(dataview);
 
             // Create the prediction engine to get the bag-of-word features extracted from the text.
             var predictionEngine = mlContext.Model.CreatePredictionEngine<TextData, TransformedTextData>(textTransformer);
@@ -46,6 +50,21 @@ namespace Microsoft.ML.Samples.Dynamic
             // Print the length of the feature vector.
             Console.WriteLine($"Number of Features: {prediction.BagOfWordFeatures.Length}");
 
+            // Preview of the produced n-grams.
+            // Get the slot names from the column's metadata.
+            // If the column is a vector column the slot names corresponds to the names associated with each position in the vector.
+            VBuffer<ReadOnlyMemory<char>> slotNames = default;
+            transformedDataView.Schema["BagOfWordFeatures"].GetSlotNames(ref slotNames);
+            var BagOfWordFeaturesColumn = transformedDataView.GetColumn<VBuffer<float>>(transformedDataView.Schema["BagOfWordFeatures"]);
+            var slots = slotNames.GetValues();
+            Console.Write("N-grams: ");
+            foreach (var featureRow in BagOfWordFeaturesColumn)
+            {
+                foreach (var item in featureRow.Items())
+                    Console.Write($"{slots[item.Key]}  ");
+                Console.WriteLine();
+            }
+
             // Print the first 10 feature values.
             Console.Write("Features: ");
             for (int i = 0; i < 10; i++)
@@ -53,7 +72,8 @@ namespace Microsoft.ML.Samples.Dynamic
 
             //  Expected output:
             //   Number of Features: 32
-            //   Features:  0.0000  0.0000  0.0000  0.0000  0.0000  0.0000  1.0000  2.0000  0.0000  0.0000
+            //   N-grams:  an|example|to  is|an|example  example|to|compute  This|is|an  compute|bag-of-word|features  bag-of-word|features|using  to|compute|bag-of-word  ML.NET's|ProduceHashedWordBags|API  as|an|index  API|produces|count  ...
+            //   Features:     0.0000        0.0000            0.0000           0.0000              0.0000                          0.0000               1.0000                         2.0000                   0.0000          0.0000         ...
         }
 
         public class TextData
