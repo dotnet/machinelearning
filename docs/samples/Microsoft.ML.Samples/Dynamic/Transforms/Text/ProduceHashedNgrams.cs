91eed9c1f34509e7c672af7e9c6f7af00b3194eb
Zeeshan Ahmed <zeahmed@microsoft.com>
2019-04-03 16:16:11 -0700

    Addressed reviewers' comments.

--- docs/samples/Microsoft.ML.Samples/Dynamic/Transforms/Text/ProduceHashedNgrams.cs
+++ docs/samples/Microsoft.ML.Samples/Dynamic/Transforms/Text/ProduceHashedNgrams.cs
@@ -16,17 +16,17 @@ namespace Microsoft.ML.Samples.Dynamic
             // Create a small dataset as an IEnumerable.
             var samples = new List<TextData>()
             {
-                new TextData(){ Text = "This is an example to compute Ngrams using hashing." },
-                new TextData(){ Text = "Ngram is a sequence of 'N' consecutive words/tokens." },
-                new TextData(){ Text = "ML.NET's ProduceHashedNgrams API produces count of Ngrams and hashes it as an index into a vector of given bit length." },
+                new TextData(){ Text = "This is an example to compute n-grams using hashing." },
+                new TextData(){ Text = "N-gram is a sequence of 'N' consecutive words/tokens." },
+                new TextData(){ Text = "ML.NET's ProduceHashedNgrams API produces count of n-grams and hashes it as an index into a vector of given bit length." },
                 new TextData(){ Text = "The hashing reduces the size of the output feature vector" },
-                new TextData(){ Text = "which is useful in case when number of Ngrams is very large." },
+                new TextData(){ Text = "which is useful in case when number of n-grams is very large." },
             };
 
             // Convert training data to IDataView.
             var dataview = mlContext.Data.LoadFromEnumerable(samples);
 
-            // A pipeline for converting text into numeric hashed Ngram features.
+            // A pipeline for converting text into numeric hashed n-gram features.
             // The following call to 'ProduceHashedNgrams' requires the tokenized text/string as input.
             // This is acheived by calling 'TokenizeIntoWords' first followed by 'ProduceHashedNgrams'.
             // Please note that the length of the output feature vector depends on the 'numberOfBits' settings.
@@ -51,14 +51,14 @@ namespace Microsoft.ML.Samples.Dynamic
             // Print the length of the feature vector.
             Console.WriteLine($"Number of Features: {prediction.NgramFeatures.Length}");
 
-            // Preview of the produced Ngrams.
+            // Preview of the produced n-grams.
             // Get the slot names from the column's metadata.
             // If the column is a vector column the slot names corresponds to the names associated with each position in the vector.
             VBuffer<ReadOnlyMemory<char>> slotNames = default;
             transformedDataView.Schema["NgramFeatures"].GetSlotNames(ref slotNames);
             var NgramFeaturesColumn = transformedDataView.GetColumn<VBuffer<float>>(transformedDataView.Schema["NgramFeatures"]);
             var slots = slotNames.GetValues();
-            Console.Write("Ngrams: ");
+            Console.Write("N-grams: ");
             foreach (var featureRow in NgramFeaturesColumn)
             {
                 foreach (var item in featureRow.Items())
@@ -73,8 +73,8 @@ namespace Microsoft.ML.Samples.Dynamic
 
             //  Expected output:
             //   Number of Features:  32
-            //   Ngrams:   This|is|an  example|to|compute  compute|Ngrams|using  Ngrams|using|hashing.  an|example|to  is|an|example  a|sequence|of  of|'N'|consecutive  is|a|sequence  Ngram|is|a  ...
-            //   Features:   0.0000          0.0000               2.0000               0.0000               0.0000        1.0000          0.0000        0.0000              1.0000          0.0000  ...
+            //   N-grams:   This|is|an  example|to|compute  compute|n-grams|using  n-grams|using|hashing.  an|example|to  is|an|example  a|sequence|of  of|'N'|consecutive  is|a|sequence  N-gram|is|a  ...
+            //   Features:    0.0000          0.0000               2.0000               0.0000               0.0000        1.0000          0.0000        0.0000              1.0000          0.0000  ...
         }
 
         public class TextData
--- docs/samples/Microsoft.ML.Samples/Dynamic/Transforms/Text/ProduceNgrams.cs
+++ docs/samples/Microsoft.ML.Samples/Dynamic/Transforms/Text/ProduceNgrams.cs
@@ -16,23 +16,23 @@ namespace Microsoft.ML.Samples.Dynamic
             // Create a small dataset as an IEnumerable.
             var samples = new List<TextData>()
             {
-                new TextData(){ Text = "This is an example to compute Ngrams." },
-                new TextData(){ Text = "Ngram is a sequence of 'N' consecutive words/tokens." },
-                new TextData(){ Text = "ML.NET's ProduceNgrams API produces vector of Ngrams." },
-                new TextData(){ Text = "Each position in the vector corresponds to a particular Ngram." },
+                new TextData(){ Text = "This is an example to compute n-grams." },
+                new TextData(){ Text = "N-gram is a sequence of 'N' consecutive words/tokens." },
+                new TextData(){ Text = "ML.NET's ProduceNgrams API produces vector of n-grams." },
+                new TextData(){ Text = "Each position in the vector corresponds to a particular n-gram." },
                 new TextData(){ Text = "The value at each position corresponds to," },
-                new TextData(){ Text = "the number of times Ngram occured in the data (Tf), or" },
-                new TextData(){ Text = "the inverse of the number of documents that contain the Ngram (Idf)," },
+                new TextData(){ Text = "the number of times n-gram occured in the data (Tf), or" },
+                new TextData(){ Text = "the inverse of the number of documents that contain the n-gram (Idf)," },
                 new TextData(){ Text = "or compute both and multiply together (Tf-Idf)." },
             };
 
             // Convert training data to IDataView.
             var dataview = mlContext.Data.LoadFromEnumerable(samples);
 
-            // A pipeline for converting text into numeric Ngram features.
+            // A pipeline for converting text into numeric n-gram features.
             // The following call to 'ProduceNgrams' requires the tokenized text/string as input.
             // This is acheived by calling 'TokenizeIntoWords' first followed by 'ProduceNgrams'.
-            // Please note that the length of the output feature vector depends on the Ngram settings.
+            // Please note that the length of the output feature vector depends on the n-gram settings.
             var textPipeline = mlContext.Transforms.Text.TokenizeIntoWords("Tokens", "Text")
                 // 'ProduceNgrams' takes key type as input. Converting the tokens into key type using 'MapValueToKey'.
                 .Append(mlContext.Transforms.Conversion.MapValueToKey("Tokens"))
@@ -45,7 +45,7 @@ namespace Microsoft.ML.Samples.Dynamic
             var textTransformer = textPipeline.Fit(dataview);
             var transformedDataView = textTransformer.Transform(dataview);
 
-            // Create the prediction engine to get the Ngram features extracted from the text.
+            // Create the prediction engine to get the n-gram features extracted from the text.
             var predictionEngine = mlContext.Model.CreatePredictionEngine<TextData, TransformedTextData>(textTransformer);
 
             // Convert the text into numeric features.
@@ -54,14 +54,14 @@ namespace Microsoft.ML.Samples.Dynamic
             // Print the length of the feature vector.
             Console.WriteLine($"Number of Features: {prediction.NgramFeatures.Length}");
 
-            // Preview of the produced Ngrams.
+            // Preview of the produced n-grams.
             // Get the slot names from the column's metadata.
             // If the column is a vector column the slot names corresponds to the names associated with each position in the vector.
             VBuffer<ReadOnlyMemory<char>> slotNames = default;
             transformedDataView.Schema["NgramFeatures"].GetSlotNames(ref slotNames);
             var NgramFeaturesColumn = transformedDataView.GetColumn<VBuffer<float>>(transformedDataView.Schema["NgramFeatures"]);
             var slots = slotNames.GetValues();
-            Console.Write("Ngrams: ");
+            Console.Write("N-grams: ");
             foreach (var featureRow in NgramFeaturesColumn)
             {
                 foreach (var item in featureRow.Items())
@@ -76,8 +76,8 @@ namespace Microsoft.ML.Samples.Dynamic
 
             //  Expected output:
             //   Number of Features: 52
-            //   Ngrams:   This|is|an  is|an|example  an|example|to  example|to|compute  to|compute|Ngrams.  Ngram|is|a  is|a|sequence  a|sequence|of  sequence|of|'N'  of|'N'|consecutive  ...
-            //   Features:    1.0000      1.0000          1.0000           1.0000             1.0000            0.0000      0.0000          0.0000          0.0000          0.0000          ...
+            //   N-grams:   This|is|an  is|an|example  an|example|to  example|to|compute  to|compute|n-grams.  N-gram|is|a  is|a|sequence  a|sequence|of  sequence|of|'N'  of|'N'|consecutive  ...
+            //   Features:     1.0000      1.0000          1.0000           1.0000             1.0000            0.0000      0.0000          0.0000          0.0000          0.0000          ...
         }
 
         public class TextData
