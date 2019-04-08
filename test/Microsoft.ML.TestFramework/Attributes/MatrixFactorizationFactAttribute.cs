2d1afe1bf6e850f4f3bb642fb546e83ff115f084
Wei-Sheng Chin <wechi@microsoft.com>
2019-04-08 10:10:31 -0700

    Remove old message

--- test/Microsoft.ML.TestFramework/Attributes/MatrixFactorizationFactAttribute.cs
+++ test/Microsoft.ML.TestFramework/Attributes/MatrixFactorizationFactAttribute.cs
@@ -10,7 +10,7 @@ namespace Microsoft.ML.TestFramework.Attributes
     /// </summary>
     public sealed class MatrixFactorizationFactAttribute : EnvironmentSpecificFactAttribute
     {
-        public MatrixFactorizationFactAttribute() : base("Disabled - this test is being fixed as part of https://github.com/dotnet/machinelearning/issues/1441")
+        public MatrixFactorizationFactAttribute() : base("")
         {
         }
 
