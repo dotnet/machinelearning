5d2da69a3447b2730aa12aadf3f22f44c6bc8b93
Ivan Matantsev <ivmatan@microsoft.com>
2019-04-09 16:06:02 -0700

    revert program.cs

--- docs/samples/Microsoft.ML.Samples/Program.cs
+++ docs/samples/Microsoft.ML.Samples/Program.cs
@@ -6,7 +6,7 @@ namespace Microsoft.ML.Samples
     {
         static void Main(string[] args)
         {
-            NormalizeSupervisedBinning.Example();
+            ReplaceMissingValues.Example();
         }
     }
 }
