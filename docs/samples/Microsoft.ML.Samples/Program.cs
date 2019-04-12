d6edf806a42a6d9a7e508b134022894fb555da64
Zeeshan Siddiqui <mzs@microsoft.com>
2019-04-12 15:32:42 -0700

    PR feedback.

--- docs/samples/Microsoft.ML.Samples/Program.cs
+++ docs/samples/Microsoft.ML.Samples/Program.cs
@@ -1,43 +1,15 @@
-﻿using System;
-using System.Reflection;
-using Samples.Dynamic;
+﻿using System.Reflection;
 
 namespace Microsoft.ML.Samples
 {
     public static class Program
     {
-        public static void Main(string[] args)
-        {
-            CalculateFeatureContribution.Example();
-
-            if (args.Length > 0 && args[0] == "-runall")
-                RunAll();
-        }
+        public static void Main(string[] args) => RunAll();
 
         internal static void RunAll()
         {
             foreach (var type in Assembly.GetExecutingAssembly().GetTypes())
-            {
-                var method = type.GetMethod("Example", BindingFlags.NonPublic | BindingFlags.Public
-                    | BindingFlags.Static | BindingFlags.FlattenHierarchy);
-
-                if (method != null)
-                {
-                    try
-                    {
-                        method.Invoke(null, null);
-                    }
-                    catch (Exception ex)
-                    {
-                        // Print the exception for debugging.
-                        Console.Write(type);
-                        Console.Write(ex);
-
-                        // Throw to fail.
-                        throw ex;
-                    }
-                }
-            }
+                type.GetMethod("Example", BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)?.Invoke(null, null);
         }
     }
 }
