a2687a2017dff3f6e57a4604341e66e80d72882d
Artidoro Pagnoni <arpagnon@microsoft.com>
2019-04-18 17:58:40 -0700

    XML doc for transforms

--- src/Microsoft.ML.Transforms/CustomMappingCatalog.cs
+++ src/Microsoft.ML.Transforms/CustomMappingCatalog.cs
@@ -4,6 +4,7 @@
 
 using System;
 using Microsoft.ML.Data;
+using Microsoft.ML.Runtime;
 using Microsoft.ML.Transforms;
 
 namespace Microsoft.ML
@@ -14,15 +15,23 @@ namespace Microsoft.ML
     public static class CustomMappingCatalog
     {
         /// <summary>
-        /// Create a custom mapping of input columns to output columns.
+        /// Create a <see cref="CustomMappingEstimator{TSrc, TDst}"/>, which applies a custom mapping of input columns to output columns.
         /// </summary>
         /// <typeparam name="TSrc">The class defining which columns to take from the incoming data.</typeparam>
         /// <typeparam name="TDst">The class defining which new columns are added to the data.</typeparam>
         /// <param name="catalog">The transform catalog</param>
-        /// <param name="mapAction">The mapping action. This must be thread-safe and free from side effects.</param>
-        /// <param name="contractName">The contract name, used by ML.NET for loading the model. If <c>null</c> is specified, such a trained model would not be save-able.</param>
-        /// <param name="inputSchemaDefinition">Additional parameters for schema mapping between <typeparamref name="TSrc"/> and input data.</param>
-        /// <param name="outputSchemaDefinition">Additional parameters for schema mapping between <typeparamref name="TDst"/> and output data.</param>
+        /// <param name="mapAction">The mapping action. This must be thread-safe and free from side effects.
+        /// If the resulting transformer needs to be save-able, the class defining <paramref name="mapAction"/> should implement
+        /// <see cref="CustomMappingFactory{TSrc, TDst}"/> and needs to be decorated with
+        /// <see cref="CustomMappingFactoryAttributeAttribute"/> with the provided <paramref name="contractName"/>.
+        /// The assembly containing the class should be registered in the environment where it is loaded back
+        /// using <see cref="ComponentCatalog.RegisterAssembly(System.Reflection.Assembly, bool)"/>.</param>
+        /// <param name="contractName">The contract name, used by ML.NET for loading the model.
+        /// If <see langword="null"/> is specified, resulting transformer would not be save-able.</param>
+        /// <param name="inputSchemaDefinition">Additional parameters for schema mapping between <typeparamref name="TSrc"/> and input data.
+        /// Useful when dealing with annotations.</param>
+        /// <param name="outputSchemaDefinition">Additional parameters for schema mapping between <typeparamref name="TDst"/> and output data.
+        /// Useful when dealing with annotations.</param>
         /// <example>
         /// <format type="text/markdown">
         /// <![CDATA[
