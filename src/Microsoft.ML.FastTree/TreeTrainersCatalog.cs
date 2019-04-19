4b106f1d1cdea460e6ab3ba39a38e33d023a6e91
Zeeshan Siddiqui <mzs@microsoft.com>
2019-04-19 11:49:10 -0700

    PR feedback.

--- src/Microsoft.ML.FastTree/TreeTrainersCatalog.cs
+++ src/Microsoft.ML.FastTree/TreeTrainersCatalog.cs
@@ -388,7 +388,7 @@ namespace Microsoft.ML
         /// </summary>
         /// <param name="catalog">The <see cref="BinaryClassificationCatalog"/>.</param>
         /// <param name="labelColumnName">The name of the label column. The column data must be <see cref="System.Boolean"/>.</param>
-        /// <param name="featureColumnName">The name of the feature column.  The column data must be a known-sized vector of <see cref="System.Single"/>.</param>
+        /// <param name="featureColumnName">The name of the feature column. The column data must be a known-sized vector of <see cref="System.Single"/>.</param>
         /// <param name="exampleWeightColumnName">The name of the example weight column (optional).</param>
         /// <param name="numberOfTrees">Total number of decision trees to create in the ensemble.</param>
         /// <param name="numberOfLeaves">The maximum number of leaves per decision tree.</param>
@@ -414,7 +414,7 @@ namespace Microsoft.ML
         }
 
         /// <summary>
-        /// Creates <see cref="FastForestBinaryTrainer"/>, which predicts a target using a decision tree regression model with advanced options.
+        /// Create <see cref="FastForestBinaryTrainer"/> with <see cref="FastForestBinaryTrainer.Options"/>, which predicts a target using a decision tree regression model.
         /// </summary>
         /// <param name="catalog">The <see cref="BinaryClassificationCatalog"/>.</param>
         /// <param name="options">Trainer options.</param>
