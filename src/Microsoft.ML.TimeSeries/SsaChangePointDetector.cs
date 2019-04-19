cc0edfa54a8a562362bb36e2c6ec00fbcb283176
Wei-Sheng Chin <wechi@microsoft.com>
2019-04-19 15:31:58 -0700

    Polishment

--- src/Microsoft.ML.TimeSeries/SsaChangePointDetector.cs
+++ src/Microsoft.ML.TimeSeries/SsaChangePointDetector.cs
@@ -228,24 +228,7 @@ namespace Microsoft.ML.Transforms.TimeSeries
     /// In principle, SSA performs spectral analysis on the input time-series where each component in the spectrum corresponds to a trend, seasonal or noise component in the time-series.
     /// For details of the Singular Spectrum Analysis (SSA), refer to [this document](http://arxiv.org/pdf/1206.6910.pdf).
     ///
-    /// ### Anomaly Scorer
-    /// Once the raw score at a timestamp is computed by SSA, it is fed to the anomaly scorer component to calculate the final anomaly score at that timestamp.
-    /// There are two statistics involved in this scorer, p-value and martingale score.
-    ///
-    /// The p-value score indicates the p-value of the current computed raw score according to a distribution of raw scores.
-    /// Here, the distribution is estimated based on the most recent raw score values up to certain depth back in the history.
-    /// More specifically, this distribution is estimated using [kernel density estimation](https://en.wikipedia.org/wiki/Kernel_density_estimation) with the Gaussian [kernels](https://en.wikipedia.org/wiki/Kernel_(statistics)#In_non-parametric_statistics) of adaptive bandwidth.
-    /// The p-value score is always in $[0, 1]$, and the lower its value, the more likely the current point is an outlier.
-    ///
-    /// The martingale score is an extra level of scoring that is built upon the p-value scores.
-    /// The idea is based on the [Exchangeability Martingales](https://arxiv.org/pdf/1204.3251.pdf) that detect a change of distribution over a stream of i.i.d. values.
-    /// In short, the value of the martingale score starts increasing significantly when a sequence of small p-values detected in a row; this indicates the change of the distribution of the underlying data generation process.
-    /// For this very reason, the martingale score is used for change point detection.
-    /// Given a sequence of most recently observed p-values, $p1, \dots, p_n$, the martingale score is computed as:​ $s(p1, \dots, p_n) = \prod_{i=1}^n  \beta(p_i)$.
-    /// There are two choices of $\beta$: $\beta(p) = e p^{\epsilon - 1}$ for $0 < \epsilon < 1$ or $\beta(p) = \int_{0}^1 \epsilon p^{\epsilon - 1} d\epsilon$.
-    ///
-    /// If the martingle score exceeds $s(q_1, \dots, q_n)$ where $q_i=1 - \frac{\text{confidence}}{100}$, the associated timestamp may get a non-zero alert value.
-    /// Note that $\text{confidence}$ is defined in the signature of [DetectChangePointBySsa](xref:Microsoft.ML.TimeSeriesCatalog.DetectChangePointBySsa(Microsoft.ML.TransformsCatalog,System.String,System.String,System.Int32,System.Int32,System.Int32,System.Int32,Microsoft.ML.Transforms.TimeSeries.ErrorFunction,Microsoft.ML.Transforms.TimeSeries.MartingaleType,System.Double)).
+    /// [!include[io](~/../docs/samples/docs/api-reference/time-series-scorer.md)]
     /// ]]>
     /// </format>
     /// </remarks>
