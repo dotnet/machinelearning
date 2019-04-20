d3a5555128c75c48c25ba6f982e05f8cc88fc4fc
Wei-Sheng Chin <wschin@outlook.com>
2019-04-19 23:55:33 -0700

    FIx

--- src/Microsoft.ML.StandardTrainers/Standard/SdcaMulticlass.cs
+++ src/Microsoft.ML.StandardTrainers/Standard/SdcaMulticlass.cs
@@ -38,11 +38,6 @@ namespace Microsoft.ML.Trainers
     ///
     /// [!include[io](~/../docs/samples/docs/api-reference/io-columns-multiclass-classification.md)]
     ///
-    /// | Output Column Name | Column Type | Description|
-    /// | -- | -- | -- |
-    /// | `Score` | array of<xref:System.Single> | The scores of all classes. Higher value means higher probability to fall into the associated class. If the i-th element has the largest value, the predicted label index would be i. Note that i is zero-based index. |
-    /// | `PredictedLabel` | <xref:System.UInt32> | The predicted label's index. If it's value is i, the actual label would be the i-th category in the key-valued input label type. Note that i is zero-based index. | 
-    ///
     /// ### Trainer Characteristics
     /// |  |  |
     /// | -- | -- |
