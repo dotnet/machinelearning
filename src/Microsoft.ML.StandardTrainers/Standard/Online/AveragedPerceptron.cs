668bb83783ed5a9a54765426bf22d52d0d0583b9
Yael Dekel <yaeld@microsoft.com>
2019-04-23 12:47:18 -0700

    Fix #3471

--- src/Microsoft.ML.StandardTrainers/Standard/Online/AveragedPerceptron.cs
+++ src/Microsoft.ML.StandardTrainers/Standard/Online/AveragedPerceptron.cs
@@ -42,7 +42,7 @@ namespace Microsoft.ML.Trainers
     ///
     /// ### Training Algorithm Details
     /// The perceptron is a classification algorithm that makes its predictions by finding a separating hyperplane.
-    /// For instance, with feature values $f0, f1,..., f_{D-1}$, the prediction is given by determining what side of the hyperplane the point falls into.
+    /// For instance, with feature values $f_0, f_1,..., f_{D-1}$, the prediction is given by determining what side of the hyperplane the point falls into.
     /// That is the same as the sign of the feautures' weighted sum, i.e. $\sum_{i = 0}^{D-1} (w_i * f_i) + b$, where $w_0, w_1,..., w_{D-1}$
     /// are the weights computed by the algorithm, and $b$ is the bias computed by the algorithm.
     ///
