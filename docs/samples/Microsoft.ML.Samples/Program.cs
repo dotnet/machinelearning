7a3d9bee2d770f89df454798c0f202c040474ba4
Artidoro Pagnoni <arpagnon@microsoft.com>
2019-04-24 20:08:28 -0700

    confusion matrix for multiclass samples

--- docs/samples/Microsoft.ML.Samples/Program.cs
+++ docs/samples/Microsoft.ML.Samples/Program.cs
@@ -1,11 +1,37 @@
 ﻿using System;
 using System.Reflection;
+using Samples.Dynamic.Trainers.MulticlassClassification;
 
 namespace Microsoft.ML.Samples
 {
     public static class Program
     {
-        public static void Main(string[] args) => RunAll();
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(nameof(LbfgsMaximumEntropy));
+            LbfgsMaximumEntropy.Example();
+            Console.WriteLine(nameof(LbfgsMaximumEntropyWithOptions));
+            LbfgsMaximumEntropyWithOptions.Example();
+            Console.WriteLine(nameof(LightGbm));
+            LightGbm.Example();
+            Console.WriteLine(nameof(LightGbmWithOptions));
+            LightGbmWithOptions.Example();
+            Console.WriteLine(nameof(NaiveBayes));
+            NaiveBayes.Example();
+            Console.WriteLine(nameof(OneVersusAll));
+            OneVersusAll.Example();
+            Console.WriteLine(nameof(PairwiseCoupling));
+            PairwiseCoupling.Example();
+            Console.WriteLine(nameof(SdcaMaximumEntropy));
+            SdcaMaximumEntropy.Example();
+            Console.WriteLine(nameof(SdcaMaximumEntropyWithOptions));
+            SdcaMaximumEntropyWithOptions.Example();
+            Console.WriteLine(nameof(SdcaNonCalibrated));
+            SdcaNonCalibrated.Example();
+            Console.WriteLine(nameof(SdcaNonCalibratedWithOptions));
+            SdcaNonCalibratedWithOptions.Example();
+
+        }
 
         internal static void RunAll()
         {
