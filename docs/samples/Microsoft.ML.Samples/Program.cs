1b2be0f1cacd47e3da01850bdd54b34b966e88b7
Zeeshan Siddiqui <mzs@microsoft.com>
2019-05-13 13:45:18 -0700

    cleanup.

--- docs/samples/Microsoft.ML.Samples/Program.cs
+++ docs/samples/Microsoft.ML.Samples/Program.cs
@@ -10,7 +10,7 @@ namespace Microsoft.ML.Samples
 
         internal static void RunAll()
         {
-            /*int samples = 0;
+            int samples = 0;
             foreach (var type in Assembly.GetExecutingAssembly().GetTypes())
             {
                 var sample = type.GetMethod("Example", BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy);
@@ -23,8 +23,7 @@ namespace Microsoft.ML.Samples
                 }
             }
 
-            Console.WriteLine("Number of samples that ran without any exception: " + samples);*/
-            ProduceWordBags.Example();
+            Console.WriteLine("Number of samples that ran without any exception: " + samples);
         }
     }
 }
