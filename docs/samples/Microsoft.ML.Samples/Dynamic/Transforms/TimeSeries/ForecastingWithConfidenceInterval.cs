139447751cde06598de2c2cbe741cc720082a086
Zeeshan Siddiqui <mzs@microsoft.com>
2019-05-31 17:31:21 -0700

    Samples.

--- docs/samples/Microsoft.ML.Samples/Dynamic/Transforms/TimeSeries/ForecastingWithConfidenceInterval.cs
+++ docs/samples/Microsoft.ML.Samples/Dynamic/Transforms/TimeSeries/ForecastingWithConfidenceInterval.cs
@@ -6,7 +6,7 @@ using Microsoft.ML.TimeSeries;
 
 namespace Samples.Dynamic
 {
-    public static class Forecasting
+    public static class ForecastingWithConfidenceInternal
     {
         // This example creates a time series (list of Data with the i-th element corresponding to the i-th time slot) and then
         // does forecasting.
@@ -45,19 +45,28 @@ namespace Samples.Dynamic
             // Setup arguments.
             var inputColumnName = nameof(TimeSeriesData.Value);
 
-            // Train the forecasting detector.
+            // Train the forecasting model.
             var model = ml.Forecasting.AdaptiveSingularSpectrumSequenceModeler(inputColumnName, data.Count, SeasonalitySize + 1, SeasonalitySize,
-                1, AdaptiveSingularSpectrumSequenceModeler.RankSelectionMethod.Exact, null, SeasonalitySize / 2, false, false);
+                1, AdaptiveSingularSpectrumSequenceModeler.RankSelectionMethod.Exact, null, SeasonalitySize / 2, shouldComputeForecastIntervals: true, false);
 
             // Train.
             model.Train(dataView);
 
-            // Forecast next five values.
-            var forecast = model.Forecast(5);
+            // Forecast next five values with confidence internal.
+            float[] forecast;
+            float[] confidenceIntervalLowerBounds;
+            float[] confidenceIntervalUpperBounds;
+            model.ForecastWithConfidenceIntervals(5, out forecast, out confidenceIntervalLowerBounds, out confidenceIntervalUpperBounds);
             Console.WriteLine($"Forecasted values:");
             Console.WriteLine("[{0}]", string.Join(", ", forecast));
+            Console.WriteLine($"Confidence intervals:");
+            for(int index = 0; index < 5; index++)
+                Console.Write($"[{0} - {1}] ", confidenceIntervalLowerBounds[index], confidenceIntervalUpperBounds[index]);
+
             // Forecasted values:
             // [2.452744, 2.589339, 2.729183, 2.873005, 3.028931]
+            // Confidence intervals:
+            // [0 - 1] [0 - 1] [0 - 1] [0 - 1] [0 - 1]
 
             // Update with new observations.
             dataView = ml.Data.LoadFromEnumerable(new List<TimeSeriesData>() { new TimeSeriesData(0), new TimeSeriesData(0), new TimeSeriesData(0), new TimeSeriesData(0) });
@@ -70,15 +79,33 @@ namespace Samples.Dynamic
             var modelCopy = ml.Model.LoadForecastingModel<float>("model.zip");
 
             // Forecast with the checkpointed model loaded from disk.
-            var forecastCopy = modelCopy.Forecast(5);
+            float[] forecastCopy;
+            float[] confidenceIntervalLowerBoundsCopy;
+            float[] confidenceIntervalUpperBoundsCopy;
+            modelCopy.ForecastWithConfidenceIntervals(5, out forecastCopy, out confidenceIntervalLowerBoundsCopy, out confidenceIntervalUpperBoundsCopy);
+            Console.WriteLine($"Forecasted values:");
             Console.WriteLine("[{0}]", string.Join(", ", forecastCopy));
+            Console.WriteLine($"Confidence intervals:");
+            for (int index = 0; index < 5; index++)
+                Console.Write($"[{0} - {1}] ", confidenceIntervalLowerBoundsCopy[index], confidenceIntervalUpperBoundsCopy[index]);
+
+            // Forecasted values:
             // [0.8681176, 0.8185108, 0.8069275, 0.84405, 0.9455081]
+            // Confidence intervals:
+            // [0 - 1] [0 - 1] [0 - 1] [0 - 1] [0 - 1]
 
             // Forecast with the original model(that was checkpointed to disk).
-            forecast = model.Forecast(5);
+            model.ForecastWithConfidenceIntervals(5, out forecast, out confidenceIntervalLowerBounds, out confidenceIntervalUpperBounds);
+            Console.WriteLine($"Forecasted values:");
             Console.WriteLine("[{0}]", string.Join(", ", forecast));
-            // [0.8681176, 0.8185108, 0.8069275, 0.84405, 0.9455081]
+            Console.WriteLine($"Confidence intervals:");
+            for (int index = 0; index < 5; index++)
+                Console.Write($"[{0} - {1}] ", confidenceIntervalLowerBounds[index], confidenceIntervalUpperBounds[index]);
 
+            // Forecasted values:
+            // [0.8681176, 0.8185108, 0.8069275, 0.84405, 0.9455081]
+            // Confidence intervals:
+            // [0 - 1] [0 - 1] [0 - 1] [0 - 1] [0 - 1]
         }
 
         class TimeSeriesData
