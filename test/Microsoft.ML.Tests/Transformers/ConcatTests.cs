2ddb9eea43446d1931723b629bf850ad1f9e9d9e
Abhishek Goswami <abgoswam@gmail.com>
2019-06-03 15:59:28 -0700

    check for number of input columns in concat transform (#3809)

--- test/Microsoft.ML.Tests/Transformers/ConcatTests.cs
+++ test/Microsoft.ML.Tests/Transformers/ConcatTests.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.IO;
 using Microsoft.ML.Data;
 using Microsoft.ML.Data.IO;
@@ -15,6 +16,25 @@ namespace Microsoft.ML.Tests.Transformers
     {
         public ConcatTests(ITestOutputHelper output) : base(output)
         {
+        }
+
+        [Fact]
+        void TestConcatNoInputColumns()
+        {
+            var thrown = false;
+
+            try
+            {
+                var pipe = ML.Transforms.Concatenate("Features");
+            }
+            catch(Exception ex)
+            {
+                Assert.Contains("Input columns not specified", ex.Message);
+                thrown = true;
+
+            }
+            Assert.True(thrown);
+            Done();
         }
 
         [Fact]
