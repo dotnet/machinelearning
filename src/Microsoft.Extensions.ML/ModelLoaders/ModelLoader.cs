40609d5be023274e2b63f9ec52eba6e625f8f105
Eric Erhardt <eric.erhardt@microsoft.com>
2019-06-12 20:40:19 -0500

    Add Microsoft.Extensions.ML integration package. (#3827)
    * Add Microsoft.Extensions.ML integration package.
    
    This package makes it easier to use ML.NET with app models that support Microsoft.Extensions - i.e. ASP.NET and Azure Functions.
    
    Specifically it contains functionality for:
    
    - Dependency Injection
    - Pooling PredictionEngines
    - Reloading models when the file or URI has changed
    - Hooking ML.NET logging to Microsoft.Extensions.Logging
    
    Fix #3239
    
    * Add XML doc comments.
    Format the code so lines aren't so long.
    Remove unnecessary IPredictionEnginePoolBuilder interface, and just use a regular class.
    
    * Respond to PR feedback.
    
    * PR feedback
    
    Rename MLContextOptions to MLOptions. Make MLContext lazy loaded, so callers can provide their own.
    
    Use the loaded model in the test.

---
+++ src/Microsoft.Extensions.ML/ModelLoaders/ModelLoader.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Extensions.Primitives;
+using Microsoft.ML;
+
+namespace Microsoft.Extensions.ML
+{
+    /// <summary>
+    /// Defines a class that provides the mechanisms to load an ML.NET model
+    /// and to propagate notifications that the source of the model has changed.
+    /// </summary>
+    public abstract class ModelLoader
+    {
+        /// <summary>
+        /// Gets an object that can propagate notifications that
+        /// the model has changed.
+        /// </summary>
+        public abstract IChangeToken GetReloadToken();
+
+        /// <summary>
+        /// Gets the ML.NET model.
+        /// </summary>
+        public abstract ITransformer GetModel();
+    }
+}
---
+++ src/Microsoft.Extensions.ML/PredictionEnginePoolPolicy.cs
@@ -0,0 +1,54 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.ObjectPool;
+using Microsoft.ML;
+
+namespace Microsoft.Extensions.ML
+{
+    /// <summary>
+    /// <see cref="IPooledObjectPolicy{T}"/> for <see cref="PredictionEngine{TData, TPrediction}"/>
+    /// which is responsible for creating pooled objects, and when to return objects to the pool.
+    /// </summary>
+    internal class PredictionEnginePoolPolicy<TData, TPrediction>
+        : IPooledObjectPolicy<PredictionEngine<TData, TPrediction>>
+        where TData : class
+        where TPrediction : class, new()
+    {
+        private readonly MLContext _mlContext;
+        private readonly ITransformer _model;
+        private readonly List<WeakReference> _references;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="PredictionEnginePoolPolicy{TData, TPrediction}"/>.
+        /// </summary>
+        /// <param name="mlContext">
+        /// <see cref="MLContext"/> used to load the model.
+        /// </param>
+        /// <param name="model">The transformer to use for prediction.</param>
+        public PredictionEnginePoolPolicy(MLContext mlContext, ITransformer model)
+        {
+            _mlContext = mlContext;
+            _model = model;
+            _references = new List<WeakReference>();
+        }
+
+        /// <inheritdoc />
+        public PredictionEngine<TData, TPrediction> Create()
+        {
+            var engine = _mlContext.Model.CreatePredictionEngine<TData, TPrediction>(_model);
+            _references.Add(new WeakReference(engine));
+            return engine;
+        }
+
+        /// <inheritdoc />
+        public bool Return(PredictionEngine<TData, TPrediction> obj)
+        {
+            return _references.Any(x => x.Target == obj);
+        }
+    }
+}
