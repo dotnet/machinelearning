eb4c40809ece49619579b781573fa60eb2c80409
Zeeshan Siddiqui <mzs@microsoft.com>
2019-06-17 19:09:59 -0700

    Remove assert.

--- src/Microsoft.ML.TimeSeries/SequentialTransformerBase.cs
+++ src/Microsoft.ML.TimeSeries/SequentialTransformerBase.cs
@@ -515,7 +515,6 @@ namespace Microsoft.ML.Transforms.TimeSeries
             {
                 Contracts.CheckValue(bindings, nameof(bindings));
                 Contracts.CheckValue(input, nameof(input));
-                Contracts.Check(Utils.Size(getters) == bindings.GetActiveInput((i) => true).Length);
                 _schema = bindings.Schema;
                 _input = input;
                 _getters = getters ?? new Delegate[0];
