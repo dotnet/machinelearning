f3d36b2bc4fa5c7eb84a793101f13990cbaa400a
Pranov Duggasani <t-prdug@microsoft.com>
2019-07-01 13:25:54 -0700

    Changes to folder for TensorFlowNet Integration

---
+++ src/Microsoft.ML.TensorFlowImageAPI/TensorFlowNetUtils.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Text;
+using Microsoft.ML.Data;
+using Microsoft.ML.Runtime;
+using Tensorflow;
+
+namespace Microsoft.ML.TensorFlowImageAPI
+{
+    public static class TensorFlowNetUtils
+    {
+        // A TensorFlow frozen model is a single file. An un-frozen (SavedModel) on the other hand has a well-defined folder structure.
+        // Given a modelPath, this utility method determines if we should treat it as a SavedModel or not
+        internal static bool IsSavedModel(IHostEnvironment env, string modelPath)
+        {
+            Contracts.Check(env != null, nameof(env));
+            env.CheckNonWhiteSpace(modelPath, nameof(modelPath));
+            FileAttributes attr = File.GetAttributes(modelPath);
+            return attr.HasFlag(FileAttributes.Directory);
+        }
+
+        // Load Saved Model
+
+        internal static Session LoadTFSession(IExceptionContext ectx, byte[] modelBytes, string modelFile = null)
+        {
+            Graph graph = tf.Graph().as_default();
+            try
+            {
+                graph.Import(modelBytes);
+            }
+            catch (Exception ex)
+            {
+                if (!string.IsNullOrEmpty(modelFile))
+                    throw ectx.Except($"TensorFlow exception triggered while loading model from '{modelFile}'");
+#pragma warning disable MSML_NoMessagesForLoadContext
+                throw ectx.ExceptDecode(ex, "Tensorflow exception triggered while loading model.");
+#pragma warning restore MSML_NoMessagesForLoadContext
+
+            }
+            return new Session(graph);
+        }
+
+        //private static Session LoadTFSession(IHostEnvironment env, string exportDirSavedModel)
+        //{
+        //    Contracts.Check(env != null, nameof(env));
+        //    env.CheckValue(exportDirSavedModel, nameof(exportDirSavedModel));
+        //    var sessionOptions = new TFSessionOptions();
+        //    var tags = new string[] { "serve" };
+        //    var graph = new TFGraph();
+        //    var metaGraphDef = new TFBuffer();
+
+        //    return TFSession.FromSavedModel(sessionOptions, null, exportDirSavedModel, tags, graph, metaGraphDef);
+        //}
+        internal static void CreateFolderWithAclIfNotExists(IHostEnvironment env, string folder)
+        {
+            Contracts.Check(env != null, nameof(env));
+            env.CheckNonWhiteSpace(folder, nameof(folder));
+
+            //if directory exists, do nothing.
+            if (Directory.Exists(folder))
+                return;
+
+            WindowsIdentity currentIdentity = null;
+            try
+            {
+                currentIdentity = WindowsIdentity.GetCurrent();
+            }
+            catch (PlatformNotSupportedException)
+            { }
+
+            if (currentIdentity != null && new WindowsPrincipal(currentIdentity).IsInRole(WindowsBuiltInRole.Administrator))
+            {
+                // Create high integrity dir and set no delete policy for all files under the directory.
+                // In case of failure, throw exception.
+                CreateTempDirectoryWithAcl(folder, currentIdentity.User.ToString());
+            }
+            else
+            {
+                try
+                {
+                    Directory.CreateDirectory(folder);
+                }
+                catch (Exception exc)
+                {
+                    throw Contracts.ExceptParam(nameof(folder), $"Failed to create folder for the provided path: {folder}. \nException: {exc.Message}");
+                }
+            }
+        }
+
+        private static void CreateTempDirectoryWithAcl(string folder, string identity)
+        {
+            // Dacl Sddl string:
+            // D: Dacl type
+            // D; Deny access
+            // OI; Object inherit ace
+            // SD; Standard delete function
+            // wIdentity.User Sid of the given user.
+            // A; Allow access
+            // OICI; Object inherit, container inherit
+            // FA File access
+            // BA Built-in administrators
+            // S: Sacl type
+            // ML;; Mandatory Label
+            // NW;;; No write policy
+            // HI High integrity processes only
+            string sddl = "D:(D;OI;SD;;;" + identity + ")(A;OICI;FA;;;BA)S:(ML;OI;NW;;;HI)";
+
+            try
+            {
+                var dir = Directory.CreateDirectory(folder);
+                DirectorySecurity dirSec = new DirectorySecurity();
+                dirSec.SetSecurityDescriptorSddlForm(sddl);
+                dirSec.SetAccessRuleProtection(true, false);  // disable inheritance
+                dir.SetAccessControl(dirSec);
+
+                // Cleaning out the directory, in case someone managed to sneak in between creation and setting ACL.
+                DirectoryInfo dirInfo = new DirectoryInfo(folder);
+                foreach (FileInfo file in dirInfo.GetFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo subDirInfo in dirInfo.GetDirectories())
+                {
+                    subDirInfo.Delete(true);
+                }
+            }
+            catch (Exception exc)
+            {
+                throw Contracts.ExceptParam(nameof(folder), $"Failed to create folder for the provided path: {folder}. \nException: {exc.Message}");
+            }
+        }
+
+        internal static void DeleteFolderWithRetries(IHostEnvironment env, string folder)
+        {
+            Contracts.Check(env != null, nameof(env));
+            int currentRetry = 0;
+            int maxRetryCount = 10;
+            using (var ch = env.Start("Delete folder"))
+            {
+                for (; ; )
+                {
+                    try
+                    {
+                        currentRetry++;
+                        Directory.Delete(folder, true);
+                        break;
+                    }
+                    catch (IOException e)
+                    {
+                        if (currentRetry > maxRetryCount)
+                            throw;
+                        ch.Info("Error deleting folder. {0}. Retry,", e.Message);
+                    }
+                }
+            }
+        }
+        internal static bool IsTypeSupported(TF_DataType tfoutput)
+        {
+            var num = (int)tfoutput;
+            if ((num >= 1 & num <= 23) | (num >= 101 & num <= 103)) {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        internal static PrimitiveDataViewType Tf2MlNetType(TF_DataType type)
+        {
+            var mlNetType = Tf2MlNetTypeOrNull(type);
+            if (mlNetType == null)
+                throw new NotSupportedException("TensorFlow type not supported.");
+            return mlNetType;
+        }
+
+        private static PrimitiveDataViewType Tf2MlNetTypeOrNull(TF_DataType type)
+        {
+            switch (type)
+            {
+                case TF_DataType.TF_FLOAT:
+                    return NumberDataViewType.Single;
+                case TF_DataType.DtFloatRef:
+                    return NumberDataViewType.Single;
+                case TF_DataType.TF_DOUBLE:
+                    return NumberDataViewType.Double;
+                case TF_DataType.TF_UINT8:
+                    return NumberDataViewType.Byte;
+                case TF_DataType.TF_UINT16:
+                    return NumberDataViewType.UInt16;
+                case TF_DataType.TF_UINT32:
+                    return NumberDataViewType.UInt32;
+                case TF_DataType.TF_UINT64:
+                    return NumberDataViewType.UInt64;
+                case TF_DataType.TF_INT8:
+                    return NumberDataViewType.SByte;
+                case TF_DataType.TF_INT16:
+                    return NumberDataViewType.Int16;
+                case TF_DataType.TF_INT32:
+                    return NumberDataViewType.Int32;
+                case TF_DataType.TF_INT64:
+                    return NumberDataViewType.Int64;
+                case TF_DataType.TF_BOOL:
+                    return BooleanDataViewType.Instance;
+                case TF_DataType.TF_STRING:
+                    return TextDataViewType.Instance;
+                default:
+                    return null;
+            }
+        }
+    }
+}
