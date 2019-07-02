93a98febb571718c6d7a9ea300af95f74c216a2c
Sierra Lee <sierrajadelee@gmail.com>
2019-07-01 21:11:33 -0700

    Update Program.cs

--- docs/samples/Microsoft.ML.Samples/Program.cs
+++ docs/samples/Microsoft.ML.Samples/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Reflection;
+using Samples.Dynamic;
 
 namespace Microsoft.ML.Samples
 {
@@ -9,13 +10,12 @@ namespace Microsoft.ML.Samples
 
         internal static void RunAll()
         {
-            // Samples counter.
             int samples = 0;
             foreach (var type in Assembly.GetExecutingAssembly().GetTypes())
             {
                 var sample = type.GetMethod("Example", BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy);
-
-                if (sample != null)
+                
+                
                 {
                     Console.WriteLine(type.Name);
                     sample.Invoke(null, null);
