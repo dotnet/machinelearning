edcb4f56da5c093f6f3a709ea89fee125510195c
Sierra Lee <sierrajadelee@gmail.com>
2019-07-01 20:03:52 -0700

    Update FieldAwareFactorizationMachineWithOptions.cs
    fixing whitespace

--- docs/samples/Microsoft.ML.Samples/Dynamic/Trainers/BinaryClassification/FieldAwareFactorizationMachineWithOptions.cs
+++ docs/samples/Microsoft.ML.Samples/Dynamic/Trainers/BinaryClassification/FieldAwareFactorizationMachineWithOptions.cs
@@ -10,21 +10,21 @@ namespace Samples.Dynamic.Trainers.BinaryClassification
     public static class FieldAwareFactorizationMachineWithOptions
     {
         // This example first train a field-aware factorization to binary
-		// classification, measure the trained model's quality, and finally
+	// classification, measure the trained model's quality, and finally
         // use the trained model to make prediction.
         public static void Example()
         {
             // Create a new context for ML.NET operations. It can be used for
-			// exception tracking and logging, as a catalog of available operations
-			// and as the source of randomness. Setting the seed to a fixed number
-			// in this example to make outputs deterministic.
+	    // exception tracking and logging, as a catalog of available operations
+	    // and as the source of randomness. Setting the seed to a fixed number
+	    // in this example to make outputs deterministic.
             var mlContext = new MLContext(seed: 0);
 
             // Create a list of training data points.
             IEnumerable<DataPoint> data = GenerateRandomDataPoints(500);
 
             // Convert the list of data points to an IDataView object, which is
-			// consumable by ML.NET API.
+	    // consumable by ML.NET API.
             var trainingData = mlContext.Data.LoadFromEnumerable(data);
 
             // Define trainer options.
@@ -32,7 +32,7 @@ namespace Samples.Dynamic.Trainers.BinaryClassification
             {
                 FeatureColumnName = nameof(DataPoint.Field0),
                 ExtraFeatureColumns = 
-				    new[] { nameof(DataPoint.Field1), nameof(DataPoint.Field2) },
+		    new[] { nameof(DataPoint.Field1), nameof(DataPoint.Field2) },
 
                 LabelColumnName = nameof(DataPoint.Label),
                 LambdaLatent = 0.01f,
@@ -45,12 +45,12 @@ namespace Samples.Dynamic.Trainers.BinaryClassification
             // Define the trainer.
             // This trainer trains field-aware factorization (FFM)
             // for binary classification.
-			// See https://www.csie.ntu.edu.tw/~cjlin/papers/ffm.pdf for the theory
-			// behind and
-			// https://github.com/wschin/fast-ffm/blob/master/fast-ffm.pdf for the
-			// training algorithm implemented in ML.NET.
+	    // See https://www.csie.ntu.edu.tw/~cjlin/papers/ffm.pdf for the theory
+	    // behind and
+	    // https://github.com/wschin/fast-ffm/blob/master/fast-ffm.pdf for the
+	    // training algorithm implemented in ML.NET.
             var pipeline = mlContext.BinaryClassification.Trainers
-			    .FieldAwareFactorizationMachine(options);
+	        .FieldAwareFactorizationMachine(options);
 
 
             // Train the model.
@@ -61,7 +61,7 @@ namespace Samples.Dynamic.Trainers.BinaryClassification
 
             // Measure the quality of the trained model.
             var metrics = mlContext.BinaryClassification
-			    .Evaluate(transformedTrainingData);
+	        .Evaluate(transformedTrainingData);
 
             // Show the quality metrics.
             PrintMetrics(metrics);
@@ -90,17 +90,18 @@ namespace Samples.Dynamic.Trainers.BinaryClassification
 
             // Create prediction function from the trained model.
             var engine = mlContext.Model
-			    .CreatePredictionEngine<DataPoint, Result>(model);
+	        .CreatePredictionEngine<DataPoint, Result>(model);
 
             // Make some predictions.
             foreach(var dataPoint in data.Take(5))
             {
                 var result = engine.Predict(dataPoint);
                 Console.WriteLine($"Actual label: {dataPoint.Label}, "
-				    + $"predicted label: {result.PredictedLabel}, "
+		    + $"predicted label: {result.PredictedLabel}, "
                     + $"score of being positive class: {result.Score}, "
-					+ $"and probability of beling positive class: "
-					+ $"{result.Probability}.");
+		    + $"and probability of beling positive class: "
+		    + $"{result.Probability}.");
+		    
             }
 
             // Expected output:
@@ -121,7 +122,7 @@ namespace Samples.Dynamic.Trainers.BinaryClassification
             public bool Label { get; set; }
 
             // Features from the first field. Note that different fields can have
-			// different numbers of features.
+	    // different numbers of features.
             [VectorType(featureLength)]
             public float[] Field0 { get; set; }
 
@@ -135,7 +136,7 @@ namespace Samples.Dynamic.Trainers.BinaryClassification
         }
 
         // This class defines objects produced by trained model. The trained model
-		// maps a DataPoint to a Result.
+	// maps a DataPoint to a Result.
         public class Result
         {
             // Label.
@@ -150,7 +151,7 @@ namespace Samples.Dynamic.Trainers.BinaryClassification
 
         // Function used to create toy data sets.
         private static IEnumerable<DataPoint> GenerateRandomDataPoints(
-		    int exampleCount, int seed = 0)
+	    int exampleCount, int seed = 0)
 
         {
             var rnd = new Random(seed);
@@ -158,7 +159,7 @@ namespace Samples.Dynamic.Trainers.BinaryClassification
             for (int i = 0; i < exampleCount; ++i)
             {
                 // Initialize an example with a random label and an empty feature
-				// vector.
+		// vector.
                 var sample = new DataPoint()
                 {
                     Label = rnd.Next() % 2 == 0,
@@ -169,9 +170,9 @@ namespace Samples.Dynamic.Trainers.BinaryClassification
 
                 // Fill feature vectors according the assigned label.
                 // Notice that features from different fields have different biases
-				// and therefore different distributions. In practices such as game
-				// recommendation, one may use one field to store features from user
-				// profile and another field to store features from game profile.
+		// and therefore different distributions. In practices such as game
+		// recommendation, one may use one field to store features from user
+		// profile and another field to store features from game profile.
                 for (int j = 0; j < featureLength; ++j)
                 {
                     var value0 = (float)rnd.NextDouble();
@@ -200,18 +201,18 @@ namespace Samples.Dynamic.Trainers.BinaryClassification
 
         // Function used to show evaluation metrics such as accuracy of predictions.
         private static void PrintMetrics(
-		    CalibratedBinaryClassificationMetrics metrics)
+	    CalibratedBinaryClassificationMetrics metrics)
 
         {
             Console.WriteLine($"Accuracy: {metrics.Accuracy:F2}");
             Console.WriteLine($"AUC: {metrics.AreaUnderRocCurve:F2}");
             Console.WriteLine($"F1 Score: {metrics.F1Score:F2}");
             Console.WriteLine($"Negative Precision: {metrics.NegativePrecision:F2}")
-			    ;
+	        ;
 
             Console.WriteLine($"Negative Recall: {metrics.NegativeRecall:F2}");
             Console.WriteLine($"Positive Precision: {metrics.PositivePrecision:F2}")
-			    ;
+	        ;
 
             Console.WriteLine($"Positive Recall: {metrics.PositiveRecall:F2}");
             Console.WriteLine($"Log Loss: {metrics.LogLoss:F2}");
