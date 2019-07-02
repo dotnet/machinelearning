9c6325ff7d7aa00b88bda1824218245cb84e9177
Rayan Krishnan <t-rakri@microsoft.com>
2019-07-01 19:13:42 -0700

    transform/text and projection formatted to 85 char

--- docs/samples/Microsoft.ML.Samples/Dynamic/Transforms/Text/FeaturizeText.cs
+++ docs/samples/Microsoft.ML.Samples/Dynamic/Transforms/Text/FeaturizeText.cs
@@ -8,41 +8,58 @@ namespace Samples.Dynamic
     {
         public static void Example()
         {
-            // Create a new ML context, for ML.NET operations. It can be used for exception tracking and logging, 
-            // as well as the source of randomness.
+            // Create a new ML context, for ML.NET operations. It can be used for
+            // exception tracking and logging, as well as the source of randomness.
             var mlContext = new MLContext();
 
             // Create a small dataset as an IEnumerable.
             var samples = new List<TextData>()
             {
-                new TextData(){ Text = "ML.NET's FeaturizeText API uses a composition of several basic transforms to convert text into numeric features." },
-                new TextData(){ Text = "This API can be used as a featurizer to perform text classification." },
-                new TextData(){ Text = "There are a number of approaches to text classification." },
-                new TextData(){ Text = "One of the simplest and most common approaches is called “Bag of Words”." },
-                new TextData(){ Text = "Text classification can be used for a wide variety of tasks" },
-                new TextData(){ Text = "such as sentiment analysis, topic detection, intent identification etc." },
+                new TextData(){ Text = "ML.NET's FeaturizeText API uses a " +
+                    "composition of several basic transforms to convert text " +
+                    "into numeric features." },
+
+                new TextData(){ Text = "This API can be used as a featurizer to " +
+                    "perform text classification." },
+
+                new TextData(){ Text = "There are a number of approaches to text " +
+                    "classification." },
+
+                new TextData(){ Text = "One of the simplest and most common " +
+                    "approaches is called “Bag of Words”." },
+
+                new TextData(){ Text = "Text classification can be used for a " +
+                    "wide variety of tasks" },
+
+                new TextData(){ Text = "such as sentiment analysis, topic " +
+                    "detection, intent identification etc." },
             };
 
             // Convert training data to IDataView.
             var dataview = mlContext.Data.LoadFromEnumerable(samples);
 
             // A pipeline for converting text into numeric features.
-            // The following call to 'FeaturizeText' instantiates 'TextFeaturizingEstimator' with default parameters.
+            // The following call to 'FeaturizeText' instantiates 
+            // 'TextFeaturizingEstimator' with default parameters.
             // The default settings for the TextFeaturizingEstimator are
             //      * StopWordsRemover: None
             //      * CaseMode: Lowercase
             //      * OutputTokensColumnName: None
-            //      * KeepDiacritics: false, KeepPunctuations: true, KeepNumbers: true
+            //      * KeepDiacritics: false, KeepPunctuations: true, KeepNumbers:
+            //          true
             //      * WordFeatureExtractor: NgramLength = 1
             //      * CharFeatureExtractor: NgramLength = 3, UseAllLengths = false
             // The length of the output feature vector depends on these settings.
-            var textPipeline = mlContext.Transforms.Text.FeaturizeText("Features", "Text");
+            var textPipeline = mlContext.Transforms.Text.FeaturizeText("Features",
+                "Text");
             
             // Fit to data.
             var textTransformer = textPipeline.Fit(dataview);
 
-            // Create the prediction engine to get the features extracted from the text.
-            var predictionEngine = mlContext.Model.CreatePredictionEngine<TextData, TransformedTextData>(textTransformer);
+            // Create the prediction engine to get the features extracted from the
+            // text.
+            var predictionEngine = mlContext.Model.CreatePredictionEngine<TextData,
+                TransformedTextData>(textTransformer);
 
             // Convert the text into numeric features.
             var prediction = predictionEngine.Predict(samples[0]);
--- docs/samples/Microsoft.ML.Samples/Dynamic/Transforms/Text/ProduceNgrams.cs
+++ docs/samples/Microsoft.ML.Samples/Dynamic/Transforms/Text/ProduceNgrams.cs
@@ -10,34 +10,52 @@ namespace Samples.Dynamic
     {
         public static void Example()
         {
-            // Create a new ML context, for ML.NET operations. It can be used for exception tracking and logging, 
-            // as well as the source of randomness.
+            // Create a new ML context, for ML.NET operations. It can be used for
+            // exception tracking and logging, as well as the source of randomness.
             var mlContext = new MLContext();
 
             // Create a small dataset as an IEnumerable.
             var samples = new List<TextData>()
             {
                 new TextData(){ Text = "This is an example to compute n-grams." },
-                new TextData(){ Text = "N-gram is a sequence of 'N' consecutive words/tokens." },
-                new TextData(){ Text = "ML.NET's ProduceNgrams API produces vector of n-grams." },
-                new TextData(){ Text = "Each position in the vector corresponds to a particular n-gram." },
-                new TextData(){ Text = "The value at each position corresponds to," },
-                new TextData(){ Text = "the number of times n-gram occured in the data (Tf), or" },
-                new TextData(){ Text = "the inverse of the number of documents that contain the n-gram (Idf)," },
-                new TextData(){ Text = "or compute both and multiply together (Tf-Idf)." },
+                new TextData(){ Text = "N-gram is a sequence of 'N' consecutive " +
+                    "words/tokens." },
+
+                new TextData(){ Text = "ML.NET's ProduceNgrams API produces " +
+                    "vector of n-grams." },
+
+                new TextData(){ Text = "Each position in the vector corresponds " +
+                    "to a particular n-gram." },
+
+                new TextData(){ Text = "The value at each position corresponds " +
+                    "to," },
+
+                new TextData(){ Text = "the number of times n-gram occured in " +
+                    "the data (Tf), or" },
+
+                new TextData(){ Text = "the inverse of the number of documents " +
+                    "that contain the n-gram (Idf)," },
+
+                new TextData(){ Text = "or compute both and multiply together " +
+                    "(Tf-Idf)." },
             };
 
             // Convert training data to IDataView.
             var dataview = mlContext.Data.LoadFromEnumerable(samples);
 
             // A pipeline for converting text into numeric n-gram features.
-            // The following call to 'ProduceNgrams' requires the tokenized text/string as input.
-            // This is acheived by calling 'TokenizeIntoWords' first followed by 'ProduceNgrams'.
-            // Please note that the length of the output feature vector depends on the n-gram settings.
-            var textPipeline = mlContext.Transforms.Text.TokenizeIntoWords("Tokens", "Text")
-                // 'ProduceNgrams' takes key type as input. Converting the tokens into key type using 'MapValueToKey'.
+            // The following call to 'ProduceNgrams' requires the tokenized
+            // text /string as input. This is acheived by calling 
+            // 'TokenizeIntoWords' first followed by 'ProduceNgrams'. Please note
+            // that the length of the output feature vector depends on the n-gram
+            // settings.
+            var textPipeline = mlContext.Transforms.Text.TokenizeIntoWords("Tokens",
+                "Text")
+                // 'ProduceNgrams' takes key type as input. Converting the tokens
+                // into key type using 'MapValueToKey'.
                 .Append(mlContext.Transforms.Conversion.MapValueToKey("Tokens"))
-                .Append(mlContext.Transforms.Text.ProduceNgrams("NgramFeatures", "Tokens",
+                .Append(mlContext.Transforms.Text.ProduceNgrams("NgramFeatures",
+                    "Tokens",
                     ngramLength: 3,
                     useAllLengths: false,
                     weighting: NgramExtractingEstimator.WeightingCriteria.Tf));
@@ -46,21 +64,26 @@ namespace Samples.Dynamic
             var textTransformer = textPipeline.Fit(dataview);
             var transformedDataView = textTransformer.Transform(dataview);
 
-            // Create the prediction engine to get the n-gram features extracted from the text.
-            var predictionEngine = mlContext.Model.CreatePredictionEngine<TextData, TransformedTextData>(textTransformer);
+            // Create the prediction engine to get the n-gram features extracted
+            // from the text.
+            var predictionEngine = mlContext.Model.CreatePredictionEngine<TextData,
+                TransformedTextData>(textTransformer);
 
             // Convert the text into numeric features.
             var prediction = predictionEngine.Predict(samples[0]);
 
             // Print the length of the feature vector.
-            Console.WriteLine($"Number of Features: {prediction.NgramFeatures.Length}");
+            Console.WriteLine("Number of Features: " + prediction.NgramFeatures
+                .Length);
 
             // Preview of the produced n-grams.
             // Get the slot names from the column's metadata.
-            // The slot names for a vector column corresponds to the names associated with each position in the vector.
+            // The slot names for a vector column corresponds to the names
+            // associated with each position in the vector.
             VBuffer<ReadOnlyMemory<char>> slotNames = default;
             transformedDataView.Schema["NgramFeatures"].GetSlotNames(ref slotNames);
-            var NgramFeaturesColumn = transformedDataView.GetColumn<VBuffer<float>>(transformedDataView.Schema["NgramFeatures"]);
+            var NgramFeaturesColumn = transformedDataView.GetColumn<VBuffer<
+                float>>(transformedDataView.Schema["NgramFeatures"]);
             var slots = slotNames.GetValues();
             Console.Write("N-grams: ");
             foreach (var featureRow in NgramFeaturesColumn)
