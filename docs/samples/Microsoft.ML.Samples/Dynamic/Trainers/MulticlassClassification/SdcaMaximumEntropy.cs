4fecfb200d270810b2f556e1f4fc0f349a606284
Sierra Lee <sierrajadelee@gmail.com>
2019-07-02 17:20:36 -0700

    Reformatting MulticlassClassification samples to width 85 (#3942)
    * reformatted multiclassclassification samples
    
    * fixing errors
    
    * reformatted MulticlassClassification samples
    
    * Update LbfgsMaximumEntropy.cs
    
    getting rid of whitespace
    
    * Update LbfgsMaximumEntropy.cs
    
    * Update LbfgsMaximumEntropyWithOptions.cs
    
    getting rid of whitespace
    
    * Update LightGbmWithOptions.cs
    
    fixing whitespace
    
    * Update LbfgsMaximumEntropy.cs
    
    * Update LightGbm.cs
    
    fixing whitespace
    
    * Update LightGbm.cs
    
    fixing whitespace
    
    * Update LightGbmWithOptions.cs
    
    fixing whitespace
    
    * Update MulticlassClassification.ttinclude
    
    fixing whitespace
    
    * Update MulticlassClassification.ttinclude
    
    fixing whitespace
    
    * Update NaiveBayes.cs
    
    fixing whitespace
    
    * Update NaiveBayes.tt
    
    fixing whitespace
    
    * Update NaiveBayes.tt
    
    * Update OneVersusAll.cs
    
    fixing whitespace
    
    * Update PairwiseCoupling.cs
    
    fixing whitespace
    
    * Update SdcaMaximumEntropy.cs
    
    fixing whitespace
    
    * Update SdcaMaximumEntropyWithOptions.cs
    
    fixing whitespace
    
    * Update SdcaNonCalibrated.cs
    
    fixing whitespace
    
    * Update SdcaNonCalibratedWithOptions.cs
    
    fixing whitespace
    
    * Update SdcaNonCalibrated.cs
    
    fixing whitespace
    
    * Update SdcaNonCalibrated.cs
    
    * Update LbfgsMaximumEntropy.cs
    
    * Update LbfgsMaximumEntropy.cs
    
    * Update LbfgsMaximumEntropyWithOptions.cs
    
    * Update LightGbm.cs
    
    * Update LightGbmWithOptions.cs
    
    * Update MulticlassClassification.ttinclude
    
    * Update NaiveBayes.cs
    
    * Update OneVersusAll.cs
    
    * Update PairwiseCoupling.cs
    
    * Update SdcaMaximumEntropy.cs
    
    * Update SdcaMaximumEntropy.cs
    
    * Update SdcaMaximumEntropyWithOptions.cs
    
    * Update SdcaNonCalibrated.cs
    
    * Update SdcaNonCalibratedWithOptions.cs
    
    * fixed tabbing issue
    
    * fixed indentations
    
    * aligned comments
    
    * fixed some indentation and spacing issues
    
    * fixed extra empty lines
    
    * fixed some more indentation issue

--- docs/samples/Microsoft.ML.Samples/Dynamic/Trainers/MulticlassClassification/SdcaMaximumEntropy.cs
+++ docs/samples/Microsoft.ML.Samples/Dynamic/Trainers/MulticlassClassification/SdcaMaximumEntropy.cs
@@ -10,45 +10,56 @@ namespace Samples.Dynamic.Trainers.MulticlassClassification
     {
         public static void Example()
         {
-            // Create a new context for ML.NET operations. It can be used for exception tracking and logging, 
-            // as a catalog of available operations and as the source of randomness.
-            // Setting the seed to a fixed number in this example to make outputs deterministic.
+            // Create a new context for ML.NET operations. It can be used for
+            // exception tracking and logging, as a catalog of available operations
+            // and as the source of randomness. Setting the seed to a fixed number
+            // in this example to make outputs deterministic.
             var mlContext = new MLContext(seed: 0);
 
             // Create a list of training data points.
             var dataPoints = GenerateRandomDataPoints(1000);
 
-            // Convert the list of data points to an IDataView object, which is consumable by ML.NET API.
+            // Convert the list of data points to an IDataView object, which is
+            // consumable by ML.NET API.
             var trainingData = mlContext.Data.LoadFromEnumerable(dataPoints);
 
-            // ML.NET doesn't cache data set by default. Therefore, if one reads a data set from a file and accesses it many times,
-			// it can be slow due to expensive featurization and disk operations. When the considered data can fit into memory,
-			// a solution is to cache the data in memory. Caching is especially helpful when working with iterative algorithms 
-			// which needs many data passes.
-			trainingData = mlContext.Data.Cache(trainingData);
+            // ML.NET doesn't cache data set by default. Therefore, if one reads a
+            // data set from a file and accesses it many times, it can be slow due
+            // to expensive featurization and disk operations. When the considered
+            // data can fit into memory, a solution is to cache the data in memory.
+            // Caching is especially helpful when working with iterative algorithms 
+            // which needs many data passes.
+            trainingData = mlContext.Data.Cache(trainingData);
 
             // Define the trainer.
             var pipeline =
-                    // Convert the string labels into key types.
-                    mlContext.Transforms.Conversion.MapValueToKey(nameof(DataPoint.Label))
-                    // Apply SdcaMaximumEntropy multiclass trainer.
-                    .Append(mlContext.MulticlassClassification.Trainers.SdcaMaximumEntropy());
+                // Convert the string labels into key types.
+                mlContext.Transforms.Conversion
+                .MapValueToKey(nameof(DataPoint.Label))
+                // Apply SdcaMaximumEntropy multiclass trainer.
+                .Append(mlContext.MulticlassClassification.Trainers
+                .SdcaMaximumEntropy());
 
             // Train the model.
             var model = pipeline.Fit(trainingData);
 
-            // Create testing data. Use different random seed to make it different from training data.
-            var testData = mlContext.Data.LoadFromEnumerable(GenerateRandomDataPoints(500, seed: 123));
+            // Create testing data. Use different random seed to make it different
+            // from training data.
+            var testData = mlContext.Data
+                .LoadFromEnumerable(GenerateRandomDataPoints(500, seed: 123));
 
             // Run the model on test data set.
             var transformedTestData = model.Transform(testData);
 
             // Convert IDataView object to a list.
-            var predictions = mlContext.Data.CreateEnumerable<Prediction>(transformedTestData, reuseRowObject: false).ToList();
+            var predictions = mlContext.Data
+                .CreateEnumerable<Prediction>(transformedTestData,
+                reuseRowObject: false).ToList();
 
             // Look at 5 predictions
             foreach (var p in predictions.Take(5))
-                Console.WriteLine($"Label: {p.Label}, Prediction: {p.PredictedLabel}");
+                Console.WriteLine($"Label: {p.Label}, " + 
+                    $"Prediction: {p.PredictedLabel}");
 
             // Expected output:
             //   Label: 1, Prediction: 1
@@ -58,7 +69,9 @@ namespace Samples.Dynamic.Trainers.MulticlassClassification
             //   Label: 3, Prediction: 3
 
             // Evaluate the overall metrics
-            var metrics = mlContext.MulticlassClassification.Evaluate(transformedTestData);
+            var metrics = mlContext.MulticlassClassification
+                .Evaluate(transformedTestData);
+
             PrintMetrics(metrics);
             
             // Expected output:
@@ -77,8 +90,11 @@ namespace Samples.Dynamic.Trainers.MulticlassClassification
             //   Precision ||0.9130 |0.9538 |0.8494 |
         }
 
-        // Generates random uniform doubles in [-0.5, 0.5) range with labels 1, 2 or 3.
-        private static IEnumerable<DataPoint> GenerateRandomDataPoints(int count, int seed=0)
+        // Generates random uniform doubles in [-0.5, 0.5)
+        // range with labels 1, 2 or 3.
+        private static IEnumerable<DataPoint> GenerateRandomDataPoints(int count,
+            int seed=0)
+
         {
             var random = new Random(seed);
             float randomFloat() => (float)(random.NextDouble() - 0.5);
@@ -90,13 +106,17 @@ namespace Samples.Dynamic.Trainers.MulticlassClassification
                 {
                     Label = (uint)label,
                     // Create random features that are correlated with the label.
-                    // The feature values are slightly increased by adding a constant multiple of label.
-                    Features = Enumerable.Repeat(label, 20).Select(x => randomFloat() + label * 0.2f).ToArray()
+                    // The feature values are slightly increased by adding a
+                    // constant multiple of label.
+                    Features = Enumerable.Repeat(label, 20)
+                        .Select(x => randomFloat() + label * 0.2f).ToArray()
+
                 };
             }
         }
 
-        // Example with label and 20 feature values. A data set is a collection of such examples.
+        // Example with label and 20 feature values. A data set is a collection of
+        // such examples.
         private class DataPoint
         {
             public uint Label { get; set; }
@@ -119,8 +139,11 @@ namespace Samples.Dynamic.Trainers.MulticlassClassification
             Console.WriteLine($"Micro Accuracy: {metrics.MicroAccuracy:F2}");
             Console.WriteLine($"Macro Accuracy: {metrics.MacroAccuracy:F2}");
             Console.WriteLine($"Log Loss: {metrics.LogLoss:F2}");
-            Console.WriteLine($"Log Loss Reduction: {metrics.LogLossReduction:F2}\n");
+            Console.WriteLine(
+                $"Log Loss Reduction: {metrics.LogLossReduction:F2}\n");
+
             Console.WriteLine(metrics.ConfusionMatrix.GetFormattedConfusionTable());
         }
     }
 }
+
