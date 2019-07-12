33079ecb52145fda53add8b2eaa876131f9c5516
Eric Erhardt <eric.erhardt@microsoft.com>
2019-07-12 16:54:45 -0500

    Initial implementation of DatabaseLoader.

---
+++ src/Microsoft.ML.Data/DataLoadSave/Database/DatabaseLoaderCursor.cs
@@ -0,0 +1,153 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Data;
+using Microsoft.ML.Internal.Utilities;
+using Microsoft.ML.Runtime;
+
+namespace Microsoft.ML.Data
+{
+    public sealed partial class DatabaseLoader
+    {
+        private sealed class Cursor : RootCursorBase
+        {
+            private readonly Bindings _bindings;
+            private readonly IDataReader _input;
+            private readonly bool[] _active; // Which columns are active.
+            private readonly Delegate[] _getters;
+
+            // This holds the overall line from the line reader currently served up in the cursor.
+            private long _total;
+            private bool _disposed;
+
+            public override long Batch => 0;
+
+            private Cursor(DatabaseLoader parent, IDataReader input, bool[] active)
+                : base(parent._host)
+            {
+                Ch.Assert(active == null || active.Length == parent._bindings.OutputSchema.Count);
+                Ch.CheckValue(input, nameof(input));
+
+                _total = -1;
+                _bindings = parent._bindings;
+                _active = active;
+                _input = input;
+
+                _getters = new Delegate[_bindings.Infos.Length];
+                for (int i = 0; i < _getters.Length; i++)
+                {
+                    if (_active != null && !_active[i])
+                        continue;
+                    _getters[i] = CreateGetterDelegate(i);
+                    Ch.Assert(_getters[i] != null);
+                }
+            }
+
+            public static DataViewRowCursor Create(DatabaseLoader parent, Func<IDataReader> input, bool[] active)
+            {
+                Contracts.AssertValue(parent);
+                Contracts.AssertValue(input);
+                Contracts.Assert(active == null || active.Length == parent._bindings.OutputSchema.Count);
+
+                return new Cursor(parent, input(), active);
+            }
+
+            public override ValueGetter<DataViewRowId> GetIdGetter()
+            {
+                return
+                    (ref DataViewRowId val) =>
+                    {
+                        Ch.Check(IsGood, RowCursorUtils.FetchValueStateError);
+                        val = new DataViewRowId((ulong)_total, 0);
+                    };
+            }
+
+            public override DataViewSchema Schema => _bindings.OutputSchema;
+
+            protected override void Dispose(bool disposing)
+            {
+                if (_disposed)
+                    return;
+                if (disposing)
+                {
+                    _input.Dispose();
+                }
+                _disposed = true;
+                base.Dispose(disposing);
+            }
+
+            protected override bool MoveNextCore()
+            {
+                if (_input.Read())
+                {
+                    return true;
+                }
+
+                return false;
+            }
+
+            /// <summary>
+            /// Returns whether the given column is active in this row.
+            /// </summary>
+            public override bool IsColumnActive(DataViewSchema.Column column)
+            {
+                Ch.Check(column.Index < _bindings.Infos.Length);
+                return _active == null || _active[column.Index];
+            }
+
+            /// <summary>
+            /// Returns a value getter delegate to fetch the value of column with the given columnIndex, from the row.
+            /// This throws if the column is not active in this row, or if the type
+            /// <typeparamref name="TValue"/> differs from this column's type.
+            /// </summary>
+            /// <typeparam name="TValue"> is the column's content type.</typeparam>
+            /// <param name="column"> is the output column whose getter should be returned.</param>
+            public override ValueGetter<TValue> GetGetter<TValue>(DataViewSchema.Column column)
+            {
+                Ch.CheckParam(column.Index < _getters.Length, nameof(column), "requested column not valid.");
+                Ch.Check(IsColumnActive(column));
+
+                var fn = _getters[column.Index] as ValueGetter<TValue>;
+                if (fn == null)
+                    throw Ch.Except("Invalid TValue in GetGetter: '{0}'", typeof(TValue));
+                return fn;
+            }
+
+            private Delegate CreateGetterDelegate(int col)
+            {
+                return Utils.MarshalInvoke(CreateGetterDelegate<int>, _bindings.Infos[col].ColType.RawType, col);
+            }
+
+            private Delegate CreateGetterDelegate<TValue>(int col)
+            {
+                var colInfo = _bindings.Infos[col];
+                Ch.Assert(colInfo.ColType.RawType == typeof(TValue));
+
+                if (typeof(TValue) == typeof(int))
+                {
+                    return CreateInt32GetterDelegate(colInfo) as ValueGetter<TValue>;
+                }
+                if (typeof(TValue) == typeof(float))
+                {
+                    return CreateFloatGetterDelegate(colInfo) as ValueGetter<TValue>;
+                }
+
+                throw new NotSupportedException();
+            }
+
+            private ValueGetter<int> CreateInt32GetterDelegate(ColInfo colInfo)
+            {
+                int columnIndex = colInfo.SourceIndex;
+                return (ref int value) => value = _input.GetInt32(columnIndex);
+            }
+
+            private ValueGetter<float> CreateFloatGetterDelegate(ColInfo colInfo)
+            {
+                int columnIndex = colInfo.SourceIndex;
+                return (ref float value) => value = _input.GetFloat(columnIndex);
+            }
+        }
+    }
+}
---
+++ test/Microsoft.ML.Tests/DatabaseLoaderTests.cs
@@ -0,0 +1,86 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.ML.Data;
+using Microsoft.ML.TestFramework;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.ML.Tests
+{
+    public class DatabaseLoaderTests : BaseTestClass
+    {
+        public DatabaseLoaderTests(ITestOutputHelper output)
+            : base(output)
+        {
+        }
+
+        [Fact]
+        public void TryItOut()
+        {
+            var mlContext = new MLContext(seed: 1);
+
+            var connectionString = @"Server=(localdb)\mssqllocaldb;Database=EFGetStarted.AspNetCore.NewDb;Trusted_Connection=True;ConnectRetryCount=0";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand("SELECT SepalLength, SepalWidth, PetalLength, PetalWidth, Label FROM IrisData", connection))
+                {
+                    var loader = new DatabaseLoader(mlContext, new DatabaseLoader.Options()
+                    {
+                        Columns = new[]
+                        {
+                            new DatabaseLoader.Column() { Name = "SepalLength", Source = 0},
+                            new DatabaseLoader.Column() { Name = "SepalWidth", Source = 1},
+                            new DatabaseLoader.Column() { Name = "PetalLength", Source = 2},
+                            new DatabaseLoader.Column() { Name = "PetalWidth", Source = 3},
+                            new DatabaseLoader.Column() { Name = "Label", Type = DbType.Int32, Source = 4},
+                        }
+                    });
+
+                    var pipeline = mlContext.Transforms.Conversion.MapValueToKey("Label")
+                        .Append(mlContext.Transforms.Concatenate("Features", "SepalLength", "SepalWidth", "PetalLength", "PetalWidth"))
+                        //.AppendCacheCheckpoint(mlContext)
+                        .Append(mlContext.MulticlassClassification.Trainers.LightGbm())
+                        .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+
+                    IDataView trainingData = loader.Load(() => command.ExecuteReader());
+                    //trainingData = mlContext.Data.Cache(trainingData, "SepalLength", "SepalWidth", "PetalLength", "PetalWidth", "Label");
+                    var model = pipeline.Fit(trainingData);
+
+                    var engine = mlContext.Model.CreatePredictionEngine<IrisData, IrisPrediction>(model);
+
+                    var prediction = engine.Predict(new IrisData()
+                    {
+                        SepalLength = 4.5f,
+                        SepalWidth = 5.6f,
+                        PetalLength = 0.5f,
+                        PetalWidth = 0.5f,
+                    });
+
+                    Assert.Equal(2, prediction.PredictedLabel);
+                }
+            }
+        }
+
+        public class IrisData
+        {
+            public float SepalLength;
+            public float SepalWidth;
+            public float PetalLength;
+            public float PetalWidth;
+            public int Label;
+        }
+
+        public class IrisPrediction
+        {
+            public int PredictedLabel;
+            public float[] Score;
+        }
+
+    }
+}
