a22e0447b85d7f6eabf7a97d6ec55f3625f913ee
Xiaoyun Zhang <xiaoyuz@microsoft.com>
2019-07-26 14:24:00 -0700

    add CodeGen Library

---
+++ src/Microsoft.ML.CodeGen/CodeGenerator/CSharp/CodeGeneratorSettings.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.ML.AutoML;
+
+namespace Microsoft.ML.CLI.CodeGenerator.CSharp
+{
+    public class CodeGeneratorSettings
+    {
+        public string LabelName { get; set; }
+
+        public string ModelPath { get; set; }
+
+        public string OutputName { get; set; }
+
+        public string OutputBaseDir { get; set; }
+
+        public string TrainDataset { get; set; }
+
+        public string TestDataset { get; set; }
+
+        internal TaskKind MlTask { get; set; }
+
+    }
+}
