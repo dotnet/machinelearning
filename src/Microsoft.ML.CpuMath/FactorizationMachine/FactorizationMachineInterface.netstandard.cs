5d90c11bf5d433f539d9c23bb20ebd9cc9cc4b4c
Poornima Kumar <poornima.kumar@intel.com>
2019-08-06 01:38:09 -0500

    Added AVX/FMA intrinsics to Factorization Machine (#3940)

--- src/Microsoft.ML.CpuMath/FactorizationMachine/FactorizationMachineInterface.netstandard.cs
+++ src/Microsoft.ML.CpuMath/FactorizationMachine/FactorizationMachineInterface.netstandard.cs
@@ -2,42 +2,13 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Runtime.InteropServices;
-using System.Security;
-using Microsoft.ML.Internal.CpuMath;
-using Microsoft.ML.Runtime;
+using Microsoft.ML.Internal.CpuMath.Core;
 
-namespace Microsoft.ML.Trainers
+namespace Microsoft.ML.Internal.CpuMath.FactorizationMachine
 {
-    internal static unsafe class FieldAwareFactorizationMachineInterface
+    [BestFriend]
+    internal static unsafe partial class FieldAwareFactorizationMachineInterface
     {
-        internal const string NativePath = "FactorizationMachineNative";
-        public const int CbAlign = 16;
-
-        private static bool Compat(AlignedArray a)
-        {
-            Contracts.AssertValue(a);
-            Contracts.Assert(a.Size > 0);
-            return a.CbAlign == CbAlign;
-        }
-
-        private static unsafe float* Ptr(AlignedArray a, float* p)
-        {
-            Contracts.AssertValue(a);
-            float* q = p + a.GetBase((long)p);
-            Contracts.Assert(((long)q & (CbAlign - 1)) == 0);
-            return q;
-        }
-
-        [DllImport(NativePath), SuppressUnmanagedCodeSecurity]
-        public static extern void CalculateIntermediateVariablesNative(int fieldCount, int latentDim, int count, int* /*const*/ fieldIndices, int* /*const*/ featureIndices,
-            float* /*const*/ featureValues, float* /*const*/ linearWeights, float* /*const*/ latentWeights, float* latentSum, float* response);
-
-        [DllImport(NativePath), SuppressUnmanagedCodeSecurity]
-        public static extern void CalculateGradientAndUpdateNative(float lambdaLinear, float lambdaLatent, float learningRate, int fieldCount, int latentDim, float weight,
-            int count, int* /*const*/ fieldIndices, int* /*const*/ featureIndices, float* /*const*/ featureValues, float* /*const*/ latentSum, float slope,
-            float* linearWeights, float* latentWeights, float* linearAccumulatedSquaredGrads, float* latentAccumulatedSquaredGrads);
-
         public static void CalculateIntermediateVariables(int fieldCount, int latentDim, int count, int[] fieldIndices, int[] featureIndices, float[] featureValues,
             float[] linearWeights, AlignedArray latentWeights, AlignedArray latentSum, ref float response)
         {
