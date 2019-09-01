4b2a977f157fac29daf3d07acb28251fdb78c083
Xiaoyun Zhang <xiaoyuz@microsoft.com>
2019-09-01 11:11:35 -0700

    add codegen.test proj

---
+++ test/Microsoft.ML.CodeGenerator.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.ML.CodeGenerator.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
