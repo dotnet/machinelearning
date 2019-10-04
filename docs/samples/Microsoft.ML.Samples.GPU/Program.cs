483b22e24b64fc6f82db831e79abd6c74523b94f
Antonio Velazquez <anvelazq@microsoft.com>
2019-10-04 15:42:02 -0700

    Adding a Program.cs to ML.Samples.GPU project, different from the one in ML.Samples

---
+++ docs/samples/Microsoft.ML.Samples.GPU/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using Samples.Dynamic;
+
+namespace Microsoft.ML.Samples
+{
+    public static class Program
+    {
+        public static void Main(string[] args) => RunAll();
+
+        internal static void RunAll()
+        {
+            int samples = 0;
+            foreach (var type in Assembly.GetExecutingAssembly().GetTypes())
+            {
+                var sample = type.GetMethod("Example", BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy);
+
+                if (sample != null)
+                {
+                    Console.WriteLine(type.Name);
+                    sample.Invoke(null, null);
+                    samples++;
+                }
+            }
+
+            Console.WriteLine("Number of samples that ran without any exception: " + samples);
+        }
+    }
+}
