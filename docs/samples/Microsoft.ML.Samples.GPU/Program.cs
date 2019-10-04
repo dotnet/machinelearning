58dcb47b98f59bc1d6652429d86e20e8f068b143
Antonio Velazquez <anvelazq@microsoft.com>
2019-10-04 16:00:22 -0700

    Removing the content in the ML.Samples.GPU main method

--- docs/samples/Microsoft.ML.Samples.GPU/Program.cs
+++ docs/samples/Microsoft.ML.Samples.GPU/Program.cs
@@ -10,20 +10,7 @@ namespace Microsoft.ML.Samples
 
         internal static void RunAll()
         {
-            int samples = 0;
-            foreach (var type in Assembly.GetExecutingAssembly().GetTypes())
-            {
-                var sample = type.GetMethod("Example", BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy);
-
-                if (sample != null)
-                {
-                    Console.WriteLine(type.Name);
-                    sample.Invoke(null, null);
-                    samples++;
-                }
-            }
-
-            Console.WriteLine("Number of samples that ran without any exception: " + samples);
+            
         }
     }
 }
