0c7c28d70cc69dadfe5236aacbcdb1f59fa79593
Brian Stark <54910472+bpstark@users.noreply.github.com>
2019-10-22 16:40:14 -0700

    Added support for resnet50 architecture for image classification (#4349)
    * Added support for resnet50 architecture for image classification
    
    Created a sample to show usage of resnet50 V2.
    Tested against Tensorflow we can see there is a definitive discrepancy
    in accuracy.
    
    Training for ~136 epochs (TF uses steps rather than epochs) we are able
    to achieve an accuracy of 78.7% in Tensorflow, compared to our accuracy
    which was 54.6%. This discrepancy can be accounted for due to the way in
    which TF adjusts the learning rate over time. We have already begun to
    make those changes to our code, and will be added in a separate change.
    
    * Added benchmark for image classification
    
    Added a benchmark for image classification which uses resnet50 and the
    small flowers dataset.
    
    In adding the benchmark found that ModelSavePath was not wired, and due
    to how benchmarks run I needed this, so wired that up as well.
    
    Additionally removed the sample for resnet50 as it added no value
    
    * fixed comments.
    
    * address comments.
    
    * fix build issue.

--- src/Microsoft.ML.Dnn/ImageClassificationTransform.cs
+++ src/Microsoft.ML.Dnn/ImageClassificationTransform.cs
@@ -132,7 +132,7 @@ namespace Microsoft.ML.Transforms
 
             return new ImageClassificationTransformer(env, DnnUtils.LoadTFSession(env, modelBytes), outputs, inputs,
                 addBatchDimensionInput, 1, labelColumn, checkpointName, arch,
-                scoreColumnName, predictedColumnName, learningRate, null, classCount, true, predictionTensorName,
+                scoreColumnName, predictedColumnName, learningRate, null, null, classCount, true, predictionTensorName,
                 softMaxTensorName, jpegDataTensorName, resizeTensorName, keyValueAnnotations);
 
         }
@@ -157,7 +157,7 @@ namespace Microsoft.ML.Transforms
         internal ImageClassificationTransformer(IHostEnvironment env, ImageClassificationEstimator.Options options, DnnModel tensorFlowModel, IDataView input)
             : this(env, tensorFlowModel.Session, options.OutputColumns, options.InputColumns, null, options.BatchSize,
                   options.LabelColumnName, options.FinalModelPrefix, options.Arch, options.ScoreColumnName,
-                  options.PredictedLabelColumnName, options.LearningRate, input.Schema)
+                  options.PredictedLabelColumnName, options.LearningRate, options.ModelSavePath, input.Schema)
         {
             Contracts.CheckValue(env, nameof(env));
             env.CheckValue(options, nameof(options));
@@ -775,7 +775,7 @@ namespace Microsoft.ML.Transforms
         internal ImageClassificationTransformer(IHostEnvironment env, Session session, string[] outputColumnNames,
             string[] inputColumnNames,
             bool? addBatchDimensionInput, int batchSize, string labelColumnName, string finalModelPrefix, Architecture arch,
-            string scoreColumnName, string predictedLabelColumnName, float learningRate, DataViewSchema inputSchema, int? classCount = null, bool loadModel = false,
+            string scoreColumnName, string predictedLabelColumnName, float learningRate, string modelSavePath, DataViewSchema inputSchema, int? classCount = null, bool loadModel = false,
             string predictionTensorName = null, string softMaxTensorName = null, string jpegDataTensorName = null, string resizeTensorName = null, string[] labelAnnotations = null)
             : base(Contracts.CheckRef(env, nameof(env)).Register(nameof(ImageClassificationTransformer)))
 
@@ -813,7 +813,7 @@ namespace Microsoft.ML.Transforms
             else
                 _classCount = classCount.Value;
 
-            _checkpointPath = Path.Combine(Directory.GetCurrentDirectory(), finalModelPrefix + ModelLocation[arch]);
+            _checkpointPath = modelSavePath != null ? modelSavePath : Path.Combine(Directory.GetCurrentDirectory(), finalModelPrefix + ModelLocation[arch]);
 
             // Configure bottleneck tensor based on the model.
             if (arch == ImageClassificationEstimator.Architecture.ResnetV2101)
@@ -830,6 +830,11 @@ namespace Microsoft.ML.Transforms
             {
                 _bottleneckOperationName = "import/MobilenetV2/Logits/Squeeze";
                 _inputTensorName = "import/input";
+            }
+            else if (arch == ImageClassificationEstimator.Architecture.ResnetV250)
+            {
+                _bottleneckOperationName = "resnet_v2_50/SpatialSqueeze";
+                _inputTensorName = "input";
             }
 
             _outputs = new[] { scoreColumnName, predictedLabelColumnName };
@@ -1086,7 +1091,8 @@ namespace Microsoft.ML.Transforms
         {
             ResnetV2101,
             InceptionV3,
-            MobilenetV2
+            MobilenetV2,
+            ResnetV250
         };
 
         /// <summary>
@@ -1096,7 +1102,8 @@ namespace Microsoft.ML.Transforms
         {
             { Architecture.ResnetV2101, @"resnet_v2_101_299.meta" },
             { Architecture.InceptionV3, @"InceptionV3.meta" },
-            { Architecture.MobilenetV2, @"mobilenet_v2.meta" }
+            { Architecture.MobilenetV2, @"mobilenet_v2.meta" },
+            { Architecture.ResnetV250, @"resnet_v2_50_299.meta" }
         };
 
         /// <summary>
@@ -1106,7 +1113,8 @@ namespace Microsoft.ML.Transforms
         {
             { Architecture.ResnetV2101, new Tuple<int, int>(299,299) },
             { Architecture.InceptionV3, new Tuple<int, int>(299,299) },
-            { Architecture.MobilenetV2, new Tuple<int, int>(224,224) }
+            { Architecture.MobilenetV2, new Tuple<int, int>(224,224) },
+            { Architecture.ResnetV250, new Tuple<int, int>(299,299) }
         };
 
         /// <summary>
@@ -1424,6 +1432,12 @@ namespace Microsoft.ML.Transforms
             /// </summary>
             [Argument(ArgumentType.AtMostOnce, HelpText = "Callback to report metrics during training and validation phase.", SortOrder = 15)]
             public ImageClassificationMetricsCallback MetricsCallback = null;
+
+            /// <summary>
+            /// Indicates the path where the newly retrained model should be saved.
+            /// </summary>
+            [Argument(ArgumentType.AtMostOnce, HelpText = "Indicates the path where the newly retrained model should be saved.", SortOrder = 15)]
+            public string ModelSavePath = null;
 
             /// <summary>
             /// Indicates to evaluate the model on train set after every epoch.
