471795c5190e423d6eb44dc11942b604702e2669
Aishwarya Bhandare <aibhanda@microsoft.com>
2019-10-25 13:29:24 -0700

    Added comments to ResnetV2101TransferLearningTrainTestSplit

--- docs/samples/Microsoft.ML.Samples/Dynamic/Trainers/MulticlassClassification/ImageClassification/ResnetV2101TransferLearningTrainTestSplit.cs
+++ docs/samples/Microsoft.ML.Samples/Dynamic/Trainers/MulticlassClassification/ImageClassification/ResnetV2101TransferLearningTrainTestSplit.cs
@@ -18,16 +18,17 @@ namespace Samples.Dynamic
     {
         public static void Example()
         {
+            // Set the path for input images
             string assetsRelativePath = @"../../../assets";
             string assetsPath = GetAbsolutePath(assetsRelativePath);
 
-            var outputMlNetModelFilePath = Path.Combine(assetsPath, "outputs",
-                "imageClassifier.zip");
+            //var outputMlNetModelFilePath = Path.Combine(assetsPath, "outputs",
+            //    "imageClassifier.zip");
 
             string imagesDownloadFolderPath = Path.Combine(assetsPath, "inputs",
                 "images");
 
-            //Download the image set and unzip
+            //Download the image set and unzip, set the path to image folder
             string finalImagesFolderName = DownloadImageSet(
                 imagesDownloadFolderPath);
             string fullImagesetFolderPath = Path.Combine(
@@ -38,13 +39,17 @@ namespace Samples.Dynamic
 
                 MLContext mlContext = new MLContext(seed: 1);
 
-                //Load all the original images info
+                // Load all the original images info
                 IEnumerable<ImageData> images = LoadImagesFromDirectory(
                     folder: fullImagesetFolderPath, useFolderNameAsLabel: true);
 
+                // Shuffle images
                 IDataView shuffledFullImagesDataset = mlContext.Data.ShuffleRows(
                     mlContext.Data.LoadFromEnumerable(images));
 
+                // Apply transforms to the input dataset:
+                // MapValueToKey : map 'string' type labels to keys
+                // LoadImages : load raw images to "Image" column
                 shuffledFullImagesDataset = mlContext.Transforms.Conversion
                         .MapValueToKey("Label")
                     .Append(mlContext.Transforms.LoadRawImageBytes("Image",
@@ -52,14 +57,14 @@ namespace Samples.Dynamic
                     .Fit(shuffledFullImagesDataset)
                     .Transform(shuffledFullImagesDataset);
 
-                // Split the data 90:10 into train and test sets, train and
-                // evaluate.
+                // Split the data 90:10 into train and test sets
                 TrainTestData trainTestData = mlContext.Data.TrainTestSplit(
                     shuffledFullImagesDataset, testFraction: 0.1, seed: 1);
 
                 IDataView trainDataset = trainTestData.TrainSet;
                 IDataView testDataset = trainTestData.TestSet;
 
+                // Set the options for ImageClassification
                 var options = new ImageClassificationTrainer.Options()
                 { 
                     FeatureColumnName = "Image",
@@ -75,7 +80,9 @@ namespace Samples.Dynamic
                     ValidationSet = testDataset
                 };
 
-                var pipeline = mlContext.MulticlassClassification.Trainers.ImageClassification(options)
+                // Create the ImageClassification pipeline
+                var pipeline = mlContext.MulticlassClassification.Trainers.
+                    ImageClassification(options)
                     .Append(mlContext.Transforms.Conversion.MapKeyToValue(
                         outputColumnName: "PredictedLabel", 
                         inputColumnName: "PredictedLabel"));
@@ -88,6 +95,21 @@ namespace Samples.Dynamic
                 // Measuring training time
                 var watch = System.Diagnostics.Stopwatch.StartNew();
 
+            // Train the model
+            // This involves calculating the bottleneck values, and then
+            // training the final layer. Sample output is: 
+            // Phase: Bottleneck Computation, Dataset used: Train, Image Index:   1
+            // Phase: Bottleneck Computation, Dataset used: Train, Image Index:   2
+            // ...
+            // Phase: Training, Dataset used:      Train, Batch Processed Count:  18,
+            //    Learning Rate:       0.01 Epoch:   0, Accuracy:  0.9166667, 
+            //    Cross-Entropy:  0.4866541
+            // ...
+            // Phase: Training, Dataset used:      Train, Batch Processed Count:  18,
+            //    Learning Rate:       0.01 Epoch:  49, Accuracy:          1, 
+            //    Cross-Entropy: 0.01758162
+            // Phase: Training, Dataset used: Validation, Batch Processed Count: 3, 
+            //    Epoch: 49, Accuracy: 0.8666666
                 var trainedModel = pipeline.Fit(trainDataset);
 
                 watch.Stop();
@@ -96,19 +118,27 @@ namespace Samples.Dynamic
                 Console.WriteLine("Training with transfer learning took: " +
                     (elapsedMs / 1000).ToString() + " seconds");
 
+                // Save the trained model
                 mlContext.Model.Save(trainedModel, shuffledFullImagesDataset.Schema,
                     "model.zip");
 
+                // Load the trained and saved model for prediction
                 ITransformer loadedModel;
                 DataViewSchema schema;
                 using (var file = File.OpenRead("model.zip"))
                     loadedModel = mlContext.Model.Load(file, out schema);
 
+                // Evaluate the model on the test dataset
+                // Sample output:
+                // Making bulk predictions and evaluating model's quality...
+                // Micro - accuracy: 0.851851851851852,macro - accuracy = 0.85
                 EvaluateModel(mlContext, testDataset, loadedModel);
 
                 watch = System.Diagnostics.Stopwatch.StartNew();
 
-                // Predict image class using an in-memory image.
+                // Predict on a single image class using an in-memory image.
+                // Sample output:
+                // Scores : [0.04382296,6.549581E-05,0.002181591,0.9519566,0.001973327], Predicted Label : daisy
                 TrySinglePrediction(fullImagesetFolderPath, mlContext, loadedModel);
 
                 watch.Stop();
@@ -126,6 +156,7 @@ namespace Samples.Dynamic
             Console.ReadKey();
         }
 
+        // Predict on a single image
         private static void TrySinglePrediction(string imagesForPredictions,
             MLContext mlContext, ITransformer trainedModel)
         {
@@ -134,30 +165,34 @@ namespace Samples.Dynamic
                 .CreatePredictionEngine<InMemoryImageData, 
                 ImagePrediction>(trainedModel);
 
+            // Load test images
             IEnumerable<InMemoryImageData> testImages = 
                 LoadInMemoryImagesFromDirectory(imagesForPredictions, false);
 
+            // Create an in-memory image object from the first image in the test data
             InMemoryImageData imageToPredict = new InMemoryImageData
             {
                 Image = testImages.First().Image
             };
 
+            // Predict on the single image
             var prediction = predictionEngine.Predict(imageToPredict);
 
             Console.WriteLine($"Scores : [{string.Join(",", prediction.Score)}], " +
                 $"Predicted Label : {prediction.PredictedLabel}");
         }
 
-
+        // Evaluate the trained model on the passed test dataset
         private static void EvaluateModel(MLContext mlContext,
             IDataView testDataset, ITransformer trainedModel)
         {
             Console.WriteLine("Making bulk predictions and evaluating model's " +
                 "quality...");
 
-            // Measuring time
+            // Measuring time to evaluate
             var watch2 = System.Diagnostics.Stopwatch.StartNew();
 
+            // Evaluate the model on the test data and get the evaluation metrics
             IDataView predictions = trainedModel.Transform(testDataset);
             var metrics = mlContext.MulticlassClassification.Evaluate(predictions);
 
@@ -171,6 +206,7 @@ namespace Samples.Dynamic
                 (elapsed2Ms / 1000).ToString() + " seconds");
         }
 
+        //Load the Image Data from input directory
         public static IEnumerable<ImageData> LoadImagesFromDirectory(string folder,
             bool useFolderNameAsLabel = true)
         {
@@ -205,6 +241,7 @@ namespace Samples.Dynamic
             }
         }
 
+        // Load In memory raw images from directory
         public static IEnumerable<InMemoryImageData> 
             LoadInMemoryImagesFromDirectory(string folder, 
                 bool useFolderNameAsLabel = true)
@@ -240,6 +277,7 @@ namespace Samples.Dynamic
             }
         }
 
+        // Download and unzip the image dataset
         public static string DownloadImageSet(string imagesDownloadFolder)
         {
             // get a set of images to teach the network about the new classes
@@ -257,6 +295,7 @@ namespace Samples.Dynamic
             return Path.GetFileNameWithoutExtension(fileName);
         }
 
+        // Download file to destination directory from input URL
         public static bool Download(string url, string destDir, string destFileName)
         {
             if (destFileName == null)
@@ -286,6 +325,7 @@ namespace Samples.Dynamic
             return true;
         }
 
+        // Unzip the file to destination folder
         public static void UnZip(String gzArchiveName, String destFolder)
         {
             var flag = gzArchiveName.Split(Path.DirectorySeparatorChar)
@@ -312,6 +352,7 @@ namespace Samples.Dynamic
             Console.WriteLine("Extracting is completed.");
         }
 
+        // Get absolute path from relative path
         public static string GetAbsolutePath(string relativePath)
         {
             FileInfo _dataRoot = new FileInfo(typeof(
@@ -324,6 +365,7 @@ namespace Samples.Dynamic
             return fullPath;
         }
 
+        // InMemoryImageData class holding the raw image byte array and label
         public class InMemoryImageData
         {
             [LoadColumn(0)]
@@ -333,6 +375,7 @@ namespace Samples.Dynamic
             public string Label;
         }
 
+        // ImageData class holding the imagepath and label
         public class ImageData
         {
             [LoadColumn(0)]
@@ -342,6 +385,7 @@ namespace Samples.Dynamic
             public string Label;
         }
 
+        // ImagePrediction class holding the score and predicted label metrics
         public class ImagePrediction
         {
             [ColumnName("Score")]
