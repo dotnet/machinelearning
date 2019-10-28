e5218a141dcdd8c745264bcdf3b422fa9b317689
Maryam Ariyan <maryam.ariyan@microsoft.com>
2019-10-28 10:42:15 -0700

    nit fix: formatting issue

--- src/Microsoft.ML.CodeGenerator/CodeGenerator/CSharp/TrainerGenerators.cs
+++ src/Microsoft.ML.CodeGenerator/CodeGenerator/CSharp/TrainerGenerators.cs
@@ -576,7 +576,7 @@ namespace Microsoft.ML.CodeGenerator.CSharp
                         { "MatrixColumnIndexColumnName","matrixColumnIndexColumnName" },
                         { "MatrixRowIndexColumnName","matrixRowIndexColumnName" },
                         { "LabelColumnName","labelColumnName" }
-                };
+                    };
                 }
             }
 
