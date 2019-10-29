5f780be4cc2ae6ff4ca305cd3e73e7d44fff7dca
Frank Dong <frdong@microsoft.com>
2019-10-29 14:24:21 -0700

    use relative path

--- test/Microsoft.ML.NugetPackageVersionUpdater/get-latest-package-version.bat
+++ test/Microsoft.ML.NugetPackageVersionUpdater/get-latest-package-version.bat
@@ -1,1 +1,1 @@
-$(Build.SourcesDirectory)\Tools\nuget.exe list Microsoft.ML -Source https://pkgs.dev.azure.com/dnceng/public/_packaging/MachineLearning/nuget/v3/index.json -Prerelease > latest_versions.txt
+..\..\Tools\nuget.exe list Microsoft.ML -Source https://pkgs.dev.azure.com/dnceng/public/_packaging/MachineLearning/nuget/v3/index.json -Prerelease > latest_versions.txt
