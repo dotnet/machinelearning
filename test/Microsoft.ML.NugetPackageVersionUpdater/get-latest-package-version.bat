0d5b513c3b91025712321c33ad3eff996e32582d
Frank Dong <frdong@microsoft.com>
2019-10-29 09:59:19 -0700

    test against private nuget feed

--- test/Microsoft.ML.NugetPackageVersionUpdater/get-latest-package-version.bat
+++ test/Microsoft.ML.NugetPackageVersionUpdater/get-latest-package-version.bat
@@ -1,3 +1,1 @@
-:: nuget list Microsoft.ML -Source https://devdiv.pkgs.visualstudio.com/_packaging/MachineLearning/nuget/v3/index.json -Prerelease > latest_versions.txt
-
-nuget list ML -Source https://api.nuget.org/v3/index.json -Prerelease > latest_versions.txt
+nuget list Microsoft.ML -Source https://devdiv.pkgs.visualstudio.com/_packaging/MachineLearning/nuget/v3/index.json -Prerelease > latest_versions.txt
