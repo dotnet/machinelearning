c127e2cf5b606f014217625145e30b47c3d86958
Jeremy Tieman <jotieman@microsoft.com>
2019-10-29 12:39:00 -0700

    Added copyright header

--- src/Microsoft.ML.StandardTrainers/IValueMapper.cs
+++ src/Microsoft.ML.StandardTrainers/IValueMapper.cs
@@ -1,4 +1,8 @@
-﻿namespace Microsoft.ML.Data
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Microsoft.ML.Data
 {
     [BestFriend]
     internal delegate void ValueMapperEncrypted<TVal1, TVal2, TVal3>(in TVal1 val1, in TVal2 val2, ref TVal3 val3);
