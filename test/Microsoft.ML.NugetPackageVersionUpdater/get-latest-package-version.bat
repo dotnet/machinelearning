80368713d0392f92ff6ab798fa0601e6813cd78d
Frank Dong <frdong@microsoft.com>
2019-10-29 11:39:26 -0700

    fix path

--- test/Microsoft.ML.NugetPackageVersionUpdater/get-latest-package-version.bat
+++ test/Microsoft.ML.NugetPackageVersionUpdater/get-latest-package-version.bat
@@ -1,1 +1,1 @@
-nuget list Microsoft.ML -Source https://pkgs.dev.azure.com/dnceng/public/_packaging/MachineLearning/nuget/v3/index.json -Prerelease > latest_versions.txt
+$(Build.SourcesDirectory)\Tools\nuget.exe list Microsoft.ML -Source https://pkgs.dev.azure.com/dnceng/public/_packaging/MachineLearning/nuget/v3/index.json -Prerelease > latest_versions.txt
