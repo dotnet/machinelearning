2dda1c06bfd2010c2f532112d5785cd9b3d62037
Jeremy Tieman <jotieman@microsoft.com>
2019-10-31 17:41:22 -0700

    Moved tests to correct directory

--- test/Microsoft.ML.SEAL.Tests/SEALTests.cs
+++ test/Microsoft.ML.SEAL.Tests/SEALTests.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.IO;
+using System.Linq;
 using Microsoft.ML.RunTests;
 using Microsoft.ML.SEAL;
 using Microsoft.Research.SEAL;
@@ -34,7 +35,6 @@ namespace Microsoft.ML.Tests.SEAL
         private class TestClass
         {
             public double[] plaintext;
-            //public Ciphertext[] ciphertext;
         }
 
         [Fact]
@@ -93,7 +93,6 @@ namespace Microsoft.ML.Tests.SEAL
             }
         }
 
-        /*
         [Fact]
         public void EncryptedEvaluation()
         {
@@ -111,7 +110,27 @@ namespace Microsoft.ML.Tests.SEAL
             // algorithms which needs many data passes. Since SDCA is the case, we cache.
             data = mlContext.Data.Cache(data);
 
-            var context = GlobalContext.CKKSContext;
+            // Step 2: Create a binary classifier.
+            // We set the "Label" column as the label of the dataset, and the "Features" column as the features column.
+            var pipeline = mlContext.BinaryClassification.Trainers.SdcaLogisticRegression(labelColumnName: "Label", featureColumnName: "Features", l2Regularization: 0.001f);
+
+            // Step 3: Train the pipeline created.
+            var unencryptedModel = pipeline.Fit(data);
+
+            var coeffModuli = new[] {
+                new SmallModulus(36028797005856769),
+                new SmallModulus(36028797001138177),
+                new SmallModulus(18014398492704769),
+                new SmallModulus(18014398491918337)
+            };
+            var scale = 1152921504606846976;
+            var polyModDegree = 8192UL;
+            var encParams = new EncryptionParameters(SchemeType.CKKS)
+            {
+                PolyModulusDegree = polyModDegree,
+                CoeffModulus = coeffModuli
+            };
+            var context = new SEALContext(encParams);
             var keygen = new KeyGenerator(context);
 
             using (var fs = File.Open("secret.key", FileMode.OpenOrCreate))
@@ -124,47 +143,41 @@ namespace Microsoft.ML.Tests.SEAL
                 keygen.PublicKey.Save(fs2);
             }
 
-            var scale = 1152921504606846976;
-            var polyModDegree = 8192;
-            var bitSizes = new[] {
-                36028797005856769,
-                36028797001138177,
-                18014398492704769,
-                18014398491918337
-            };
-
-            var encryptPipeline = Microsoft.ML.SEAL.SealCatalog.EncryptFeatures(true, scale, polyModDegree, "public.key", bitSizes, "ciphertext", "plaintext");
+            var encryptPipeline = ML.Transforms.EncryptFeatures(true,
+                scale,
+                polyModDegree,
+                "public.key",
+                coeffModuli,
+                "Ciphertext",
+                "Features");
 
             // Step 2: Create a binary classifier.
             // We set the "Label" column as the label of the dataset, and the "Features" column as the features column.
-            var esdcaPipeline = encryptPipeline.Append(mlContext.BinaryClassification.Trainers.EncryptedSdcaLogisticRegression(polyModDegree: polyModDegree,
-                bitSizes: bitSizes, scale: scale, labelColumnName: "Label", featureColumnName: "Features", l2Regularization: 0.001f));
+            var esdcaPipeline = encryptPipeline.Append(mlContext.BinaryClassification.Trainers.EncryptedSdcaLogisticRegression(polyModulusDegree: polyModDegree,
+                coeffModuli: coeffModuli, scale: scale, labelColumnName: "Label", featureColumnName: "Ciphertext", l2Regularization: 0.001f));
 
-            var decryptPipeline = esdcaPipeline.Append(Microsoft.ML.SEAL.SealCatalog.EncryptFeatures(false, scale, polyModDegree, "private.key", bitSizes,
-                "plaintext", "ciphertext"));
+            var decryptPipeline = esdcaPipeline.Append(encryptPipeline.Append(ML.Transforms.EncryptFeatures(false,
+                scale,
+                polyModDegree,
+                "secret.key",
+                coeffModuli,
+                "Plaintext",
+                "Label")));
 
             // Step 3: Train the pipeline created.
-            var model = decryptPipeline.Fit(data);
+            var encryptedModel = decryptPipeline.Fit(data);
 
             // Step 4: Make prediction and evaluate its quality (on training set).
-            var prediction = model.Transform(data);
-            var metrics = mlContext.BinaryClassification.Evaluate(prediction);
+            var unencryptedPrediction = unencryptedModel.Transform(data);
+            var encryptedPrediction = encryptedModel.Transform(data);
+            var rawUnencryptedPrediction = mlContext.Data.CreateEnumerable<SamplesUtils.DatasetUtils.CalibratedBinaryClassifierOutput>(unencryptedPrediction, false);
+            var rawEncryptedPrediction = mlContext.Data.CreateEnumerable<SamplesUtils.DatasetUtils.CalibratedBinaryClassifierOutput>(encryptedPrediction, false);
+            var bothPredictions = rawUnencryptedPrediction.Zip(rawEncryptedPrediction, (u, e) => new { unencrypted = u, encrypted = e });
 
-            // Check a few metrics to make sure the trained model is ok.
-            Assert.InRange(metrics.AreaUnderRocCurve, 0.9, 1);
-            Assert.InRange(metrics.LogLoss, 0, 0.5);
-
-            var rawPrediction = mlContext.Data.CreateEnumerable<SamplesUtils.DatasetUtils.CalibratedBinaryClassifierOutput>(prediction, false);
-
-            // Step 5: Inspect the prediction of the first example.
-            var first = rawPrediction.First();
-            // This is a positive example.
-            Assert.True(first.Label);
-            // Positive example should have non-negative score.
-            Assert.True(first.Score > 0);
-            // Positive example should have high probability of belonging the positive class.
-            Assert.InRange(first.Probability, 0.8, 1);
+            foreach (var rawPrediction in bothPredictions)
+            {
+                Assert.Equal(rawPrediction.unencrypted, rawPrediction.encrypted);
+            }
         }
-        */
     }
 }
