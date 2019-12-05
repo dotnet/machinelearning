dacc3bd300bcd692c4d0cd185f79a7b90fd83d8d
Yael Dekel <yaeld@microsoft.com>
2019-12-05 14:39:18 +0200

    Undo changes in Samples project

--- docs/samples/Microsoft.ML.Samples/Program.cs
+++ docs/samples/Microsoft.ML.Samples/Program.cs
@@ -13,8 +13,6 @@ namespace Microsoft.ML.Samples
             int samples = 0;
             foreach (var type in Assembly.GetExecutingAssembly().GetTypes())
             {
-                if (type.Name != "ResnetV2101TransferLearningTrainTestSplit")
-                    continue;
                 var sample = type.GetMethod("Example", BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy);
 
                 if (sample != null)
