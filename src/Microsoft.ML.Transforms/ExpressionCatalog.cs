09bab0b40af83f8777a5ed48ef5f5b9ebe0c6c7b
Yael Dekel <yaeld@microsoft.com>
2020-01-01 13:39:39 +0200

    Fix documentation and some code review comments

--- src/Microsoft.ML.Transforms/ExpressionCatalog.cs
+++ src/Microsoft.ML.Transforms/ExpressionCatalog.cs
@@ -17,6 +17,13 @@ namespace Microsoft.ML
         /// This column's data type will be the same as that of the input column.</param>
         /// <param name="expression">The expression to apply to <paramref name="inputColumnNames"/> to create the column <paramref name="outputColumnName"/>.</param>
         /// <param name="inputColumnNames">The names of the input columns.</param>
+        /// <example>
+        /// <format type="text/markdown">
+        /// <![CDATA[
+        /// [!code-csharp[Expression](~/../docs/samples/docs/samples/Microsoft.ML.Samples/Dynamic/Transforms/Expression.cs)]
+        /// ]]>
+        /// </format>
+        /// </example>
         public static ExpressionEstimator Expression(this TransformsCatalog catalog, string outputColumnName, string expression, params string[] inputColumnNames)
             => new ExpressionEstimator(CatalogUtils.GetEnvironment(catalog), new ExpressionEstimator.ColumnOptions(outputColumnName, inputColumnNames, expression));
     }
