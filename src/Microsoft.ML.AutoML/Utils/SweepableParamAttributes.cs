43af6e7a271331270ac610a3ad1ed96102292dc0
LittleLittleCloud <bigmiao.zhang@gmail.com>
2020-01-09 10:28:55 -0800

    add invariantculture to both side

--- src/Microsoft.ML.AutoML/Utils/SweepableParamAttributes.cs
+++ src/Microsoft.ML.AutoML/Utils/SweepableParamAttributes.cs
@@ -145,7 +145,7 @@ namespace Microsoft.ML.AutoML
 
         public override void SetUsingValueText(string valueText)
         {
-            RawValue = float.Parse(valueText);
+            RawValue = float.Parse(valueText, CultureInfo.InvariantCulture);
         }
 
         public override SweepableParam Clone() =>
