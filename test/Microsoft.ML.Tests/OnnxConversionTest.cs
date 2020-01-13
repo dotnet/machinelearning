1b6cb4f142553606b62bcf6520981c34aca2906b
Harish S. Kulkarni <harishsk@microsoft.com>
2020-01-13 11:05:53 -0800

    Added custom data transform for OnnxTransform to fix output schema format

--- test/Microsoft.ML.Tests/OnnxConversionTest.cs
+++ test/Microsoft.ML.Tests/OnnxConversionTest.cs
@@ -1322,14 +1322,12 @@ namespace Microsoft.ML.Tests
                 new TextLoader.Column("Shape", DataKind.Int32, 3),
                 new TextLoader.Column("Adhesion", DataKind.Int32, 4),
                 new TextLoader.Column("EpithelialSize", DataKind.Int32, 5),
-                new TextLoader.Column("BareNuclei", DataKind.Single, 6),
                 new TextLoader.Column("BlandChromatin", DataKind.Int32, 7),
                 new TextLoader.Column("NormalNucleoli", DataKind.Int32, 8),
                 new TextLoader.Column("Mitoses", DataKind.Int32, 9),
             });
 
-            var pipeline = mlContext.Transforms.ReplaceMissingValues("BareNuclei")
-                           .Append(mlContext.Transforms.SelectColumns(new[] { "Size", "Shape", "Thickness", "Label" }));
+            var pipeline = mlContext.Transforms.SelectColumns(new[] { "Size", "Shape", "Thickness", "Label" });
 
             var model = pipeline.Fit(dataView);
             var transformedData = model.Transform(dataView);
@@ -1349,6 +1347,8 @@ namespace Microsoft.ML.Tests
                 var onnxTransformer = onnxEstimator.Fit(dataView);
                 var onnxResult = onnxTransformer.Transform(dataView);
 
+                // Verify that onnx output has only the four columns we selected from the input
+                Assert.Equal(4, outputNames.Length);
                 Assert.Equal("Size1", outputNames[0]);
                 Assert.Equal("Shape1", outputNames[1]);
                 Assert.Equal("Thickness1", outputNames[2]);
@@ -1359,6 +1359,13 @@ namespace Microsoft.ML.Tests
                 CompareSelectedScalarColumns<int>("Thickness", "Thickness1", transformedData, onnxResult);
                 CompareSelectedScalarColumns<bool>("Label", "Label1", transformedData, onnxResult);
             }
+
+            onnxFileName = "SelectColumns.txt";
+            var subDir = Path.Combine("..", "..", "BaselineOutput", "Common", "Onnx", "Transforms");
+            var onnxTextModelPath = GetOutputPath(subDir, onnxFileName);
+            SaveOnnxModel(onnxModel, null, onnxTextModelPath);
+            CheckEquality(subDir, onnxFileName, digitsOfPrecision: 1);
+
             Done();
         }
 
