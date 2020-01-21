72297604a93ac1f40edc5464d9e173d33e379e59
Antonio Velazquez <anvelazq@microsoft.com>
2020-01-21 11:04:11 -0800

    Remove MoveMany() comment from TrainerUtils

--- src/Microsoft.ML.Data/Training/TrainerUtils.cs
+++ src/Microsoft.ML.Data/Training/TrainerUtils.cs
@@ -564,7 +564,7 @@ namespace Microsoft.ML.Trainers
         /// <summary>
         /// This fetches and validates values for the standard active columns.
         /// It is called automatically by MoveNext(). Client code should only need
-        /// to deal with this if it calls MoveNext() or MoveMany() on the underlying
+        /// to deal with this if it calls MoveNext() on the underlying
         /// <see cref="DataViewRowCursor"/> directly. That is, this is only for very advanced scenarios.
         /// </summary>
         public virtual bool Accept()
