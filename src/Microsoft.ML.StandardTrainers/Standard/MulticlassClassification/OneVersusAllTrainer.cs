e9a7af41525bb234f7f8c74abe152196da126ba9
Keren Fuentes <dkeren@seas.upenn.edu>
2020-01-23 15:52:41 -0800

    reformatted spacing

--- src/Microsoft.ML.StandardTrainers/Standard/MulticlassClassification/OneVersusAllTrainer.cs
+++ src/Microsoft.ML.StandardTrainers/Standard/MulticlassClassification/OneVersusAllTrainer.cs
@@ -546,6 +546,7 @@ namespace Microsoft.ML.Trainers
                     predictorOutputNames[0] = ctx.AddIntermediateVariable(NumberDataViewType.UInt32, $"{DefaultColumnNames.PredictedLabel}_{i}", true);
                     predictorOutputNames[1] = ctx.AddIntermediateVariable(NumberDataViewType.Single, $"{DefaultColumnNames.Score}_{i}", true);
                     predictorOutputNames[2] = ctx.AddIntermediateVariable(NumberDataViewType.Single, $"{DefaultColumnNames.Probability}_{i}", true);
+
                     string clipInput = predictorOutputNames[2];
 
                     var pred = Predictors[i] as ISingleCanSaveOnnx;
@@ -658,7 +659,6 @@ namespace Microsoft.ML.Trainers
 
             public override bool SaveAsOnnx(OnnxContext ctx, string[] outputNames, string featureColumn)
             {
-
                 var probabilityOutputs = base.SaveAsOnnxPreProcess(ctx, featureColumn, false);
 
                 string opType = "Concat";
