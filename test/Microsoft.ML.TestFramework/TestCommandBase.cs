7e6e194f7d756cb245e658e9ae804ecf591684f2
Mustafa Bal <mubal@microsoft.com>
2020-01-29 16:02:21 +0300

    Updated type of CustomGains

--- test/Microsoft.ML.TestFramework/TestCommandBase.cs
+++ test/Microsoft.ML.TestFramework/TestCommandBase.cs
@@ -1271,7 +1271,10 @@ namespace Microsoft.ML.RunTests
             var dataPath = CreateOutputPath("Traindata.txt");
             File.WriteAllLines(dataPath.Path, new string[] {
                 "0,0,1,2", "3,0,4,5", "4,0,5,6", "0,1,1,2", "3,1,4,5", "5,1,5,6" });
+            // This customGains variable is obsolete, and its usage is discouraged in lieu of customGainDouble.
+            // The type of customGains (string) mismatches with the type of customGains (double) in ranking trainers.
             const string customGains = "0,3,7,15,35,63";
+            double[] customGainsDouble = new double[] { 0, 3, 7, 15, 35, 63 };
 
             RunMTAThread(() =>
             {
