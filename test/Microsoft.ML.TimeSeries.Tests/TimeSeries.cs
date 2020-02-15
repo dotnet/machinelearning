f59f09bc742fd4518640ee780fdcb8da34a09546
frank-dong-ms <55860649+frank-dong-ms@users.noreply.github.com>
2020-02-14 16:19:07 -0800

    clean using

--- test/Microsoft.ML.TimeSeries.Tests/TimeSeries.cs
+++ test/Microsoft.ML.TimeSeries.Tests/TimeSeries.cs
@@ -2,11 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using System.IO;
 using System.Linq;
 using Microsoft.ML.TestFramework.Attributes;
-using Microsoft.ML.TestFrameworkCommon.Attributes;
 using Microsoft.ML.Transforms.TimeSeries;
 using Xunit;
 using Xunit.Abstractions;
