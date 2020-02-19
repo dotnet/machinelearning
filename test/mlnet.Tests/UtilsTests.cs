67badab1c803585aaaa729cd664658c1a82e6e62
zHaytam <haytam.zanid@gmail.com>
2020-02-19 21:12:03 +0100

    Add SanitizeTest in UtilsTests
    This tests a couple of cases to check if Sanitize is working properly

---
+++ test/mlnet.Tests/UtilsTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.ML.CLI.Utilities;
+
+namespace mlnet.Tests
+{
+    [TestClass]
+    public class UtilsTests
+    {
+
+        [DataTestMethod]
+        [DataRow("NameWithoutSpaces", '_', "NameWithoutSpaces")]
+        [DataRow("Name With Spaces", '.', "Name.With.Spaces")]
+        [DataRow("학습유형들에", '_', "학습유형들에")]
+        public void SanitizeTest(string input, char replacement, string expected)
+        {
+            Assert.AreEqual(Utils.Sanitize(input, replacement), expected);
+        }
+
+    }
+}
