1d904397eb86666c9563e59f2ffdf17bbc591381
Mustafa Bal <balmustafa117@gmail.com>
2020-03-13 12:01:58 -0700

    Added hanging test mem dump feature (#4931)
    * Added ability to collect memory dumps on hanging and crashing tests
    
    Added ability to collect memory dumps on hanging and crashing tests through VSTest Tasks and Procdump. Also added an example VSTest version of the ColumnTypes XUnit test file.
    
    * Update ColumnTypesVSTask.cs
    
    * Improvements
    
    * Build changes
    
    * Removed example VSTest and added explanation in developer guide for collecting memory dumps
    
    * Added downloading procdump.zip instead of adding to repo directly

--- init-tools.cmd
+++ init-tools.cmd
@@ -67,6 +67,13 @@ if NOT [%AGENT_ID%] == [] (
   reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\Windows Error Reporting\LocalDumps" /f /v DumpFolder /t REG_SZ /d "%~dp0CrashDumps"
 )
 
+:: install procdump.exe to take process dump when test crashes, hangs or fails
+echo Installing procdump.exe
+powershell -Command "Invoke-WebRequest https://download.sysinternals.com/files/Procdump.zip -UseBasicParsing -outfile procdump.zip | Out-Null"
+powershell -Command "Expand-Archive -Force procdump.zip Tools"
+del /f procdump.zip
+echo Finish install procdump.exe
+
 :: install the extra runtime first, so the SDK install will overwrite the root dotnet executable
 echo Installing dotnet runtime %DOTNET_EXTRA_RUNTIME_VERSION%...
 set DOTNET_EXTRA_RUNTIME_ZIP_NAME=dotnet-runtime-%DOTNET_EXTRA_RUNTIME_VERSION%-win-%ARCH%.zip
