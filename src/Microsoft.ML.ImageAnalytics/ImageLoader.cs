22a2107d3e796781f8ffe26a7b062cdeb1f14d4a
Mustafa Bal <mubal@microsoft.com>
2020-03-26 12:43:50 -0700

    The provided imageFolder path may be null or empty

--- src/Microsoft.ML.ImageAnalytics/ImageLoader.cs
+++ src/Microsoft.ML.ImageAnalytics/ImageLoader.cs
@@ -103,14 +103,16 @@ namespace Microsoft.ML.Data
         internal ImageLoadingTransformer(IHostEnvironment env, string imageFolder = null, bool type = true, params (string outputColumnName, string inputColumnName)[] columns)
             : base(Contracts.CheckRef(env, nameof(env)).Register(nameof(ImageLoadingTransformer)), columns)
         {
-            // Throws ArgumentException  if given imageFolder path is invalid or empty. Note: imageFolder may be null in this case.
-            if (imageFolder != null)
+            // Throws ArgumentException if given imageFolder path is invalid. Note: imageFolder may be null or empty in this case.
+            if (String.IsNullOrEmpty(imageFolder))
+                ImageFolder = null;
+            else
+            {
                 if (Directory.Exists(imageFolder))
                     ImageFolder = Path.GetFullPath(imageFolder);
                 else
                     throw new ArgumentException(String.Format("Directory \"{0}\" does not exist.", imageFolder));
-            else
-                ImageFolder = null;
+            }
             _useImageType = type;
         }
 
