79c95d34e0d464c4eae9da455bd4b5d3cfc1d16e
Michael Sharp <51342856+michaelgsharp@users.noreply.github.com>
2020-04-16 16:05:36 -0700

    Short grain dropper (#5034)
    * rabase on master
    
    * changes due to pr comments

---
+++ test/Microsoft.ML.Core.Tests/UnitTests/TestFeaturizersEntryPoints.cs
@@ -0,0 +1,250 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Microsoft.ML.Calibrators;
+using Microsoft.ML.Core.Tests.UnitTests;
+using Microsoft.ML.Data;
+using Microsoft.ML.Data.IO;
+using Microsoft.ML.EntryPoints;
+using Microsoft.ML.Featurizers;
+using Microsoft.ML.Internal.Utilities;
+using Microsoft.ML.Model;
+using Microsoft.ML.Model.OnnxConverter;
+using Microsoft.ML.Runtime;
+using Microsoft.ML.TestFramework.Attributes;
+using Microsoft.ML.TestFrameworkCommon;
+using Microsoft.ML.TestFrameworkCommon.Attributes;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Trainers.Ensemble;
+using Microsoft.ML.Trainers.FastTree;
+using Microsoft.ML.Trainers.LightGbm;
+using Microsoft.ML.Transforms;
+using Microsoft.ML.Transforms.Text;
+using Microsoft.ML.Transforms.TimeSeries;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Xunit;
+using Xunit.Abstractions;
+
+using Microsoft.ML.RunTests;
+
+using Microsoft.ML.Tests.Transformers;
+namespace Microsoft.ML.RunTests
+{
+    public class TestFeaturizersEntryPoints : CoreBaseTestClass
+    {
+        public TestFeaturizersEntryPoints(ITestOutputHelper output) : base(output)
+        {
+            Env.ComponentCatalog.RegisterAssembly(typeof(ShortGrainDropperEstimator).Assembly);
+        }
+
+        [NotCentOS7Fact]
+        public void ShortDrop_LargeNumberTest()
+        {
+            MLContext mlContext = new MLContext(1);
+            var dataList = new[] {
+                new { Grain = "one", Target = 1.0 },
+                new { Grain = "one", Target = 2.0 },
+                new { Grain = "one", Target = 3.0 },
+                new { Grain = "one", Target = 4.0 }
+            };
+            var data = mlContext.Data.LoadFromEnumerable(dataList);
+
+            string inputGraph = @"
+            {
+                'Nodes':
+                [{
+                    'Name': 'Transforms.ShortDrop',
+                    'Inputs': {
+                            'GrainColumns': ['Grain'],
+                            'Data' : '$data',
+                            'MinRows' : 4294967294
+                    },
+                    'Outputs' : {
+                        'OutputData': '$outputData',
+                        'Model': '$Var_test'
+                    }
+                    }
+                ]
+            }
+            ";
+
+            JObject graph = JObject.Parse(inputGraph);
+
+            var runner = new GraphRunner(Env, graph[FieldNames.Nodes] as JArray);
+
+            runner.SetInput("data", data);
+            runner.RunAll();
+
+            var output = runner.GetOutput<IDataView>("outputData");
+            var schema = output.Schema;
+
+            // Output schema should equal input schema.
+            Assert.Equal(data.Schema, schema);
+            var debugView = output.Preview();
+            var rows = debugView.RowView;
+            var cols = debugView.ColumnView;
+
+            // Since min row count is 4294967294 and we only have 1, should have no rows back.
+            Assert.True(rows.Length == 0);
+
+            // Also make sure that the value column was dropped correctly.
+            Assert.True(cols[0].Values.Length == 0);
+            Assert.True(cols[1].Values.Length == 0);
+
+            Done();
+        }
+        [NotCentOS7Fact]
+        public void ShortDrop_EntryPointTest()
+        {
+            MLContext mlContext = new MLContext(1);
+            var dataList = new[] {
+                new { Grain = "one", Target = 1.0 },
+                new { Grain = "one", Target = 2.0 },
+                new { Grain = "one", Target = 3.0 },
+                new { Grain = "one", Target = 4.0 }
+            };
+            var data = mlContext.Data.LoadFromEnumerable(dataList);
+
+            string inputGraph = @"
+            {
+                'Nodes':
+                [{
+                    'Name': 'Transforms.ShortDrop',
+                    'Inputs': {
+                            'GrainColumns': ['Grain'],
+                            'Data' : '$data',
+                            'MinRows' : 2
+                    },
+                    'Outputs' : {
+                        'OutputData': '$outputData',
+                        'Model': '$Var_test'
+                    }
+                    }
+                ]
+            }
+            ";
+
+            JObject graph = JObject.Parse(inputGraph);
+
+            var runner = new GraphRunner(Env, graph[FieldNames.Nodes] as JArray);
+
+            runner.SetInput("data", data);
+            runner.RunAll();
+
+            Done();
+        }
+        [NotCentOS7Fact]
+        public void ShortDrop_Drop()
+        {
+            MLContext mlContext = new MLContext(1);
+            var dataList = new[] {
+                new { Grain = "one", Target = 1.0 }
+            };
+            var data = mlContext.Data.LoadFromEnumerable(dataList);
+
+            string inputGraph = @"
+            {
+                'Nodes':
+                [{
+                    'Name': 'Transforms.ShortDrop',
+                    'Inputs': {
+                            'GrainColumns': ['Grain'],
+                            'Data' : '$data',
+                            'MinRows' : 2
+                    },
+                    'Outputs' : {
+                        'OutputData': '$outputData',
+                        'Model': '$Var_test'
+                    }
+                    }
+                ]
+            }
+            ";
+
+            JObject graph = JObject.Parse(inputGraph);
+
+            var runner = new GraphRunner(Env, graph[FieldNames.Nodes] as JArray);
+
+            runner.SetInput("data", data);
+            runner.RunAll();
+
+            var output = runner.GetOutput<IDataView>("outputData");
+            var schema = output.Schema;
+
+            // Output schema should equal input schema.
+            Assert.Equal(data.Schema, schema);
+            var debugView = output.Preview();
+            var rows = debugView.RowView;
+            var cols = debugView.ColumnView;
+
+            // Since min row count is 2 and we only have 1, should have no rows back.
+            Assert.True(rows.Length == 0);
+
+            // Also make sure that the value column was dropped correctly.
+            Assert.True(cols[0].Values.Length == 0);
+            Assert.True(cols[1].Values.Length == 0);
+
+            Done();
+        }
+        [NotCentOS7Fact]
+        public void ShortDrop_Keep()
+        {
+            MLContext mlContext = new MLContext(1);
+            var dataList = new[] {
+                new { Grain = "one", Target = 1 },
+                new { Grain = "one", Target = 1 }
+            };
+            var data = mlContext.Data.LoadFromEnumerable(dataList);
+
+            string inputGraph = @"
+            {
+                'Nodes':
+                [{
+                    'Name': 'Transforms.ShortDrop',
+                    'Inputs': {
+                            'GrainColumns': ['Grain'],
+                            'Data' : '$data',
+                            'MinRows' : 2
+                    },
+                    'Outputs' : {
+                        'OutputData': '$outputData',
+                        'Model': '$Var_test'
+                    }
+                    }
+                ]
+            }
+            ";
+
+            JObject graph = JObject.Parse(inputGraph);
+
+            var runner = new GraphRunner(Env, graph[FieldNames.Nodes] as JArray);
+
+            runner.SetInput("data", data);
+            runner.RunAll();
+            var output = runner.GetOutput<IDataView>("outputData");
+            var schema = output.Schema;
+
+            // Output schema should equal input schema.
+            Assert.Equal(data.Schema, schema);
+            var debugView = output.Preview();
+            var rows = debugView.RowView;
+            var cols = debugView.ColumnView;
+
+            // Since min row count is 2 and we have 2, should have all rows back.
+            Assert.True(rows.Length == 2);
+
+            // Also make sure that the value column was dropped correctly.
+            Assert.True(cols[0].Values.Length == 2);
+            Assert.True(cols[1].Values.Length == 2);
+
+            Done();
+        }
+    }
+}
---
+++ test/Microsoft.ML.Tests/Transformers/ShortGrainDropperTests.cs
@@ -0,0 +1,211 @@
+﻿using Microsoft.ML.Data;
+using Microsoft.ML.Featurizers;
+using Microsoft.ML.RunTests;
+using System;
+using System.Collections.Generic;
+using Xunit;
+using Xunit.Abstractions;
+using System.Linq;
+using Microsoft.ML.TestFramework.Attributes;
+
+namespace Microsoft.ML.Tests.Transformers
+{
+    public class ShortGrainDropperTests : TestDataPipeBase
+    {
+        public ShortGrainDropperTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [NotCentOS7Fact]
+        public void TestInvalidGrainTypes()
+        {
+            MLContext mlContext = new MLContext(1);
+            var dataList = new[] { new { GrainA = 4 } };
+            var data = mlContext.Data.LoadFromEnumerable(dataList);
+
+            // Build the pipeline, should error on fit and on GetOutputSchema
+            var pipeline = mlContext.Transforms.DropShortGrains(new string[] { "GrainA" }, 2);
+
+            Assert.Throws<InvalidOperationException>(() => pipeline.Fit(data));
+            Assert.Throws<InvalidOperationException>(() => pipeline.GetOutputSchema(SchemaShape.Create(data.Schema)));
+
+            Done();
+        }
+
+        [NotCentOS7Fact]
+        public void ConstructorParamterTest()
+        {
+            MLContext mlContext = new MLContext(1);
+            var dataList = new[] { new { GrainA = 4 } };
+            var data = mlContext.Data.LoadFromEnumerable(dataList);
+
+            // Make sure invalid constructor args throw.
+            Assert.Throws<InvalidOperationException>(() => mlContext.Transforms.DropShortGrains(new string[] { "GrainA" }, 0));
+            Assert.Throws<ArgumentOutOfRangeException>(() => mlContext.Transforms.DropShortGrains(new string[] { }, 2));
+            Assert.Throws<ArgumentNullException>(() => mlContext.Transforms.DropShortGrains(null, 2));
+
+            Done();
+        }
+
+        [NotCentOS7Fact]
+        public void SimpleDropTest()
+        {
+            MLContext mlContext = new MLContext(1);
+            var dataList = new[] { new { GrainA = "Grain", Value = 0 } };
+            var data = mlContext.Data.LoadFromEnumerable(dataList);
+
+            var pipeline = mlContext.Transforms.DropShortGrains(new string[] { "GrainA" }, 2);
+            var model = pipeline.Fit(data);
+            var output = model.Transform(data);
+            var schema = output.Schema;
+
+            // Output schema should equal input schema.
+            Assert.Equal(data.Schema, schema);
+            var debugView = output.Preview();
+            var rows = debugView.RowView;
+            var cols = debugView.ColumnView;
+
+            // Since min row count is 2 and we only have 1, should have no rows back.
+            Assert.True(rows.Length == 0);
+
+            // Also make sure that the value column was dropped correctly.
+            Assert.True(cols[0].Values.Length == 0);
+            Assert.True(cols[1].Values.Length == 0);
+
+            TestEstimatorCore(pipeline, data);
+            Done();
+        }
+
+        [NotCentOS7Fact]
+        public void SimpleKeepTest()
+        {
+            MLContext mlContext = new MLContext(1);
+            var dataList = new[] {
+                new { GrainA = "Grain", Value = 0 },
+                new { GrainA = "Grain", Value = 1 }
+            };
+            var data = mlContext.Data.LoadFromEnumerable(dataList);
+
+            var pipeline = mlContext.Transforms.DropShortGrains(new string[] { "GrainA" }, 2);
+            var model = pipeline.Fit(data);
+            var output = model.Transform(data);
+            var schema = output.Schema;
+
+            // Output schema should equal input schema.
+            Assert.Equal(data.Schema, schema);
+            var debugView = output.Preview();
+            var rows = debugView.RowView;
+            var cols = debugView.ColumnView;
+
+            // Since min row count is 2 and we have 2, should have all rows back.
+            Assert.True(rows.Length == 2);
+
+            // Also make sure that the value column was kept correctly.
+            Assert.True(cols[0].Values.Length == 2);
+            Assert.True(cols[1].Values.Length == 2);
+
+            TestEstimatorCore(pipeline, data);
+            Done();
+        }
+
+        [NotCentOS7Fact]
+        public void MultipleGrainsKeepTest()
+        {
+            MLContext mlContext = new MLContext(1);
+            var dataList = new[] {
+                new { GrainA = "GrainA", Value = 0 },
+                new { GrainA = "GrainB", Value = 1 },
+                new { GrainA = "GrainA", Value = 2 },
+                new { GrainA = "GrainB", Value = 3 }
+            };
+            var data = mlContext.Data.LoadFromEnumerable(dataList);
+
+            var pipeline = mlContext.Transforms.DropShortGrains(new string[] { "GrainA" }, 2);
+            var model = pipeline.Fit(data);
+            var output = model.Transform(data);
+            var schema = output.Schema;
+
+            // Output schema should equal input schema.
+            Assert.Equal(data.Schema, schema);
+            var debugView = output.Preview();
+            var rows = debugView.RowView;
+            var cols = debugView.ColumnView;
+
+            // Since min row count is 2 and we have 2 of grain A and 2 of grain B, should have all rows back.
+            Assert.True(rows.Length == 4);
+
+            // Also make sure that the value column was kept correctly.
+            Assert.True(cols[0].Values.Length == 4);
+            Assert.True(cols[1].Values.Length == 4);
+
+            TestEstimatorCore(pipeline, data);
+            Done();
+        }
+
+        [NotCentOS7Fact]
+        public void MultipleGrainsPartialDropTest()
+        {
+            MLContext mlContext = new MLContext(1);
+            var dataList = new[] {
+                new { GrainA = "GrainA", Value = 0 },
+                new { GrainA = "GrainB", Value = 0 },
+                new { GrainA = "GrainA", Value = 1 }
+            };
+            var data = mlContext.Data.LoadFromEnumerable(dataList);
+
+            var pipeline = mlContext.Transforms.DropShortGrains(new string[] { "GrainA" }, 2);
+            var model = pipeline.Fit(data);
+            var output = model.Transform(data);
+            var schema = output.Schema;
+
+            // Output schema should equal input schema.
+            Assert.Equal(data.Schema, schema);
+            var debugView = output.Preview();
+            var rows = debugView.RowView;
+            var cols = debugView.ColumnView;
+
+            // Since min row count is 2 and we have 2 of grain A and 1 of grain B, should have all GrainA rows back and not GrainB.
+            Assert.True(rows.Length == 2);
+
+            // Also make sure that the value column was kept correctly.
+            Assert.True(cols[0].Values.Length == 2);
+            Assert.True(cols[1].Values.Length == 2);
+
+            TestEstimatorCore(pipeline, data);
+            Done();
+        }
+
+        [NotCentOS7Fact]
+        public void MultipleGrainsAllDropTest()
+        {
+            MLContext mlContext = new MLContext(1);
+            var dataList = new[] {
+                new { GrainA = "GrainA", Value = 0 },
+                new { GrainA = "GrainB", Value = 0 },
+                new { GrainA = "GrainA", Value = 1 }
+            };
+            var data = mlContext.Data.LoadFromEnumerable(dataList);
+
+            var pipeline = mlContext.Transforms.DropShortGrains(new string[] { "GrainA" }, 3);
+            var model = pipeline.Fit(data);
+            var output = model.Transform(data);
+            var schema = output.Schema;
+
+            // Output schema should equal input schema.
+            Assert.Equal(data.Schema, schema);
+            var debugView = output.Preview();
+            var rows = debugView.RowView;
+            var cols = debugView.ColumnView;
+
+            // Since min row count is 3 and we have 3 of grain A and 1 of grain B, should have no rows back.
+            Assert.True(rows.Length == 0);
+
+            // Also make sure that the value column was kept correctly.
+            Assert.True(cols[0].Values.Length == 0);
+            Assert.True(cols[1].Values.Length == 0);
+
+            TestEstimatorCore(pipeline, data);
+            Done();
+        }
+    }
+}
