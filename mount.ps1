88a13d3aaf8f9017cb342129bd638074e57474e4
Frank Dong <frdong@microsoft.com>
2020-04-23 16:41:06 -0700

    try put nuget packages to shared folder instead of local to save disk space

---
+++ mount.ps1
@@ -0,0 +1,9 @@
+$connectTestResult = Test-NetConnection -ComputerName nugetpackage.file.core.windows.net -Port 445
+if ($connectTestResult.TcpTestSucceeded) {
+    # Save the password so the drive will persist on reboot
+    cmd.exe /C "cmdkey /add:`"nugetpackage.file.core.windows.net`" /user:`"Azure\nugetpackage`" /pass:`"bALT8KhVcCefQFvd2Is97b5fHZpSoBL7Au0I1nAdj0hiIjikpjNoMJLjtVxVooY+INazcHDcXX4pMdF9qMBsqw==`""
+    # Mount the drive
+    New-PSDrive -Name Z -PSProvider FileSystem -Root "\\nugetpackage.file.core.windows.net\nugettestfrdong"-Persist
+} else {
+    Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
+}
