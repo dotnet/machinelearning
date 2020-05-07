fb5a88980d9d8592b6d9088b32645ed020b97a46
suxi-ms <suxi@microsoft.com>
2020-05-07 11:56:16 +0800

    update to anomalydetection catalog

--- docs/samples/Microsoft.ML.Samples/Dynamic/Transforms/TimeSeries/LocalizeRootCause.cs
+++ docs/samples/Microsoft.ML.Samples/Dynamic/Transforms/TimeSeries/LocalizeRootCause.cs
@@ -19,7 +19,7 @@ namespace Samples.Dynamic
             var data = new RootCauseLocalizationInput(timestamp, GetAnomalyDimension(), new List<MetricSlice>() { new MetricSlice(timestamp, GetPoints()) }, AggregateType.Sum, AGG_SYMBOL);
 
             // Get the root cause localization result
-            RootCause prediction = mlContext.Transforms.LocalizeRootCause(data);
+            RootCause prediction = mlContext.AnomalyDetection.LocalizeRootCause(data);
 
             // Print the localization result.
             int count = 0;
