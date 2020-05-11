bc1fd867ff2d7f1ef0fc690aafe632cd6bf24cef
Shakira Sun <61308292+suxi-ms@users.noreply.github.com>
2020-05-12 01:36:13 +0800

    add root cause localization transformer (#4925)
    * add root cause localization transformer
    
    * add test cases
    
    * revert sln changes
    
    * add evaluation
    
    * temp save for internal review
    
    * rename function
    
    * temp save bottom up points for switch desktop
    
    * update from laptop
    
    * save for add test
    
    * add root cause localization algorithm
    
    * add root cause localization algorithm
    
    * print score, path and directions in sample
    
    * merge with master
    
    * extract root cause analyzer
    
    * refine code
    
    * merge with master
    
    * add evaluatin
    
    * some refine for code
    
    * fix some typo
    
    * remove unused code
    
    * reformat code
    
    * updates
    
    * update from review
    
    * read double for beta
    
    * remove SignatureDataTransform constructor
    
    * update
    
    * update
    
    * remove white space
    
    * refine internal logic
    
    * update
    
    * update
    
    * updated test
    
    * update score
    
    * update variable name
    
    * add some comments
    
    * refine internal function
    
    * handle for infinity and nan
    
    * rename the algorithm by removing DT
    
    * Update src/Microsoft.ML.TimeSeries/RootCauseAnalyzer.cs
    
    Co-Authored-By: Justin Ormont <justinormont@users.noreply.github.com>
    
    * fix type
    
    * add an else branch when delta is negative
    
    * update model signature
    
    * update rca interface by removing transformer
    
    * add more documents
    
    * update
    
    * update
    
    * update the constructor
    
    * update comments
    
    * fix typo
    
    Co-authored-by: Justin Ormont <justinormont@users.noreply.github.com>

--- test/Microsoft.ML.TimeSeries.Tests/TimeSeriesDirectApi.cs
+++ test/Microsoft.ML.TimeSeries.Tests/TimeSeriesDirectApi.cs
@@ -1,12 +1,15 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Collections.Generic;
 using System.IO;
 using Microsoft.ML.Data;
 using Microsoft.ML.TestFramework;
 using Microsoft.ML.TestFramework.Attributes;
+using Microsoft.ML.TestFrameworkCommon;
+using Microsoft.ML.TimeSeries;
 using Microsoft.ML.Transforms.TimeSeries;
 using Xunit;
 using Xunit.Abstractions;
@@ -89,6 +92,9 @@ namespace Microsoft.ML.Tests
             [VectorType(3)]
             public double[] Prediction { get; set; }
         }
+
+        private static Object _rootCauseAggSymbol = "##SUM##";
+
 
         [Fact]
         public void ChangeDetection()
@@ -507,7 +513,7 @@ namespace Microsoft.ML.Tests
             // The forecasted results should be the same because the state of the models
             // is the same.
             Assert.Equal(result.Forecast, resultCopy.Forecast);
-            
+
         }
 
         [Theory]
@@ -563,6 +569,109 @@ namespace Microsoft.ML.Tests
                     Assert.Equal(0, prediction.Prediction[0]);
                 k += 1;
             }
+        }
+
+        [Fact]
+        public void RootCauseLocalization()
+        {
+            // Create an root cause localizatiom input
+            var rootCauseLocalizationInput = new RootCauseLocalizationInput(GetRootCauseTimestamp(), GetRootCauseAnomalyDimension(), new List<MetricSlice>() { new MetricSlice(GetRootCauseTimestamp(), GetRootCauseLocalizationPoints()) }, AggregateType.Sum, _rootCauseAggSymbol);
+
+            var ml = new MLContext(1);
+            RootCause rootCause = ml.AnomalyDetection.LocalizeRootCause(rootCauseLocalizationInput);
+
+            Assert.NotNull(rootCause);
+            Assert.Equal(1, (int)rootCause.Items.Count);
+            Assert.Equal(3, (int)rootCause.Items[0].Dimension.Count);
+            Assert.Equal(AnomalyDirection.Up, rootCause.Items[0].Direction);
+            Assert.Equal(1, (int)rootCause.Items[0].Path.Count);
+            Assert.Equal("DataCenter", rootCause.Items[0].Path[0]);
+
+            Dictionary<string, Object> expectedDim = new Dictionary<string, Object>();
+            expectedDim.Add("Country", "UK");
+            expectedDim.Add("DeviceType", _rootCauseAggSymbol);
+            expectedDim.Add("DataCenter", "DC1");
+
+            foreach (KeyValuePair<string, object> pair in rootCause.Items[0].Dimension)
+            {
+                Assert.Equal(expectedDim[pair.Key], pair.Value);
+            }
+        }
+
+        private static List<Point> GetRootCauseLocalizationPoints()
+        {
+            List<Point> points = new List<Point>();
+
+            Dictionary<string, Object> dic1 = new Dictionary<string, Object>();
+            dic1.Add("Country", "UK");
+            dic1.Add("DeviceType", "Laptop");
+            dic1.Add("DataCenter", "DC1");
+            points.Add(new Point(200, 100, true, dic1));
+
+            Dictionary<string, Object> dic2 = new Dictionary<string, Object>();
+            dic2.Add("Country", "UK");
+            dic2.Add("DeviceType", "Mobile");
+            dic2.Add("DataCenter", "DC1");
+            points.Add(new Point(1000, 100, true, dic2));
+
+            Dictionary<string, Object> dic3 = new Dictionary<string, Object>();
+            dic3.Add("Country", "UK");
+            dic3.Add("DeviceType", _rootCauseAggSymbol);
+            dic3.Add("DataCenter", "DC1");
+            points.Add(new Point(1200, 200, true, dic3));
+
+            Dictionary<string, Object> dic4 = new Dictionary<string, Object>();
+            dic4.Add("Country", "UK");
+            dic4.Add("DeviceType", "Laptop");
+            dic4.Add("DataCenter", "DC2");
+            points.Add(new Point(100, 100, false, dic4));
+
+            Dictionary<string, Object> dic5 = new Dictionary<string, Object>();
+            dic5.Add("Country", "UK");
+            dic5.Add("DeviceType", "Mobile");
+            dic5.Add("DataCenter", "DC2");
+            points.Add(new Point(200, 200, false, dic5));
+
+            Dictionary<string, Object> dic6 = new Dictionary<string, Object>();
+            dic6.Add("Country", "UK");
+            dic6.Add("DeviceType", _rootCauseAggSymbol);
+            dic6.Add("DataCenter", "DC2");
+            points.Add(new Point(300, 300, false, dic6));
+
+            Dictionary<string, Object> dic7 = new Dictionary<string, Object>();
+            dic7.Add("Country", "UK");
+            dic7.Add("DeviceType", _rootCauseAggSymbol);
+            dic7.Add("DataCenter", _rootCauseAggSymbol);
+            points.Add(new Point(1500, 500, true, dic7));
+
+            Dictionary<string, Object> dic8 = new Dictionary<string, Object>();
+            dic8.Add("Country", "UK");
+            dic8.Add("DeviceType", "Laptop");
+            dic8.Add("DataCenter", _rootCauseAggSymbol);
+            points.Add(new Point(300, 200, true, dic8));
+
+            Dictionary<string, Object> dic9 = new Dictionary<string, Object>();
+            dic9.Add("Country", "UK");
+            dic9.Add("DeviceType", "Mobile");
+            dic9.Add("DataCenter", _rootCauseAggSymbol);
+            points.Add(new Point(1200, 300, true, dic9));
+
+            return points;
+        }
+
+        private static Dictionary<string, Object> GetRootCauseAnomalyDimension()
+        {
+            Dictionary<string, Object> dim = new Dictionary<string, Object>();
+            dim.Add("Country", "UK");
+            dim.Add("DeviceType", _rootCauseAggSymbol);
+            dim.Add("DataCenter", _rootCauseAggSymbol);
+
+            return dim;
+        }
+
+        private static DateTime GetRootCauseTimestamp()
+        {
+            return new DateTime(2020, 3, 23, 0, 0, 0);
         }
     }
 }
