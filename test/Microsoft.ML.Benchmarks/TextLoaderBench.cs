098a56eaec98e9a482b623c8b18521bdb29703a7
Antonio Velazquez <anvelazq@microsoft.com>
2020-05-21 11:51:54 -0700

    Updated benchmark

--- test/Microsoft.ML.Benchmarks/TextLoaderBench.cs
+++ test/Microsoft.ML.Benchmarks/TextLoaderBench.cs
@@ -46,8 +46,8 @@ namespace Microsoft.ML.Benchmarks
                 HasHeader = false,
                 Separators = new char[] { ',' },
                 AllowQuoting = true,
-                //ReadMultilines = true,
-                //EscapeChar = '\\',
+                ReadMultilines = true,
+                EscapeChar = '\\',
             });
 
             _dataView = textLoader.Load(path);
@@ -72,31 +72,34 @@ namespace Microsoft.ML.Benchmarks
                 }
             }
 
+            //* Summary *
+
             //BenchmarkDotNet = v0.12.0, OS = Windows 10.0.18363
             //Intel Core i7 - 8650U CPU 1.90GHz(Kaby Lake R), 1 CPU, 8 logical and 4 physical cores
             //.NET Core SDK = 3.1.100 - preview3 - 014645
             //    [Host]     : .NET Core 2.1.13(CoreCLR 4.6.28008.01, CoreFX 4.6.28008.01), X64 RyuJIT
-            //  Job - RRATUY : .NET Core 2.1.13(CoreCLR 4.6.28008.01, CoreFX 4.6.28008.01), X64 RyuJIT
+            //  Job - XQBLAM : .NET Core 2.1.13(CoreCLR 4.6.28008.01, CoreFX 4.6.28008.01), X64 RyuJIT
 
             //Arguments =/ p:Configuration = Release  Toolchain = netcoreapp2.1  IterationCount = 1
             //LaunchCount = 3  MaxIterationCount = 20  RunStrategy = ColdStart
             //UnrollFactor = 1  WarmupCount = 1
 
-            //  | Method                | Mean      | Error     | StdDev    | Extra Metric  |
-            //  | ----------------------| ---------:| ---------:| ---------:| -------------:|
-            //  | TestTextLoaderGetters | 887.3 ms  | 810.6 ms  | 44.43 ms  | -             |
+            //| Method                  | Mean      | Error     | StdDev    | Extra Metric  |
+            //| ----------------------  | --------: | ---------:| ---------:| -------------:|
+            //| TestTextLoaderGetters   | 1.012 s   | 0.6649 s  | 0.0364 s  | -             |
 
             //// * Legends *
             //Mean         : Arithmetic mean of all measurements
             //Error        : Half of 99.9 % confidence interval
             // StdDev       : Standard deviation of all measurements
             // Extra Metric: Value of the provided extra metric
-            //  1 ms: 1 Millisecond(0.001 sec)
+            //  1 s: 1 Second(1 sec)
 
             //// ***** BenchmarkRunner: End *****
             //// ** Remained 0 benchmark(s) to run **
-            // Run time: 00:00:14(14.51 sec), executed benchmarks: 1
-            //Global total time: 00:00:26(26.3 sec), executed benchmarks: 1
+            //            Run time: 00:00:16(16.05 sec), executed benchmarks: 1
+
+            //Global total time: 00:00:33(33.18 sec), executed benchmarks: 1
 
             return;
         }
