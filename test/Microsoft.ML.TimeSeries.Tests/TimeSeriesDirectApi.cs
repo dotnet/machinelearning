0fb9dc982d46f306287014afcba57f0dc33a4e29
Harish S. Kulkarni <harishsk@microsoft.com>
2020-05-22 11:08:23 -0700

    Renamed variables

--- test/Microsoft.ML.TimeSeries.Tests/TimeSeriesDirectApi.cs
+++ test/Microsoft.ML.TimeSeries.Tests/TimeSeriesDirectApi.cs
@@ -688,63 +688,63 @@ namespace Microsoft.ML.Tests
             }
         }
 
-        private static List<Point> GetRootCauseLocalizationPoints()
+        private static List<TimeSeriesPoint> GetRootCauseLocalizationPoints()
         {
-            List<Point> points = new List<Point>();
+            List<TimeSeriesPoint> points = new List<TimeSeriesPoint>();
 
             Dictionary<string, Object> dic1 = new Dictionary<string, Object>();
             dic1.Add("Country", "UK");
             dic1.Add("DeviceType", "Laptop");
             dic1.Add("DataCenter", "DC1");
-            points.Add(new Point(200, 100, true, dic1));
+            points.Add(new TimeSeriesPoint(200, 100, true, dic1));
 
             Dictionary<string, Object> dic2 = new Dictionary<string, Object>();
             dic2.Add("Country", "UK");
             dic2.Add("DeviceType", "Mobile");
             dic2.Add("DataCenter", "DC1");
-            points.Add(new Point(1000, 100, true, dic2));
+            points.Add(new TimeSeriesPoint(1000, 100, true, dic2));
 
             Dictionary<string, Object> dic3 = new Dictionary<string, Object>();
             dic3.Add("Country", "UK");
             dic3.Add("DeviceType", _rootCauseAggSymbol);
             dic3.Add("DataCenter", "DC1");
-            points.Add(new Point(1200, 200, true, dic3));
+            points.Add(new TimeSeriesPoint(1200, 200, true, dic3));
 
             Dictionary<string, Object> dic4 = new Dictionary<string, Object>();
             dic4.Add("Country", "UK");
             dic4.Add("DeviceType", "Laptop");
             dic4.Add("DataCenter", "DC2");
-            points.Add(new Point(100, 100, false, dic4));
+            points.Add(new TimeSeriesPoint(100, 100, false, dic4));
 
             Dictionary<string, Object> dic5 = new Dictionary<string, Object>();
             dic5.Add("Country", "UK");
             dic5.Add("DeviceType", "Mobile");
             dic5.Add("DataCenter", "DC2");
-            points.Add(new Point(200, 200, false, dic5));
+            points.Add(new TimeSeriesPoint(200, 200, false, dic5));
 
             Dictionary<string, Object> dic6 = new Dictionary<string, Object>();
             dic6.Add("Country", "UK");
             dic6.Add("DeviceType", _rootCauseAggSymbol);
             dic6.Add("DataCenter", "DC2");
-            points.Add(new Point(300, 300, false, dic6));
+            points.Add(new TimeSeriesPoint(300, 300, false, dic6));
 
             Dictionary<string, Object> dic7 = new Dictionary<string, Object>();
             dic7.Add("Country", "UK");
             dic7.Add("DeviceType", _rootCauseAggSymbol);
             dic7.Add("DataCenter", _rootCauseAggSymbol);
-            points.Add(new Point(1500, 500, true, dic7));
+            points.Add(new TimeSeriesPoint(1500, 500, true, dic7));
 
             Dictionary<string, Object> dic8 = new Dictionary<string, Object>();
             dic8.Add("Country", "UK");
             dic8.Add("DeviceType", "Laptop");
             dic8.Add("DataCenter", _rootCauseAggSymbol);
-            points.Add(new Point(300, 200, true, dic8));
+            points.Add(new TimeSeriesPoint(300, 200, true, dic8));
 
             Dictionary<string, Object> dic9 = new Dictionary<string, Object>();
             dic9.Add("Country", "UK");
             dic9.Add("DeviceType", "Mobile");
             dic9.Add("DataCenter", _rootCauseAggSymbol);
-            points.Add(new Point(1200, 300, true, dic9));
+            points.Add(new TimeSeriesPoint(1200, 300, true, dic9));
 
             return points;
         }
