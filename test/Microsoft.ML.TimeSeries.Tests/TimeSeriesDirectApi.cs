53d5c0dd13b76b48035bdd5fc37e40e3b3399547
Lisa Hua <lisahua46@gmail.com>
2020-06-15 12:57:09 -0700

    fix unit tests

--- test/Microsoft.ML.TimeSeries.Tests/TimeSeriesDirectApi.cs
+++ test/Microsoft.ML.TimeSeries.Tests/TimeSeriesDirectApi.cs
@@ -699,14 +699,14 @@ namespace Microsoft.ML.Tests
             var input = new List<double>() {18.004, 87.401, 87.411, 18.088, 18.017, 87.759, 33.996, 18.043, 87.853, 18.364, 18.004, 86.992, 87.555,
                 18.088, 18.029, 87.906, 87.471, 18.039, 18.099, 87.403, 18.030, 72.991, 87.804, 18.381, 18.016, 87.145, 87.771,
                 18.029, 18.084, 87.976, 34.913, 18.064, 18.302, 87.723, 18.001, 86.401, 87.344, 18.295, 18.002, 87.793,
-                87.531, 18.055, 18.005, 87.947, 18.003, 72.743, 87.722, 18.142 }.Select(t => new TimeSeriesData((float)t));
+                87.531, 18.055, 18.005, 87.947, 18.003, 72.743, 87.722, 18.142 }.Select(t => new TimeSeriesDataDouble() { Value = t });
 
             var mlContext = new MLContext();
 
             var dataView = mlContext.Data.LoadFromEnumerable(input);
             SeasonalityDetector seasonalityDetector = new SeasonalityDetector();
 
-            int period = mlContext.AnomalyDetection.DetectSeasonality(dataView, "Value", seasonalityWindowSize);
+            int period = mlContext.AnomalyDetection.DetectSeasonality(dataView, nameof(TimeSeriesDataDouble.Value), seasonalityWindowSize);
             Assert.Equal(expectedPeriod, period);
         }
 
