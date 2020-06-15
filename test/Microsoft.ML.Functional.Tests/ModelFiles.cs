4ba050d0d7f9d11fa58b1f9eaba40ece1aaa56f4
unknown <frdong@microsoft.com>
2020-06-15 19:50:58 +0000

    change test to test on both case

--- test/Microsoft.ML.Functional.Tests/ModelFiles.cs
+++ test/Microsoft.ML.Functional.Tests/ModelFiles.cs
@@ -231,19 +231,32 @@ namespace Microsoft.ML.Functional.Tests
         [Fact]
         public void LoadModelWithOptionalColumnTransform()
         {
+            SchemaDefinition inputSchemaDefinition = SchemaDefinition.Create(typeof(ModelInput));
+            inputSchemaDefinition[nameof(ModelInput.CategoricalFeatures)].ColumnType = new VectorDataViewType(TextDataViewType.Instance, 5);
+            inputSchemaDefinition[nameof(ModelInput.NumericalFeatures)].ColumnType = new VectorDataViewType(NumberDataViewType.Single, 3);
+
             var mlContext = new MLContext(1);
             ITransformer trainedModel;
             DataViewSchema dataViewSchema;
             trainedModel = mlContext.Model.Load(TestCommon.GetDataPath(DataDir, "backcompat", "modelwithoptionalcolumntransform.zip"), out dataViewSchema);
 
-            var model = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(trainedModel, inputSchema: dataViewSchema);
-            var prediction = model.Predict(new ModelInput() { 
-                CategoricalFeatures = new[] { "ABC", "ABC", "ABC", "ABC", "ABC" }, 
-                NumericalFeatures = new float [] { 1, 1, 1 },
+            var modelInput = new ModelInput()
+            {
+                CategoricalFeatures = new[] { "ABC", "ABC", "ABC", "ABC", "ABC" },
+                NumericalFeatures = new float[] { 1, 1, 1 },
                 Label = 1
-            });
+            };
+
+            // test create prediction engine with user defined schema
+            var model = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(trainedModel, inputSchemaDefinition: inputSchemaDefinition);
+            var prediction = model.Predict(modelInput);
+
+            // test create prediction engine with schema loaded from model
+            var model2 = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(trainedModel, inputSchema: dataViewSchema);
+            var prediction2 = model2.Predict(modelInput);
 
             Assert.Equal(1, prediction.Score[0]);
+            Assert.Equal(1, prediction2.Score[0]);
         }
 
         [Fact]
