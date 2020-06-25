111b5c465da18f8afbee6a9022cb538f80344dd9
Klaus Marius Hansen <klausmh@microsoft.com>
2020-06-25 13:13:46 -0700

    Handled review comments

--- docs/samples/Microsoft.ML.Samples/Program.cs
+++ docs/samples/Microsoft.ML.Samples/Program.cs
@@ -1,20 +1,19 @@
 using System;
 using System.Reflection;
-using System.Text.RegularExpressions;
 using Samples.Dynamic;
 
 namespace Microsoft.ML.Samples
 {
     public static class Program
     {
-        public static void Main(string[] args) => RunAll(args?[0]);
+        public static void Main(string[] args) => RunAll(args == null || args.Length == 0 ? null : args[0]);
 
         internal static void RunAll(string name = null)
         {
             int samples = 0;
             foreach (var type in Assembly.GetExecutingAssembly().GetTypes())
             {
-                if (name?.Equals(type.Name) ?? false)
+                if (name == null || name.Equals(type.Name))
                 {
                     var sample = type.GetMethod("Example", BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy);
 
