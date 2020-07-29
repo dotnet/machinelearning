3ab5678d85e1fdad69307bee2bb69ec94b1f3a39
J W <woodjon@gmail.com>
2020-07-29 06:07:40 -0400

    Add regression scenario to make sure async fix works

--- src/Microsoft.ML.SamplesUtils/SamplesDatasetUtils.cs
+++ src/Microsoft.ML.SamplesUtils/SamplesDatasetUtils.cs
@@ -185,6 +185,49 @@ namespace Microsoft.ML.SamplesUtils
             return path;
         }
 
+        public static string DownloadTaxiFareData()
+        {
+            string githubPath = "https://raw.githubusercontent.com/dotnet/machinelearning-samples/master/samples/csharp/getting-started/Regression_TaxiFarePrediction/TaxiFarePrediction/Data/taxi-fare-train.csv";
+            string dataFile = "taxi-fare-train.csv";
+
+            Download(githubPath, dataFile);
+
+            return dataFile;
+        }
+
+        public static IDataView LoadTaxiFareDataset(MLContext context)
+        {
+            var dataFile = DownloadTaxiFareData();
+
+            var dataView = context.Data.LoadFromTextFile<TaxiData>(dataFile, hasHeader: true, separatorChar: ',');
+
+            return dataView;
+        }
+
+        public class TaxiData
+        {
+            [LoadColumn(0)]
+            public string VendorId;
+
+            [LoadColumn(1)]
+            public string RateCode;
+
+            [LoadColumn(2)]
+            public float PassengerCount;
+
+            [LoadColumn(3)]
+            public float TripTime;
+
+            [LoadColumn(4)]
+            public float TripDistance;
+
+            [LoadColumn(5)]
+            public string PaymentType;
+
+            [LoadColumn(6)]
+            public float FareAmount;
+        }
+
         private static string Download(string baseGitPath, string dataFile)
         {
             if (File.Exists(dataFile))
---
+++ test/Microsoft.ML.Tests/Scenarios/RegressionTest.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+
+namespace Microsoft.ML.Scenarios
+{
+    public partial class ScenariosTests
+    {
+        [Fact]
+        public void TestRegressionScenario()
+        {
+            var context = new MLContext();
+
+            IDataView taxiData = Microsoft.ML.SamplesUtils.DatasetUtils.LoadTaxiFareDataset(context);
+            var splitData = context.Data.TrainTestSplit(taxiData, testFraction: 0.2);
+
+            IDataView trainingDataView = context.Data.FilterRowsByColumn(splitData.TrainSet, "FareAmount", lowerBound: 1, upperBound: 150);
+
+            var dataProcessPipeline = context.Transforms.CopyColumns(outputColumnName: "Label", inputColumnName: "FareAmount")
+                                        .Append(context.Transforms.Categorical.OneHotEncoding(outputColumnName: "VendorIdEncoded", inputColumnName: "VendorId"))
+                                        .Append(context.Transforms.Categorical.OneHotEncoding(outputColumnName: "RateCodeEncoded", inputColumnName: "RateCode"))
+                                        .Append(context.Transforms.Categorical.OneHotEncoding(outputColumnName: "PaymentTypeEncoded", inputColumnName: "PaymentType"))
+                                        .Append(context.Transforms.NormalizeMeanVariance(outputColumnName: "PassengerCount"))
+                                        .Append(context.Transforms.NormalizeMeanVariance(outputColumnName: "TripTime"))
+                                        .Append(context.Transforms.NormalizeMeanVariance(outputColumnName: "TripDistance"))
+                                        .Append(context.Transforms.Concatenate("Features", "VendorIdEncoded", "RateCodeEncoded", "PaymentTypeEncoded", "PassengerCount",
+                                            "TripTime", "TripDistance"));
+
+            var trainer = context.Regression.Trainers.Sdca(labelColumnName: "Label", featureColumnName: "Features");
+            var trainingPipeline = dataProcessPipeline.Append(trainer);
+
+            var model = trainingPipeline.Fit(trainingDataView);
+
+            var predictions = model.Transform(splitData.TestSet);
+
+            var metrics = context.Regression.Evaluate(predictions);
+
+            Assert.True(metrics.RSquared > .9);
+            Assert.True(metrics.RootMeanSquaredError > 2);
+        }
+    }
+}
