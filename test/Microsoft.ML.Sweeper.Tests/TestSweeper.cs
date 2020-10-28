c522dcb211d472cd4be959a2aec976240544d278
Antonio Velazquez <anvelazq@microsoft.com>
2020-10-27 20:33:56 -0700

    Added russian culture info test

--- test/Microsoft.ML.Sweeper.Tests/TestSweeper.cs
+++ test/Microsoft.ML.Sweeper.Tests/TestSweeper.cs
@@ -636,9 +636,22 @@ namespace Microsoft.ML.Sweeper.RunTests
             Assert.True(sweeps == null || sweeps.Length <= 5);
         }
 
-        [Fact]
-        public void TestSmacSweeper()
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void TestSmacSweeper(bool foreignCulture)
         {
+            var originalCulture = Thread.CurrentThread.CurrentCulture;
+
+            if (foreignCulture)
+            {
+                // Run this test using Russian culture info, so to test
+                // the Sweeper ability to correctly handle hyperparameters
+                // represented with a decimal separator of comma instead of
+                // period.
+                Thread.CurrentThread.CurrentCulture = new CultureInfo("ru-RU");
+            }
+
             var random = new Random(42);
             var env = new MLContext(42);
             const int maxInitSweeps = 5;
@@ -686,6 +699,10 @@ namespace Microsoft.ML.Sweeper.RunTests
             }
             // Because only unique configurations are considered, the number asked for may exceed the number actually returned.
             Assert.True(sweeps.Length <= 5);
+
+            //CultureInfo.DefaultThreadCurrentCulture = idk;
+            if(foreignCulture)
+                Thread.CurrentThread.CurrentCulture = originalCulture;
         }
     }
 }
