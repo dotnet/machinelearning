f9b4b08c9289d428e7365a8d27184a978eece63b
Aleksei Smirnov <asmirnov@hazeltree.com>
2021-06-03 21:04:48 +0300

    Feature/5820 dataframe extend groupby (#5821)
    * fix #5767 issue with DataFrame Merge method
    
    * #5820 Extend DataFrame GroupBy operations
    
    * #5820 fix code review findings
    
    * #5820 fix code review findings
    
    * Update src/Microsoft.Data.Analysis/GroupBy.cs
    
    * Fix remaining comments
    
    Co-authored-by: Prashanth Govindarajan <prgovi@microsoft.com>

--- src/Microsoft.Data.Analysis/DataFrame.cs
+++ src/Microsoft.Data.Analysis/DataFrame.cs
@@ -367,6 +367,25 @@ namespace Microsoft.Data.Analysis
             DataFrameColumn column = _columnCollection[columnIndex];
             return column.GroupBy(columnIndex, this);
         }
+                
+        /// <summary>
+        /// Groups the rows of the <see cref="DataFrame"/> by unique values in the <paramref name="columnName"/> column.
+        /// </summary>
+        /// <typeparam name="TKey">Type of column used for grouping</typeparam>
+        /// <param name="columnName">The column used to group unique values</param>
+        /// <returns>A GroupBy object that stores the group information.</returns>
+        public GroupBy<TKey> GroupBy<TKey>(string columnName)
+        {
+            GroupBy<TKey> group = GroupBy(columnName) as GroupBy<TKey>;
+
+            if (group == null)
+            {
+                DataFrameColumn column = this[columnName];
+                throw new InvalidCastException(String.Format(Strings.BadColumnCastDuringGrouping, columnName, column.DataType, typeof(TKey)));
+            }
+
+            return group;
+        }
 
         // In GroupBy and ReadCsv calls, columns get resized. We need to set the RowCount to reflect the true Length of the DataFrame. This does internal validation
         internal void SetTableRowCount(long rowCount)
