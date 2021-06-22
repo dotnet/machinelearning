442c29c1412d496500e387566567c097986123dc
Michael Sharp <51342856+michaelgsharp@users.noreply.github.com>
2021-06-21 22:41:12 -0700

    Fixes Macos CI issue (#5854)

--- build/libomp.rb
+++ build/libomp.rb
@@ -1,3 +1,6 @@
+# typed: false
+# frozen_string_literal: true
+
 class Libomp < Formula
   desc "LLVM's OpenMP runtime library"
   homepage "https://openmp.llvm.org/"
@@ -5,15 +8,14 @@ class Libomp < Formula
   sha256 "30662b632f5556c59ee9215c1309f61de50b3ea8e89dcc28ba9a9494bba238ff"
 
   bottle do
-    cellar :any
-    sha256 "c8788028105e9ec32e29bcdba8c7b550c2afd96b3f0a7bd0d6b6136a8729174a" => :mojave
-    sha256 "046f659ad8a2cc336049a1e7f7be0dac2f2e28ce322409f10396f9582b94b660" => :high_sierra
-    sha256 "ba1b9a78326c671fb864a95d93f2fcf47f3af2ef8f12f6c7ee54a7c0cf7802b4" => :sierra
-    sha256 "0ea5b6ae3e9e0b32a2b49a7430fb0f653430107164089b067a7b0039e91ce527" => :el_capitan
+    sha256 cellar: :any, mojave:      "c8788028105e9ec32e29bcdba8c7b550c2afd96b3f0a7bd0d6b6136a8729174a"
+    sha256 cellar: :any, high_sierra: "046f659ad8a2cc336049a1e7f7be0dac2f2e28ce322409f10396f9582b94b660"
+    sha256 cellar: :any, sierra:      "ba1b9a78326c671fb864a95d93f2fcf47f3af2ef8f12f6c7ee54a7c0cf7802b4"
+    sha256 cellar: :any, el_capitan:  "0ea5b6ae3e9e0b32a2b49a7430fb0f653430107164089b067a7b0039e91ce527"
   end
 
   depends_on "cmake" => :build
-  depends_on :macos => :yosemite
+  depends_on macos: :yosemite
 
   def install
     system "cmake", ".", *std_cmake_args
@@ -22,20 +24,21 @@ class Libomp < Formula
     system "make", "install"
   end
 
-  def caveats; <<~EOS
-    On Apple Clang, you need to add several options to use OpenMP's front end
-    instead of the standard driver option. This usually looks like
-      -Xpreprocessor -fopenmp -lomp
+  def caveats
+    <<~EOS
+      On Apple Clang, you need to add several options to use OpenMP's front end
+      instead of the standard driver option. This usually looks like
+        -Xpreprocessor -fopenmp -lomp
 
-    You might need to make sure the lib and include directories are discoverable
-    if #{HOMEBREW_PREFIX} is not searched:
+      You might need to make sure the lib and include directories are discoverable
+      if #{HOMEBREW_PREFIX} is not searched:
 
-      -L#{opt_lib} -I#{opt_include}
+        -L#{opt_lib} -I#{opt_include}
 
-    For CMake, the following flags will cause the OpenMP::OpenMP_CXX target to
-    be set up correctly:
-      -DOpenMP_CXX_FLAGS="-Xpreprocessor -fopenmp -I#{opt_include}" -DOpenMP_CXX_LIB_NAMES="omp" -DOpenMP_omp_LIBRARY=#{opt_lib}/libomp.dylib
-  EOS
+      For CMake, the following flags will cause the OpenMP::OpenMP_CXX target to
+      be set up correctly:
+        -DOpenMP_CXX_FLAGS="-Xpreprocessor -fopenmp -I#{opt_include}" -DOpenMP_CXX_LIB_NAMES="omp" -DOpenMP_omp_LIBRARY=#{opt_lib}/libomp.dylib
+    EOS
   end
 
   test do
@@ -56,7 +59,7 @@ class Libomp < Formula
       }
     EOS
     system ENV.cxx, "-Werror", "-Xpreprocessor", "-fopenmp", "test.cpp",
-                    "-L#{lib}", "-lomp", "-o", "test"
+           "-L#{lib}", "-lomp", "-o", "test"
     system "./test"
   end
 end
