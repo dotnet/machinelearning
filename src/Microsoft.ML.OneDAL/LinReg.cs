c8c62cd522d53864f7d2e64f54404591da336383
Alexander Andreev <alexander.andreev@intel.com>
2021-09-01 22:44:36 +0300

    Initial integration for oneDAL LinearRegression

--- src/Microsoft.ML.OneDAL/LinReg.cs
+++ src/Microsoft.ML.OneDAL/LinReg.cs
@@ -18,6 +18,7 @@ using Microsoft.ML.Trainers;
     LinRegTrainer.UserNameValue,
     LinRegTrainer.LoadNameValue,
     LinRegTrainer.ShortName)]
+<<<<<<< HEAD
 [assembly: LoadableClass(typeof(LinearRegressionModelParameters), null, typeof(SignatureLoadModel),
     "oneDAL Linear Regression Executor",
     LinearRegressionModelParameters.LoaderSignature)]
@@ -25,6 +26,18 @@ using Microsoft.ML.Trainers;
 namespace Microsoft.ML.Trainers
 {
     public sealed class LinRegTrainer : TrainerEstimatorBase<RegressionPredictionTransformer<LinearRegressionModelParameters>, LinearRegressionModelParameters>
+=======
+
+[assembly: LoadableClass(typeof(LinRegParameters), null, typeof(SignatureLoadModel),
+    "oneDAL Linear Regression Executor",
+    LinRegParameters.LoaderSignature)]
+
+[assembly: LoadableClass(typeof(void), typeof(LinRegTrainer), null, typeof(SignatureEntryPointModule), LinRegTrainer.LoadNameValue)]
+
+namespace Microsoft.ML.Trainers
+{
+    public sealed class LinRegTrainer : TrainerEstimatorBase<RegressionPredictionTransformer<LinRegParameters>, LinRegParameters>
+>>>>>>> cce1bc84 (Initial integration for oneDAL LinearRegression)
     {
         public sealed class Options : TrainerInputBaseWithWeight
         {
@@ -35,6 +48,7 @@ namespace Microsoft.ML.Trainers
         internal const string ShortName = "linreg";
         internal const string Summary = "oneDAL Linear Regression";
 
+<<<<<<< HEAD
 	private protected override PredictionKind PredictionKind => PredictionKind.Regression;
 
 	private static readonly TrainerInfo _info = new TrainerInfo(caching: false);
@@ -42,24 +56,46 @@ namespace Microsoft.ML.Trainers
 	public override TrainerInfo Info => _info;
 
 	internal LinRegTrainer(IHostEnvironment env, Options options)
+=======
+        private protected override PredictionKind PredictionKind => PredictionKind.Regression;
+
+        private static readonly TrainerInfo _info = new TrainerInfo(caching: false);
+
+        public override TrainerInfo Info => _info;
+
+        internal LinRegTrainer(IHostEnvironment env, Options options)
+>>>>>>> cce1bc84 (Initial integration for oneDAL LinearRegression)
             : base(Contracts.CheckRef(env, nameof(env)).Register(LoadNameValue), TrainerUtils.MakeR4VecFeature(options.FeatureColumnName),
                   TrainerUtils.MakeR4ScalarColumn(options.LabelColumnName), TrainerUtils.MakeR4ScalarWeightColumn(options.ExampleWeightColumnName))
         {
             Host.CheckValue(options, nameof(options));
         }
 
+<<<<<<< HEAD
 	private protected override RegressionPredictionTransformer<LinearRegressionModelParameters> MakeTransformer(LinearRegressionModelParameters model, DataViewSchema trainSchema)
              => new RegressionPredictionTransformer<LinearRegressionModelParameters>(Host, model, trainSchema, FeatureColumn.Name);
 
 	private protected override SchemaShape.Column[] GetOutputColumnsCore(SchemaShape inputSchema)
+=======
+        private protected override RegressionPredictionTransformer<LinRegParameters> MakeTransformer(LinRegParameters model, DataViewSchema trainSchema)
+             => new RegressionPredictionTransformer<LinRegParameters>(Host, model, trainSchema, FeatureColumn.Name);
+
+        private protected override SchemaShape.Column[] GetOutputColumnsCore(SchemaShape inputSchema)
+>>>>>>> cce1bc84 (Initial integration for oneDAL LinearRegression)
         {
             return new[]
             {
                 new SchemaShape.Column(DefaultColumnNames.Score, SchemaShape.Column.VectorKind.Scalar, NumberDataViewType.Single, false, new SchemaShape(AnnotationUtils.GetTrainerOutputAnnotation()))
             };
+<<<<<<< HEAD
 	}
 
         private protected override LinearRegressionModelParameters TrainModelCore(TrainContext context)
+=======
+        }
+
+        private protected override LinRegParameters TrainModelCore(TrainContext context)
+>>>>>>> cce1bc84 (Initial integration for oneDAL LinearRegression)
         {
             using (var ch = Host.Start("Training"))
             {
@@ -67,18 +103,27 @@ namespace Microsoft.ML.Trainers
                 var examples = context.TrainingSet;
                 ch.CheckParam(examples.Schema.Feature.HasValue, nameof(examples), "Need a feature column");
                 ch.CheckParam(examples.Schema.Label.HasValue, nameof(examples), "Need a labelColumn column");
+<<<<<<< HEAD
  
+=======
+
+>>>>>>> cce1bc84 (Initial integration for oneDAL LinearRegression)
                 // The labelColumn type must be either Float or a key type based on int (if allowKeyLabels is true).
                 var typeLab = examples.Schema.Label.Value.Type;
                 if (typeLab != NumberDataViewType.Single)
                     throw ch.Except("Incompatible labelColumn column type {0}, must be {1}", typeLab, NumberDataViewType.Single);
+<<<<<<< HEAD
  
+=======
+
+>>>>>>> cce1bc84 (Initial integration for oneDAL LinearRegression)
                 // The feature type must be a vector of Float.
                 var typeFeat = examples.Schema.Feature.Value.Type as VectorDataViewType;
                 if (typeFeat == null || !typeFeat.IsKnownSize)
                     throw ch.Except("Incompatible feature column type {0}, must be known sized vector of {1}", typeFeat, NumberDataViewType.Single);
                 if (typeFeat.ItemType != NumberDataViewType.Single)
                     throw ch.Except("Incompatible feature column type {0}, must be vector of {1}", typeFeat, NumberDataViewType.Single);
+<<<<<<< HEAD
  
                 CursOpt cursorOpt = CursOpt.Label | CursOpt.Features;
                 if (examples.Schema.Weight.HasValue)
@@ -95,11 +140,33 @@ namespace Microsoft.ML.Trainers
             Host.AssertValue(ch);
             ch.AssertValue(cursorFactory);
  
+=======
+
+                CursOpt cursorOpt = CursOpt.Label | CursOpt.Features;
+                if (examples.Schema.Weight.HasValue)
+                    cursorOpt |= CursOpt.Weight;
+
+                var cursorFactory = new FloatLabelCursor.Factory(examples, cursorOpt);
+
+                return TrainCore(ch, cursorFactory, typeFeat.Size);
+            }
+        }
+
+        private LinRegParameters TrainCore(IChannel ch, FloatLabelCursor.Factory cursorFactory, int featureCount)
+        {
+            Host.AssertValue(ch);
+            ch.AssertValue(cursorFactory);
+
+>>>>>>> cce1bc84 (Initial integration for oneDAL LinearRegression)
             int m = featureCount + 1;
             var betasArray = new float[m];
             List<float> labelsList = new List<float>();
             List<float> featuresList = new List<float>();
+<<<<<<< HEAD
  
+=======
+
+>>>>>>> cce1bc84 (Initial integration for oneDAL LinearRegression)
             int n = 0;
             using (var cursor = cursorFactory.Create())
             {
@@ -107,10 +174,17 @@ namespace Microsoft.ML.Trainers
                 {
                     labelsList.Add(cursor.Label);
                     var values = cursor.Features.GetValues();
+<<<<<<< HEAD
  
                     ch.Assert(cursor.Features.IsDense);
                     ch.Assert(values.Length + 1 == m);
  
+=======
+
+                    ch.Assert(cursor.Features.IsDense);
+                    ch.Assert(values.Length + 1 == m);
+
+>>>>>>> cce1bc84 (Initial integration for oneDAL LinearRegression)
                     for (int j = 0; j < m - 1; ++j)
                     {
                         featuresList.Add(values[j]);
@@ -119,12 +193,21 @@ namespace Microsoft.ML.Trainers
                 }
                 if (cursor.BadFeaturesRowCount > 0)
                     ch.Warning("Skipped {0} instances with missing features/labelColumn during training", cursor.SkippedRowCount);
+<<<<<<< HEAD
  
             }
  
             float[] labels = labelsList.ToArray();
             float[] features = featuresList.ToArray();
  
+=======
+
+            }
+
+            float[] labels = labelsList.ToArray();
+            float[] features = featuresList.ToArray();
+
+>>>>>>> cce1bc84 (Initial integration for oneDAL LinearRegression)
             // oneDAL part
             unsafe
             {
@@ -133,13 +216,18 @@ namespace Microsoft.ML.Trainers
                     OneDAL.LinearRegressionSingle(featuresPtr, labelsPtr, betasPtr, n, m - 1);
                 }
             }
+<<<<<<< HEAD
  
+=======
+
+>>>>>>> cce1bc84 (Initial integration for oneDAL LinearRegression)
             float bias = betasArray[0];
             var weights = new float[m - 1];
             for (int i = 1; i < m; ++i)
             {
                 weights[i - 1] = betasArray[i];
             }
+<<<<<<< HEAD
  
             var weightsBuffer = new VBuffer<float>(m - 1, weights);
 	     
@@ -155,3 +243,90 @@ namespace Microsoft.ML.Trainers
 }
 
  
+=======
+
+            var weightsBuffer = new VBuffer<float>(m - 1, weights);
+
+            return new LinRegParameters(Host, in weightsBuffer, bias);
+        }
+
+        internal static class OneDAL
+        {
+            [DllImport("_oneDALWrapper.so", EntryPoint = "linearRegressionSingle")]
+            public unsafe static extern void LinearRegressionSingle(void* features, void* labels, void* betas, int nRows, int nColumns);
+        }
+    }
+
+    public sealed class LinRegParameters : RegressionModelParameters
+    {
+        internal const string LoaderSignature = "LinearRegressionExec";
+        internal const string RegistrationName = "LinearRegressionPredictor";
+
+        private static VersionInfo GetVersionInfo()
+        {
+            return new VersionInfo(
+                modelSignature: "DALRGRS",
+                verWrittenCur: 0x00010001,
+                verReadableCur: 0x00010001,
+                verWeCanReadBack: 0x00010001,
+                loaderSignature: LoaderSignature,
+                loaderAssemblyName: typeof(LinRegParameters).Assembly.FullName);
+        }
+
+        internal LinRegParameters(IHostEnvironment env, in VBuffer<float> weights, float bias)
+            : base(env, RegistrationName, in weights, bias)
+        {
+        }
+
+        private LinRegParameters(IHostEnvironment env, ModelLoadContext ctx)
+            : base(env, RegistrationName, ctx)
+        {
+            Host.CheckDecode(Weight.IsDense);
+        }
+
+        private protected override void SaveCore(ModelSaveContext ctx)
+        {
+            base.SaveCore(ctx);
+            ctx.SetVersionInfo(GetVersionInfo());
+        }
+
+        private static void TValueCheckDecode(Double param, Double tvalue)
+        {
+            Contracts.CheckDecode(Math.Sign(param) == Math.Sign(tvalue));
+        }
+
+        private static void ProbCheckDecode(Double p)
+        {
+            Contracts.CheckDecode(0 <= p && p <= 1);
+        }
+
+        internal static LinRegParameters Create(IHostEnvironment env, ModelLoadContext ctx)
+        {
+            Contracts.CheckValue(env, nameof(env));
+            env.CheckValue(ctx, nameof(ctx));
+            ctx.CheckAtModel(GetVersionInfo());
+            return new LinRegParameters(env, ctx);
+        }
+
+        private protected override void SaveSummary(TextWriter writer, RoleMappedSchema schema)
+        {
+            var names = default(VBuffer<ReadOnlyMemory<char>>);
+            AnnotationUtils.GetSlotNames(schema, RoleMappedSchema.ColumnRole.Feature, Weight.Length, ref names);
+
+            writer.WriteLine("Linear Regression Model Summary");
+
+            writer.WriteLine();
+            writer.WriteLine("Index\tName\tWeight");
+            const string format = "{0}\t{1}\t{2}";
+            writer.WriteLine(format, "", "Bias", Bias);
+            Contracts.Assert(Weight.IsDense);
+            var coeffs = Weight.GetValues();
+            for (int i = 0; i < coeffs.Length; i++)
+            {
+                var name = names.GetItemOrDefault(i);
+                writer.WriteLine(format, i, name.IsEmpty ? $"f{i}" : name.ToString(), coeffs[i]);
+            }
+        }
+    }
+}
+>>>>>>> cce1bc84 (Initial integration for oneDAL LinearRegression)
--- src/Microsoft.ML.OneDAL/OneDALCatalog.cs
+++ src/Microsoft.ML.OneDAL/OneDALCatalog.cs
@@ -7,11 +7,19 @@ namespace Microsoft.ML
 {
     public static class OneDALCatalog
     {
+<<<<<<< HEAD
 	public static LinRegTrainer LinReg(this RegressionCatalog.RegressionTrainers catalog,
                	      		    	   string labelColumnName = DefaultColumnNames.Label,
 					   string featureColumnName = DefaultColumnNames.Features,
 					   string exampleWeightColumnName = null)
 	{
+=======
+        public static LinRegTrainer LinReg(this RegressionCatalog.RegressionTrainers catalog,
+            string labelColumnName = DefaultColumnNames.Label,
+            string featureColumnName = DefaultColumnNames.Features,
+            string exampleWeightColumnName = null)
+        {
+>>>>>>> cce1bc84 (Initial integration for oneDAL LinearRegression)
             Contracts.CheckValue(catalog, nameof(catalog));
             var env = CatalogUtils.GetEnvironment(catalog);
             var options = new LinRegTrainer.Options
@@ -20,6 +28,7 @@ namespace Microsoft.ML
                 FeatureColumnName = featureColumnName,
                 ExampleWeightColumnName = exampleWeightColumnName
             };
+<<<<<<< HEAD
  
             return new LinRegTrainer(env, options);
 	}
@@ -30,6 +39,19 @@ namespace Microsoft.ML
             Contracts.CheckValue(catalog, nameof(catalog));
             Contracts.CheckValue(options, nameof(options));
  
+=======
+
+            return new LinRegTrainer(env, options);
+        }
+
+        public static LinRegTrainer LinReg(
+            this RegressionCatalog.RegressionTrainers catalog,
+            LinRegTrainer.Options options)
+        {
+            Contracts.CheckValue(catalog, nameof(catalog));
+            Contracts.CheckValue(options, nameof(options));
+
+>>>>>>> cce1bc84 (Initial integration for oneDAL LinearRegression)
             var env = CatalogUtils.GetEnvironment(catalog);
             return new LinRegTrainer(env, options);
         }
