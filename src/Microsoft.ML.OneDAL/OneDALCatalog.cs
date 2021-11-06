9e73b85e97adc49c9d643d71df26770d78465d29
Weilin Wang <weilin.wang@intel.com>
2021-11-06 14:29:04 -0700

    Fix rebase merge conflit error

--- src/Microsoft.ML.OneDAL/OneDALCatalog.cs
+++ src/Microsoft.ML.OneDAL/OneDALCatalog.cs
@@ -7,19 +7,11 @@ namespace Microsoft.ML
 {
     public static class OneDALCatalog
     {
-<<<<<<< HEAD
-	public static LinRegTrainer LinReg(this RegressionCatalog.RegressionTrainers catalog,
-               	      		    	   string labelColumnName = DefaultColumnNames.Label,
-					   string featureColumnName = DefaultColumnNames.Features,
-					   string exampleWeightColumnName = null)
-	{
-=======
-        public static LinRegTrainer LinReg(this RegressionCatalog.RegressionTrainers catalog,
-            string labelColumnName = DefaultColumnNames.Label,
-            string featureColumnName = DefaultColumnNames.Features,
-            string exampleWeightColumnName = null)
-        {
->>>>>>> cce1bc84 (Initial integration for oneDAL LinearRegression)
+	    public static LinRegTrainer LinReg(this RegressionCatalog.RegressionTrainers catalog,
+                  	      		    	   string labelColumnName = DefaultColumnNames.Label,
+	    				                   string featureColumnName = DefaultColumnNames.Features,
+	    				                   string exampleWeightColumnName = null)
+	    {
             Contracts.CheckValue(catalog, nameof(catalog));
             var env = CatalogUtils.GetEnvironment(catalog);
             var options = new LinRegTrainer.Options
@@ -28,30 +20,16 @@ namespace Microsoft.ML
                 FeatureColumnName = featureColumnName,
                 ExampleWeightColumnName = exampleWeightColumnName
             };
-<<<<<<< HEAD
  
             return new LinRegTrainer(env, options);
-	}
+	    }
 
-	public static LinRegTrainer LinReg(this RegressionCatalog.RegressionTrainers catalog,
-	       	      		           LinRegTrainer.Options options)
+	    public static LinRegTrainer LinReg(this RegressionCatalog.RegressionTrainers catalog,
+	           	      		           LinRegTrainer.Options options)
         {
             Contracts.CheckValue(catalog, nameof(catalog));
             Contracts.CheckValue(options, nameof(options));
  
-=======
-
-            return new LinRegTrainer(env, options);
-        }
-
-        public static LinRegTrainer LinReg(
-            this RegressionCatalog.RegressionTrainers catalog,
-            LinRegTrainer.Options options)
-        {
-            Contracts.CheckValue(catalog, nameof(catalog));
-            Contracts.CheckValue(options, nameof(options));
-
->>>>>>> cce1bc84 (Initial integration for oneDAL LinearRegression)
             var env = CatalogUtils.GetEnvironment(catalog);
             return new LinRegTrainer(env, options);
         }
