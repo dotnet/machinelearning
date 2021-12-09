294a6513ea109804719e128d14727ed78aa2d81e
Naphtali Davies <naphtali.davies@zerto.com>
2021-12-09 12:02:10 +0200

    Train2 commit

--- test/Microsoft.ML.Benchmarks/Program.cs
+++ test/Microsoft.ML.Benchmarks/Program.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+//Making more changes for traning purpose
+
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Diagnosers;
 using BenchmarkDotNet.Jobs;
