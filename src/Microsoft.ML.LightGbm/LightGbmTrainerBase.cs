1a6966057e42c5334193dd9ab14f527faa7ef70b
Antti Andy Törrönen <antti@kwork.me>
2022-01-31 09:30:06 +0200

    num_iterations instead of alias

--- src/Microsoft.ML.LightGbm/LightGbmTrainerBase.cs
+++ src/Microsoft.ML.LightGbm/LightGbmTrainerBase.cs
@@ -58,7 +58,7 @@ namespace Microsoft.ML.Trainers.LightGbm
                {nameof(L2CategoricalRegularization),          "cat_l2" },
                {nameof(HandleMissingValue),                   "use_missing" },
                {nameof(UseZeroAsMissingValue),                "zero_as_missing" },
-               {nameof(NumberOfIterations),                   "num_trees" }
+               {nameof(NumberOfIterations),                   "num_iterations" }
             };
 
             private protected string GetOptionName(string name)
