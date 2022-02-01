badc83a97aaf15777ab282849e9293e411103bdd
Antti "Andy" Törrönen <antti@kwork.me>
2022-02-01 13:53:04 +0200

    LightGBM default metric EvaluateMetricType.None

--- src/Microsoft.ML.LightGbm/LightGbmBinaryTrainer.cs
+++ src/Microsoft.ML.LightGbm/LightGbmBinaryTrainer.cs
@@ -163,7 +163,7 @@ namespace Microsoft.ML.Trainers.LightGbm
             [Argument(ArgumentType.AtMostOnce,
                 HelpText = "Evaluation metrics.",
                 ShortName = "em")]
-            public EvaluateMetricType EvaluationMetric = EvaluateMetricType.Logloss;
+            public EvaluateMetricType EvaluationMetric = EvaluateMetricType.None;
 
             static Options()
             {
