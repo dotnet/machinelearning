6f4624b64daac073e10b8b3fe4bcdeb2cc2a11b3
XiaoYun Zhang <xiaoyuz@microsoft.com>
2022-02-28 13:03:58 -0800

    add entity and estimator class

---
+++ src/Microsoft.ML.AutoML/SweepableEstimator/Estimator.cs
@@ -0,0 +1,85 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.ML.SearchSpace;
+using Microsoft.ML.AutoML.CodeGen;
+using Newtonsoft.Json;
+
+namespace Microsoft.ML.AutoML
+{
+    /// <summary>
+    /// abstract class for estimator.
+    /// </summary>
+    public class Estimator
+    {
+        protected Estimator()
+        {
+            this.Parameter = Parameter.CreateNestedParameter();
+        }
+
+        internal Estimator(EstimatorType estimatorType)
+            : this()
+        {
+            this.EstimatorType = estimatorType;
+        }
+
+        public EstimatorType EstimatorType { get; set; }
+
+        public Parameter Parameter { get; set; }
+
+        public static Entity operator +(Estimator left, Estimator right)
+        {
+            return new OneOfEntity()
+            {
+                Left = new EstimatorEntity(left),
+                Right = new EstimatorEntity(right),
+            };
+        }
+
+        public static Entity operator +(Entity left, Estimator right)
+        {
+            return new OneOfEntity()
+            {
+                Left = left,
+                Right = new EstimatorEntity(right),
+            };
+        }
+
+        public static Entity operator +(Estimator left, Entity right)
+        {
+            return new OneOfEntity()
+            {
+                Left = new EstimatorEntity(left),
+                Right = right,
+            };
+        }
+
+        public static Entity operator *(Estimator left, Estimator right)
+        {
+            return new ConcatenateEntity()
+            {
+                Left = new EstimatorEntity(left),
+                Right = new EstimatorEntity(right),
+            };
+        }
+
+        public static Entity operator *(Entity left, Estimator right)
+        {
+            return new ConcatenateEntity()
+            {
+                Left = left,
+                Right = new EstimatorEntity(right),
+            };
+        }
+
+        public static Entity operator *(Estimator left, Entity right)
+        {
+            return new ConcatenateEntity()
+            {
+                Left = new EstimatorEntity(left),
+                Right = right,
+            };
+        }
+    }
+}
