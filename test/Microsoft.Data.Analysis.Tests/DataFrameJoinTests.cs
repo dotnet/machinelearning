358a5dd3a27c93390262a99b1f3167e680c753fa
Maxim Zasov <mzasov@hazeltree.com>
2022-04-11 12:59:14 +0300

    add tests

--- test/Microsoft.Data.Analysis.Tests/DataFrameJoinTests.cs
+++ test/Microsoft.Data.Analysis.Tests/DataFrameJoinTests.cs
@@ -28,6 +28,52 @@ namespace Microsoft.Data.Analysis.Tests
         }
 
         [Fact]
+        public void DataFrameJoinTests_GetSortedListsIntersection_EmptyCollections_FirstIsNotEmpty_EmptyResult()
+        {
+            // Arrange
+
+            var collection1 = new Collection<long>()
+            {
+                111,
+                222,
+                333
+            };
+
+            var collection2 = new Collection<long>();
+
+            // Act
+
+            var intersection = DataFrameJoinExtensions.GetSortedListsIntersection(collection1, collection2);
+
+            // Assert
+
+            Assert.Equal(0, intersection.Count);
+        }
+
+        [Fact]
+        public void DataFrameJoinTests_GetSortedListsIntersection_EmptyCollections_SecondIsNotEmpty_EmptyResult()
+        {
+            // Arrange
+
+            var collection1 = new Collection<long>();
+
+            var collection2 = new Collection<long>()
+            {
+                111,
+                222,
+                333
+            };
+
+            // Act
+
+            var intersection = DataFrameJoinExtensions.GetSortedListsIntersection(collection1, collection2);
+
+            // Assert
+
+            Assert.Equal(0, intersection.Count);
+        }
+
+        [Fact]
         public void DataFrameJoinTests_GetSortedListsIntersection_SortedCollections_WithoutIntersection_Success()
         {
             // Arrange
