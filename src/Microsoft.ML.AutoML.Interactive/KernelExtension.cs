aaad18b812206dc3e59ec21c55ac9357b46900b3
Jake Radzikowski <JakeRad@Microsoft.com>
2022-06-29 17:07:54 -0700

    Initial check-in of AutoML Interactive Extension

---
+++ src/Microsoft.ML.AutoML.Interactive/KernelExtension.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.Data.Analysis;
+using Microsoft.DotNet.Interactive;
+using Microsoft.DotNet.Interactive.Formatting;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Plotly.NET.CSharp;
+using static Microsoft.DotNet.Interactive.Formatting.PocketViewTags;
+
+
+namespace Microsoft.ML.AutoML
+{
+    public class KernelExtension : IKernelExtension
+    {
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+        public Task OnLoadAsync(Kernel kernel)
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+        {
+            Formatter.Register<NotebookMonitor>((monitor, writer) =>
+            {
+                WriteSummary(monitor, writer);
+                WriteChart(monitor, writer);
+                WriteTable(monitor, writer);
+            }, "text/html");
+
+            return Task.CompletedTask;
+        }
+
+        private static void WriteSummary(NotebookMonitor monitor, TextWriter writer)
+        {
+
+            var summary = new List<IHtmlContent>();
+
+            if (monitor.BestTrial != null)
+            {
+                summary.Add(h3("Best Run"));
+                summary.Add(p($"Trial: {monitor.BestTrial.TrialSettings.TrialId}"));
+                summary.Add(p($"Trainer: {monitor.BestTrial.TrialSettings.Pipeline}".Replace("Unknown=>", "")));
+            }
+            if (monitor.ActiveTrial != null)
+            {
+
+                var activeRunParam = JsonSerializer.Serialize(monitor.ActiveTrial.Parameter, new JsonSerializerOptions() { WriteIndented = true, });
+
+                summary.Add(h3("Active Run"));
+                summary.Add(p($"Trial: {monitor.ActiveTrial.TrialId}"));
+                summary.Add(p($"Trainer: {monitor.ActiveTrial.Pipeline}".Replace("Unknown=>", "")));
+                summary.Add(p($"Parameters: {activeRunParam}"));
+            }
+
+            writer.Write(div(summary));
+        }
+
+        private static void WriteChart(NotebookMonitor monitor, TextWriter writer)
+        {
+            var x = monitor.CompletedTrials.Select(x => x.TrialSettings.TrialId);
+            var y = monitor.CompletedTrials.Select(x => x.Metric);
+
+            var chart = Chart.Point<int, double, string>(x, y, "Hello")
+            .WithTraceInfo(ShowLegend: false)
+            .WithXAxisStyle<double, double, string>(TitleText: "Trial", ShowGrid: false)
+            .WithYAxisStyle<double, double, string>(TitleText: "Metric", ShowGrid: false);
+
+            Formatter.GetPreferredFormatterFor(typeof(Plotly.NET.GenericChart.GenericChart), "text/html").Format(chart, writer);
+        }
+
+        private static void WriteTable(NotebookMonitor notebookMonitor, TextWriter writer)
+        {
+            Formatter.GetPreferredFormatterFor(typeof(DataFrame), "text/html").Format(notebookMonitor.DataFrame, writer);
+        }
+    }
+}
---
+++ src/Microsoft.ML.AutoML.Interactive/NotebookMonitor.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.DotNet.Interactive;
+using System.Collections.Generic;
+using Microsoft.Data.Analysis;
+using System;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+
+namespace Microsoft.ML.AutoML
+{
+    public class NotebookMonitor : IMonitor
+    {
+        private DisplayedValue? ValueToUpdate;
+        private DateTime _lastUpdate = DateTime.MinValue;
+
+        public TrialResult? BestTrial { get; set; }
+        public TrialResult? MostRecentTrial { get; set; }
+        public TrialSettings? ActiveTrial { get; set; }
+        public List<TrialResult> CompletedTrials { get; set; }
+        public DataFrame DataFrame { get; set; }
+
+        public NotebookMonitor()
+        {
+            this.CompletedTrials = new List<TrialResult>();
+            this.DataFrame = new DataFrame(new PrimitiveDataFrameColumn<int>("Trial"), new PrimitiveDataFrameColumn<float>("Metric"), new StringDataFrameColumn("Trainer"), new StringDataFrameColumn("Parameters"));
+        }
+
+        public void ReportBestTrial(TrialResult result)
+        {
+            this.BestTrial = result;
+            Update();
+        }
+
+        public void ReportCompletedTrial(TrialResult result)
+        {
+            this.MostRecentTrial = result;
+            this.CompletedTrials.Add(result);
+
+            var activeRunParam = JsonSerializer.Serialize(result.TrialSettings.Parameter, new JsonSerializerOptions() { WriteIndented = false, });
+
+            this.DataFrame.Append(new List<KeyValuePair<string, object>>()
+            {
+                new KeyValuePair<string, object>("Trial",result.TrialSettings.TrialId),
+                new KeyValuePair<string, object>("Metric", result.Metric),
+                new KeyValuePair<string, object>("Trainer",result.TrialSettings.Pipeline.ToString().Replace("Unknown=>","")),
+                new KeyValuePair<string, object>("Parameters",activeRunParam),
+            }, true);
+            Update();
+        }
+
+        public void ReportFailTrial(TrialResult result)
+        {
+            // TODO figure out what to do with failed trials.
+            Update();
+        }
+
+        public void ReportRunningTrial(TrialSettings setting)
+        {
+            this.ActiveTrial = setting;
+            Update();
+        }
+
+        private bool updatePending = false;
+        public void Update()
+        {
+            Task.Run(async () =>
+            {
+                if (updatePending == true)
+                {
+                    // Keep waiting
+                }
+                else
+                {
+                    int timeRemaining = 5000 - (int)(DateTime.Now.Millisecond - this._lastUpdate.Millisecond);
+                    updatePending = true;
+                    if (timeRemaining > 0)
+                    {
+                        await Task.Delay(timeRemaining);
+                    }
+                    if (this.ValueToUpdate != null)
+                    {
+                        updatePending = false;
+                        this._lastUpdate = DateTime.Now;
+                        this.ValueToUpdate.Update(this);
+                    }
+                }
+            });
+
+        }
+
+        public void SetUpdate(DisplayedValue valueToUpdate)
+        {
+            this.ValueToUpdate = valueToUpdate;
+            Update();
+        }
+    }
+}
