924ae7a48da1dd075fca72ad7ff10cd2939ece66
Jake <31937616+JakeRadMSFT@users.noreply.github.com>
2022-07-06 11:55:22 -0700

    Add AutoML Interactive Extension (#6243)
    * Initial check-in of AutoML Interactive Extension
    
    * Remove warning disable code
    
    * clean up warnings
    
    * Add chart header and work around Plotly.NET issue.
    
    * Fix chart title.
    
    * Improve styling and add header to Table.
    
    * Update comments and remove some items from NoWarn.
    
    * Remove trailing semicolon
    
    * Resolve PR feedback
    
    * Move _updatePending = false outside of if check.
    
    * Resolve PR Feedback
    
    * Pull out ActionThrottler into class and fix delay bug.

---
+++ src/Microsoft.ML.AutoML.Interactive/AutoMLMonitorKernelExtension.cs
@@ -0,0 +1,103 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.AspNetCore.Html;
+using Microsoft.Data.Analysis;
+using Microsoft.DotNet.Interactive;
+using Microsoft.DotNet.Interactive.Commands;
+using Microsoft.DotNet.Interactive.Formatting;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Plotly.NET.CSharp;
+using static Microsoft.DotNet.Interactive.Formatting.PocketViewTags;
+
+
+namespace Microsoft.ML.AutoML
+{
+    public class AutoMLMonitorKernelExtension : IKernelExtension
+    {
+        public async Task OnLoadAsync(Kernel kernel)
+        {
+            Formatter.Register<NotebookMonitor>((monitor, writer) =>
+            {
+                WriteSummary(monitor, writer);
+                WriteChart(monitor, writer);
+                WriteTable(monitor, writer);
+            }, "text/html");
+
+            if (Kernel.Root?.FindKernel("csharp") is { } csKernel)
+            {
+                await LoadExtensionApiAsync(csKernel);
+            }
+        }
+
+        private static async Task LoadExtensionApiAsync(Kernel cSharpKernel)
+        {
+            await cSharpKernel.SendAsync(new SubmitCode($@"#r ""{typeof(AutoMLMonitorKernelExtension).Assembly.Location}""
+using {typeof(NotebookMonitor).Namespace};"));
+        }
+
+        private static void WriteSummary(NotebookMonitor monitor, TextWriter writer)
+        {
+
+            var summary = new List<IHtmlContent>();
+
+            if (monitor.BestTrial != null)
+            {
+                var bestTrialParam = JsonSerializer.Serialize(monitor.BestTrial.TrialSettings.Parameter, new JsonSerializerOptions() { WriteIndented = true, });
+                summary.Add(h3("Best Trial"));
+                summary.Add(p($"Id: {monitor.BestTrial.TrialSettings.TrialId}"));
+                summary.Add(p($"Trainer: {monitor.BestTrial.TrialSettings.Pipeline}".Replace("Unknown=>", "")));
+                summary.Add(p($"Parameters: {bestTrialParam}"));
+            }
+            if (monitor.ActiveTrial != null)
+            {
+
+                var activeTrialParam = JsonSerializer.Serialize(monitor.ActiveTrial.Parameter, new JsonSerializerOptions() { WriteIndented = true, });
+
+                summary.Add(h3("Active Trial"));
+                summary.Add(p($"Id: {monitor.ActiveTrial.TrialId}"));
+                summary.Add(p($"Trainer: {monitor.ActiveTrial.Pipeline}".Replace("Unknown=>", "")));
+                summary.Add(p($"Parameters: {activeTrialParam}"));
+            }
+
+            writer.Write(div(summary));
+        }
+
+        private static void WriteChart(NotebookMonitor monitor, TextWriter writer)
+        {
+            var x = monitor.CompletedTrials.Select(x => x.TrialSettings.TrialId);
+            var y = monitor.CompletedTrials.Select(x => x.Metric);
+
+            var chart = Chart.Point<int, double, string>(x, y, "Plot Metrics over Trials.")
+            .WithTraceInfo(ShowLegend: false)
+            .WithXAxisStyle<double, double, string>(TitleText: "Trial", ShowGrid: false)
+            .WithYAxisStyle<double, double, string>(TitleText: "Metric", ShowGrid: false);
+
+            var chartHeader = new List<IHtmlContent>();
+            chartHeader.Add(h3("Plot Metrics over Trials"));
+            writer.Write(div(chartHeader));
+
+
+            Formatter.GetPreferredFormatterFor(typeof(Plotly.NET.GenericChart.GenericChart), "text/html").Format(chart, writer);
+
+            // Works around issue with earlier versions of Plotly.NET - https://github.com/plotly/Plotly.NET/pull/305
+            if (writer.ToString().EndsWith("</div    \r\n"))
+            {
+                writer.Write(">");
+            }
+        }
+
+        private static void WriteTable(NotebookMonitor notebookMonitor, TextWriter writer)
+        {
+            var tableHeader = new List<IHtmlContent>();
+            tableHeader.Add(h3("All Trials Table"));
+            writer.Write(div(tableHeader));
+            Formatter.GetPreferredFormatterFor(typeof(DataFrame), "text/html").Format(notebookMonitor.TrialData, writer);
+        }
+    }
+}
