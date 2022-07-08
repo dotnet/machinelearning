99a0f4c77c5c54895d08c20713362de26fd6cd82
XiaoYun Zhang <xiaoyuz@microsoft.com>
2022-07-08 13:58:33 -0700

    update

---
+++ src/Microsoft.ML.Fairlearn/Utilities.cs
@@ -0,0 +1,41 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.ML.Data;
+using Microsoft.ML.Fairlearn.reductions;
+using Microsoft.ML.SearchSpace;
+using Microsoft.ML.SearchSpace.Option;
+
+namespace Microsoft.ML.Fairlearn
+{
+    public static class Utilities
+    {
+        public static SearchSpace.SearchSpace GenerateBinaryClassificationLambdaSearchSpace(MLContext context, Moment moment, float gridLimit, bool negativeAllowed = true)
+        {
+            var searchSpace = new SearchSpace.SearchSpace();
+            var convertToString = context.Transforms.Conversion.ConvertType(moment.SensitiveFeatureColumn.Name, moment.SensitiveFeatureColumn.Name, Data.DataKind.String);
+            var sensitiveFeatureColumnValue = convertToString.Fit(moment.X).Transform(moment.X).GetColumn<string>(moment.SensitiveFeatureColumn.Name).Distinct();
+
+            // for different_bound only
+            // if sensitive feature column value is "a", "b", "c",
+            // the search space will contains 6 options with name format {sensitive column value}_{pos/neg}
+            // a_pos, a_neg, b_pos, b_neg, c_pos, c_neg.
+
+            foreach (var p in from _groupValue in sensitiveFeatureColumnValue
+                              from _indicator in new[] { "pos", "neg" }
+                              select new { _groupValue, _indicator })
+            {
+                var option = new UniformSingleOption(-gridLimit, gridLimit, defaultValue: 0);
+                var optionName = $"{p._groupValue}_{p._indicator}";
+                searchSpace[optionName] = option;
+            }
+
+            return searchSpace;
+        }
+    }
+}
---
+++ test/Microsoft.ML.Fairlearn.Tests/UtilitiesTest.cs
@@ -0,0 +1,40 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Microsoft.Data.Analysis;
+using Microsoft.ML.Fairlearn.reductions;
+using Xunit;
+
+namespace Microsoft.ML.Fairlearn.Tests
+{
+    public class UtilitiesTest
+    {
+        [Fact]
+        public void Generate_binary_classification_lambda_search_space_test()
+        {
+            var context = new MLContext();
+            var moment = new ClassificationMoment();
+            var X = this.CreateDummyDataset();
+            moment.LoadData(X, X["y_true"], X["sentitiveFeature"] as StringDataFrameColumn);
+
+            var searchSpace = Utilities.GenerateBinaryClassificationLambdaSearchSpace(context, moment, 5);
+            searchSpace.Keys.Should().BeEquivalentTo("a_pos", "a_neg", "b_pos", "b_neg");
+
+        }
+        private DataFrame CreateDummyDataset()
+        {
+            var df = new DataFrame();
+            df["X"] = DataFrameColumn.Create("X", new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+            df["y_true"] = DataFrameColumn.Create("y_true", new[] { true, true, true, true, true, true, true, false, false, false });
+            df["y_pred"] = DataFrameColumn.Create("y_pred", new[] { true, true, true, true, false, false, false, true, false, false });
+            df["sentitiveFeature"] = DataFrameColumn.Create("sentitiveFeature", new[] { "a", "b", "a", "a", "b", "a", "b", "b", "a", "b" });
+
+            return df;
+        }
+    }
+}
