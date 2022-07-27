426385f1095e58feab9b5d76fbf803c03e83cc97
Jordi Ramos <jordi1215.jrc@gmail.com>
2022-07-27 10:41:18 -0700

    Created a trail runner for grid search
    built the first prototype for the gridSearchTrialRunner. We have enabled a seperate training set for training and testing set for validating the result, which is a different approach from the original implementation.

--- src/Microsoft.ML.Fairlearn/reductions/GridSearchTrialRunner.cs
+++ src/Microsoft.ML.Fairlearn/reductions/GridSearchTrialRunner.cs
@@ -5,8 +5,12 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Text;
+using Microsoft.Data.Analysis;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.ML.AutoML;
+using Microsoft.ML.Data;
 
 namespace Microsoft.ML.Fairlearn.reductions
 {
@@ -18,62 +22,76 @@ namespace Microsoft.ML.Fairlearn.reductions
     /// 4. train
     /// 5. calculate metric = observe loss + fairness loss
     /// </summary>
-    internal class GridSearchTrailRunner : ITrialRunner
+    public class GridSearchTrailRunner : ITrialRunner
     {
         private readonly MLContext _context;
-        private readonly IDatasetManager _datasetManager;
-        private readonly IMetricManager _metricManager;
-        public GridSearchTrailRunner(MLContext context, IDatasetManager datasetManager, IMetricManager metricManager)
+        private readonly IDataView _trainDataset;
+        private readonly IDataView _testDataset;
+        private readonly string _labelColumn;
+
+        public GridSearchTrailRunner(MLContext context, IDataView trainDataset, IDataView testDataset, string labelColumn)
         {
             _context = context;
-            _metricManager = metricManager;
-            _datasetManager = datasetManager;
+            this._trainDataset = trainDataset;
+            this._testDataset = testDataset;
+            this._labelColumn = labelColumn;
         }
 
         public TrialResult Run(TrialSettings settings, IServiceProvider provider)
         {
-            if (_datasetManager is TrainTestDatasetManager datasetSettings
-                && _metricManager is BinaryMetricManager metricSettings)
+            var moment = provider.GetService<ClassificationMoment>();
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            //DataFrameColumn signedWeights = null;
+
+            var pipeline = settings.Pipeline.BuildTrainingPipeline(_context, settings.Parameter);
+
+            // get lambda 
+            var lambdas = settings.Parameter["_lambda_search_space"];
+            var key = lambdas.Keys;
+            // (sign, group, value)
+            var lambdasValue = key.Select(x =>
             {
-                var stopWatch = new Stopwatch();
-                stopWatch.Start();
+                var sign = x.Split('_')[1] == "pos" ? "+" : "-";
+                var e = x.Split('_')[0];
+                var value = lambdas[x].AsType<float>();
 
-                var pipeline = settings.Pipeline.BuildTrainingPipeline(_context, settings.Parameter);
-                var model = pipeline.Fit(datasetSettings.TrainDataset);
-                var eval = model.Transform(datasetSettings.TestDataset);
-                //TODO: calcualte fairnessLost
-                double fairnessLost = 0.0f;
-                var metrics = _context.BinaryClassification.EvaluateNonCalibrated(eval, metricSettings.LabelColumn, predictedLabelColumnName: metricSettings.PredictedColumn);
-                var observedLoss = metricSettings.Metric switch
-                {
-                    BinaryClassificationMetric.PositivePrecision => metrics.PositivePrecision,
-                    BinaryClassificationMetric.Accuracy => metrics.Accuracy,
-                    BinaryClassificationMetric.AreaUnderRocCurve => metrics.AreaUnderRocCurve,
-                    BinaryClassificationMetric.AreaUnderPrecisionRecallCurve => metrics.AreaUnderPrecisionRecallCurve,
-                    _ => throw new NotImplementedException($"{metricSettings.Metric} is not supported!"),
-                };
-                // the metric should be the combination of the observed loss from the model and the fairness loss
-                double metric = 0.0f;
-                if (metricSettings.IsMaximize == true)
-                {
-                    metric = observedLoss - fairnessLost;
-                }
-                else
-                {
-                    metric = observedLoss + fairnessLost;
-                }
+                return (sign, e, value);
+            });
 
-                stopWatch.Stop();
+            var df = new DataFrame();
+            df["sign"] = DataFrameColumn.Create("sign", lambdasValue.Select(x => x.sign));
+            df["group_id"] = DataFrameColumn.Create("group_id", lambdasValue.Select(x => x.e));
+            df["value"] = DataFrameColumn.Create("value", lambdasValue.Select(x => x.value));
+            moment.LoadData(this._trainDataset, DataFrameColumn.Create("y", this._trainDataset.GetColumn<bool>(this._labelColumn)), DataFrameColumn.Create("group_id", this._trainDataset.GetColumn<string>("sensitiveFeature")));
+            var signWeightColumn = moment.SignedWeights(df);
+            var trainDataset = this._trainDataset.ToDataFrame();
+            trainDataset["signedWeight"] = signWeightColumn;
+            var model = pipeline.Fit(trainDataset);
+            // returns an IDataview object that contains the predictions
+            var eval = model.Transform(this._testDataset);
+            // extract the predicted label and convert it to 1.0f and 0.0 so that we can feed that into the gamma function
+            var predictedLabel = eval.GetColumn<bool>("PredictedLabel").Select(b => b ? 1f : 0f).ToArray();
+            var column = DataFrameColumn.Create<float>("pred", predictedLabel);
+            //Get the gamma based on the predicted label of the testDataset
+            moment.LoadData(this._testDataset, DataFrameColumn.Create("y", eval.GetColumn<bool>(this._labelColumn)), DataFrameColumn.Create("group_id", eval.GetColumn<string>("sensitiveFeature")));
+            DataFrame gamma = moment.Gamma(column);
+            double fairnessLost = Convert.ToSingle(gamma["value"].Max());
+            var metrics = _context.BinaryClassification.EvaluateNonCalibrated(eval, this._labelColumn);
+            // the metric should be the combination of the observed loss from the model and the fairness loss
+            double metric = 0.0f;
+            metric = metrics.Accuracy - fairnessLost;
 
-                return new TrialResult()
-                {
-                    Metric = metric,
-                    Model = model,
-                    TrialSettings = settings,
-                    DurationInMilliseconds = stopWatch.ElapsedMilliseconds,
-                };
-            }
-            throw new ArgumentException();
+            stopWatch.Stop();
+
+            return new FairnessTrialResult()
+            {
+                FairnessMetric = fairnessLost,
+                Metric = metric,
+                Model = model,
+                TrialSettings = settings,
+                DurationInMilliseconds = stopWatch.ElapsedMilliseconds,
+            };
         }
     }
 }
