11b1819990fd3324cf6854ffbfee805cc2bb3a1d
R. G. Esteves <rodolfo.g.esteves@intel.com>
2022-08-18 10:19:37 -0700

    Basic infra for XGBoost wrapper

---
+++ src/Native/XGBoostNative/XGBoostWrapper.cpp
@@ -0,0 +1,7 @@
+#include <cstdio.h>
+#include <cstdlib.h>
+#include <xgboost/c_api.h>
+
+EXPORT_API(void) xgboostClfToDMatrix(void* featuresPtr, void* labels, void* results, long long nRows, long long nCols)
+{
+}
