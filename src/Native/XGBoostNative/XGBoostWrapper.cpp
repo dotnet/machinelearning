3fb42282ad688497f59835779c56793bd44bc95a
R. G. Esteves <rgesteves@gmail.com>
2022-09-01 06:10:06 -0700

    Building against XGBoost from repo, not Debian/Ubuntu packages

--- src/Native/XGBoostNative/XGBoostWrapper.cpp
+++ src/Native/XGBoostNative/XGBoostWrapper.cpp
@@ -1,7 +1,12 @@
-#include <cstdio.h>
-#include <cstdlib.h>
+//#include <cstdio>
+//#include <cstdlib>
 #include <xgboost/c_api.h>
+#include "../Stdafx.h"
 
 EXPORT_API(void) xgboostClfToDMatrix(void* featuresPtr, void* labels, void* results, long long nRows, long long nCols)
 {
+  DMatrixHandle dmatrix;
+  XGDMatrixCreateFromMat((float*)featuresPtr, nRows, nCols, 0, &dmatrix);
+
+  return;
 }
