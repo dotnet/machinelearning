30af02d1c822b7e683fe17de3d9203961df7031f
XiaoYun Zhang <xiaoyuz@microsoft.com>
2022-10-20 12:34:01 -0700

    register formatter for parameter type

--- src/Microsoft.ML.AutoML.Interactive/AutoMLMonitorKernelExtension.cs
+++ src/Microsoft.ML.AutoML.Interactive/AutoMLMonitorKernelExtension.cs
@@ -14,7 +14,7 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using Plotly.NET.CSharp;
 using static Microsoft.DotNet.Interactive.Formatting.PocketViewTags;
-
+using Microsoft.ML.SearchSpace;
 
 namespace Microsoft.ML.AutoML
 {
@@ -22,6 +22,7 @@ namespace Microsoft.ML.AutoML
     {
         public async Task OnLoadAsync(Kernel kernel)
         {
+            Formatter.Register<Parameter>((parameter) => JsonSerializer.Serialize(parameter, new JsonSerializerOptions() { WriteIndented = true, }));
             Formatter.Register<NotebookMonitor>((monitor, writer) =>
             {
                 WriteSummary(monitor, writer);
