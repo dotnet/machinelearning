e24dc803a734caf12c435f17c2e4e71df513b576
R. G. Esteves <rgesteves@gmail.com>
2023-01-12 15:15:04 -0800

    Bail to default implementation upon any unforeseen situation

--- src/Microsoft.ML.OneDal/OneDalUtils.cs
+++ src/Microsoft.ML.OneDal/OneDalUtils.cs
@@ -3,8 +3,8 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
-using System.IO;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using Microsoft.ML.Internal.Utilities;
@@ -17,37 +17,36 @@ namespace Microsoft.ML.OneDal
     internal static class OneDalUtils
     {
 
-#if false
         [BestFriend]
         internal static bool IsDispatchingEnabled()
         {
-            return Environment.GetEnvironmentVariable("MLNET_BACKEND") == "ONEDAL" &&
-                System.Runtime.InteropServices.RuntimeInformation.ProcessArchitecture == System.Runtime.InteropServices.Architecture.X64;
-        }
-#else
-        [BestFriend]
-        internal static bool IsDispatchingEnabled()
-        {
-            if (Environment.GetEnvironmentVariable("MLNET_BACKEND") == "ONEDAL" &&
-                System.Runtime.InteropServices.RuntimeInformation.ProcessArchitecture == System.Runtime.InteropServices.Architecture.X64)
+            try
             {
-                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                if (Environment.GetEnvironmentVariable("MLNET_BACKEND") == "ONEDAL" &&
+                    System.Runtime.InteropServices.RuntimeInformation.ProcessArchitecture == System.Runtime.InteropServices.Architecture.X64)
                 {
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    {
 #if NETFRAMEWORK
                 // AppContext not available in the framework, user needs to set PATH manually
                 // this will probably result in a runtime error where the user needs to set the PATH
 #else
-                    var currentDir = AppContext.BaseDirectory;
-                    var nativeLibs = Path.Combine(currentDir, "runtimes", "win-x64", "native");
-                    var originalPath = Environment.GetEnvironmentVariable("PATH");
-                    Environment.SetEnvironmentVariable("PATH", nativeLibs + ";" + originalPath);
+                        var currentDir = AppContext.BaseDirectory;
+                        var nativeLibs = Path.Combine(currentDir, "runtimes", "win-x64", "native");
+                        var originalPath = Environment.GetEnvironmentVariable("PATH");
+                        Environment.SetEnvironmentVariable("PATH", nativeLibs + ";" + originalPath);
 #endif
+                    }
+                    return true;
                 }
-                return true;
+                return false;
             }
-            return false;
+            catch (Exception)
+            {
+                // fallback to default algorithm implementation if dispatch fails
+                return false;
+            }
         }
-#endif
 
         [BestFriend]
         internal static long GetTrainData(IChannel channel, FloatLabelCursor.Factory cursorFactory, ref List<float> featuresList, ref List<float> labelsList, int numberOfFeatures)
