3d705bf05d3be5e5232089c3524213ebbae2911f
andrei-faber <89944741+andrei-faber@users.noreply.github.com>
2023-05-08 23:07:05 -0600

    Added ADO.NET importing/exporting functionality to DataFrame (#5975)
    * refactoring - removed copy/paste in DataFrame.CreateColumn()
    
    * added a universal loading method and export to DataTable
    
    * added tests for new loading/saving methods in DataFrame
    
    * improved error handling - DataFrame.LoadFrom()
    
    * DataFrame - importing and exporting data using ADO.NET providers
    
    * DataFrame.LoadFrom() - use async
    
    * DataFrame.LoadFrom() - minor refactorings
    
    * Update Microsoft.Data.Analysis.Tests.csproj
    
    Changed version of System.Data.SQLite
    
    * Update Microsoft.Data.Analysis.Tests.csproj
    
    * fixed chown command
    
    * sql db test path change
    
    * sql db test path change
    
    * sql db test fix
    
    * sql db test fix
    
    ---------
    
    Co-authored-by: Michael Sharp <51342856+michaelgsharp@users.noreply.github.com>
    Co-authored-by: Michael Sharp <misharp@microsoft.com>

---
+++ src/Microsoft.Data.Analysis/Extensions.cs
@@ -0,0 +1,37 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Text;
+
+namespace Microsoft.Data.Analysis
+{
+    public static class Extensions
+    {
+        public static DbDataAdapter CreateDataAdapter(this DbProviderFactory factory, DbConnection connection, string tableName)
+        {
+            var query = connection.CreateCommand();
+            query.CommandText = $"SELECT * FROM {tableName}";
+            var res = factory.CreateDataAdapter();
+            res.SelectCommand = query;
+            return res;
+        }
+
+        public static bool TryOpen(this DbConnection connection)
+        {
+            if (connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
