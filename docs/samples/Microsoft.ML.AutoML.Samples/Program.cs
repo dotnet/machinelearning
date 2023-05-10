e6015896cefc5f94376923568e2478eee6740d56
XiaoYun Zhang <xiaoyuz@microsoft.com>
2023-05-10 11:30:40 -0700

    checkout test file

--- docs/samples/Microsoft.ML.AutoML.Samples/Program.cs
+++ docs/samples/Microsoft.ML.AutoML.Samples/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Microsoft.ML.Data;
 
 namespace Microsoft.ML.AutoML.Samples
 {
@@ -7,29 +6,10 @@ namespace Microsoft.ML.AutoML.Samples
     {
         public static void Main(string[] args)
         {
-            var context = new MLContext(1);
-            context.Log += (o, e) =>
-            {
-                if (e.Source.StartsWith("AutoMLExperiment"))
-                {
-                    Console.WriteLine(e.RawMessage);
-                }
-            };
-            var trainPath = @"D:/large_csv.csv";
-            var dataset = context.Data.LoadFromTextFile<ModelInput>(trainPath, ',', hasHeader: false);
-            var experiment = context.Auto().CreateExperiment();
-            var label = "Entry(Text)";
-            var pipeline = context.Transforms.Conversion.MapValueToKey(label, label)
-                                .Append(context.Auto().MultiClassification(label, featureColumnName: "_data", useFastTree: true, useFastForest: false, useLgbm: false, fastTreeOption: new CodeGen.FastTreeOption { DiskTranspose = true, }));
-
-            experiment.SetDataset(context.Data.TrainTestSplit(dataset))
-                    .SetMulticlassClassificationMetric(MulticlassClassificationMetric.MacroAccuracy, label)
-                    .SetPipeline(pipeline)
-                    .SetTrainingTimeInSeconds(10000);
-
-            experiment.Run();
             try
             {
+                AutoMLExperiment.RunAsync().Wait();
+
                 RecommendationExperiment.Run();
                 Console.Clear();
 
@@ -57,27 +37,5 @@ namespace Microsoft.ML.AutoML.Samples
 
             Console.ReadLine();
         }
-    }
-
-    class ModelInput
-    {
-        [LoadColumn(0), NoColumn]
-        public string _data0 { get; set; }
-
-        [LoadColumn(1), NoColumn]
-        public float ignoreData1 { get; set; }
-
-        [LoadColumn(2, 4205)]
-        public float[] _data { get; set; }
-
-        [LoadColumn(4206), NoColumn]//(4206,4208)]
-        public float _ignoreData4206 { get; set; }
-        [LoadColumn(4207), NoColumn]//(4206,4208)]
-        public float _ignoreData4207 { get; set; }
-        [LoadColumn(4208), NoColumn]//(4206,4208)]
-        public float _ignoreData4208 { get; set; }
-
-        [LoadColumn(4209), ColumnName("Entry(Text)")]
-        public string _label { get; set; }
     }
 }
--- test/Microsoft.ML.AutoML.Tests/AutoMLExperimentTests.cs
+++ test/Microsoft.ML.AutoML.Tests/AutoMLExperimentTests.cs
@@ -13,12 +13,10 @@ using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.Data.Analysis;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.ML.Data;
 using Microsoft.ML.AutoML.CodeGen;
 using Microsoft.ML.Runtime;
 using Microsoft.ML.TestFramework;
 using Microsoft.ML.TestFramework.Attributes;
-using Tensorflow;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -75,7 +73,7 @@ namespace Microsoft.ML.AutoML.Test
             // the following experiment set memory usage limit to 0.01mb
             // so all trials should be canceled and there should be no successful trials.
             // therefore when experiment finishes, it should throw timeout exception with no model trained message.
-            experiment.SetTrainingTimeInSeconds(10)
+            experiment.SetMaxModelToExplore(10)
                       .SetTrialRunner((serviceProvider) =>
                       {
                           var channel = serviceProvider.GetService<IChannel>();
@@ -83,8 +81,7 @@ namespace Microsoft.ML.AutoML.Test
                           return new DummyTrialRunner(settings, 5, channel);
                       })
                       .SetTuner<RandomSearchTuner>()
-                      .SetMaximumMemoryUsageInMegaByte(0.01)
-                      .SetPerformanceMonitor<DummyPeformanceMonitor>();
+                      .SetMaximumMemoryUsageInMegaByte(0.01);
 
             var runExperimentAction = async () => await experiment.RunAsync();
             await runExperimentAction.Should().ThrowExactlyAsync<TimeoutException>();
@@ -154,6 +151,13 @@ namespace Microsoft.ML.AutoML.Test
         public async Task AutoMLExperiment_finish_training_when_time_is_up_Async()
         {
             var context = new MLContext(1);
+            context.Log += (o, e) =>
+            {
+                if (e.Source.StartsWith("AutoMLExperiment"))
+                {
+                    this.Output.WriteLine(e.RawMessage);
+                }
+            };
 
             var experiment = context.Auto().CreateExperiment();
             experiment.SetTrainingTimeInSeconds(5)
@@ -161,7 +165,7 @@ namespace Microsoft.ML.AutoML.Test
                       {
                           var channel = serviceProvider.GetService<IChannel>();
                           var settings = serviceProvider.GetService<AutoMLExperiment.AutoMLExperimentSettings>();
-                          return new DummyTrialRunner(settings, 1, channel);
+                          return new DummyTrialRunner(settings, 0, channel);
                       })
                       .SetTuner<RandomSearchTuner>();
 
@@ -171,6 +175,32 @@ namespace Microsoft.ML.AutoML.Test
             var res = await experiment.RunAsync(cts.Token);
             res.Metric.Should().BeGreaterThan(0);
             cts.IsCancellationRequested.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task AutoMLExperiment_finish_training_when_reach_to_max_model_async()
+        {
+            var context = new MLContext(1);
+            var experiment = context.Auto().CreateExperiment();
+            experiment.SetMaxModelToExplore(5)
+                      .SetTrialRunner((serviceProvider) =>
+                      {
+                          var channel = serviceProvider.GetService<IChannel>();
+                          var settings = serviceProvider.GetService<AutoMLExperiment.AutoMLExperimentSettings>();
+                          return new DummyTrialRunner(settings, 1, channel);
+                      })
+                      .SetTuner<RandomSearchTuner>();
+
+            var runModelCounts = 0;
+            context.Log += (o, e) =>
+            {
+                if (e.RawMessage.Contains("Update Completed Trial"))
+                {
+                    runModelCounts++;
+                }
+            };
+            await experiment.RunAsync();
+            runModelCounts.Should().Be(5);
         }
 
 
@@ -188,12 +218,12 @@ namespace Microsoft.ML.AutoML.Test
             var data = DatasetUtil.GetUciAdultDataView();
             var experiment = context.Auto().CreateExperiment();
             var pipeline = context.Auto().Featurizer(data, "_Features_", excludeColumns: new[] { DatasetUtil.UciAdultLabel })
-                                .Append(context.Auto().BinaryClassification(DatasetUtil.UciAdultLabel, "_Features_", useLgbm: false, useSdca: false, useLbfgs: false));
+                                .Append(context.Auto().BinaryClassification(DatasetUtil.UciAdultLabel, "_Features_", useLgbm: false, useSdcaLogisticRegression: false, useLbfgsLogisticRegression: false));
 
             experiment.SetDataset(context.Data.TrainTestSplit(data))
                     .SetBinaryClassificationMetric(BinaryClassificationMetric.AreaUnderRocCurve, DatasetUtil.UciAdultLabel)
                     .SetPipeline(pipeline)
-                    .SetTrainingTimeInSeconds(1);
+                    .SetMaxModelToExplore(1);
 
             var result = await experiment.RunAsync();
             result.Metric.Should().BeGreaterThan(0.8);
@@ -213,12 +243,12 @@ namespace Microsoft.ML.AutoML.Test
             var data = DatasetUtil.GetUciAdultDataView();
             var experiment = context.Auto().CreateExperiment();
             var pipeline = context.Auto().Featurizer(data, "_Features_", excludeColumns: new[] { DatasetUtil.UciAdultLabel })
-                                .Append(context.Auto().BinaryClassification(DatasetUtil.UciAdultLabel, "_Features_", useLgbm: false, useSdca: false, useLbfgs: false));
+                                .Append(context.Auto().BinaryClassification(DatasetUtil.UciAdultLabel, "_Features_", useLgbm: false, useSdcaLogisticRegression: false, useLbfgsLogisticRegression: false));
 
             experiment.SetDataset(data, 5)
                     .SetBinaryClassificationMetric(BinaryClassificationMetric.AreaUnderRocCurve, DatasetUtil.UciAdultLabel)
                     .SetPipeline(pipeline)
-                    .SetTrainingTimeInSeconds(10);
+                    .SetMaxModelToExplore(1);
 
             var result = await experiment.RunAsync();
             result.Metric.Should().BeGreaterThan(0.8);
@@ -230,7 +260,7 @@ namespace Microsoft.ML.AutoML.Test
             var context = new MLContext(1);
             context.Log += (o, e) =>
             {
-                if (e.Source.StartsWith("AutoMLExperiment"))
+                if (e.RawMessage.Contains("Trial"))
                 {
                     this.Output.WriteLine(e.RawMessage);
                 }
@@ -240,12 +270,12 @@ namespace Microsoft.ML.AutoML.Test
             var label = "Label";
             var pipeline = context.Auto().Featurizer(data, excludeColumns: new[] { label })
                                 .Append(context.Transforms.Conversion.MapValueToKey(label, label))
-                                .Append(context.Auto().MultiClassification(label, useLgbm: false, useSdca: false, useLbfgs: false));
+                                .Append(context.Auto().MultiClassification(label, useLgbm: false, useSdcaMaximumEntrophy: false, useLbfgsMaximumEntrophy: false));
 
             experiment.SetDataset(data, 5)
                     .SetMulticlassClassificationMetric(MulticlassClassificationMetric.MacroAccuracy, label)
                     .SetPipeline(pipeline)
-                    .SetTrainingTimeInSeconds(10);
+                    .SetMaxModelToExplore(1);
 
             var result = await experiment.RunAsync();
             result.Metric.Should().BeGreaterThan(0.8);
@@ -267,12 +297,12 @@ namespace Microsoft.ML.AutoML.Test
             var label = "Label";
             var pipeline = context.Auto().Featurizer(data, excludeColumns: new[] { label })
                                 .Append(context.Transforms.Conversion.MapValueToKey(label, label))
-                                .Append(context.Auto().MultiClassification(label, useLgbm: false, useSdca: false, useLbfgs: false));
+                                .Append(context.Auto().MultiClassification(label, useLgbm: false, useSdcaMaximumEntrophy: false, useLbfgsMaximumEntrophy: false));
 
             experiment.SetDataset(context.Data.TrainTestSplit(data))
                     .SetMulticlassClassificationMetric(MulticlassClassificationMetric.MacroAccuracy, label)
                     .SetPipeline(pipeline)
-                    .SetTrainingTimeInSeconds(10);
+                    .SetMaxModelToExplore(1);
 
             var result = await experiment.RunAsync();
             result.Metric.Should().BeGreaterThan(0.8);
@@ -294,12 +324,12 @@ namespace Microsoft.ML.AutoML.Test
             var experiment = context.Auto().CreateExperiment();
             var label = DatasetUtil.TaxiFareLabel;
             var pipeline = context.Auto().Featurizer(train, excludeColumns: new[] { label })
-                                .Append(context.Auto().Regression(label, useLgbm: false, useSdca: false, useLbfgs: false));
+                                .Append(context.Auto().Regression(label, useLgbm: false, useSdca: false, useLbfgsPoissonRegression: false));
 
             experiment.SetDataset(train, test)
                     .SetRegressionMetric(RegressionMetric.RSquared, label)
                     .SetPipeline(pipeline)
-                    .SetTrainingTimeInSeconds(50);
+                    .SetMaxModelToExplore(1);
 
             var result = await experiment.RunAsync();
             result.Metric.Should().BeGreaterThan(0.5);
@@ -309,125 +339,64 @@ namespace Microsoft.ML.AutoML.Test
         public async Task AutoMLExperiment_Taxi_Fare_CV_5_Test()
         {
             var context = new MLContext(1);
-            context.Log += (o, e) =>
-            {
-                if (e.Source.StartsWith("AutoMLExperiment"))
-                {
-                    this.Output.WriteLine(e.RawMessage);
-                }
-            };
             var train = DatasetUtil.GetTaxiFareTrainDataView();
             var experiment = context.Auto().CreateExperiment();
             var label = DatasetUtil.TaxiFareLabel;
             var pipeline = context.Auto().Featurizer(train, excludeColumns: new[] { label })
-                                .Append(context.Auto().Regression(label, useLgbm: false, useSdca: false, useLbfgs: false));
+                                .Append(context.Auto().Regression(label, useLgbm: false, useSdca: false, useLbfgsPoissonRegression: false));
 
             experiment.SetDataset(train, 5)
                     .SetRegressionMetric(RegressionMetric.RSquared, label)
                     .SetPipeline(pipeline)
-                    .SetTrainingTimeInSeconds(50);
+                    .SetMaxModelToExplore(1);
 
             var result = await experiment.RunAsync();
             result.Metric.Should().BeGreaterThan(0.5);
         }
 
-        public async Task Generate_300GB_csv()
-        {
-            var rnd = new Random();
-            ModelInput GenerateRandomRow()
-            {
-                return new ModelInput
-                {
-                    _data0 = rnd.NextSingle() > 0.5 ? "a" : "b",
-                    ignoreData1 = rnd.NextSingle(),
-                    _data = Enumerable.Repeat(rnd.NextSingle(), 4204).ToArray(),
-                    _ignoreData4206 = rnd.NextSingle(),
-                    _ignoreData4207 = rnd.NextSingle(),
-                    _ignoreData4208 = rnd.NextSingle(),
-                    _label = rnd.NextSingle() > 0.5 ? "True" : "False",
-                };
-            }
-
-            var filePath = @"D:/large_csv.csv";
-            var fileInfo = new FileInfo(filePath);
-
-            using (var fileStream = fileInfo.Open(FileMode.Append))
-            using (var stream = new StreamWriter(fileStream))
-            {
-                var i = 0;
-                while ((!fileInfo.Exists || fileInfo.Length < 300.0 * 1024 * 1024 * 1024) && i < 100)
-                {
-                    fileInfo.Refresh();
-                    Output.WriteLine($"{fileInfo.Length / (1024 * 1024 * 1024 * 1.0)}");
-                    var taskNum = 10;
-                    var taskPool = new Task<string>[taskNum];
-                    for (int _i = 0; _i != taskNum; ++_i)
-                    {
-                        var t = Task.Factory.StartNew(() =>
-                        {
-                            var sb = new StringBuilder();
-                            var rows = Enumerable.Range(0, 10000).Select(i => GenerateRandomRow());
-                            foreach (var row in rows)
-                            {
-                                var line = $"\"{row._data0}\",{row.ignoreData1.ToString("F2")},{string.Join(",", row._data.Select(d => d.ToString("F2")))}, {row._ignoreData4206.ToString("F2")}, {row._ignoreData4207.ToString("F2")}, {row._ignoreData4208.ToString("F2")},\"{row._label}\"";
-                                sb.AppendLine(line);
-                            }
-
-                            return sb.ToString();
-                        });
-                        taskPool[_i] = t;
-                    }
-
-                    var getRandomRow = await Task.WhenAll(taskPool);
-                    foreach (var row in getRandomRow)
-                    {
-                        await stream.WriteAsync(row);
-                    }
-
-                    await stream.FlushAsync();
-                    i++;
-                }
-            }
-        }
-
-        public async Task Large_csv_test()
+        [Fact]
+        public async Task AutoMLExperiment_Taxi_Fare_CV_5_SamplingKey_Test()
         {
             var context = new MLContext(1);
-            context.Log += (o, e) =>
-            {
-                if (e.Source.StartsWith("AutoMLExperiment"))
-                {
-                    this.Output.WriteLine(e.RawMessage);
-                }
-            };
-            var trainPath = @"D:/large_csv.csv";
-            var dataset = context.Data.LoadFromTextFile<ModelInput>(trainPath, ',', hasHeader: false);
+            var train = DatasetUtil.GetTaxiFareTrainDataView();
             var experiment = context.Auto().CreateExperiment();
-            var label = "Entry(Text)";
-            var pipeline = context.Auto().Featurizer(dataset, excludeColumns: new[] { label })
-                                .Append(context.Transforms.Conversion.MapValueToKey(label, label))
-                                .Append(context.Auto().MultiClassification(label));
+            var label = DatasetUtil.TaxiFareLabel;
+            var pipeline = context.Auto().Featurizer(train, excludeColumns: new[] { label })
+                                .Append(context.Auto().Regression(label, useLgbm: false, useSdca: false, useLbfgsPoissonRegression: false));
 
-            experiment.SetDataset(context.Data.TrainTestSplit(dataset))
-                    .SetMulticlassClassificationMetric(MulticlassClassificationMetric.MacroAccuracy, label)
+            experiment.SetDataset(train, 5, "vendor_id")
+                    .SetRegressionMetric(RegressionMetric.RSquared, label)
                     .SetPipeline(pipeline)
-                    .SetTrainingTimeInSeconds(50);
+                    .SetMaxModelToExplore(1);
 
             var result = await experiment.RunAsync();
-            result.Metric.Should().BeGreaterThan(0.5);
+            result.Metric.Should().BeGreaterThan(0.2);
+            result.Metric.Should().BeLessThan(0.5);
+        }
+
+        [Fact]
+        public void AutoMLExperiment_should_use_seed_from_context_if_provided()
+        {
+            var context = new MLContext();
+            var experiment = context.Auto().CreateExperiment();
+            var settings = experiment.ServiceCollection.BuildServiceProvider().GetRequiredService<AutoMLExperiment.AutoMLExperimentSettings>();
+            settings.Seed.Should().BeNull();
+
+            context = new MLContext(1);
+            experiment = context.Auto().CreateExperiment();
+            settings = experiment.ServiceCollection.BuildServiceProvider().GetRequiredService<AutoMLExperiment.AutoMLExperimentSettings>();
+            settings.Seed.Should().Be(1);
         }
     }
 
     class DummyTrialRunner : ITrialRunner
     {
         private readonly int _finishAfterNSeconds;
-        private readonly CancellationToken _ct;
         private readonly IChannel _logger;
 
         public DummyTrialRunner(AutoMLExperiment.AutoMLExperimentSettings automlSettings, int finishAfterNSeconds, IChannel logger)
         {
             _finishAfterNSeconds = finishAfterNSeconds;
-            _ct = automlSettings.CancellationToken;
             _logger = logger;
         }
 
@@ -439,7 +408,7 @@ namespace Microsoft.ML.AutoML.Test
         {
             _logger.Info("Update Running Trial");
             await Task.Delay(_finishAfterNSeconds * 1000, ct);
-            _ct.ThrowIfCancellationRequested();
+            ct.ThrowIfCancellationRequested();
             _logger.Info("Update Completed Trial");
             var metric = 1.000 + 0.01 * settings.TrialId;
             return new TrialResult
@@ -462,9 +431,7 @@ namespace Microsoft.ML.AutoML.Test
             _checkIntervalInMilliseconds = 1000;
         }
 
-        public event EventHandler<double> CpuUsage;
-
-        public event EventHandler<double> MemoryUsageInMegaByte;
+        public event EventHandler<TrialPerformanceMetrics> PerformanceMetricsUpdated;
 
         public void Dispose()
         {
@@ -480,6 +447,10 @@ namespace Microsoft.ML.AutoML.Test
             return 1000;
         }
 
+        public void OnPerformanceMetricsUpdatedHandler(TrialSettings trialSettings, TrialPerformanceMetrics metrics, CancellationTokenSource trialCancellationTokenSource)
+        {
+        }
+
         public void Start()
         {
             if (_timer == null)
@@ -487,13 +458,17 @@ namespace Microsoft.ML.AutoML.Test
                 _timer = new System.Timers.Timer(_checkIntervalInMilliseconds);
                 _timer.Elapsed += (o, e) =>
                 {
-                    CpuUsage?.Invoke(this, 100);
-                    MemoryUsageInMegaByte?.Invoke(this, 1000);
+                    PerformanceMetricsUpdated?.Invoke(this, new TrialPerformanceMetrics() { PeakCpuUsage = 100, PeakMemoryUsage = 1000 });
                 };
 
                 _timer.AutoReset = true;
-                _timer.Enabled = true;
             }
+            _timer.Enabled = true;
+        }
+
+        public void Pause()
+        {
+            _timer.Enabled = false;
         }
 
         public void Stop()
@@ -502,27 +477,5 @@ namespace Microsoft.ML.AutoML.Test
             _timer?.Dispose();
             _timer = null;
         }
-    }
-
-    class ModelInput
-    {
-        [LoadColumn(0), NoColumn]
-        public string _data0 { get; set; }
-
-        [LoadColumn(1), NoColumn]
-        public float ignoreData1 { get; set; }
-
-        [LoadColumn(2, 4205)]
-        public float[] _data { get; set; }
-
-        [LoadColumn(4206), NoColumn]//(4206,4208)]
-        public float _ignoreData4206 { get; set; }
-        [LoadColumn(4207), NoColumn]//(4206,4208)]
-        public float _ignoreData4207 { get; set; }
-        [LoadColumn(4208), NoColumn]//(4206,4208)]
-        public float _ignoreData4208 { get; set; }
-
-        [LoadColumn(4209), ColumnName("Entry(Text)")]
-        public string _label { get; set; }
     }
 }
