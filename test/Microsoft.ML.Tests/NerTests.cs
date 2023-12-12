4221b0f22cfa3722c1ff613c469685e5e1a06dd0
Michael Sharp <misharp@microsoft.com>
2023-12-12 10:04:05 -0700

    ner options fix

--- test/Microsoft.ML.Tests/NerTests.cs
+++ test/Microsoft.ML.Tests/NerTests.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using Microsoft.ML.Data;
 using Microsoft.ML.RunTests;
 using Microsoft.ML.TorchSharp;
+using Microsoft.ML.TorchSharp.NasBert;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -43,7 +44,7 @@ namespace Microsoft.ML.Tests
                 new List<TestSingleSentenceData>(new TestSingleSentenceData[] {
                     new TestSingleSentenceData()
                     {   // Testing longer than 512 words.
-                        Sentence = "Alice and Bob live in the USA",
+                        Sentence = "Alice and Bob live in the liechtenstein",
                         Label = new string[]{"PERSON", "0", "PERSON", "0", "0", "0", "COUNTRY"}
                     },
                      new TestSingleSentenceData()
@@ -55,6 +56,52 @@ namespace Microsoft.ML.Tests
             var chain = new EstimatorChain<ITransformer>();
             var estimator = chain.Append(ML.Transforms.Conversion.MapValueToKey("Label", keyData: labels))
                .Append(ML.MulticlassClassification.Trainers.NamedEntityRecognition(outputColumnName: "outputColumn"))
+               .Append(ML.Transforms.Conversion.MapKeyToValue("outputColumn"));
+
+            var estimatorSchema = estimator.GetOutputSchema(SchemaShape.Create(dataView.Schema));
+            Assert.Equal(3, estimatorSchema.Count);
+            Assert.Equal("outputColumn", estimatorSchema[2].Name);
+            Assert.Equal(TextDataViewType.Instance, estimatorSchema[2].ItemType);
+
+            var transformer = estimator.Fit(dataView);
+            var transformerSchema = transformer.GetOutputSchema(dataView.Schema);
+
+            Assert.Equal(5, transformerSchema.Count);
+            Assert.Equal("outputColumn", transformerSchema[4].Name);
+
+            TestEstimatorCore(estimator, dataView, shouldDispose: true);
+            transformer.Dispose();
+        }
+
+        [Fact]
+        public void TestSimpleNerOptions()
+        {
+            var labels = ML.Data.LoadFromEnumerable(
+                new[] {
+                new Label { Key = "PERSON" },
+                new Label { Key = "CITY" },
+                new Label { Key = "COUNTRY"  }
+                });
+
+            var options = new NerTrainer.NerOptions();
+            options.PredictionColumnName = "outputColumn";
+
+            var dataView = ML.Data.LoadFromEnumerable(
+                new List<TestSingleSentenceData>(new TestSingleSentenceData[] {
+                    new TestSingleSentenceData()
+                    {   // Testing longer than 512 words.
+                        Sentence = "Alice and Bob live in the USA",
+                        Label = new string[]{"PERSON", "0", "PERSON", "0", "0", "0", "COUNTRY"}
+                    },
+                     new TestSingleSentenceData()
+                     {
+                        Sentence = "Alice and Bob live in the USA",
+                        Label = new string[]{"PERSON", "0", "PERSON", "0", "0", "0", "COUNTRY"}
+                     },
+                }));
+            var chain = new EstimatorChain<ITransformer>();
+            var estimator = chain.Append(ML.Transforms.Conversion.MapValueToKey("Label", keyData: labels))
+               .Append(ML.MulticlassClassification.Trainers.NameEntityRecognition(options))
                .Append(ML.Transforms.Conversion.MapKeyToValue("outputColumn"));
 
             var estimatorSchema = estimator.GetOutputSchema(SchemaShape.Create(dataView.Schema));
