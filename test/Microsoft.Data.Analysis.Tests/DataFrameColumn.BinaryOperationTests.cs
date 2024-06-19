2e3d7dcb3b830480ea409a98a74cd5463a7eb54b
Aleksei Smirnov <tlalok@inbox.ru>
2024-06-19 11:15:45 +0300

    Remove needless changes

--- test/Microsoft.Data.Analysis.Tests/DataFrameColumn.BinaryOperationTests.cs
+++ test/Microsoft.Data.Analysis.Tests/DataFrameColumn.BinaryOperationTests.cs
@@ -1243,7 +1243,7 @@ namespace Microsoft.Data.Analysis.Tests
             ByteDataFrameColumn column = new ByteDataFrameColumn("Byte", columnEnumerable);
             var otherColumnEnumerable = Enumerable.Range(1, 10).Select(x => (byte)x);
             ByteDataFrameColumn otherColumn = new ByteDataFrameColumn("Byte", otherColumnEnumerable);
-            Int32DataFrameColumn columnResult = column.Divide(otherColumn);
+            Int32DataFrameColumn columnResult = column / otherColumn;
             var verify = Enumerable.Range(1, 10).Select(x => (int)(1));
             var verifyColumn = new Int32DataFrameColumn("Verify", verify);
             Assert.Equal(columnResult.Length, verify.Count());
