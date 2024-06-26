77e90243e2118fdfc5d15371802812f92f248f69
Xiaoyun Zhang <xiaoyuz@microsoft.com>
2024-06-26 09:43:31 -0700

    Add GenAI core package (#7177)
    * add GenAI.Core project
    
    * fix format
    
    * move causalml generate method to causalmlpipelineextension
    
    * fix build error from api breakchange in torchsharp
    
    * update package reference
    
    * fix build error
    
    * Update job-template.yml
    
    * Update Microsoft.ML.TorchSharp.Tests.csproj
    
    * Update Microsoft.ML.TorchSharp.Tests.csproj
    
    * revert change for update torchsharp runtime
    
    * use explicit torchsharp version
    
    * fix comments

---
+++ src/Microsoft.ML.GenAI.Core/Extension/ModuleExtension.cs
@@ -0,0 +1,241 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Microsoft.ML.GenAI.Core;
+using Microsoft.ML.GenAI.Core.Extension;
+using TorchSharp;
+using static TorchSharp.torch;
+
+namespace Microsoft.ML.GenAI.Core.Extension;
+
+internal static class ModuleExtension
+{
+    public static long GetSizeInBytes(this nn.Module model)
+    {
+        var stateDict = model.state_dict();
+        long size = 0;
+        foreach (var (_, value) in stateDict)
+        {
+            size += value.numel() * value.element_size();
+        }
+
+        return size;
+    }
+
+    public static Dictionary<string, long> GetSizeForEachDynamicLayerInBytes(this nn.Module model)
+    {
+        var stateDict = model.named_children();
+        if (stateDict.Count() == 0)
+        {
+            return new();
+        }
+        else
+        {
+            var dict = new Dictionary<string, long>();
+
+            foreach (var (key, value) in stateDict)
+            {
+                if (value is IDynamicLoadModule)
+                {
+                    dict[key] = value.GetSizeInBytes();
+                }
+                else
+                {
+                    var subDict = value.GetSizeForEachDynamicLayerInBytes();
+                    foreach (var (subKey, subValue) in subDict)
+                    {
+                        dict[key + "." + subKey] = subValue;
+                    }
+                }
+            }
+
+            return dict;
+        }
+    }
+
+    public static void ToQuantizedModule<T>(
+        this T model)
+        where T : nn.Module
+    {
+        foreach (var (_, value) in model.named_children())
+        {
+            if (value is IQuantizeModule quantizeModule)
+            {
+                quantizeModule.Quantize();
+            }
+            else
+            {
+                value.ToQuantizedModule();
+            }
+        }
+    }
+
+    public static T ToDynamicLoadingModel<T>(
+        this T model,
+        Dictionary<string, string> deviceMap,
+        string targetDevice)
+        where T : nn.Module
+    {
+        if (deviceMap.Count == 0)
+        {
+            model.to(new Device(targetDevice));
+
+            return model;
+        }
+
+        // for each module in the model, update device if it is IDynamicLoadModule
+        foreach (var (key, value) in model.named_children())
+        {
+            if (value is IDynamicLoadModule dynamicModule)
+            {
+                var device = deviceMap[key];
+                if (device != targetDevice)
+                {
+                    dynamicModule.LoadToDeviceFunc = (nn.Module module) =>
+                    {
+                        module.to(new Device(targetDevice));
+                    };
+                    dynamicModule.UnloadFromDeviceFunc = (nn.Module module) =>
+                    {
+                        module.to(new Device(device));
+                    };
+                }
+
+                value.to(new Device(device));
+            }
+            else
+            {
+                var childrenDeviceMap = deviceMap.Where(x => x.Key.StartsWith($"{key}.")).ToDictionary(x => x.Key.Substring($"{key}.".Length), x => x.Value);
+                value.ToDynamicLoadingModel(childrenDeviceMap, targetDevice);
+            }
+        }
+
+        return model;
+    }
+
+    /// <summary>
+    /// Infer the device map for each layer in the model.
+    /// The device map is a dictionary where the key is the device id (e.g. "cuda:0") and the value is the memory size in bytes of the device.
+    /// When inferring the device map, each layer in the model will be placed on the device in the order of the devices list.
+    /// </summary>
+    /// <param name="model"></param>
+    /// <param name="devices">a list of device ids (e.g. ["cuda:0", "cpu", "disk"])</param>
+    /// <param name="deviceSizeMapInByte">a map where the key is the device id (e.g. "cuda:0") and the value is the memory size in bytes of the device</param>
+    /// <returns></returns>
+    public static Dictionary<string, string> InferDeviceMapForEachLayer(
+        this nn.Module model,
+        string[] devices,
+        Dictionary<string, long> deviceSizeMapInByte)
+    {
+        var layerSizeMap = model.GetSizeForEachDynamicLayerInBytes();
+        var sizeToRemainOnEachDevice = 2 * layerSizeMap.Max(x => x.Value);
+        var deviceMap = new Dictionary<string, string>();
+        foreach (var device in devices)
+        {
+            long size = deviceSizeMapInByte[device];
+            var remainingLayerSizeMap = layerSizeMap.Where(x => !deviceMap.ContainsKey(x.Key)).ToDictionary(x => x.Key, x => x.Value);
+            // larger layer fit first
+            foreach (var (key, value) in remainingLayerSizeMap.OrderByDescending(x => x.Value))
+            {
+                if (size >= value)
+                {
+                    deviceMap[key] = device;
+                    size -= value;
+                }
+
+                if (size < sizeToRemainOnEachDevice)
+                {
+                    break;
+                }
+            }
+        }
+
+        return deviceMap;
+    }
+
+    public static string Peek(this nn.Module model)
+    {
+        var sb = new StringBuilder();
+        var stateDict = model.state_dict();
+        // preview state_dict
+        int i = 0;
+        foreach (var (key, value) in stateDict.OrderBy(x => x.Key, StringComparer.OrdinalIgnoreCase))
+        {
+            var str = value.Peek(key);
+            sb.AppendLine($"{i}: {str}");
+            i++;
+        }
+
+        var res = sb.ToString();
+
+        return res;
+    }
+
+    public static string PeekShape(this nn.Module model)
+    {
+        var sb = new StringBuilder();
+        var stateDict = model.state_dict();
+        // preview state_dict
+        int i = 0;
+        foreach (var (key, value) in stateDict.OrderBy(x => x.Key, StringComparer.OrdinalIgnoreCase))
+        {
+            // shape str: [x, y, z]
+            var shapeStr = string.Join(", ", value.shape);
+            sb.AppendLine($"{i}: {key} shape: [{shapeStr}]");
+            i++;
+        }
+
+        var res = sb.ToString();
+
+        return res;
+    }
+
+    public static void LoadStateDict(this Dictionary<string, Tensor> dict, string location)
+    {
+        using FileStream stream = File.OpenRead(location);
+        using BinaryReader reader = new BinaryReader(stream);
+        var num = reader.Decode();
+        for (int i = 0; i < num; i++)
+        {
+            var key = reader.ReadString();
+            Tensor tensor = dict[key];
+
+            var originalDevice = tensor.device;
+            var originalType = tensor.dtype;
+            if (tensor.dtype == ScalarType.BFloat16)
+            {
+                tensor = tensor.to_type(ScalarType.Float32);
+            }
+
+            TensorExtensionMethods.Load(ref tensor!, reader, skip: false);
+
+            tensor = tensor!.to_type(originalType);
+            dict[key] = tensor;
+        }
+    }
+
+    public static long Decode(this BinaryReader reader)
+    {
+        long num = 0L;
+        int num2 = 0;
+        while (true)
+        {
+            long num3 = reader.ReadByte();
+            num += (num3 & 0x7F) << num2 * 7;
+            if ((num3 & 0x80) == 0L)
+            {
+                break;
+            }
+
+            num2++;
+        }
+
+        return num;
+    }
+}
---
+++ src/Microsoft.ML.GenAI.Core/Extension/TensorExtension.cs
@@ -0,0 +1,33 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using TorchSharp;
+using static TorchSharp.torch;
+
+namespace Microsoft.ML.GenAI.Core.Extension;
+
+internal static class TensorExtension
+{
+    public static string Peek(this Tensor tensor, string id, int n = 10)
+    {
+        var device = tensor.device;
+        var dType = tensor.dtype;
+        // if type is fp16, convert to fp32
+        if (tensor.dtype == ScalarType.Float16)
+        {
+            tensor = tensor.to_type(ScalarType.Float32);
+        }
+        tensor = tensor.cpu();
+        var shapeString = string.Join(',', tensor.shape);
+        var tensor1D = tensor.reshape(-1);
+        var tensorIndex = torch.arange(tensor1D.shape[0], dtype: ScalarType.Float32).to(tensor1D.device).sqrt();
+        var avg = (tensor1D * tensorIndex).sum();
+        avg = avg / tensor1D.sum();
+        // keep four decimal places
+        avg = avg.round(4);
+        var str = $"{id}: sum: {avg.ToSingle()}  dType: {dType} shape: [{shapeString}]";
+
+        return str;
+    }
+}
