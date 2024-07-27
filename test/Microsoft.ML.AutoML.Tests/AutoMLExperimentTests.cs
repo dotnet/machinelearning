e73304cb79d5e3a4072f5f8af8b5f808bd5028c4
Michael Sharp <misharp@microsoft.com>
2024-07-27 15:58:19 -0600

    skipping test

--- test/Microsoft.ML.AutoML.Tests/AutoMLExperimentTests.cs
+++ test/Microsoft.ML.AutoML.Tests/AutoMLExperimentTests.cs
@@ -148,7 +148,8 @@ namespace Microsoft.ML.AutoML.Test
             res.Metric.Should().BeGreaterThan(0);
         }
 
-        [Fact]
+        //https://github.com/dotnet/machinelearning/issues/7203
+        [X86X64Fact("Fails on Linux Arm machines")]
         public async Task AutoMLExperiment_finish_training_when_time_is_up_Async()
         {
             var context = new MLContext(1);
