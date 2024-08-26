a6ca6a9f7a7a4babad9a8bf04420d04331f674b6
MHReza <mh.shahmorady@gmail.com>
2024-08-26 23:20:31 +0330

    Refactor namespace from Microsoft.ML.ModelBuilder.SweepableEstimator.CodeGenerator to Microsoft.ML.AutoML.SourceGenerator

--- tools-local/Microsoft.ML.AutoML.SourceGenerator/SweepableEstimatorGenerator.cs
+++ tools-local/Microsoft.ML.AutoML.SourceGenerator/SweepableEstimatorGenerator.cs
@@ -2,19 +2,14 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
-using Microsoft.ML.AutoML.SourceGenerator;
-using SweepableEstimator = Microsoft.ML.AutoML.SourceGenerator.Template.SweepableEstimator;
 using SweepableEstimatorT = Microsoft.ML.AutoML.SourceGenerator.Template.SweepableEstimator_T_;
-namespace Microsoft.ML.ModelBuilder.SweepableEstimator.CodeGenerator
+namespace Microsoft.ML.AutoML.SourceGenerator
 {
     [Generator]
     public sealed class SweepableEstimatorGenerator : ISourceGenerator
