6e005eb5bf9c965afc7f582e7d8f8952db6b57da
XiaoYun Zhang <xiaoyuz@microsoft.com>
2024-09-03 11:17:23 -0700

    add mistral and tests

---
+++ src/Microsoft.ML.GenAI.Mistral/MistralConfig.cs
@@ -0,0 +1,88 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Text.Json.Serialization;
+using Microsoft.ML.GenAI.Core;
+using TorchSharp;
+
+namespace Microsoft.ML.GenAI.Mistral;
+
+public class MistralConfig
+{
+    public MistralConfig()
+    {
+        this.AttentionBias = false;
+        this.AttentionDropout = 0.0;
+        this.HiddenAct = "silu";
+        this.HiddenSize = 4096;
+        this.InitializerRange = 0.02;
+        this.IntermediateSize = 14336;
+        this.MaxPositionEmbeddings = 131072;
+        this.MlpBias = false;
+        this.NumAttentionHeads = 32;
+        this.NumHiddenLayers = 32;
+        this.NumKeyValueHeads = 8;
+        this.PretrainingTp = 1;
+        this.RmsNormEps = 1e-05f;
+        this.RopeScaling = new RopeScalingConfig();
+        this.RopeTheta = 500000.0;
+        this.TieWordEmbeddings = false;
+        this.VocabSize = 128256;
+        this.AttnImplementation = "eager";
+        this.DType = torch.ScalarType.BFloat16;
+    }
+
+    [JsonPropertyName("attention_bias")]
+    public bool AttentionBias { get; set; }
+
+    [JsonPropertyName("attention_dropout")]
+    public double AttentionDropout { get; set; }
+
+    [JsonPropertyName("hidden_act")]
+    public string HiddenAct { get; set; }
+
+    [JsonPropertyName("hidden_size")]
+    public int HiddenSize { get; set; }
+
+    [JsonPropertyName("initializer_range")]
+    public double InitializerRange { get; set; }
+
+    [JsonPropertyName("intermediate_size")]
+    public int IntermediateSize { get; set; }
+
+    [JsonPropertyName("max_position_embeddings")]
+    public int MaxPositionEmbeddings { get; set; }
+
+    [JsonPropertyName("mlp_bias")]
+    public bool MlpBias { get; set; }
+
+    [JsonPropertyName("num_attention_heads")]
+    public int NumAttentionHeads { get; set; }
+
+    [JsonPropertyName("num_hidden_layers")]
+    public int NumHiddenLayers { get; set; }
+
+    [JsonPropertyName("num_key_value_heads")]
+    public int NumKeyValueHeads { get; set; }
+
+    [JsonPropertyName("pretraining_tp")]
+    public int PretrainingTp { get; set; }
+
+    [JsonPropertyName("rms_norm_eps")]
+    public float RmsNormEps { get; set; }
+
+    public RopeScalingConfig RopeScaling { get; set; }
+
+    [JsonPropertyName("rope_theta")]
+    public double RopeTheta { get; set; }
+
+    [JsonPropertyName("tie_word_embeddings")]
+    public bool TieWordEmbeddings { get; set; }
+
+    [JsonPropertyName("vocab_size")]
+    public int VocabSize { get; set; }
+    public int? PadTokenId { get; set; }
+    public torch.ScalarType DType { get; set; }
+    public string AttnImplementation { get; set; }
+}
---
+++ test/Microsoft.ML.GenAI.Mistral.Tests/Mistral_V0_3Tests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApprovalTests.Namers;
+using ApprovalTests.Reporters;
+using ApprovalTests;
+using Xunit;
+
+namespace Microsoft.ML.GenAI.Mistral.Tests;
+
+public class Mistral_V0_3Tests
+{
+    [Fact]
+    [UseReporter(typeof(DiffReporter))]
+    [UseApprovalSubdirectory("Approvals")]
+    public void TokenizerTest()
+    {
+        var modelWeightFolder = Path.Join("C:\\Users\\xiaoyuz\\source\\repos\\Mistral-7B-Instruct-v0.3");
+        var tokenizer = MistralTokenizerHelper.FromPretrained(modelWeightFolder);
+
+        var messages = new string[]
+        {
+            // system : You are a helpful assistant that can answer questions about the weather.
+            // tool: [get-weather-tool-call]
+            // user : What's the weather like in Paris?
+            // assistant: // get-weather-tool-call
+            // tool: get-weather-tool-call-result
+            // assistant: The current temperature in Paris is 22.0 degrees celsius.
+            """
+            <s>[AVAILABLE_TOOLS][{"type": "function", "function": {"name": "get_current_weather", "description": "Get the current weather", "parameters": {"type": "object", "properties": {"location": {"type": "string", "description": "The city and state, e.g. San Francisco, CA"}, "format": {"type": "string", "enum": ["celsius", "fahrenheit"], "description": "The temperature unit to use. Infer this from the users location."}}, "required": ["location", "format"]}}}][/AVAILABLE_TOOLS][INST] What's the weather like in Paris?[/INST][TOOL_CALLS] [{"name": "get_current_temperature", "arguments": {"location": "Paris, France", "unit": "celsius"}, "id": "9Ae3bDc2F"}]</s>[TOOL_RESULTS] {"content": 22.0, "call_id": "9Ae3bDc2F"}[/TOOL_RESULTS] The current temperature in Paris is 22.0 degrees celsius.</s>
+            """
+        };
+
+        var sb = new StringBuilder();
+        foreach (var message in messages)
+        {
+            var tokenizeIds = tokenizer.EncodeToIds(message, true, false);
+            var decodeToString = tokenizer.Decode(tokenizeIds);
+            sb.AppendLine(decodeToString);
+            var tokenizedStr = string.Join(", ", tokenizeIds.Select(x => x.ToString()));
+
+            sb.AppendLine(tokenizedStr);
+        }
+        Approvals.Verify(sb.ToString());
+    }
+}
