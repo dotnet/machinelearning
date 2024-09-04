f5514d3a12871bdfe7bef1bb652d486ba655b622
XiaoYun Zhang <xiaoyuz@microsoft.com>
2024-09-04 08:55:41 -0700

    add file header

--- test/Microsoft.ML.GenAI.Mistral.Tests/Mistral_7B_Instruct_V0_3Tests.cs
+++ test/Microsoft.ML.GenAI.Mistral.Tests/Mistral_7B_Instruct_V0_3Tests.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 using System.Text;
-using System.Threading.Tasks;
+using ApprovalTests;
 using ApprovalTests.Namers;
 using ApprovalTests.Reporters;
-using ApprovalTests;
-using Xunit;
-using TorchSharp;
-using Microsoft.ML.GenAI.Core.Extension;
 using AutoGen.Core;
+using Microsoft.ML.GenAI.Core.Extension;
+using TorchSharp;
+using Xunit;
 
 namespace Microsoft.ML.GenAI.Mistral.Tests;
 
