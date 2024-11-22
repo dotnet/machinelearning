ba47ab8dd80dd2fb67f1954dcf7ce6b3826488b8
XiaoYun Zhang <xiaoyuz@microsoft.com>
2024-11-22 10:38:14 -0800

    update

--- docs/samples/Microsoft.ML.GenAI.Samples/Program.cs
+++ docs/samples/Microsoft.ML.GenAI.Samples/Program.cs
@@ -2,5 +2,5 @@
 using Microsoft.ML.GenAI.Samples.Llama;
 using Microsoft.ML.GenAI.Samples.MEAI;
 
-await SFT_Llama_3_2_1B.Train(@"C:\Users\xiaoyuz\source\repos\Llama-3.2-1B-Instruct", checkPointName: "contoso-llama-3.1-1b.safetensors");
+await SFT_Llama_3_2_1B.Train(@"C:\Users\xiaoyuz\source\repos\Llama-3.2-1B-Instruct", checkPointName: "model.safetensors");
 //await Phi3.RunAsync(@"C:\Users\xiaoyuz\source\repos\Phi-3-mini-4k-instruct");
